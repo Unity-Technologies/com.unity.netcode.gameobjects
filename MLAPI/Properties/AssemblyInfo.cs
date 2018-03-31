ca82895691bda35c6b74d7618950c9bdb72f7369
Albin Corén <2108U9@gmail.com>
2018-03-31 03:32:49 +0200

    Added better description to AssemblyInfo

--- MLAPI/Properties/AssemblyInfo.cs
+++ MLAPI/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MLAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Game networking stack")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MLAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Albin Corén 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
