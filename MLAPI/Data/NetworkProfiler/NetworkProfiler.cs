7a4d1872f130196733c5c4877d71d45c9dc85f5b
Albin Corén <twoten@twoten.io>
2018-05-15 12:08:33 +0200

    Added NetworkProfiler

---
+++ MLAPI/Data/NetworkProfiler/NetworkProfiler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace MLAPI.Data.NetworkProfiler
+{
+    public static class NetworkProfiler
+    {
+        const int tickCount = 1024;
+        public static readonly FixedQueue<ProfilerTick> Ticks = new FixedQueue<ProfilerTick>(tickCount);
+        private static ProfilerTick CurrentTick;
+
+        internal static void StartTick(TickType type)
+        {
+            if (Ticks.Count == tickCount)
+                Ticks.Dequeue();
+            
+            ProfilerTick tick = new ProfilerTick()
+            {
+                StartTime = Time.unscaledTime,
+                EndTime = -1,
+                Type = type
+            };
+            Ticks.Enqueue(tick);
+            CurrentTick = tick;
+        }
+
+        internal static void EndTick()
+        {
+            if (CurrentTick == null)
+                return;
+            CurrentTick.EndTime = Time.unscaledTime;
+            CurrentTick = null;
+        }
+
+        internal static void StartEvent(TickType eventType, uint bytes, string channelName, string messageType)
+        {
+            if (CurrentTick == null)
+                return;
+            CurrentTick.StartEvent(eventType, bytes, channelName, messageType);
+        }
+
+        internal static void EndEvent()
+        {
+            if (CurrentTick == null)
+                return;
+            CurrentTick.EndEvent();
+        }
+    }
+}
---
+++ MLAPI/Data/NetworkProfiler/ProfilerTickData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MLAPI.Data.NetworkProfiler
+{
+    public enum TickType
+    {
+        Event,
+        Receive,
+        Send
+    }
+    public class ProfilerTick
+    {
+        private readonly List<TickEvent> Events = new List<TickEvent>();
+
+        internal void EndEvent()
+        {
+            for (int i = Events.Count - 1; i >= 0; i--)
+            {
+                if (Events[i].EndTime == -1)
+                {
+                    Events[i].EndTime = Time.unscaledTime;
+                    return;
+                }
+            }
+        }
+
+        internal void StartEvent(TickType type, uint bytes, string channelName, string messageType)
+        {
+            TickEvent tickEvent = new TickEvent()
+            {
+                Bytes = bytes,
+                ChannelName = string.IsNullOrEmpty(channelName) ? "NONE" : channelName,
+                MessageType = string.IsNullOrEmpty(messageType) ? "NONE" : messageType,
+                EndTime = -1,
+                StartTime = Time.unscaledTime,
+                EventType = type
+            };
+            Events.Add(tickEvent);
+        }
+
+        public TickType Type;
+        public float StartTime;
+        public float EndTime;
+    }
+
+    public class TickEvent
+    {
+        public TickType EventType;
+        public uint Bytes;
+        public string ChannelName;
+        public string MessageType;
+        public float StartTime = -1;
+        public float EndTime = -1;
+    }
+}
