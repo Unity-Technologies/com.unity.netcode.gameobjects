6bd6cdf805b0a8f175ebfac89b07ac9b7eb8c652
Paul Pacheco <paulpach@gmail.com>
2018-06-19 18:24:41 -0500

    Testing with NUnit instead

---
+++ MLAPI-Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace MLAPITests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
