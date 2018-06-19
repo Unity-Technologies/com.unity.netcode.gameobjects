25dd880c2c14f60e3368c87503adfb4f4c9c8f56
Paul Pacheco <paulpach@gmail.com>
2018-06-19 18:40:05 -0500

    Add nunit again with a failed test

---
+++ MLAPI-Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace MLAPITests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.True(false);
+        }
+    }
+}
