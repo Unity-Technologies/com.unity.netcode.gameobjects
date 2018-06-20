c1ad72c54d504a78c17cd327ad9ee15066c846a8
Albin Corén <2108U9@gmail.com>
2018-06-20 22:10:12 +0200

    Renmaed MLAPI.Tests project

---
+++ MLAPI.Tests/NetworkingManagerComponents/Cryptography/MessageDigestTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MLAPI.tests.NetworkingManagerComponents.Cryptography
+{
+    using MLAPI.NetworkingManagerComponents.Cryptography;
+    using NUnit.Framework;
+    using System.Security.Cryptography;
+
+    [TestFixture]
+    public class MessageDigestTest
+    {
+        [Test]
+        public void CompareWithNative()
+        {
+            Random dataRnd = new Random(0);
+            Random lengthRnd = new Random(0);
+            for (int i = 0; i < 10; i++)
+            {
+                using (SHA1Managed sha = new SHA1Managed())
+                {
+                    byte[] data = new byte[lengthRnd.Next(100)];
+                    dataRnd.NextBytes(data);
+                    byte[] managed = sha.ComputeHash(data);
+                    byte[] recursive = MessageDigest.SHA1_Opt(data).ToArray();
+                    Assert.That(managed, Is.EquivalentTo(recursive));
+                }
+            }
+        }
+    }
+}
---
+++ MLAPI.Tests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using System;
+namespace MLAPI.tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
