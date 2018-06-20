c58cf48c2b9706bc5f782a0c625aa9b435c8e023
Paul Pacheco <paulpach@gmail.com>
2018-06-19 20:43:30 -0500

    Test writing a boolean

---
+++ MLAPI.tests/NetworkingManagerComponents/Binary/BitWriterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MLAPI.tests.NetworkingManagerComponents.Binary
+{
+    using MLAPI.NetworkingManagerComponents.Binary;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class BitWriterTest
+    {
+        [Test]
+        public void TestWritingTrue()
+        {
+            BitWriter bitWriter = BitWriter.Get();
+
+            bitWriter.WriteBool(true);
+
+            byte[] result = bitWriter.Finalize();
+
+            Assert.That(result.Length, Is.EqualTo(1));
+            Assert.That(result[0], Is.EqualTo(1));
+        }
+
+
+    }
+}
