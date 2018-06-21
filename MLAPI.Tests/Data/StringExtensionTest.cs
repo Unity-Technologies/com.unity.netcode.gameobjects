de3723072f92afd916f903456a7596f98dc41903
Paul Pacheco <paul.pacheco@aa.com>
2018-06-21 11:12:39 -0500

    Add a stable 32 bit hash to strings

---
+++ MLAPI.Tests/Data/StringExtensionTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MLAPI.Tests.Data
+{
+    using MLAPI.Data;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class StringExtensionTest
+    {
+
+        [Test]
+        public static void CheckHash()
+        {
+            string str1 = "string 1";
+            string str2 = "string 2";
+
+            Assert.That(str1.GetStableHash32(), Is.Not.EqualTo(str2.GetStableHash32()));
+        }
+
+
+    }
+}
---
+++ MLAPI/Data/StringExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MLAPI.Data
+{
+    public static class StringExtension
+    {
+        private const uint FNV_offset_basis32 = 2166136261;
+        private const uint FNV_prime32 = 16777619;
+
+        /// <summary>
+        /// non cryptographic stable hash code,  
+        /// it will always return the same hash for the same
+        /// string.  
+        /// 
+        /// This is simply an implementation of FNV-1
+        /// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+        /// </summary>
+        /// <returns>The stable hash32.</returns>
+        /// <param name="txt">Text.</param>
+        public static uint GetStableHash32(this string txt)
+        {
+            unchecked
+            {
+                uint hash = FNV_offset_basis32;
+                for (int i = 0; i < txt.Length; i++)
+                {
+                    uint ch = txt[i];
+                    hash = hash * FNV_prime32;
+                    hash = hash ^ ch;
+                }
+                return hash;
+            }
+        }
+    }
+}
