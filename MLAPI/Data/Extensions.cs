eed3d9e5f0b08c9507a2cd7692e437eb78b016cb
Paul Pacheco <paul.pacheco@aa.com>
2018-06-21 12:22:57 -0500

    Add 16 bit hash code based on FNV-1

--- MLAPI/Data/Extensions.cs
+++ MLAPI/Data/Extensions.cs
@@ -15,6 +15,24 @@ namespace MLAPI.Data
         /// it will always return the same hash for the same
         /// string.  
         /// 
+        /// This is simply an implementation of FNV-1 32 bit xor folded to 16 bit
+        /// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+        /// </summary>
+        /// <returns>The stable hash32.</returns>
+        /// <param name="txt">Text.</param>
+        public static ushort GetStableHash16(this string txt)
+        {
+            uint hash32 = txt.GetStableHash32();
+
+            return (ushort)((hash32 >> 16) ^ hash32);
+        }
+
+
+        /// <summary>
+        /// non cryptographic stable hash code,  
+        /// it will always return the same hash for the same
+        /// string.  
+        /// 
         /// This is simply an implementation of FNV-1 32 bit
         /// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
         /// </summary>
@@ -58,6 +76,23 @@ namespace MLAPI.Data
                 }
                 return hash;
             }
+        }
+
+        /// <summary>
+        /// non cryptographic stable hash code,  
+        /// it will always return the same hash for the same
+        /// string.  
+        /// 
+        /// This is simply an implementation of FNV-1 32 bit xor folded to 16 bit
+        /// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+        /// </summary>
+        /// <returns>The stable hash32.</returns>
+        /// <param name="txt">Text.</param>
+        public static ushort GetStableHash16(this byte[] bytes)
+        {
+            uint hash32 = bytes.GetStableHash32();
+
+            return (ushort)((hash32 >> 16) ^ hash32);
         }
 
         /// <summary>
