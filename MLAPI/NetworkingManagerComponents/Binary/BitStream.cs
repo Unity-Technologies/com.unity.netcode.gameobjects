0b579b411b1c52620e50581bb9314f5bb46c05e0
GabrielTofvesson <gabriel.tofvesson@gmail.com>
2018-06-29 10:40:32 +0200

    Whoopsie

--- MLAPI/NetworkingManagerComponents/Binary/BitStream.cs
+++ MLAPI/NetworkingManagerComponents/Binary/BitStream.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using static MLAPI.NetworkingManagerComponents.Binary.Arithmetic;
 
