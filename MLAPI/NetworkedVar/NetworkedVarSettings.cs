9c1435d578bf027f6b182cc47ce76186d521ec3a
Albin Corén <twoten@twoten.io>
2019-08-07 22:42:52 +0200

    docs(xml): Fixed NetworkedVarSettings SendTickrate documentation

--- MLAPI/NetworkedVar/NetworkedVarSettings.cs
+++ MLAPI/NetworkedVar/NetworkedVarSettings.cs
@@ -5,7 +5,7 @@ namespace MLAPI.NetworkedVar
     /// </summary>
     /// <param name="clientId">The clientId whose permissions to check</param>
     public delegate bool NetworkedVarPermissionsDelegate(ulong clientId);
-    
+
     /// <summary>
     /// The settings class used by the build in NetworkVar implementations
     /// </summary>
@@ -29,7 +29,8 @@ namespace MLAPI.NetworkedVar
         public NetworkedVarPermissionsDelegate ReadPermissionCallback = null;
         /// <summary>
         /// The maximum times per second this var will be synced.
-        /// Less than or equal to 0 will cause the variable to sync as soon as possible after being changed.
+        /// A value of 0 will cause the variable to sync as soon as possible after being changed.
+        /// A value of less than 0 will cause the variable to sync only at once at spawn and not update again.
         /// </summary>
         public float SendTickrate = 0;
         /// <summary>
@@ -37,13 +38,13 @@ namespace MLAPI.NetworkedVar
         /// Variables with different channels will be split into different packets
         /// </summary>
         public string SendChannel = "MLAPI_DEFAULT_MESSAGE";
-        
+
         /// <summary>
         /// Constructs a new NetworkedVarSettings instance
         /// </summary>
         public NetworkedVarSettings()
         {
-            
+
         }
     }
 }
