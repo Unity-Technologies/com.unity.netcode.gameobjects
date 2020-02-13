54f71f0941610eb659d648d730d2494dff63a053
Albin Corén <twoten@twoten.io>
2020-02-13 17:22:50 +0100

    fix: Fixed buffered messages not being consumed on first connect

--- MLAPI/Messaging/InternalMessageHandler.cs
+++ MLAPI/Messaging/InternalMessageHandler.cs
@@ -286,6 +286,21 @@ namespace MLAPI.Messaging
 
                             NetworkedObject netObject = SpawnManager.CreateLocalNetworkedObject(softSync, instanceId, prefabHash, parentNetworkId, pos, rot);
                             SpawnManager.SpawnNetworkedObjectLocally(netObject, networkId, softSync, isPlayerObject, ownerId, continuationStream, false, 0, true, false);
+
+                            Queue<BufferManager.BufferedMessage> bufferQueue = BufferManager.ConsumeBuffersForNetworkId(networkId);
+
+                            // Apply buffered messages
+                            if (bufferQueue != null)
+                            {
+                                while (bufferQueue.Count > 0)
+                                {
+                                    BufferManager.BufferedMessage message = bufferQueue.Dequeue();
+
+                                    NetworkingManager.Singleton.HandleIncomingData(message.sender, message.channelName, new ArraySegment<byte>(message.payload.GetBuffer(), (int)message.payload.Position, (int)message.payload.Length), message.receiveTime, false);
+
+                                    BufferManager.RecycleConsumedBufferedMessage(message);
+                                }
+                            }
                         }
 
                         NetworkingManager.Singleton.IsConnectedClient = true;
