925124ff9997abe5c819aa4c4a768a46d03429e1
Andrew Spiering <aspiering@gmail.com>
2020-12-15 00:05:18 -0800

    fix: Fixing an issue with latest trunk and ILPP
    - Fixed an issue where on latest 21.1 you could not build ILPP without some PrivateCorelib issues
    
    - Removed UTP package as it causes issues with burst and ILPP that needs to be resolved at some future point
    
    - Updated Burst to latest 1.43 and Mono to 1.10.1-preview.1 (this is the legit package not the shim package)
    
    All this is compat back to 2018.4

--- com.unity.multiplayer.mlapi/Editor/CodeGen/NetworkBehaviourILPP.cs
+++ com.unity.multiplayer.mlapi/Editor/CodeGen/NetworkBehaviourILPP.cs
@@ -35,6 +35,7 @@ namespace MLAPI.Editor.CodeGen
             {
                 SymbolStream = new MemoryStream(compiledAssembly.InMemoryAssembly.PdbData),
                 SymbolReaderProvider = new PortablePdbReaderProvider(),
+                ReflectionImporterProvider = new PostProcessorReflectionImporterProvider(),
                 ReadingMode = ReadingMode.Immediate
             };
 
@@ -77,6 +78,34 @@ namespace MLAPI.Editor.CodeGen
             assemblyDefinition.Write(pe, writerParameters);
 
             return new ILPostProcessResult(new InMemoryAssembly(pe.ToArray(), pdb.ToArray()), _diagnostics);
+        }
+
+
+        internal class PostProcessorReflectionImporterProvider : IReflectionImporterProvider
+        {
+            public IReflectionImporter GetReflectionImporter(ModuleDefinition module)
+            {
+                return new PostProcessorReflectionImporter(module);
+            }
+        }
+
+        internal class PostProcessorReflectionImporter : DefaultReflectionImporter
+        {
+            private const string SystemPrivateCoreLib = "System.Private.CoreLib";
+            private AssemblyNameReference _correctCorlib;
+
+            public PostProcessorReflectionImporter(ModuleDefinition module) : base(module)
+            {
+                _correctCorlib = module.AssemblyReferences.FirstOrDefault(a => a.Name == "mscorlib" || a.Name == "netstandard" || a.Name == SystemPrivateCoreLib);
+            }
+
+            public override AssemblyNameReference ImportReference(AssemblyName reference)
+            {
+                if (_correctCorlib != null && reference.Name == SystemPrivateCoreLib)
+                    return _correctCorlib;
+
+                return base.ImportReference(reference);
+            }
         }
 
         private TypeReference NetworkManager_TypeRef;
