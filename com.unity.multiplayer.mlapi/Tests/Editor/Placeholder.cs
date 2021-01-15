fd74fd5bd5910e81dc175725f837211d7fd157d2
M. Fatih MAR <mfatihmar@gmail.com>
2021-01-15 04:47:53 +0000

    refactor: remove DataStreamTests

---
+++ com.unity.multiplayer.mlapi/Tests/Editor/Placeholder.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace MLAPI.Tests
+{
+    public class PlaceholderTests
+    {
+        [Test]
+        public void Foo()
+        {
+        }
+
+        [Test]
+        public void Bar()
+        {
+        }
+    }
+}
