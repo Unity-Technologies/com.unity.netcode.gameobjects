198fc18e9e48599c9f42decf30907ce01ca9f671
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2021-01-20 11:25:41 -0500

    feat: prototype SyncTransform, using netvars and local extrapolation

--- com.unity.multiplayer.mlapi/Runtime/Core/SyncTransform.cs
+++ com.unity.multiplayer.mlapi/Runtime/Core/SyncTransform.cs
@@ -9,17 +9,43 @@ namespace MLAPI
     /// with variables updating at specific place in the frame
     /// </summary>
     [AddComponentMenu("MLAPI/SyncTransform")]
+    // todo: check inheriting from NetworkedBehaviour. Currently needed for IsLocalPlayer, to synchronize position
     public class SyncTransform : NetworkedBehaviour
     {
-        NetworkedVar<Vector3> _varPos = new NetworkedVar<Vector3>();
-        NetworkedVar<Quaternion> _varRot = new NetworkedVar<Quaternion>();
-        float totalElapsed = 0.0f;
+        NetworkedVar<Vector3> m_varPos = new NetworkedVar<Vector3>();
+        NetworkedVar<Quaternion> m_varRot = new NetworkedVar<Quaternion>();
+        float m_totalElapsed = 0.0f;
         const float k_updateRate = 0.1f;
+        const float k_epsilon = 0.001f;
+
+        // data structures for interpolation
+        Vector3[] m_PosStore = new Vector3[2];
+        Quaternion[] m_RotStore = new Quaternion[2];
+        float[] m_PosTimes = new float[2];
+        float[] m_RotTimes = new float[2];
 
         SyncTransform()
         {
-            _varPos.OnValueChanged = SyncPosChanged;
-            _varRot.OnValueChanged = SyncRotChanged;
+            m_PosTimes[0] = -1.0f;
+            m_PosTimes[1] = -1.0f;
+            m_RotTimes[0] = -1.0f;
+            m_RotTimes[1] = -1.0f;
+
+            m_varPos.OnValueChanged = SyncPosChanged;
+            m_varRot.OnValueChanged = SyncRotChanged;
+        }
+
+        void SyncPosChanged(Vector3 before, Vector3 after)
+        {
+            if (!IsLocalPlayer)
+            {
+                m_PosTimes[0] = m_PosTimes[1];
+                m_PosTimes[1] = m_totalElapsed;
+                m_PosStore[0] = m_PosStore[1];
+                m_PosStore[1] = after;
+
+                gameObject.transform.position = after;
+            }
         }
 
         void SyncRotChanged(Quaternion before, Quaternion after)
@@ -27,38 +53,59 @@ namespace MLAPI
             // todo: this is problematic. Why couldn't this filtering be done server-side?
             if (!IsLocalPlayer)
             {
-                gameObject.transform.rotation = after;
-            }
-        }
+                m_RotTimes[0] = m_RotTimes[1];
+                m_RotTimes[1] = m_totalElapsed;
+                m_RotStore[0] = m_RotStore[1];
+                m_RotStore[1] = after;
 
-        void SyncPosChanged(Vector3 before, Vector3 after)
-        {
-            if (!IsLocalPlayer)
-            {
-                gameObject.transform.position = after;
+                gameObject.transform.rotation = after;
             }
         }
 
         void Start()
         {
-            _varPos.Settings.WritePermission = NetworkedVarPermission.Everyone;
-            _varRot.Settings.WritePermission = NetworkedVarPermission.Everyone;
+            m_varPos.Settings.WritePermission = NetworkedVarPermission.Everyone;
+            m_varRot.Settings.WritePermission = NetworkedVarPermission.Everyone;
         }
 
         void Update()
         {
+            float timeSinceLat = Time.deltaTime;
+            m_totalElapsed += timeSinceLat;
+
             // if this.gameObject is local let's send its position
             if (IsLocalPlayer)
             {
-                float timeSinceLat = Time.deltaTime;
-                totalElapsed += timeSinceLat;
-
-                while (totalElapsed > k_updateRate)
+                while (m_totalElapsed > k_updateRate)
                 {
-                    totalElapsed -= k_updateRate;
+                    m_totalElapsed -= k_updateRate;
 
-                    _varPos.Value = gameObject.transform.position;
-                    _varRot.Value = gameObject.transform.rotation;
+                    m_varPos.Value = gameObject.transform.position;
+                    m_varRot.Value = gameObject.transform.rotation;
+                }
+            }
+            else
+            {
+                // let's interpolate the last received transform
+                if (m_PosTimes[0] >= 0.0 && m_PosTimes[1] >= 0.0)
+                {
+                    if (m_PosTimes[1] - m_PosTimes[0] > k_epsilon)
+                    {
+                        gameObject.transform.position = Vector3.LerpUnclamped(
+                            m_PosStore[0],
+                            m_PosStore[1],
+                            (m_totalElapsed - m_PosTimes[0]) / (m_PosTimes[1] - m_PosTimes[0]));
+                    }
+                }
+                if (m_RotTimes[0] >= 0.0 && m_RotTimes[1] >= 0.0)
+                {
+                    if (m_RotTimes[1] - m_RotTimes[0] > k_epsilon)
+                    {
+                        gameObject.transform.rotation = Quaternion.SlerpUnclamped(
+                            m_RotStore[0],
+                            m_RotStore[1],
+                            (m_totalElapsed - m_RotTimes[0]) / (m_RotTimes[1] - m_RotTimes[0]));
+                    }
                 }
             }
         }
