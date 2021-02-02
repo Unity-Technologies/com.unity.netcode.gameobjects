5765b7b577821ffb684121464701558db854fbde
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-02-02 14:02:35 -0600

    fix: minor adjustment for host loopback serverRpc
    This fixes an offset issue with a host invoking a serverRpc.

--- com.unity.multiplayer.mlapi/Runtime/Core/NetworkedBehaviour.cs
+++ com.unity.multiplayer.mlapi/Runtime/Core/NetworkedBehaviour.cs
@@ -87,14 +87,15 @@ namespace MLAPI
             {
                 writer = rpcQueueContainer.BeginAddQueueItemToFrame(RpcQueueContainer.QueueItemType.ServerRpc, Time.realtimeSinceStartup, Transport.MLAPI_STDRPC_CHANNEL, 0,
                     NetworkingManager.Singleton.ServerClientId, null, QueueHistoryFrame.QueueFrameType.Outbound, NetworkUpdateManager.NetworkUpdateStage.LateUpdate);
+                if (!IsUsingBatching)
+                {
+                    writer.WriteBit(false); // Encrypted
+                    writer.WriteBit(false); // Authenticated
+                    writer.WriteBits(MLAPIConstants.MLAPI_SERVER_RPC, 6); // MessageType
+                }
             }
 
-            if (!IsUsingBatching)
-            {
-                writer.WriteBit(false); // Encrypted
-                writer.WriteBit(false); // Authenticated
-                writer.WriteBits(MLAPIConstants.MLAPI_SERVER_RPC, 6); // MessageType
-            }
+
 
             writer.WriteUInt64Packed(NetworkId); // NetworkObjectId
             writer.WriteUInt16Packed(GetBehaviourId()); // NetworkBehaviourId
