bc2752cc75cca624725adacba43cf0438c58463d
M. Fatih MAR <mfatihmar@gmail.com>
2021-02-04 23:56:05 +0000

    test: RpcBatcher Tests (#465)
    * minor refactor
    
    * add MessageBatcherTests
    
    * further refactorings
    
    * implement messagebatcher sendwiththreshold test
    
    * rename messagebatchertests as rpcbatchertests
    
    * implement rpcbatcher sendwithoutthreshold test
    
    * minor constant field name change

--- com.unity.multiplayer.mlapi/Runtime/Core/RpcBatcher.cs
+++ com.unity.multiplayer.mlapi/Runtime/Core/RpcBatcher.cs
@@ -10,29 +10,30 @@ using System.IO;
 
 namespace MLAPI
 {
-    class MessageBatcher
+    internal class RpcBatcher
     {
         public class SendStream
         {
-            public byte channel;
-            public PooledBitStream Stream = PooledBitStream.Get();
+            public byte Channel;
+            public PooledBitStream Stream;
             public PooledBitWriter Writer;
-            public bool Empty = true;
+            public bool IsEmpty = true;
 
             public SendStream()
             {
+                Stream = PooledBitStream.Get();
                 Writer = PooledBitWriter.Get(Stream);
             }
         }
 
         // Stores the stream of batched RPC to send to each client, by ClientId
-        private Dictionary<ulong, SendStream> SendDict = new Dictionary<ulong, SendStream>();
+        private readonly Dictionary<ulong, SendStream> SendDict = new Dictionary<ulong, SendStream>();
 
         // Used to store targets, internally
         private ulong[] TargetList = new ulong[0];
 
         // Used to mark longer lengths. Works because we can't have zero-sized messages
-        private const byte LongLenMarker = 0;
+        private const byte k_LongLenMarker = 0;
 
         private void PushLength(int length, ref PooledBitWriter writer)
         {
@@ -44,7 +45,7 @@ namespace MLAPI
             else
             {
                 // otherwise we write a two-byte length
-                writer.WriteByte(LongLenMarker); // mark larger size
+                writer.WriteByte(k_LongLenMarker); // mark larger size
                 writer.WriteByte((byte)(length % 256)); // write the length modulo 256
                 writer.WriteByte((byte)(length / 256)); // write the length divided by 256
             }
@@ -55,10 +56,11 @@ namespace MLAPI
             int read = messageStream.ReadByte();
             // if we read a non-zero value, we have a single byte length
             // or a -1 error we can return
-            if (read != LongLenMarker)
+            if (read != k_LongLenMarker)
             {
                 return read;
             }
+
             // otherwise, a two-byte length follows. We'll read in len1, len2
             int len1 = messageStream.ReadByte();
             if (len1 < 0)
@@ -66,6 +68,7 @@ namespace MLAPI
                 // pass errors back to caller
                 return len1;
             }
+
             int len2 = messageStream.ReadByte();
             if (len2 < 0)
             {
@@ -80,21 +83,19 @@ namespace MLAPI
         /// FillTargetList
         /// Fills a list with the ClientId's an item is targeted to
         /// </summary>
-        /// <param name="item">the FrameQueueItem we want targets for</param>
-        /// <param name="list">the list to fill</param>
-        private static void FillTargetList(in FrameQueueItem item, ref ulong[] list)
+        /// <param name="queueItem">the FrameQueueItem we want targets for</param>
+        /// <param name="networkIdList">the list to fill</param>
+        private static void FillTargetList(in RpcFrameQueueItem queueItem, ref ulong[] networkIdList)
         {
-            switch (item.queueItemType)
+            switch (queueItem.queueItemType)
             {
                 case RpcQueueContainer.QueueItemType.ServerRpc:
-                    Array.Resize(ref list, 1);
-                    list[0] = item.networkId;
+                    Array.Resize(ref networkIdList, 1);
+                    networkIdList[0] = queueItem.networkId;
                     break;
                 case RpcQueueContainer.QueueItemType.ClientRpc:
                     // copy the list
-                    list = item.clientIds.ToArray();
-                    break;
-                default:
+                    networkIdList = queueItem.clientIds.ToArray();
                     break;
             }
         }
@@ -103,10 +104,10 @@ namespace MLAPI
         /// QueueItem
         /// Add a FrameQueueItem to be sent
         /// </summary>queueItem
-        /// <param name="item">the threshold in bytes</param>
-        public void QueueItem(in FrameQueueItem item)
+        /// <param name="queueItem">the threshold in bytes</param>
+        public void QueueItem(in RpcFrameQueueItem queueItem)
         {
-            FillTargetList(item, ref TargetList);
+            FillTargetList(queueItem, ref TargetList);
 
             foreach (ulong clientId in TargetList)
             {
@@ -117,15 +118,15 @@ namespace MLAPI
                     SendDict[clientId] = new SendStream();
                 }
 
-                if (SendDict[clientId].Empty)
+                if (SendDict[clientId].IsEmpty)
                 {
-                    SendDict[clientId].Empty = false;
-                    SendDict[clientId].channel = item.channel;
+                    SendDict[clientId].IsEmpty = false;
+                    SendDict[clientId].Channel = queueItem.channel;
 
                     SendDict[clientId].Writer.WriteBit(false); // Encrypted
                     SendDict[clientId].Writer.WriteBit(false); // Authenticated
 
-                    switch (item.queueItemType)
+                    switch (queueItem.queueItemType)
                     {
                         // 6 bits are used for the message type, which is an MLAPIConstants
                         case RpcQueueContainer.QueueItemType.ServerRpc:
@@ -138,47 +139,46 @@ namespace MLAPI
                 }
 
                 // write the amounts of bytes that are coming up
-                PushLength(item.messageData.Count, ref SendDict[clientId].Writer);
+                PushLength(queueItem.messageData.Count, ref SendDict[clientId].Writer);
 
                 // write the message to send
-                SendDict[clientId].Writer.WriteBytes(item.messageData.Array, item.messageData.Count, item.messageData.Offset);
+                SendDict[clientId].Writer.WriteBytes(queueItem.messageData.Array, queueItem.messageData.Count, queueItem.messageData.Offset);
 
-                ProfilerStatManager.bytesSent.Record((int)item.messageData.Count);
+                ProfilerStatManager.bytesSent.Record(queueItem.messageData.Count);
                 ProfilerStatManager.rpcsSent.Record();
             }
         }
 
         public delegate void SendCallbackType(ulong clientId, SendStream messageStream);
-        public delegate void ReceiveCallbackType(BitStream messageStream, RpcQueueContainer.QueueItemType messageType, ulong clientId, float time);
+        public delegate void ReceiveCallbackType(BitStream messageStream, RpcQueueContainer.QueueItemType messageType, ulong clientId, float receiveTime);
 
         /// <summary>
         /// SendItems
         /// Send any batch of RPC that are of length above threshold
         /// </summary>
-        /// <param name="threshold"> the threshold in bytes</param>
+        /// <param name="thresholdBytes"> the threshold in bytes</param>
         /// <param name="sendCallback"> the function to call for sending the batch</param>
-        public void SendItems(int threshold, SendCallbackType sendCallback)
+        public void SendItems(int thresholdBytes, SendCallbackType sendCallback)
         {
             foreach (KeyValuePair<ulong, SendStream> entry in SendDict)
             {
-                if (!entry.Value.Empty)
+                if (!entry.Value.IsEmpty)
                 {
                     // read the queued message
                     int length = (int)SendDict[entry.Key].Stream.Length;
 
-                    if (length >= threshold)
+                    if (length >= thresholdBytes)
                     {
                         sendCallback(entry.Key, entry.Value);
                         // clear the batch that was sent from the SendDict
                         entry.Value.Stream.SetLength(0);
                         entry.Value.Stream.Position = 0;
-                        entry.Value.Empty = true;
+                        entry.Value.IsEmpty = true;
                         ProfilerStatManager.rpcBatchesSent.Record();
                     }
                 }
             }
         }
-
 
         /// <summary>
         /// ReceiveItems
@@ -189,35 +189,33 @@ namespace MLAPI
         /// <param name="messageType"> the message type to pass back to callback</param>
         /// <param name="clientId"> the clientId to pass back to callback</param>
         /// <param name="receiveTime"> the packet receive time to pass back to callback</param>
-        public int ReceiveItems(in BitStream messageStream, ReceiveCallbackType receiveCallback, RpcQueueContainer.QueueItemType messageType, ulong clientId, float receiveTime)
+        public void ReceiveItems(in BitStream messageStream, ReceiveCallbackType receiveCallback, RpcQueueContainer.QueueItemType messageType, ulong clientId, float receiveTime)
         {
-            PooledBitStream copy = PooledBitStream.Get();
-            do
+            using (var copy = PooledBitStream.Get())
             {
-                // read the length of the next RPC
-                int rpcSize = PopLength(messageStream);
-
-                if (rpcSize < 0)
+                do
                 {
-                    copy.Dispose();
-                    // abort if there's an error reading lengths
-                    return 0;
-                }
+                    // read the length of the next RPC
+                    int rpcSize = PopLength(messageStream);
+                    if (rpcSize < 0)
+                    {
+                        // abort if there's an error reading lengths
+                        return;
+                    }
 
-                // copy what comes after current stream position
-                long pos = messageStream.Position;
-                copy.SetLength(rpcSize);
-                copy.Position = 0;
-                Buffer.BlockCopy(messageStream.GetBuffer(), (int)pos, copy.GetBuffer(), 0, rpcSize);
+                    // copy what comes after current stream position
+                    long position = messageStream.Position;
+                    copy.SetLength(rpcSize);
+                    copy.Position = 0;
+                    Buffer.BlockCopy(messageStream.GetBuffer(), (int)position, copy.GetBuffer(), 0, rpcSize);
 
-                receiveCallback(copy, messageType, clientId, receiveTime);
+                    receiveCallback(copy, messageType, clientId, receiveTime);
 
-                // seek over the RPC
-                // RPCReceiveQueueItem peeks at content, it doesn't advance
-                messageStream.Seek(rpcSize, SeekOrigin.Current);
-            } while (messageStream.Position < messageStream.Length);
-            copy.Dispose();
-            return 0;
+                    // seek over the RPC
+                    // RPCReceiveQueueItem peeks at content, it doesn't advance
+                    messageStream.Seek(rpcSize, SeekOrigin.Current);
+                } while (messageStream.Position < messageStream.Length);
+            }
         }
     }
 }
