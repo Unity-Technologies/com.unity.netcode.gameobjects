e3200b3e89c42099b86262118896057311747806
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-02-05 12:58:56 -0600

    refactor: Simply default test
    Remoing previous test and putting in very bare minimum test.

---
+++ com.unity.multiplayer.mlapi/Tests/PlayMode/PlaceholderTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using MLAPI.Messaging;
+
+namespace MLAPI.Tests.PlayMode
+{
+    public class PlaceholderTests
+    {
+        [Test]
+        public void Foo()
+        {
+        }
+    }
+}
