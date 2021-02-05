0e61d4a3eae8aa92fbc5b5ad869c49fbb13c3e58
M. Fatih MAR <mfatihmar@gmail.com>
2021-02-05 19:39:03 +0000

    refactor: namings

--- com.unity.multiplayer.mlapi/Tests/Runtime/PlaceholderTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/PlaceholderTests.cs
@@ -1,10 +1,6 @@
-using System.Collections;
 using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
-using MLAPI.Messaging;
 
-namespace MLAPI.Tests.PlayMode
+namespace MLAPI.RuntimeTests
 {
     public class PlaceholderTests
     {
