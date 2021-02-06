ed34dbd5b3ac65238aa698634cbfb025a717536c
Matt Walsh <matt.walsh@unity3d.com>
2021-02-05 17:22:49 -0800

    remove enet test

--- com.unity.multiplayer.mlapi/Runtime/Transports/Tests/Editor/TransportTest.cs
+++ com.unity.multiplayer.mlapi/Runtime/Transports/Tests/Editor/TransportTest.cs
@@ -70,54 +70,5 @@ public class TransportTest : MonoBehaviour
         nm.StopServer();
         nm.Shutdown();
     }
-
-    [Test]
-    public void ENetCustomChannelRegistrationTest()
-    {
-        ForceNetowrkObjectShutdown();
-
-        GameObject o = new GameObject();
-        NetworkingManager nm = (NetworkingManager)o.AddComponent(typeof(NetworkingManager));
-        nm.SetSingleton();
-        nm.NetworkConfig = new NetworkConfig();
-        EnetTransform ut = (UnetTransport)o.AddComponent(typeof(UnetTransport));
-
-        ut.ServerListenPort = 7777;
-        nm.NetworkConfig.NetworkTransport = ut;
-
-        byte CustomChannel = 0;
-
-        // test 1: add a legit channel.
-        ut.Channels.Add(new UnetChannel() {Id = Channel.ChannelUnused + CustomChannel, Type = QosType.Unreliable});
-
-        try
-        {
-            nm.StartServer();
-        }
-        catch (InvalidChannelException e)
-        {
-            Assert.Fail("The UNet transport won't allow registration of a legit user channel");
-        }
-
-        nm.StopServer();
-        nm.Shutdown();
-
-        ut.Channels.Clear();
-        // test 2: add a bogus channel (one that intersects with the MLAPI built-in ones.)  Expect failure
-        ut.Channels.Add(new UnetChannel() {Id = Channel.Internal, Type = QosType.Unreliable});
-
-        try
-        {
-            nm.StartServer();
-            Assert.Fail("The Unet transport allowed registration of an MLAPI-reserved channel");
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e.Message);
-        }
-
-        nm.StopServer();
-        nm.Shutdown();
-    }
 }
 
