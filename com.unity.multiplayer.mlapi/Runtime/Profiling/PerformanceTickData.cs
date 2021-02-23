305b6dc6df527636584b8e42f422189f2c62af24
kvassall-unity <kamau.vassall@unity3d.com>
2021-02-23 13:35:18 -0600

    Simplify the code even more

--- com.unity.multiplayer.mlapi/Runtime/Profiling/PerformanceTickData.cs
+++ com.unity.multiplayer.mlapi/Runtime/Profiling/PerformanceTickData.cs
@@ -1,19 +1,31 @@
-﻿namespace MLAPI.Profiling
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MLAPI.Profiling
 {
     public class PerformanceTickData
     {
         public int tickID;
 
-        public readonly ProfilingDataStore tickData = new ProfilingDataStore();
+        readonly ProfilingDataStore m_TickData = new ProfilingDataStore();
 
         public void Increment(string fieldName, int count = 1)
         {
-            tickData.Increment(fieldName, count);
+            m_TickData.Increment(fieldName, count);
+        }
+
+        public void AddNonDuplicateData(IReadOnlyDictionary<string, int> transportProfilerData)
+        {
+            IEnumerable<KeyValuePair<string, int>> nonDuplicates = transportProfilerData.Where(entry => !m_TickData.HasData(entry.Key));
+            foreach (KeyValuePair<string, int> entry in nonDuplicates)
+            {
+                m_TickData.Add(entry.Key, entry.Value);
+            }
         }
 
         public int GetData(string fieldName)
         {
-            return tickData.GetData(fieldName);
+            return m_TickData.GetData(fieldName);
         }
     }
 }
