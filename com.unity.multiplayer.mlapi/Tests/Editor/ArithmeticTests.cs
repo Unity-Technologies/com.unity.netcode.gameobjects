d2e8b1e85e6e2884c2c5dd94f8a2664606ee376f
Albin Corén <2108U9@gmail.com>
2021-02-26 13:28:48 +0100

    test: Added BitStream and Arithmetic tests

---
+++ com.unity.multiplayer.mlapi/Tests/Editor/ArithmeticTests.cs
@@ -0,0 +1,31 @@
+using System;
+using MLAPI.Serialization;
+using NUnit.Framework;
+
+public class ArithmeticTests
+{
+    [Test]
+    public void TestCeil()
+    {
+        Assert.That(Arithmetic.CeilingExact(10, 5), Is.EqualTo(2));
+        Assert.That(Arithmetic.CeilingExact(11, 5), Is.EqualTo(3));
+        Assert.That(Arithmetic.CeilingExact(0, 5), Is.EqualTo(0));
+        Assert.That(Arithmetic.CeilingExact(1, 5), Is.EqualTo(1));
+        Assert.That(Arithmetic.CeilingExact(2, 5), Is.EqualTo(1));
+        Assert.That(Arithmetic.CeilingExact(3, 5), Is.EqualTo(1));
+        Assert.That(Arithmetic.CeilingExact(4, 5), Is.EqualTo(1));
+        Assert.That(Arithmetic.CeilingExact(5, 5), Is.EqualTo(1));
+        Assert.That(Arithmetic.CeilingExact(6, 5), Is.EqualTo(2));
+
+    }
+
+    [Test]
+    public void TestZigZag()
+    {
+        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(1234)), Is.EqualTo(1234));
+        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(-1)), Is.EqualTo(-1));
+        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(0)), Is.EqualTo(0));
+        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MaxValue)), Is.EqualTo(Int64.MaxValue));
+        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MinValue)), Is.EqualTo(Int64.MinValue));
+    }
+}
