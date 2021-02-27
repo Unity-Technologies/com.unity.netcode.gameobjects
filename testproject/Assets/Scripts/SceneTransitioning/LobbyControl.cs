982f7a8e1f9848b01bae990b2cd351298bf8ba72
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-02-27 01:47:30 -0600

    fix and format:  Client camera and CR
    Fixing some weird LFCR issues that started happening recently.
    Fixed client camera not being synchronized when joining a game already in progress.
    Fixed client not being able to exit.
    Added a flag to the in game manager to tell it whether it should exit upon being the last player or not.

--- testproject/Assets/Scripts/SceneTransitioning/LobbyControl.cs
+++ testproject/Assets/Scripts/SceneTransitioning/LobbyControl.cs
@@ -1,9 +1,303 @@
-using System.IO;using System.Collections.Generic;using MLAPI;using MLAPI.SceneManagement;using MLAPI.Messaging;using UnityEngine;using UnityEngine.UI;public class LobbyControl : NetworkedBehaviour{    [SerializeField]    bool m_LaunchAsHostInEditor;    [SerializeField]    private Text LobbyText;    [Tooltip("This value only determines if the minimum numbers of players to start are present.  All players must hit the ready button to launch.")]    [Range(1,16)]    [SerializeField]    private int m_MinPlayersToStart = 2;    private Dictionary<ulong,bool> m_ClientsInLobby;    private bool m_AllPlayersInLobby;    private string m_UserLobbyStatusText;    /// <summary>    /// Awake    /// This is one way to kick off a multiplayer session    /// </summary>    private void Awake()    {        m_ClientsInLobby = new Dictionary<ulong, bool>();#if(UNITY_EDITOR)        if( NetworkingManager.Singleton == null)        {            GlobalGameState.EditorLaunchingAsHost = m_LaunchAsHostInEditor;            //This will automatically launch the MLAPIBootStrap and then transition directly to the scene this control is contained within (for easy development of scenes)            GlobalGameState.LoadBootStrapScene();            return;        }#endif        if (NetworkingManager.Singleton.IsListening)        {            //Always add ourselves to the list at first            m_ClientsInLobby.Add(NetworkingManager.Singleton.LocalClientId, false);            NetworkingManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;            //If we are hosting, then handle the server side for detecting when clients have connected            //and when their lobby scenes are finished loading.            if(IsServer)            {                m_AllPlayersInLobby = false;                //Server will be notified when a client connects                GlobalGameState.Singleton.clientLoadedScene += ClientLoadedScene;            }            //Update our lobby            GenerateUserStatsForLobby();        }    }    private void AllPlayersLoadedScene()    {        throw new System.NotImplementedException();    }    NetworkedObject LocalPlayerObject;    private void Start()    {        FreezeAndHidePlayers();    }
+using System.IO;
+using System.Collections.Generic;
+using MLAPI;
+using MLAPI.SceneManagement;
+using MLAPI.Messaging;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyControl : NetworkedBehaviour
+{
+    [SerializeField]
+    bool m_LaunchAsHostInEditor;
+
+    [SerializeField]
+    private Text LobbyText;
+
+    [Tooltip("This value only determines if the minimum numbers of players to start are present.  All players must hit the ready button to launch.")]
+    [Range(1,16)]
+    [SerializeField]
+    private int m_MinPlayersToStart = 2;
+
+    private Dictionary<ulong,bool> m_ClientsInLobby;
+    private bool m_AllPlayersInLobby;
+    private string m_UserLobbyStatusText;
+
+    /// <summary>
+    /// Awake
+    /// This is one way to kick off a multiplayer session
+    /// </summary>
+    private void Awake()
+    {
+        m_ClientsInLobby = new Dictionary<ulong, bool>();
+#if(UNITY_EDITOR)
+        if( NetworkingManager.Singleton == null)
+        {
+            GlobalGameState.EditorLaunchingAsHost = m_LaunchAsHostInEditor;
+            //This will automatically launch the MLAPIBootStrap and then transition directly to the scene this control is contained within (for easy development of scenes)
+            GlobalGameState.LoadBootStrapScene();
+            return;
+        }
+#endif
+
+        if (NetworkingManager.Singleton.IsListening)
+        {
+            //Always add ourselves to the list at first
+            m_ClientsInLobby.Add(NetworkingManager.Singleton.LocalClientId, false);
+
+            NetworkingManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+            //If we are hosting, then handle the server side for detecting when clients have connected
+            //and when their lobby scenes are finished loading.
+            if(IsServer)
+            {
+                m_AllPlayersInLobby = false;
+                //Server will be notified when a client connects
+
+                GlobalGameState.Singleton.clientLoadedScene += ClientLoadedScene;
+            }
+            //Update our lobby
+            GenerateUserStatsForLobby();
+        }
+    }
+
+    private void AllPlayersLoadedScene()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    NetworkedObject LocalPlayerObject;
+    private void Start()
+    {
+        FreezeAndHidePlayers();
+    }
 
 
     void FreezeAndHidePlayers()
     {
-        NetworkedObject[] NetoworkedObjects = GameObject.FindObjectsOfType<NetworkedObject>();        foreach (NetworkedObject networkedObject in NetoworkedObjects)        {            RandomPlayerMover PlayerMover = networkedObject.GetComponent<RandomPlayerMover>();            if (networkedObject.IsLocalPlayer)            {                LocalPlayerObject = networkedObject;            }            if (PlayerMover)            {                if (!IsOwner)                {                    AudioListener OtherAudioListener = networkedObject.GetComponent<AudioListener>();                    if (OtherAudioListener)                    {                        OtherAudioListener.enabled = false;                    }                }                PlayerMover.OnPaused(true);                PlayerMover.OnIsHidden(true);            }        }
-    }    /// <summary>    /// GenerateUserStatsForLobby    /// Psuedo code for setting player state    /// Just updating a text field, this could use a lot of "refactoring"  :)    /// </summary>    private void GenerateUserStatsForLobby()    {        m_UserLobbyStatusText = string.Empty;        foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)        {            m_UserLobbyStatusText += "Player_" + clientLobbyStatus.Key.ToString() + "          ";            if(clientLobbyStatus.Value)            {                m_UserLobbyStatusText += "(Ready)\n";            }            else            {                m_UserLobbyStatusText += "(Not Ready)\n";            }        }
+        NetworkedObject[] NetoworkedObjects = GameObject.FindObjectsOfType<NetworkedObject>();
+        foreach (NetworkedObject networkedObject in NetoworkedObjects)
+        {
+            RandomPlayerMover PlayerMover = networkedObject.GetComponent<RandomPlayerMover>();
+            if (networkedObject.IsLocalPlayer)
+            {
+                LocalPlayerObject = networkedObject;
+            }
+            if (PlayerMover)
+            {
+                if (!IsOwner)
+                {
+                    AudioListener OtherAudioListener = networkedObject.GetComponent<AudioListener>();
+                    if (OtherAudioListener)
+                    {
+                        OtherAudioListener.enabled = false;
+                    }
+                }
+                PlayerMover.OnPaused(true);
+                PlayerMover.OnIsHidden(true);
+            }
+        }
+    }
 
-        FreezeAndHidePlayers();    }    /// <summary>    /// Update our lobby    /// </summary>    private void OnGUI()    {        if(LobbyText != null)        {            LobbyText.text = m_UserLobbyStatusText;        }    }    /// <summary>    /// UpdateAndCheckPlayersInLobby    /// Checks to see if we have at least 2 or more people to start    /// </summary>    void UpdateAndCheckPlayersInLobby()    {        //This is game preference, but I am assuming at least 2 players?        m_AllPlayersInLobby = (m_ClientsInLobby.Count >= m_MinPlayersToStart);        foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)        {            SendClientReadyStatusUpdatesClientRpc(clientLobbyStatus.Key, clientLobbyStatus.Value);            if(!NetworkingManager.Singleton.ConnectedClients.ContainsKey(clientLobbyStatus.Key))            {                //If some clients are still loading into the lobby scene then this is false                m_AllPlayersInLobby = false;            }        }        CheckForAllPlayersReady();    }    /// <summary>    /// ClientLoadedScene    /// Invoked when a client has loaded this scene    /// </summary>    /// <param name="clientId"></param>    private void ClientLoadedScene(ulong clientId)    {        if(IsServer)        {            if(!m_ClientsInLobby.ContainsKey(clientId))            {                m_ClientsInLobby.Add(clientId, false);                GenerateUserStatsForLobby();            }            UpdateAndCheckPlayersInLobby();        }    }    /// <summary>    /// OnClientConnectedCallback    /// Since we are entering a lobby and MLAPI NetowrkingManager is spawning the player,    /// the server can be configured to only listen for connected clients at this stage.    /// </summary>    /// <param name="clientId">client that connected</param>    private void OnClientConnectedCallback(ulong clientId)    {        if(!m_ClientsInLobby.ContainsKey(clientId))        {            m_ClientsInLobby.Add(clientId, false);        }        if(IsServer)        {            UpdateAndCheckPlayersInLobby();        }        else        {            OnClientIsReadyServerRpc(NetworkingManager.Singleton.LocalClientId, false);        }        GenerateUserStatsForLobby();    }    /// <summary>    /// SendClientReadyStatusUpdatesClientRpc    /// Sent from the server to the client when a player's status is updated.    /// This also populates the connected clients' (excluding host) player state in the lobby    /// </summary>    /// <param name="clientId"></param>    /// <param name="isReady"></param>    [ClientRpc]    void SendClientReadyStatusUpdatesClientRpc(ulong clientId, bool isReady)    {        if(!IsServer)        {            if(!m_ClientsInLobby.ContainsKey(clientId))            {                m_ClientsInLobby.Add(clientId,isReady);            }            else            {                 m_ClientsInLobby[clientId] = isReady;            }            GenerateUserStatsForLobby();        }    }    /// <summary>    /// CheckForAllPlayersReady    /// Checks to see if all players are ready, and if so launches the game    /// </summary>    private void CheckForAllPlayersReady()    {        if(m_AllPlayersInLobby)        {            bool AllPlayersAreReady = true;            foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)            {                if(!clientLobbyStatus.Value)                {                    //If some clients are still loading into the lobby scene then this is false                    AllPlayersAreReady = false;                }            }            //Only if all players are ready            if(AllPlayersAreReady)            {                //Remove our client connected callback                NetworkingManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;                //Remove our scene loaded callback                GlobalGameState.Singleton.clientLoadedScene -= ClientLoadedScene;                //Transition to the ingame scene                GlobalGameState.Singleton.SetGameState(GlobalGameState.GameStates.InGame);            }        }    }    /// <summary>    /// PlayerIsReady    /// Tied to the Ready button in the InvadersLobby scene    /// </summary>    public void PlayerIsReady()    {        if(IsServer)        {            m_ClientsInLobby[NetworkingManager.Singleton.ServerClientId] = !m_ClientsInLobby[NetworkingManager.Singleton.ServerClientId];            UpdateAndCheckPlayersInLobby();        }        else        {            m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId] =  !m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId];            OnClientIsReadyServerRpc(NetworkingManager.Singleton.LocalClientId,m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId]);        }        GenerateUserStatsForLobby();    }    /// <summary>    /// OnClientIsReadyServerRpc    /// Sent to the server when the player clicks the ready button    /// </summary>    /// <param name="clientid">clientId that is ready</param>    [ServerRpc(RequireOwnership = false)]    void OnClientIsReadyServerRpc(ulong clientid, bool isReady)    {        if(m_ClientsInLobby.ContainsKey(clientid))        {            m_ClientsInLobby[clientid] = isReady;            UpdateAndCheckPlayersInLobby();            GenerateUserStatsForLobby();        }    }    private void OnDestroy()    {        if(NetworkingManager.Singleton != null)        {            NetworkingManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;            if(IsServer)            {                GlobalGameState.Singleton.clientLoadedScene -= ClientLoadedScene;            }        }    }}
+    /// <summary>
+    /// GenerateUserStatsForLobby
+    /// Psuedo code for setting player state
+    /// Just updating a text field, this could use a lot of "refactoring"  :)
+    /// </summary>
+    private void GenerateUserStatsForLobby()
+    {
+        m_UserLobbyStatusText = string.Empty;
+        foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)
+        {
+            m_UserLobbyStatusText += "Player_" + clientLobbyStatus.Key.ToString() + "          ";
+            if(clientLobbyStatus.Value)
+            {
+                m_UserLobbyStatusText += "(Ready)\n";
+            }
+            else
+            {
+                m_UserLobbyStatusText += "(Not Ready)\n";
+            }
+        }
+
+        FreezeAndHidePlayers();
+    }
+
+    /// <summary>
+    /// Update our lobby
+    /// </summary>
+    private void OnGUI()
+    {
+        if(LobbyText != null)
+        {
+            LobbyText.text = m_UserLobbyStatusText;
+        }
+    }
+
+    /// <summary>
+    /// UpdateAndCheckPlayersInLobby
+    /// Checks to see if we have at least 2 or more people to start
+    /// </summary>
+    void UpdateAndCheckPlayersInLobby()
+    {
+        //This is game preference, but I am assuming at least 2 players?
+        m_AllPlayersInLobby = (m_ClientsInLobby.Count >= m_MinPlayersToStart);
+
+        foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)
+        {
+            SendClientReadyStatusUpdatesClientRpc(clientLobbyStatus.Key, clientLobbyStatus.Value);
+            if(!NetworkingManager.Singleton.ConnectedClients.ContainsKey(clientLobbyStatus.Key))
+            {
+                //If some clients are still loading into the lobby scene then this is false
+                m_AllPlayersInLobby = false;
+            }
+        }
+        CheckForAllPlayersReady();
+    }
+
+    /// <summary>
+    /// ClientLoadedScene
+    /// Invoked when a client has loaded this scene
+    /// </summary>
+    /// <param name="clientId"></param>
+    private void ClientLoadedScene(ulong clientId)
+    {
+        if(IsServer)
+        {
+            if(!m_ClientsInLobby.ContainsKey(clientId))
+            {
+                m_ClientsInLobby.Add(clientId, false);
+                GenerateUserStatsForLobby();
+            }
+
+            UpdateAndCheckPlayersInLobby();
+        }
+    }
+
+    /// <summary>
+    /// OnClientConnectedCallback
+    /// Since we are entering a lobby and MLAPI NetowrkingManager is spawning the player,
+    /// the server can be configured to only listen for connected clients at this stage.
+    /// </summary>
+    /// <param name="clientId">client that connected</param>
+    private void OnClientConnectedCallback(ulong clientId)
+    {
+        if(!m_ClientsInLobby.ContainsKey(clientId))
+        {
+            m_ClientsInLobby.Add(clientId, false);
+
+        }
+        if(IsServer)
+        {
+            UpdateAndCheckPlayersInLobby();
+        }
+        else
+        {
+            OnClientIsReadyServerRpc(NetworkingManager.Singleton.LocalClientId, false);
+        }
+        GenerateUserStatsForLobby();
+    }
+
+    /// <summary>
+    /// SendClientReadyStatusUpdatesClientRpc
+    /// Sent from the server to the client when a player's status is updated.
+    /// This also populates the connected clients' (excluding host) player state in the lobby
+    /// </summary>
+    /// <param name="clientId"></param>
+    /// <param name="isReady"></param>
+    [ClientRpc]
+    void SendClientReadyStatusUpdatesClientRpc(ulong clientId, bool isReady)
+    {
+        if(!IsServer)
+        {
+            if(!m_ClientsInLobby.ContainsKey(clientId))
+            {
+                m_ClientsInLobby.Add(clientId,isReady);
+            }
+            else
+            {
+                 m_ClientsInLobby[clientId] = isReady;
+            }
+            GenerateUserStatsForLobby();
+        }
+    }
+
+    /// <summary>
+    /// CheckForAllPlayersReady
+    /// Checks to see if all players are ready, and if so launches the game
+    /// </summary>
+    private void CheckForAllPlayersReady()
+    {
+        if(m_AllPlayersInLobby)
+        {
+            bool AllPlayersAreReady = true;
+            foreach(KeyValuePair<ulong,bool> clientLobbyStatus in m_ClientsInLobby)
+            {
+                if(!clientLobbyStatus.Value)
+                {
+                    //If some clients are still loading into the lobby scene then this is false
+                    AllPlayersAreReady = false;
+                }
+            }
+
+            //Only if all players are ready
+            if(AllPlayersAreReady)
+            {
+                //Remove our client connected callback
+                NetworkingManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+
+                //Remove our scene loaded callback
+                GlobalGameState.Singleton.clientLoadedScene -= ClientLoadedScene;
+
+                //Transition to the ingame scene
+                GlobalGameState.Singleton.SetGameState(GlobalGameState.GameStates.InGame);
+            }
+        }
+    }
+
+    /// <summary>
+    /// PlayerIsReady
+    /// Tied to the Ready button in the InvadersLobby scene
+    /// </summary>
+    public void PlayerIsReady()
+    {
+        if(IsServer)
+        {
+            m_ClientsInLobby[NetworkingManager.Singleton.ServerClientId] = !m_ClientsInLobby[NetworkingManager.Singleton.ServerClientId];
+            UpdateAndCheckPlayersInLobby();
+        }
+        else
+        {
+            m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId] =  !m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId];
+            OnClientIsReadyServerRpc(NetworkingManager.Singleton.LocalClientId,m_ClientsInLobby[NetworkingManager.Singleton.LocalClientId]);
+        }
+        GenerateUserStatsForLobby();
+    }
+
+    /// <summary>
+    /// OnClientIsReadyServerRpc
+    /// Sent to the server when the player clicks the ready button
+    /// </summary>
+    /// <param name="clientid">clientId that is ready</param>
+    [ServerRpc(RequireOwnership = false)]
+    void OnClientIsReadyServerRpc(ulong clientid, bool isReady)
+    {
+        if(m_ClientsInLobby.ContainsKey(clientid))
+        {
+            m_ClientsInLobby[clientid] = isReady;
+            UpdateAndCheckPlayersInLobby();
+            GenerateUserStatsForLobby();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if(NetworkingManager.Singleton != null)
+        {
+            NetworkingManager.Singleton.OnClientConnectedCallback -= OnClientConnectedCallback;
+            if(IsServer)
+            {
+                GlobalGameState.Singleton.clientLoadedScene -= ClientLoadedScene;
+            }
+        }
+    }
+}
--- testproject/Assets/Scripts/SceneTransitioning/RandomPlayerMover.cs
+++ testproject/Assets/Scripts/SceneTransitioning/RandomPlayerMover.cs
@@ -1,11 +1,33 @@
-using UnityEngine;using MLAPI;using MLAPI.NetworkedVar;public class RandomPlayerMover : NetworkedBehaviour{    public Vector3 targetLocation;    public float speed = 1;    private Rigidbody m_Rigidbody;    [HideInInspector]    private bool IsPaused;    [SerializeField]    private Camera PlayerCamera;    private MeshRenderer m_MeshRenderer;
+using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkedVar;
+
+public class RandomPlayerMover : NetworkedBehaviour
+{
+    public Vector3 targetLocation;
+    public float speed = 1;
+
+    private Rigidbody m_Rigidbody;
 
     [HideInInspector]
-    [SerializeField]    private RigidbodyConstraints m_DefaultConstraints;
-    private void Start()    {        m_Rigidbody = GetComponent<Rigidbody>();
+    private bool IsPaused;
+
+    [SerializeField]
+    private Camera PlayerCamera;
+
+    private MeshRenderer m_MeshRenderer;
+
+    [HideInInspector]
+    [SerializeField]
+    private RigidbodyConstraints m_DefaultConstraints;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
 
 
-        if(m_Rigidbody)        {
+        if(m_Rigidbody)
+        {
             if(m_DefaultConstraints == RigidbodyConstraints.None)
             {
                 m_DefaultConstraints = m_Rigidbody.constraints;
@@ -13,22 +35,66 @@ using UnityEngine;using MLAPI;using MLAPI.NetworkedVar;public class RandomPl
             else
             {
                 m_Rigidbody.constraints = m_DefaultConstraints;
-            }        }
-        m_MeshRenderer = GetComponent<MeshRenderer>();        if (PlayerCamera )        {            if(PlayerCamera.enabled)            {                if(IsOwner)                {                    PlayerCamera.enabled = false;                    Camera.main.enabled = true;                }                else                {                    PlayerCamera.gameObject.SetActive(false);                }            }        }
-        var temp = transform.position;        temp.y = 0.5f;        transform.position = temp;        targetLocation = GetRandomLocation();    }    public void SetPlayerCamera()    {        if (PlayerCamera && IsOwner)        {            if(!PlayerCamera.enabled)            {                Camera.main.enabled = false;                PlayerCamera.enabled = true;            }        }    }    public void OnIsHidden(bool isHidden)    {
+            }
+        }
+
+        m_MeshRenderer = GetComponent<MeshRenderer>();
+
+        if (PlayerCamera )
+        {
+            if(PlayerCamera.enabled)
+            {
+                if(IsOwner)
+                {
+                    PlayerCamera.enabled = false;
+                    Camera.main.enabled = true;
+                }
+                else
+                {
+                    PlayerCamera.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        var temp = transform.position;
+        temp.y = 0.5f;
+        transform.position = temp;
+        targetLocation = GetRandomLocation();
+    }
+
+    public void SetPlayerCamera()
+    {
+        if (PlayerCamera && IsOwner)
+        {
+            if(!PlayerCamera.enabled)
+            {
+                PlayerCamera.enabled = true;
+                Camera.main.enabled = false;
+            }
+        }
+    }
+
+    public void OnIsHidden(bool isHidden)
+    {
         if (!m_MeshRenderer)
         {
             m_MeshRenderer = GetComponent<MeshRenderer>();
         }
-        if (m_MeshRenderer)
+
+        if (m_MeshRenderer)
         {
             m_MeshRenderer.enabled = !isHidden;
-        }    }
+        }
+    }
 
-    public void OnPaused(bool isPaused)    {        IsPaused = isPaused;
-        if(!m_Rigidbody)        {
+    public void OnPaused(bool isPaused)
+    {
+        IsPaused = isPaused;
+        if(!m_Rigidbody)
+        {
             m_Rigidbody = GetComponent<Rigidbody>();
-            if(m_Rigidbody)            {
+            if(m_Rigidbody)
+            {
                 if(m_DefaultConstraints == RigidbodyConstraints.None)
                 {
                     m_DefaultConstraints = m_Rigidbody.constraints;
@@ -36,10 +102,50 @@ using UnityEngine;using MLAPI;using MLAPI.NetworkedVar;public class RandomPl
                 else
                 {
                     m_Rigidbody.constraints = m_DefaultConstraints;
-                }            }        }
+                }
+            }
+        }
 
         if(m_Rigidbody)
-        {            if (IsPaused)            {                m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        {
+            if (IsPaused)
+            {
+                m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
                 m_Rigidbody.velocity = Vector3.zero;
-                m_Rigidbody.angularVelocity = Vector3.zero;            }            else            {                m_Rigidbody.constraints = m_DefaultConstraints;            }
-        }    }    private void FixedUpdate()    {        if(!IsPaused && IsOwner)        {            float distance = Vector3.Distance(transform.position, targetLocation);            if (distance > 0.5f)            {                var stepPosition = Vector3.MoveTowards(transform.position, targetLocation, speed * Time.fixedDeltaTime);                transform.rotation = Quaternion.LookRotation(stepPosition.normalized);                m_Rigidbody.MovePosition(stepPosition);            }            else            {                targetLocation = GetRandomLocation();            }        }    }    private Vector3 GetRandomLocation()    {        return new Vector3(Random.Range(-15.0f, 15.0f), transform.position.y, Random.Range(-15.0f, 15.0f));    }}
+                m_Rigidbody.angularVelocity = Vector3.zero;
+            }
+            else
+            {
+                m_Rigidbody.constraints = m_DefaultConstraints;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!IsPaused && IsOwner)
+        {
+            float distance = Vector3.Distance(transform.position, targetLocation);
+            if (distance > 0.5f)
+            {
+                var stepPosition = Vector3.MoveTowards(transform.position, targetLocation, speed * Time.fixedDeltaTime);
+                transform.rotation = Quaternion.LookRotation(stepPosition.normalized);
+                m_Rigidbody.MovePosition(stepPosition);
+            }
+            else
+            {
+                targetLocation = GetRandomLocation();
+            }
+
+            if(PlayerCamera != null && !PlayerCamera.enabled)
+            {
+                PlayerCamera.enabled = true;
+            }
+        }
+    }
+
+    private Vector3 GetRandomLocation()
+    {
+        return new Vector3(Random.Range(-15.0f, 15.0f), transform.position.y, Random.Range(-15.0f, 15.0f));
+    }
+}
