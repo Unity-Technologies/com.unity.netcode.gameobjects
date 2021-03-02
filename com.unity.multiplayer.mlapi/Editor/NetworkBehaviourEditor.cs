ce7acf5a46f9369aeaafd0c41071c4ad8d5f38ab
M. Fatih MAR <mfatihmar@gmail.com>
2021-03-02 19:48:02 +0000

    refactor Editor stuff

--- com.unity.multiplayer.mlapi/Editor/NetworkBehaviourEditor.cs
+++ com.unity.multiplayer.mlapi/Editor/NetworkBehaviourEditor.cs
@@ -11,114 +11,102 @@ namespace UnityEditor
     [CanEditMultipleObjects]
     public class NetworkBehaviourEditor : Editor
     {
-        private bool initialized;
-        private readonly List<string> networkVariableNames = new List<string>();
-        private readonly Dictionary<string, FieldInfo> networkVariableFields = new Dictionary<string, FieldInfo>();
-        private readonly Dictionary<string, object> networkVariableObjects = new Dictionary<string, object>();
+        private bool m_Initialized;
+        private readonly List<string> m_NetworkVariableNames = new List<string>();
+        private readonly Dictionary<string, FieldInfo> m_NetworkVariableFields = new Dictionary<string, FieldInfo>();
+        private readonly Dictionary<string, object> m_NetworkVariableObjects = new Dictionary<string, object>();
 
-        private GUIContent networkVariableLabelGuiContent;
+        private GUIContent m_NetworkVariableLabelGuiContent;
 
         private void Init(MonoScript script)
         {
-            initialized = true;
-            
-            networkVariableNames.Clear();
-            networkVariableFields.Clear();
-            networkVariableObjects.Clear();
+            m_Initialized = true;
 
-            networkVariableLabelGuiContent = new GUIContent("NetworkVariable", "This variable is a NetworkVariable. It can not be serialized and can only be changed during runtime.");
+            m_NetworkVariableNames.Clear();
+            m_NetworkVariableFields.Clear();
+            m_NetworkVariableObjects.Clear();
 
-            FieldInfo[] fields = script.GetClass().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+            m_NetworkVariableLabelGuiContent = new GUIContent("NetworkVariable", "This variable is a NetworkVariable. It can not be serialized and can only be changed during runtime.");
+
+            var fields = script.GetClass().GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
             for (int i = 0; i < fields.Length; i++)
             {
-                Type ft = fields[i].FieldType;
+                var ft = fields[i].FieldType;
                 if (ft.IsGenericType && ft.GetGenericTypeDefinition() == typeof(NetworkVariable<>) && !fields[i].IsDefined(typeof(HideInInspector), true))
                 {
-                    networkVariableNames.Add(fields[i].Name);
-                    networkVariableFields.Add(fields[i].Name, fields[i]);
+                    m_NetworkVariableNames.Add(fields[i].Name);
+                    m_NetworkVariableFields.Add(fields[i].Name, fields[i]);
                 }
             }
         }
 
-        void RenderNetworkVariable(int index)
+        private void RenderNetworkVariable(int index)
         {
-            if (!networkVariableFields.ContainsKey(networkVariableNames[index]))
+            if (!m_NetworkVariableFields.ContainsKey(m_NetworkVariableNames[index]))
             {
                 serializedObject.Update();
-                SerializedProperty scriptProperty = serializedObject.FindProperty("m_Script");
-                if (scriptProperty == null)
-                    return;
+                var scriptProperty = serializedObject.FindProperty("m_Script");
+                if (scriptProperty == null) return;
 
-                MonoScript targetScript = scriptProperty.objectReferenceValue as MonoScript;
+                var targetScript = scriptProperty.objectReferenceValue as MonoScript;
                 Init(targetScript);
             }
 
-            object value = networkVariableFields[networkVariableNames[index]].GetValue(target);
+            object value = m_NetworkVariableFields[m_NetworkVariableNames[index]].GetValue(target);
             if (value == null)
             {
-                Type fieldType = networkVariableFields[networkVariableNames[index]].FieldType;
-                INetworkVariable var = (INetworkVariable) Activator.CreateInstance(fieldType, true);
-                networkVariableFields[networkVariableNames[index]].SetValue(target, var);
+                var fieldType = m_NetworkVariableFields[m_NetworkVariableNames[index]].FieldType;
+                var networkVariable = (INetworkVariable)Activator.CreateInstance(fieldType, true);
+                m_NetworkVariableFields[m_NetworkVariableNames[index]].SetValue(target, networkVariable);
             }
-            
-            Type type = networkVariableFields[networkVariableNames[index]].GetValue(target).GetType();
-            Type genericType = type.GetGenericArguments()[0];
+
+            var type = m_NetworkVariableFields[m_NetworkVariableNames[index]].GetValue(target).GetType();
+            var genericType = type.GetGenericArguments()[0];
 
             EditorGUILayout.BeginHorizontal();
             if (genericType == typeof(string))
             {
-                NetworkVariable<string> var = (NetworkVariable<string>)networkVariableFields[networkVariableNames[index]].GetValue(target);
-                var.Value = EditorGUILayout.TextField(networkVariableNames[index], var.Value);
+                var networkVariable = (NetworkVariable<string>)m_NetworkVariableFields[m_NetworkVariableNames[index]].GetValue(target);
+                networkVariable.Value = EditorGUILayout.TextField(m_NetworkVariableNames[index], networkVariable.Value);
             }
             else if (genericType.IsValueType)
             {
-                MethodInfo method = typeof(NetworkBehaviourEditor).GetMethod("RenderNetworkVariableValueType", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.NonPublic);
-                MethodInfo genericMethod = method.MakeGenericMethod(genericType);
+                var method = typeof(NetworkBehaviourEditor).GetMethod("RenderNetworkVariableValueType", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.NonPublic);
+                var genericMethod = method.MakeGenericMethod(genericType);
                 genericMethod.Invoke(this, new[] { (object)index });
             }
             else
             {
                 EditorGUILayout.LabelField("Type not renderable");
             }
-            GUILayout.Label(networkVariableLabelGuiContent, EditorStyles.miniLabel, GUILayout.Width(EditorStyles.miniLabel.CalcSize(networkVariableLabelGuiContent).x));
+
+            GUILayout.Label(m_NetworkVariableLabelGuiContent, EditorStyles.miniLabel, GUILayout.Width(EditorStyles.miniLabel.CalcSize(m_NetworkVariableLabelGuiContent).x));
             EditorGUILayout.EndHorizontal();
         }
 
-        void RenderNetworkVariableValueType<T>(int index) where T : struct
+        private void RenderNetworkVariableValueType<T>(int index) where T : struct
         {
-            NetworkVariable<T> var = (NetworkVariable<T>)networkVariableFields[networkVariableNames[index]].GetValue(target);
-            Type type = typeof(T);
-            object val = var.Value;
-            string name = networkVariableNames[index];
+            var networkVariable = (NetworkVariable<T>)m_NetworkVariableFields[m_NetworkVariableNames[index]].GetValue(target);
+            var type = typeof(T);
+            object val = networkVariable.Value;
+            string name = m_NetworkVariableNames[index];
 
             if (NetworkManager.Singleton != null && NetworkManager.Singleton.IsListening)
             {
-                if (type == typeof(int))
-                    val = EditorGUILayout.IntField(name, (int)val);
-                else if (type == typeof(uint))
-                    val = (uint)EditorGUILayout.LongField(name, (long)((uint)val));
-                else if (type == typeof(short))
-                    val = (short)EditorGUILayout.IntField(name, (int)((short)val));
-                else if (type == typeof(ushort))
-                    val = (ushort)EditorGUILayout.IntField(name, (int)((ushort)val));
-                else if (type == typeof(sbyte))
-                    val = (sbyte)EditorGUILayout.IntField(name, (int)((sbyte)val));
-                else if (type == typeof(byte))
-                    val = (byte)EditorGUILayout.IntField(name, (int)((byte)val));
-                else if (type == typeof(long))
-                    val = EditorGUILayout.LongField(name, (long)val);
-                else if (type == typeof(ulong))
-                    val = (ulong)EditorGUILayout.LongField(name, (long)((ulong)val));
-                else if (type == typeof(bool))
-                    val = EditorGUILayout.Toggle(name, (bool)val);
-                else if (type == typeof(string))
-                    val = EditorGUILayout.TextField(name, (string)val);
-                else if (type.IsEnum)
-                    val = EditorGUILayout.EnumPopup(name, (Enum) val);
-                else
-                    EditorGUILayout.LabelField("Type not renderable");
+                if (type == typeof(int)) val = EditorGUILayout.IntField(name, (int)val);
+                else if (type == typeof(uint)) val = (uint)EditorGUILayout.LongField(name, (long)((uint)val));
+                else if (type == typeof(short)) val = (short)EditorGUILayout.IntField(name, (int)((short)val));
+                else if (type == typeof(ushort)) val = (ushort)EditorGUILayout.IntField(name, (int)((ushort)val));
+                else if (type == typeof(sbyte)) val = (sbyte)EditorGUILayout.IntField(name, (int)((sbyte)val));
+                else if (type == typeof(byte)) val = (byte)EditorGUILayout.IntField(name, (int)((byte)val));
+                else if (type == typeof(long)) val = EditorGUILayout.LongField(name, (long)val);
+                else if (type == typeof(ulong)) val = (ulong)EditorGUILayout.LongField(name, (long)((ulong)val));
+                else if (type == typeof(bool)) val = EditorGUILayout.Toggle(name, (bool)val);
+                else if (type == typeof(string)) val = EditorGUILayout.TextField(name, (string)val);
+                else if (type.IsEnum) val = EditorGUILayout.EnumPopup(name, (Enum)val);
+                else EditorGUILayout.LabelField("Type not renderable");
 
-                var.Value = (T)val;
+                networkVariable.Value = (T)val;
             }
             else
             {
@@ -129,36 +117,35 @@ namespace UnityEditor
 
         public override void OnInspectorGUI()
         {
-            if (!initialized)
+            if (!m_Initialized)
             {
                 serializedObject.Update();
-                SerializedProperty scriptProperty = serializedObject.FindProperty("m_Script");
-                if (scriptProperty == null)
-                    return;
+                var scriptProperty = serializedObject.FindProperty("m_Script");
+                if (scriptProperty == null) return;
 
-                MonoScript targetScript = scriptProperty.objectReferenceValue as MonoScript;
+                var targetScript = scriptProperty.objectReferenceValue as MonoScript;
                 Init(targetScript);
             }
 
             EditorGUI.BeginChangeCheck();
             serializedObject.Update();
 
-            for (int i = 0; i < networkVariableNames.Count; i++)
+            for (int i = 0; i < m_NetworkVariableNames.Count; i++)
+            {
                 RenderNetworkVariable(i);
+            }
 
-            SerializedProperty property = serializedObject.GetIterator();
+            var property = serializedObject.GetIterator();
             bool expanded = true;
             while (property.NextVisible(expanded))
             {
                 if (property.propertyType == SerializedPropertyType.ObjectReference)
                 {
-                    if (property.name == "m_Script")
-                        EditorGUI.BeginDisabledGroup(true);
+                    if (property.name == "m_Script") EditorGUI.BeginDisabledGroup(true);
 
                     EditorGUILayout.PropertyField(property, true);
-                                   
-                    if (property.name == "m_Script")
-                        EditorGUI.EndDisabledGroup();
+
+                    if (property.name == "m_Script") EditorGUI.EndDisabledGroup();
                 }
                 else
                 {
@@ -166,8 +153,10 @@ namespace UnityEditor
                     EditorGUILayout.PropertyField(property, true);
                     EditorGUILayout.EndHorizontal();
                 }
+
                 expanded = false;
             }
+
             serializedObject.ApplyModifiedProperties();
             EditorGUI.EndChangeCheck();
         }
--- com.unity.multiplayer.mlapi/Editor/TrackedObjectEditor.cs
+++ com.unity.multiplayer.mlapi/Editor/TrackedObjectEditor.cs
@@ -7,28 +7,29 @@ namespace UnityEditor
     [CanEditMultipleObjects]
     public class TrackedObjectEditor : Editor
     {
-        private TrackedObject trackedObject;
-        private bool initialized;
+        private TrackedObject m_TrackedObject;
+        private bool m_Initialized;
 
         private void Init()
         {
-            if (initialized)
-                return;
+            if (m_Initialized) return;
 
-            trackedObject = (TrackedObject)target;
-            initialized = true;
+            m_TrackedObject = (TrackedObject)target;
+            m_Initialized = true;
         }
 
         public override void OnInspectorGUI()
         {
             Init();
+
             base.OnInspectorGUI();
-            if(NetworkManager.Singleton != null && NetworkManager.Singleton.IsServer)
+            if (!ReferenceEquals(NetworkManager.Singleton, null) && NetworkManager.Singleton.IsServer)
             {
-                EditorGUILayout.LabelField("Total points: ", trackedObject.TotalPoints.ToString(), EditorStyles.label);
-                EditorGUILayout.LabelField("Avg time between points: ", trackedObject.AvgTimeBetweenPointsMs.ToString() + " ms", EditorStyles.label);
-                EditorGUILayout.LabelField("Total history: ", trackedObject.TotalTimeHistory.ToString() + " seconds", EditorStyles.label);
+                EditorGUILayout.LabelField("Total points: ", m_TrackedObject.TotalPoints.ToString(), EditorStyles.label);
+                EditorGUILayout.LabelField("Avg time between points: ", m_TrackedObject.AvgTimeBetweenPointsMs + " ms", EditorStyles.label);
+                EditorGUILayout.LabelField("Total history: ", m_TrackedObject.TotalTimeHistory + " seconds", EditorStyles.label);
             }
+
             Repaint();
         }
     }
