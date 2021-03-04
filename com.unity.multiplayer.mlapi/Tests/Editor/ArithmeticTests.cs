b5049933c22f5fb505bf6ea1fcc478d368e388b6
Albin Corén <2108U9@gmail.com>
2021-03-04 20:23:07 +0100

    refactor: test cleanup

--- com.unity.multiplayer.mlapi/Tests/Editor/ArithmeticTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Editor/ArithmeticTests.cs
@@ -2,30 +2,33 @@ using System;
 using MLAPI.Serialization;
 using NUnit.Framework;
 
-public class ArithmeticTests
+namespace MLAPI.EditorTests
 {
-    [Test]
-    public void TestCeil()
+    public class ArithmeticTests
     {
-        Assert.That(Arithmetic.CeilingExact(10, 5), Is.EqualTo(2));
-        Assert.That(Arithmetic.CeilingExact(11, 5), Is.EqualTo(3));
-        Assert.That(Arithmetic.CeilingExact(0, 5), Is.EqualTo(0));
-        Assert.That(Arithmetic.CeilingExact(1, 5), Is.EqualTo(1));
-        Assert.That(Arithmetic.CeilingExact(2, 5), Is.EqualTo(1));
-        Assert.That(Arithmetic.CeilingExact(3, 5), Is.EqualTo(1));
-        Assert.That(Arithmetic.CeilingExact(4, 5), Is.EqualTo(1));
-        Assert.That(Arithmetic.CeilingExact(5, 5), Is.EqualTo(1));
-        Assert.That(Arithmetic.CeilingExact(6, 5), Is.EqualTo(2));
+        [Test]
+        public void TestCeil()
+        {
+            Assert.That(Arithmetic.CeilingExact(10, 5), Is.EqualTo(2));
+            Assert.That(Arithmetic.CeilingExact(11, 5), Is.EqualTo(3));
+            Assert.That(Arithmetic.CeilingExact(0, 5), Is.EqualTo(0));
+            Assert.That(Arithmetic.CeilingExact(1, 5), Is.EqualTo(1));
+            Assert.That(Arithmetic.CeilingExact(2, 5), Is.EqualTo(1));
+            Assert.That(Arithmetic.CeilingExact(3, 5), Is.EqualTo(1));
+            Assert.That(Arithmetic.CeilingExact(4, 5), Is.EqualTo(1));
+            Assert.That(Arithmetic.CeilingExact(5, 5), Is.EqualTo(1));
+            Assert.That(Arithmetic.CeilingExact(6, 5), Is.EqualTo(2));
 
-    }
+        }
 
-    [Test]
-    public void TestZigZag()
-    {
-        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(1234)), Is.EqualTo(1234));
-        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(-1)), Is.EqualTo(-1));
-        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(0)), Is.EqualTo(0));
-        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MaxValue)), Is.EqualTo(Int64.MaxValue));
-        Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MinValue)), Is.EqualTo(Int64.MinValue));
+        [Test]
+        public void TestZigZag()
+        {
+            Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(1234)), Is.EqualTo(1234));
+            Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(-1)), Is.EqualTo(-1));
+            Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(0)), Is.EqualTo(0));
+            Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MaxValue)), Is.EqualTo(Int64.MaxValue));
+            Assert.That(Arithmetic.ZigZagDecode(Arithmetic.ZigZagEncode(Int64.MinValue)), Is.EqualTo(Int64.MinValue));
+        }
     }
 }
