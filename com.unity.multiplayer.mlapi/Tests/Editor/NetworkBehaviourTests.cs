d494d2258ab52b6cd0fad016964e6e3efbe85f73
Albin Corén <twoten@twoten.io>
2021-03-12 15:25:04 +0100

    Added tests to cover NetworkBehaviours NetworkObject property

--- com.unity.multiplayer.mlapi/Tests/Editor/NetworkBehaviourTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Editor/NetworkBehaviourTests.cs
@@ -1,5 +1,7 @@
+using System;
 using NUnit.Framework;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace MLAPI.EditorTests
 {
@@ -20,6 +22,33 @@ namespace MLAPI.EditorTests
             Object.DestroyImmediate(networkObject);
             
             Assert.That(networkBehaviour.HasNetworkObject, Is.False);
+        }
+        
+        [Test]
+        public void AccessNetworkObjectTest()
+        {
+            var gameObject = new GameObject(nameof(NetworkObject));
+            var networkBehaviour = gameObject.AddComponent<EmptyNetworkBehaviour>();
+
+            // TODO: Do we really want to throw here?
+            // Future API change: return null
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var x = networkBehaviour.NetworkObject;
+            });
+
+            var networkObject = gameObject.AddComponent<NetworkObject>();
+            
+            Assert.That(networkBehaviour.NetworkObject, Is.EqualTo(networkObject));
+            
+            Object.DestroyImmediate(networkObject);
+            
+            // TODO: Do we really want to throw here?
+            // Future API change: return null
+            Assert.Throws<NullReferenceException>(() =>
+            {
+                var x = networkBehaviour.NetworkObject;
+            });
         }
 
         public class EmptyNetworkBehaviour : NetworkBehaviour
