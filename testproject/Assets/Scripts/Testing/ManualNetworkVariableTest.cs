1817f625fd1fd82414a65890a38678f6fbb52b6f
Jeffrey Rainy <jeffreyrainy@gmail.com>
2021-03-12 14:15:44 -0500

    fix: Moving tick writing out of NetworkVariable and into NetworkBehav… (#590)
    * fix: Moving tick writing out of NetworkVariable and into NetworkBehaviour. Allows proper handling of INetworkVariable that are not plain NetworkVariable. Forces extension of the interface. issues/573, MTT-532
    
    * test[NetworkList]: Adding manual test for NetworkList
    
    * test[NetworkList]: Adding manual test for NetworkDictionary and NetworkSet

--- testproject/Assets/Scripts/Testing/ManualNetworkVariableTest.cs
+++ testproject/Assets/Scripts/Testing/ManualNetworkVariableTest.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using MLAPI.NetworkVariable;
+using MLAPI.NetworkVariable.Collections;
 
 namespace MLAPI
 {
@@ -10,7 +12,20 @@ namespace MLAPI
     [AddComponentMenu("MLAPI/ManualNetworkVariableTest")]
     public class ManualNetworkVariableTest : NetworkBehaviour
     {
-        private NetworkVariable<int> m_TestVar;
+        // testing NetworkList
+        private NetworkList<string> m_TestList = new NetworkList<string>();
+        private bool m_GotNetworkList = false;
+
+        // testing NetworkSet
+        private NetworkSet<string> m_TestSet = new NetworkSet<string>();
+        private bool m_GotNetworkSet = false;
+
+        // testing NetworkDictionary
+        private NetworkDictionary<int, string> m_TestDictionary = new NetworkDictionary<int, string>();
+        private bool m_GotNetworkDictionary = false;
+
+        // testing NetworkVariable, especially ticks
+        private NetworkVariable<int> m_TestVar = new NetworkVariable<int>();
         private int m_MinDelta = 0;
         private int m_MaxDelta = 0;
         private int m_LastRemoteTick = 0;
@@ -22,18 +37,23 @@ namespace MLAPI
 
         void Start()
         {
-            m_TestVar.OnValueChanged = ValueChanged;
+            m_TestVar.OnValueChanged += ValueChanged;
             m_TestVar.Settings.WritePermission = NetworkVariablePermission.Everyone;
+
+            m_TestList.OnListChanged += ListChanged;
+            m_TestList.Settings.WritePermission = NetworkVariablePermission.OwnerOnly;
+
+            m_TestSet.OnSetChanged += SetChanged;
+            m_TestSet.Settings.WritePermission = NetworkVariablePermission.OwnerOnly;
+
+            m_TestDictionary.OnDictionaryChanged += DictionaryChanged;
+            m_TestDictionary.Settings.WritePermission = NetworkVariablePermission.OwnerOnly;
 
             if (IsOwner)
             {
                 m_TestVar.Value = 0;
+                Debug.Log("We'll be sending " + MyMessage());
             }
-        }
-
-        void Awake()
-        {
-            Debug.Log("Awake");
         }
 
         private void FixedUpdate()
@@ -41,6 +61,41 @@ namespace MLAPI
             if (IsOwner)
             {
                 m_TestVar.Value = m_TestVar.Value + 1;
+                m_TestList.Add(MyMessage());
+                ((ICollection<string>)m_TestSet).Add(MyMessage());
+                m_TestDictionary[0] = MyMessage();
+            }
+        }
+
+        private string MyMessage()
+        {
+            return "Message from " + NetworkObjectId;
+        }
+
+        private void ListChanged(NetworkListEvent<string> listEvent)
+        {
+            if (!IsOwner && !m_GotNetworkList)
+            {
+                Debug.Log("Received: " + listEvent.Value);
+                m_GotNetworkList = true;
+            }
+        }
+
+        private void SetChanged(NetworkSetEvent<string> setEvent)
+        {
+            if (!IsOwner && !m_GotNetworkSet)
+            {
+                Debug.Log("Received: " + setEvent.Value);
+                m_GotNetworkSet = true;
+            }
+        }
+
+        private void DictionaryChanged(NetworkDictionaryEvent<int, string> dictionaryEvent)
+        {
+            if (!IsOwner && !m_GotNetworkSet)
+            {
+                Debug.Log("Received: " + dictionaryEvent.Key + ":" + dictionaryEvent.Value);
+                m_GotNetworkDictionary = true;
             }
         }
 
@@ -84,19 +139,35 @@ namespace MLAPI
             {
                 // Let's be reasonable and allow a 5 tick difference
                 // that could be due to timing difference, lag, queueing
-                if (m_Problems == "" && Math.Abs(m_MaxDelta - m_MinDelta) < 5)
+
+                if (!m_GotNetworkList)
+                {
+                    m_Problems += "Didn't receive any NetworkList updates from other machines";
+                }
+
+                if (!m_GotNetworkSet)
+                {
+                    m_Problems += "Didn't receive any NetworkSet updates from other machines";
+                }
+
+                if (!m_GotNetworkDictionary)
+                {
+                    m_Problems += "Didn't receive any NetworkDictionary updates from other machines";
+                }
+
+                if (Math.Abs(m_MaxDelta - m_MinDelta) > 5)
+                {
+                    m_Problems += "Delta range: " + m_MinDelta + " + " + m_MaxDelta + "\n";
+                }
+
+                if (m_Problems == "")
                 {
                     Debug.Log("**** TEST PASSED ****");
                 }
                 else
                 {
                     Debug.Log("**** TEST FAILED ****");
-                    Debug.Log($"Delta range: {m_MinDelta}, {m_MaxDelta}");
-
-                    if (m_Problems != "")
-                    {
-                        Debug.Log(m_Problems);
-                    }
+                    Debug.Log(m_Problems);
                 }
                 enabled = false;
             }
