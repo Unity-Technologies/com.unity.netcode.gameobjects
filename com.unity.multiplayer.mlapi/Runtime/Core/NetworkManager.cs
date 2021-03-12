03609a05f68cf602bfe1e8d887d41866c8ceb965
Matt Walsh <matt.walsh@unity3d.com>
2021-03-11 16:51:47 -0800

    log message tweak

--- com.unity.multiplayer.mlapi/Runtime/Core/NetworkManager.cs
+++ com.unity.multiplayer.mlapi/Runtime/Core/NetworkManager.cs
@@ -1502,7 +1502,7 @@ namespace MLAPI
             }
             else if (data == null)
             {
-                NetworkLog.LogWarning("No data available. Did you forget to call PerformanceDataManager.BeginNewTick() first?");
+                NetworkLog.LogWarning($"No data available. Did you forget to call {nameof(PerformanceDataManager)}.{nameof(BeginNewTick)}() first?");
             }
         }
     }
