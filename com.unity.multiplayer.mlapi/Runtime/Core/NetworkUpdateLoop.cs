28b09cba9cefb0608598d5019870ca5b08c2798b
Andrew Spiering <aspiering@gmail.com>
2021-03-11 23:58:07 -0800

    Fixing some issues that make me sad, less allocations still and fast copy!

--- com.unity.multiplayer.mlapi/Runtime/Core/NetworkUpdateLoop.cs
+++ com.unity.multiplayer.mlapi/Runtime/Core/NetworkUpdateLoop.cs
@@ -150,6 +150,96 @@ namespace MLAPI
             }
         }
 
+        internal struct NetworkInitialization
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkInitialization),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.Initialization)
+                };
+            }
+        }
+
+        internal struct NetworkEarlyUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkEarlyUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.EarlyUpdate)
+                };
+            }
+        }
+
+        internal struct NetworkFixedUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkFixedUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.FixedUpdate)
+                };
+            }
+        }
+
+        internal struct NetworkPreUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkPreUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.PreUpdate)
+                };
+            }
+        }
+
+        internal struct NetworkUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.Update)
+                };
+            }
+        }
+
+        internal struct NetworkPreLateUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkPreLateUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.PreLateUpdate)
+                };
+            }
+        }
+
+        internal struct NetworkPostLateUpdate
+        {
+            public static PlayerLoopSystem CreateLoopSystem()
+            {
+                return new PlayerLoopSystem
+                {
+                    type = typeof(NetworkPostLateUpdate),
+                    updateDelegate = () => RunNetworkUpdateStage(NetworkUpdateStage.PostLateUpdate)
+                };
+            }
+        }
+
+        internal enum PlayerloopAction
+        {
+            Before,
+            After
+        }
+
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize()
         {
@@ -157,44 +247,107 @@ namespace MLAPI
             RegisterPlayerloopSystems();
         }
 
-        private static bool CheckAddLoopDelegate(ref PlayerLoopSystem system, PlayerLoopSystem.UpdateFunction action, Type loopType, Type subLoopType = null)
+        private static bool CheckAddLoopDelegate(
+            ref PlayerLoopSystem system,
+            PlayerLoopSystem networkLoopSystem,
+            int position,
+            Type loopType,
+            Type subLoopType = null,
+            PlayerloopAction playerloopAction = PlayerloopAction.After)
         {
             if (system.type == loopType)
             {
                 if (subLoopType != null)
                 {
+                    if (system.subSystemList == null)
+                        return false;
+
                     for (int k = 0; k < system.subSystemList.Length; k++)
                     {
-                        if (CheckAddLoopDelegate(ref system.subSystemList[k], action, subLoopType))
-                            return true;
+                        PlayerLoopSystem subSystem = system.subSystemList[k];
+                        if (subSystem.type == subLoopType)
+                        {
+                            var currentPosition = k;
+                            if (playerloopAction == PlayerloopAction.After)
+                                currentPosition += 1;
+
+                            if (CheckAddLoopDelegate(ref system, networkLoopSystem, currentPosition, loopType, null, playerloopAction))
+                                return true;
+                        }
                     }
                 }
                 else
                 {
-                    system.updateDelegate += action;
-                    return true;
+                    int oldListLength = (system.subSystemList != null) ? system.subSystemList.Length : 0;
+                    var newSubsystemList = new PlayerLoopSystem[oldListLength + 1];
 
+                    // Array.Copy is a fast copy much faster than involving a List<T>
+                    // or a for loop
+                    if (position > 0)
+                    {
+                        Array.Copy(system.subSystemList, newSubsystemList, position);
+                    }
+
+                    if (position < oldListLength)
+                    {
+                        Array.Copy(system.subSystemList, position, newSubsystemList, position + 1, system.subSystemList.Length - position);
+                    }
+
+                    newSubsystemList[position] = networkLoopSystem;
+                    system.subSystemList = newSubsystemList;
+                   
+                    return true;
                 }
             }
 
             return false;
         }
 
-        private static bool CheckRemoveLoopDelegate(ref PlayerLoopSystem system, PlayerLoopSystem.UpdateFunction action, Type loopType, Type subLoopType = null)
+        private static bool CheckRemoveLoopDelegate(
+            ref PlayerLoopSystem system,
+            Type networkLoopType,
+            int position,
+            Type loopType)
         {
             if (system.type == loopType)
             {
-                if (subLoopType != null)
+                if (networkLoopType != null )
                 {
                     for (int k = 0; k < system.subSystemList.Length; k++)
                     {
-                        if (CheckRemoveLoopDelegate(ref system.subSystemList[k], action, subLoopType))
-                            return true;
+
+                        PlayerLoopSystem subSystem = system.subSystemList[k];
+                        if (subSystem.type == networkLoopType)
+                        {
+                            var currentPosition = k;
+
+                            if (CheckRemoveLoopDelegate(ref system, null, currentPosition, loopType))
+                                return true;
+                        }
                     }
                 }
                 else
                 {
-                    system.updateDelegate -= action;
+                    int oldListLength = (system.subSystemList != null) ? system.subSystemList.Length : 0;
+                    var length = 1;
+                    if (position + length > oldListLength)
+                    {
+                        length = oldListLength - position;
+                    }
+
+                    var newSubsystemList = new PlayerLoopSystem[oldListLength - length];
+                    if (position > 0)
+                    {
+                        Array.Copy(system.subSystemList, newSubsystemList, position);
+                    }
+
+                    if (position < newSubsystemList.Length)
+                    {
+                        Array.Copy(system.subSystemList, position + length, newSubsystemList, position, newSubsystemList.Length - position);
+                    }
+
+                    system.subSystemList = newSubsystemList;
+
                     return true;
                 }
             }
@@ -208,39 +361,54 @@ namespace MLAPI
 
             for (int i = 0; i < rootPlayerLoop.subSystemList.Length; i++)
             {
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.Initialization),
+                ref PlayerLoopSystem currentSystem = ref rootPlayerLoop.subSystemList[i];
+
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkInitialization.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(Initialization));
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.EarlyUpdate),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkEarlyUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(EarlyUpdate),
-                    typeof(EarlyUpdate.ScriptRunDelayedStartupFrame));
+                    typeof(EarlyUpdate.ScriptRunDelayedStartupFrame),
+                    PlayerloopAction.Before);
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.FixedUpdate),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkFixedUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(FixedUpdate),
-                    typeof(FixedUpdate.ScriptRunBehaviourFixedUpdate));
+                    typeof(FixedUpdate.ScriptRunBehaviourFixedUpdate),
+                    PlayerloopAction.Before);
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PreUpdate),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkPreUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(PreUpdate),
-                    typeof(PreUpdate.PhysicsUpdate));
+                    typeof(PreUpdate.PhysicsUpdate),
+                    PlayerloopAction.Before);
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.Update),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(Update),
-                    typeof(Update.ScriptRunBehaviourUpdate));
+                    typeof(Update.ScriptRunBehaviourUpdate),
+                    PlayerloopAction.Before);
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PreLateUpdate),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkPreLateUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(PreLateUpdate),
-                    typeof(PreLateUpdate.ScriptRunBehaviourLateUpdate));
+                    typeof(PreLateUpdate.ScriptRunBehaviourLateUpdate),
+                    PlayerloopAction.Before);
 
-                CheckAddLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PostLateUpdate),
+                CheckAddLoopDelegate(ref currentSystem,
+                    NetworkPostLateUpdate.CreateLoopSystem(),
+                    currentSystem.subSystemList.Length,
                     typeof(PostLateUpdate),
-                    typeof(PostLateUpdate.PlayerSendFrameComplete));
+                    typeof(PostLateUpdate.PlayerSendFrameComplete),
+                    PlayerloopAction.After);
             }
 
             PlayerLoop.SetPlayerLoop(rootPlayerLoop);
@@ -252,43 +420,45 @@ namespace MLAPI
 
             for (int i = 0; i < rootPlayerLoop.subSystemList.Length; i++)
             {
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.Initialization),
+                ref PlayerLoopSystem currentSystem = ref rootPlayerLoop.subSystemList[i];
+
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkInitialization),
+                    i,
                     typeof(Initialization));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.EarlyUpdate),
-                    typeof(EarlyUpdate),
-                    typeof(EarlyUpdate.ScriptRunDelayedStartupFrame));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkEarlyUpdate),
+                    i,
+                    typeof(EarlyUpdate));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.FixedUpdate),
-                    typeof(FixedUpdate),
-                    typeof(FixedUpdate.ScriptRunBehaviourFixedUpdate));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkFixedUpdate),
+                    i,
+                    typeof(FixedUpdate));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PreUpdate),
-                    typeof(PreUpdate),
-                    typeof(PreUpdate.PhysicsUpdate));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkPreUpdate),
+                    i,
+                    typeof(PreUpdate));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.Update),
-                    typeof(Update),
-                    typeof(Update.ScriptRunBehaviourUpdate));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkUpdate),
+                    i,
+                    typeof(Update));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PreLateUpdate),
-                    typeof(PreLateUpdate),
-                    typeof(PreLateUpdate.ScriptRunBehaviourLateUpdate));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkPreLateUpdate),
+                    i,
+                    typeof(PreLateUpdate));
 
-                CheckRemoveLoopDelegate(ref rootPlayerLoop.subSystemList[i],
-                    () => RunNetworkUpdateStage(NetworkUpdateStage.PostLateUpdate),
-                    typeof(PostLateUpdate),
-                    typeof(PostLateUpdate.PlayerSendFrameComplete));
+                CheckRemoveLoopDelegate(ref currentSystem,
+                    typeof(NetworkPostLateUpdate),
+                    i,
+                    typeof(PostLateUpdate));
             }
 
             PlayerLoop.SetPlayerLoop(rootPlayerLoop);
         }
-
     }
 }
