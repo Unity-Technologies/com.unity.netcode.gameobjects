8a5ab8b77b24e38ea7f5bb6f267f0f1fe58cf2bc
Noel Stephens <73188597+NoelStephensUnity@users.noreply.github.com>
2021-03-16 19:42:52 -0500

    test: update rpc queue unit tests  (#626)
    * fix: remove singleton references
    
    This removes the NetworkManager.Singleton dependencies from within the RpcQueueContainer and RpcQueueProcessor classes.
    This further strengthens the RPC Queue system as a whole, will allow for multiple instances if RpcQueueContainer, removes a limitation I set in NetworkWriter.ReadAndWrite,  and provides unit testing capabilities required for new RPC Queue unit Tests.
    
    * test: rpc queue tests
    
    The new rpc queue tests without all of the extra files associated with them.
    
    * fix: Merging Artifact
    
    This is the merging artifact I had follow me through the branches.
    
    * refactor: Better StartNetworkManager parameters
    
    This fix was inspired by Beck mentioning that the StartNetworkManager didn't have a comment that it could only start in Host mode.  I added some additional parameters to allow one to start the NetworkManager in Host, Server, or Client mode.
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/NetworkManagerHelper.cs
    
    nice find!
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * style: static Standards
    
    Renaming m_CurrentNetworkManagerMode to s_CurrentNetworkManagerMode
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/NetworkManagerHelper.cs
    
    Ahhh yeah... nicely found!
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * style
    
    Fixing camelCase.
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/NetworkManagerHelper.cs
    
    Yes, that I do!
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * style: wording
    
    Dropped a t in networkManagerComponent.  Added the t back.
    
    * refactor: debug logs to debug log
    
    A slicker version of my logging done in one pass (thanks to Fatih for the suggestion)!
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/RpcQueueTests.cs
    
    Another nice suggestion!
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/RpcQueueTests.cs
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * Update com.unity.multiplayer.mlapi/Tests/Runtime/RpcQueueTests.cs
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>
    
    * Refactor: minor adjustment to logging
    
    Fixed the scenario where it would log that "None" stopped.
    
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>

--- com.unity.multiplayer.mlapi/Tests/Runtime/RpcQueueTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/RpcQueueTests.cs
@@ -1,75 +1,44 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using NUnit.Framework;
-using MLAPI.SceneManagement;
-using MLAPI.Transports.UNET;
+using MLAPI.Messaging;
 
 namespace MLAPI.RuntimeTests
 {
     /// <summary>
-    /// The RpcQueue unit tests to validate:
-    /// - Sending and Receiving pipeline to validate that both sending and receiving pipelines are functioning properly.
-    /// - Usage of the ServerRpcParams.Send.UpdateStage and ClientRpcParams.Send.UpdateStage functionality.
-    /// - Rpcs receive will be invoked at the appropriate NetworkUpdateStage.
-    /// Requires: RpcPipelineTestComponent
+    /// The RpcQueue unit tests validate:
+    /// Maximum buffer size that can be sent (currently 1MB is the default maximum RpcQueueHistoryFrame size)
+    /// That all RPCs invoke at the appropriate NetworkUpdateStage (Client and Server)
+    /// A lower level RpcQueueContainer test that validates RpcQueueFrameItems after they have been put into the queue
     /// </summary>
-    public class RpcQueueTests
+    public class RpcQueueTests : IDisposable
     {
-        private NetworkManager m_NetworkManager;
-
         /// <summary>
-        /// Tests the egress and ingress RPC queue functionality
+        /// Tests to make sure providing differen
         /// ** This does not include any of the MLAPI to Transport code **
         /// </summary>
         /// <returns>IEnumerator</returns>
         [UnityTest]
-        public IEnumerator RpcQueueUnitTest()
+        public IEnumerator UpdateStagesInvocation()
         {
-#if UNITY_2020_2_OR_NEWER // Disabling this test on 2019.4 due to ILPP issues on Yamato CI/CD runs
-            var networkManagerObject = new GameObject(nameof(NetworkManager));
-            m_NetworkManager = networkManagerObject.AddComponent<NetworkManager>();
-            var unetTransport = networkManagerObject.AddComponent<UNetTransport>();
-            m_NetworkManager.NetworkConfig = new Configuration.NetworkConfig
-            {
-                CreatePlayerPrefab = false,
-                AllowRuntimeSceneChanges = true,
-                EnableSceneManagement = false
-            };
-            unetTransport.ConnectAddress = "127.0.0.1";
-            unetTransport.ConnectPort = 7777;
-            unetTransport.ServerListenPort = 7777;
-            unetTransport.MessageBufferSize = 65535;
-            unetTransport.MaxConnections = 100;
-            unetTransport.MessageSendMode = UNetTransport.SendMode.Immediately;
-            m_NetworkManager.NetworkConfig.NetworkTransport = unetTransport;
+ // Disabling this test on 2019.4 due to ILPP issues on Yamato CI/CD runs (UNITY_2020_2_OR_NEWER)
+ // Adding the ability to test if we are running in editor local (UNIT_MANUAL_TESTING)
+#if UNITY_2020_2_OR_NEWER || UNITY_MANUAL_TESTING
+            Assert.IsTrue(NetworkManagerHelper.StartNetworkManager());
 
-            var currentActiveScene = SceneManager.GetActiveScene();
-            var instantiatedNetworkManager = false;
-            var testsAreComplete = false;
-            var testsAreValidated = false;
-            var exceededMaximumStageIterations = false;
+            Guid updateStagesTestId = NetworkManagerHelper.AddGameNetworkObject("UpdateStagesTest");
+            var rpcPipelineTestComponent = NetworkManagerHelper.AddComponentToObject<NetworkUpdateStagesComponent>(updateStagesTestId);
+            NetworkManagerHelper.SpawnNetworkObject(updateStagesTestId);
+            var testsAreComplete = rpcPipelineTestComponent.IsTestComplete();
+            var exceededMaximumStageIterations = rpcPipelineTestComponent.ExceededMaxIterations();
 
-            //Add our test scene name
-            NetworkSceneManager.AddRuntimeSceneName(currentActiveScene.name, 0);
+            //Start testing
+            rpcPipelineTestComponent.EnableTesting = true;
 
-            //Create the player object that we will spawn as a host
-            var playerObject = new GameObject("RpcTestObject");
-            playerObject.AddComponent<NetworkObject>();
-            var rpcPipelineTestComponent = playerObject.AddComponent<RpcPipelineTestComponent>();
-
-            instantiatedNetworkManager = true;
-            Debug.Log("NetworkManager Instantiated.");
-
-            //Start as host mode as loopback only works in hostmode
-            NetworkManager.Singleton.StartHost();
-            Debug.Log("Host Started.");
-
-            //Enable the simple ping test
-            rpcPipelineTestComponent.PingSelfEnabled = true;
-
-            Debug.Log("Running RPC Queue Tests...");
+            Debug.Log("Running TestNetworkUpdateStages: ");
 
             //Wait for the rpc pipeline test to complete or if we exceeded the maximum iterations bail
             while (!testsAreComplete && !exceededMaximumStageIterations)
@@ -78,33 +47,154 @@ namespace MLAPI.RuntimeTests
                 yield return new WaitForSeconds(0.02f);
 
                 testsAreComplete = rpcPipelineTestComponent.IsTestComplete();
-                exceededMaximumStageIterations = rpcPipelineTestComponent.ExceededMaxIterations();
+                Assert.IsFalse(rpcPipelineTestComponent.ExceededMaxIterations());
             }
+            var testsAreValidated = rpcPipelineTestComponent.ValidateUpdateStages();
 
-            if (!exceededMaximumStageIterations)
-            {
-                testsAreValidated = rpcPipelineTestComponent.ValidateUpdateStages();
-            }
+            //Stop testing
+            rpcPipelineTestComponent.EnableTesting = false;
 
-            //Stop pinging
-            rpcPipelineTestComponent.PingSelfEnabled = false;
-            Debug.Log("RPC Queue Testing completed.");
+            Debug.Log($"Exiting status => {nameof(testsAreComplete)}: {testsAreComplete} - {nameof(testsAreValidated)}: {testsAreValidated} -{nameof(exceededMaximumStageIterations)}: {exceededMaximumStageIterations}");
 
-            //Stop the host
-            NetworkManager.Singleton.StopHost();
+            Assert.IsTrue(testsAreComplete && testsAreValidated);
 
-            //Shutdown the NetworkManager
-            NetworkManager.Singleton.Shutdown();
-
-            Debug.Log($"Exiting status => {nameof(testsAreComplete)}: {testsAreComplete} - {nameof(testsAreValidated)}: {testsAreValidated} - {nameof(instantiatedNetworkManager)}: {instantiatedNetworkManager} - {nameof(exceededMaximumStageIterations)}: {exceededMaximumStageIterations}");
-
-            Assert.IsTrue(testsAreComplete && testsAreValidated && instantiatedNetworkManager);
-
-            GameObject.DestroyImmediate(playerObject);
-            GameObject.DestroyImmediate(networkManagerObject);
+            //Disable this so it isn't running any longer.
+            rpcPipelineTestComponent.gameObject.SetActive(false);
 #else
             yield return null;
 #endif
+        }
+
+        /// <summary>
+        /// This tests the RPC Queue outbound and inbound buffer capabilities.
+        /// It will send
+        /// </summary>
+        /// <returns>IEnumerator</returns>
+        [UnityTest]
+        public IEnumerator BufferDataValidation()
+        {
+ // Disabling this test on 2019.4 due to ILPP issues on Yamato CI/CD runs (UNITY_2020_2_OR_NEWER)
+ // Adding the ability to test if we are running in editor local (UNIT_MANUAL_TESTING)
+#if UNITY_2020_2_OR_NEWER || UNITY_MANUAL_TESTING
+            Assert.IsTrue(NetworkManagerHelper.StartNetworkManager());
+
+            Guid gameObjectId = NetworkManagerHelper.AddGameNetworkObject("GrowingBufferObject");
+
+            var growingRpcBufferSizeComponent = NetworkManagerHelper.AddComponentToObject<BufferDataValidationComponent>(gameObjectId);
+            NetworkManagerHelper.SpawnNetworkObject(gameObjectId);
+
+            //Start Testing
+            growingRpcBufferSizeComponent.EnableTesting = true;
+
+            var testsAreComplete = growingRpcBufferSizeComponent.IsTestComplete();
+
+            //Wait for the rpc pipeline test to complete or if we exceeded the maximum iterations bail
+            while (!testsAreComplete)
+            {
+                //Wait for 20ms
+                yield return new WaitForSeconds(0.02f);
+
+                testsAreComplete = growingRpcBufferSizeComponent.IsTestComplete();
+            }
+
+            //Stop Testing
+            growingRpcBufferSizeComponent.EnableTesting = false;
+
+            //Just disable this once we are done.
+            growingRpcBufferSizeComponent.gameObject.SetActive(false);
+
+            Assert.IsTrue(testsAreComplete);
+#else
+            yield return null;
+#endif
+
+        }
+
+
+        [Test]
+        public void RpcQueueContainerClass()
+        {
+            Assert.IsTrue(NetworkManagerHelper.StartNetworkManager());
+
+            //Create a testing rpcQueueContainer that doesn't get added to the network update loop so we don't try to send or process during the test
+            var rpcQueueContainer = new RpcQueueContainer(NetworkManager.Singleton, 0, true);
+
+            //Make sure we set testing mode so we don't try to invoke rpcs
+            rpcQueueContainer.SetTestingState(true);
+
+            var maxRpcEntries = 8;
+            var messageChunkSize = 2048;
+            var preCalculatedBufferValues = new List<byte>(messageChunkSize);
+
+
+            for (int i = 0; i < messageChunkSize; i++)
+            {
+                preCalculatedBufferValues.AddRange(BitConverter.GetBytes(UnityEngine.Random.Range(0, ulong.MaxValue)));
+            }
+
+            var indexOffset = 0;
+            ulong senderNetworkId = 1;
+
+            //Create ficticious list of clients to send to
+            ulong[] psuedoClients = new ulong[]{0};
+
+            var randomGeneratedDataArray = preCalculatedBufferValues.ToArray();
+            var maximumOffsetValue = preCalculatedBufferValues.Count;
+
+            //Testing outbound side of the RpcQueueContainer
+            for (int i = 0; i < maxRpcEntries; i++)
+            {
+                //Increment our offset into our randomly generated data for next entry;
+                indexOffset = (i * messageChunkSize) % maximumOffsetValue;
+
+                var writer = rpcQueueContainer.BeginAddQueueItemToFrame(RpcQueueContainer.QueueItemType.ServerRpc, Time.realtimeSinceStartup,Transports.NetworkChannel.DefaultMessage,
+                        senderNetworkId, psuedoClients, RpcQueueHistoryFrame.QueueFrameType.Outbound, NetworkUpdateStage.PostLateUpdate);
+
+
+                writer.WriteByteArray(randomGeneratedDataArray, messageChunkSize);
+
+
+                rpcQueueContainer.EndAddQueueItemToFrame(writer, RpcQueueHistoryFrame.QueueFrameType.Outbound, NetworkUpdateStage.PostLateUpdate);
+            }
+
+            //Now verify the data by obtaining the RpcQueueHistoryFrame we just wrote to
+            var currentFrame = rpcQueueContainer.GetLoopBackHistoryFrame(RpcQueueHistoryFrame.QueueFrameType.Outbound, NetworkUpdateStage.PostLateUpdate);
+
+            //Reset our index offset
+            indexOffset = 0;
+            int queueEntryItemCount = 0;
+            //Parse through the entries written to the current RpcQueueHistoryFrame
+            var currentQueueItem = currentFrame.GetFirstQueueItem();
+            while (currentQueueItem.QueueItemType != RpcQueueContainer.QueueItemType.None)
+            {
+                //Check to make sure the wrapper information is accurate for the entry
+                Assert.AreEqual(currentQueueItem.NetworkId, senderNetworkId);
+                Assert.AreEqual(currentQueueItem.QueueItemType, RpcQueueContainer.QueueItemType.ServerRpc);
+                Assert.AreEqual(currentQueueItem.UpdateStage, NetworkUpdateStage.PostLateUpdate);
+                Assert.AreEqual(currentQueueItem.NetworkChannel, Transports.NetworkChannel.DefaultMessage);
+
+                //Validate the data in the queue
+                Assert.IsTrue(NetworkManagerHelper.BuffersMatch(currentQueueItem.MessageData.Offset, messageChunkSize, currentQueueItem.MessageData.Array, randomGeneratedDataArray));
+
+                //Prepare for next queue item
+                queueEntryItemCount++;
+                currentQueueItem = currentFrame.GetNextQueueItem();
+            }
+
+            rpcQueueContainer.Dispose();
+            rpcQueueContainer = null;
+        }
+
+        public void Dispose()
+        {
+            //Stop, shutdown, and destroy
+            NetworkManagerHelper.ShutdownNetworkManager();
+        }
+
+        public RpcQueueTests()
+        {
+            //Create, instantiate, and host
+            NetworkManagerHelper.StartNetworkManager();
         }
     }
 }
