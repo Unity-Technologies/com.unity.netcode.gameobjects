152be6b9ff435cc87b9eded50347f366cc8f9739
Albin Corén <twoten@twoten.io>
2021-03-29 12:47:54 +0200

    Made object names specific to the test

--- com.unity.multiplayer.mlapi/Tests/Editor/NetworkObjectTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Editor/NetworkObjectTests.cs
@@ -9,14 +9,14 @@ namespace MLAPI.EditorTests
         [Test]
         public void GetBehaviourIndexNone()
         {
-            var gameObject = new GameObject(nameof(NetworkObject));
+            var gameObject = new GameObject(nameof(GetBehaviourIndexNone));
             var networkObject = gameObject.AddComponent<NetworkObject>();
-            
+
             // TODO: Maybe not hardcode message?
             LogAssert.Expect(LogType.Error, $"[MLAPI] Behaviour index was out of bounds. Did you mess up the order of your {nameof(NetworkBehaviour)}s?");
             LogAssert.Expect(LogType.Error, $"[MLAPI] Behaviour index was out of bounds. Did you mess up the order of your {nameof(NetworkBehaviour)}s?");
             LogAssert.Expect(LogType.Error, $"[MLAPI] Behaviour index was out of bounds. Did you mess up the order of your {nameof(NetworkBehaviour)}s?");
-            
+
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(0), Is.Null);
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(1), Is.Null);
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(2), Is.Null);
@@ -25,22 +25,22 @@ namespace MLAPI.EditorTests
         [Test]
         public void GetBehaviourIndexOne()
         {
-            var gameObject = new GameObject(nameof(NetworkObject));
+            var gameObject = new GameObject(nameof(GetBehaviourIndexOne));
             var networkObject = gameObject.AddComponent<NetworkObject>();
             var networkBehaviour = gameObject.AddComponent<EmptyNetworkBehaviour>();
-            
+
             // TODO: Maybe not hardcode message?
             LogAssert.Expect(LogType.Error, $"[MLAPI] Behaviour index was out of bounds. Did you mess up the order of your {nameof(NetworkBehaviour)}s?");
             LogAssert.Expect(LogType.Error, $"[MLAPI] Behaviour index was out of bounds. Did you mess up the order of your {nameof(NetworkBehaviour)}s?");
-            
+
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(0), Is.EqualTo(networkBehaviour));
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(1), Is.Null);
             Assert.That(networkObject.GetNetworkBehaviourAtOrderIndex(2), Is.Null);
         }
-        
+
         public class EmptyNetworkBehaviour : NetworkBehaviour
         {
-            
+
         }
     }
 }
