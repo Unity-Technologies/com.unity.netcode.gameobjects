ce477d7916f2b7c5769617242718e2a42a74dcb0
Albin Corén <2108U9@gmail.com>
2021-03-29 10:59:47 +0000

    fix: prevent multiple connection requests being processed while pending approval (#653)
    * fix: Fix issue where multiple connection requests could be processed
    
    * typo no -> not
    
    * Made connection request duplication checks part of state
    
    Co-authored-by: Matt Walsh <69258106+mattwalsh-unity@users.noreply.github.com>
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>

--- com.unity.multiplayer.mlapi/Runtime/Connection/PendingClient.cs
+++ com.unity.multiplayer.mlapi/Runtime/Connection/PendingClient.cs
@@ -8,12 +8,12 @@ namespace MLAPI.Connection
         /// <summary>
         /// The ClientId of the client
         /// </summary>
-        public ulong ClientId;
+        public ulong ClientId { get; internal set; }
 
         /// <summary>
         /// The state of the connection process for the client
         /// </summary>
-        public State ConnectionState;
+        public State ConnectionState { get; internal set; }
 
         /// <summary>
         /// The states of a connection
@@ -21,9 +21,13 @@ namespace MLAPI.Connection
         public enum State
         {
             /// <summary>
-            /// Client is in the process of doing the connection handshake
+            /// Waiting for client to send it's initial connection request
             /// </summary>
-            PendingConnection
+            PendingConnection,
+            /// <summary>
+            /// Waiting for client connection request to be approved
+            /// </summary>
+            PendingApproval
         }
     }
 }
