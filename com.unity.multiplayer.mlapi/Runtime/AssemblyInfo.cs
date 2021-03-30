86858926a8a94521d3d3e7a8b535e49136bb04fc
M. Fatih MAR <mfatihmar@gmail.com>
2021-03-30 21:38:12 +0100

    nothing

--- com.unity.multiplayer.mlapi/Runtime/AssemblyInfo.cs
+++ com.unity.multiplayer.mlapi/Runtime/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 
 #if UNITY_EDITOR
-[assembly: InternalsVisibleTo("Unity.Multiplayer.MLAPI.Editor.CodeGen")]
 [assembly: InternalsVisibleTo("Unity.Multiplayer.MLAPI.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.Multiplayer.MLAPI.RuntimeTests")]
+[assembly: InternalsVisibleTo("Unity.Multiplayer.MLAPI.Editor.CodeGen")]
 #endif
