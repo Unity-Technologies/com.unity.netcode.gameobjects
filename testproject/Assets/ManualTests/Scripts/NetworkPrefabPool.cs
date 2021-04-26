bd14bd3b85eb79b970888547260d3610bcbba3b2
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-04-26 10:58:10 -0500

    refactor
    Removing base.NetworkStart.

--- testproject/Assets/ManualTests/Scripts/NetworkPrefabPool.cs
+++ testproject/Assets/ManualTests/Scripts/NetworkPrefabPool.cs
@@ -175,7 +175,6 @@ public class NetworkPrefabPool : NetworkBehaviour
                 StartSpawningBoxes();
             }
         }
-        base.NetworkStart();
     }
 
     #region Object Pool Related Methods
