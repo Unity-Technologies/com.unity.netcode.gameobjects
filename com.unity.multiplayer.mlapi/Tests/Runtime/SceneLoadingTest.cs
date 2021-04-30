13b346de5689f7817d3a040f38d25a2930952d61
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-04-30 18:02:09 -0500

    refactor and clean up
    Cleaned this up and added additional asserts.

--- com.unity.multiplayer.mlapi/Tests/Runtime/SceneLoadingTest.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/SceneLoadingTest.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.IO;
-using System.Linq;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using NUnit.Framework;
@@ -9,47 +7,53 @@ using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 
-using UnityEditor;
-using MLAPI.SceneManagement;
-
 namespace MLAPI.RuntimeTests
 {
-    [TestFixture]
     public class SceneLoadingTest
     {
         private NetworkManager m_NetworkManager;
 
         private bool m_SceneLoaded;
-        private bool m_HadErrors;
         private string m_TargetSceneNameToLoad;
         private Scene m_LoadedScene;
 
         [UnityTest]
         public IEnumerator SceneLoading()
         {
+            // Load the first scene with the predefined NetworkManager
             var execAssembly = Assembly.GetExecutingAssembly();
             var packagePath = UnityEditor.PackageManager.PackageInfo.FindForAssembly(execAssembly).assetPath;
             var scenePath = Path.Combine(packagePath, "Tests/Runtime/TestScenes/SceneLoadingTest.unity");           
-
-            yield return new WaitForSeconds(0.1f);
             m_TargetSceneNameToLoad = scenePath;
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
             EditorSceneManager.LoadSceneAsyncInPlayMode(scenePath, new LoadSceneParameters(LoadSceneMode.Additive));
 
-            while(!m_SceneLoaded && !m_HadErrors)
+            //Wait for the scene to load
+            var timeOut = Time.realtimeSinceStartup + 2;
+            var timedOut = false;
+            while(!m_SceneLoaded)
             {
                 yield return new WaitForSeconds(0.01f);
+                if(timeOut < Time.realtimeSinceStartup)
+                {
+                    timedOut = true;
+                    break;
+                }
             }
 
+            Assert.IsFalse(timedOut);
             Assert.IsTrue(m_SceneLoaded);
-            Assert.IsTrue(!m_HadErrors);
+            Assert.NotNull(m_LoadedScene);
 
-            if (SceneManager.GetActiveScene().name != "SceneLoadingTest")
+            // Set the scene to be active if it is not the active scene
+            if (SceneManager.GetActiveScene().name != m_LoadedScene.name)
             {
                 Debug.Log($"Loaded scene not active, activating scene: {scenePath}");
                 SceneManager.SetActiveScene(m_LoadedScene);
+                Assert.IsTrue(SceneManager.GetActiveScene().name == m_LoadedScene.name);
             }
 
+            // Get the NetworkManager instantiated from the scene
             var gameObject = GameObject.Find("NetworkManager");
 
             Assert.IsNotNull(gameObject);
@@ -58,29 +62,54 @@ namespace MLAPI.RuntimeTests
 
             Assert.IsNotNull(m_NetworkManager);
 
+            // Start in host mode
             if (m_NetworkManager)
             {
-                m_NetworkManager.NetworkConfig.AllowRuntimeSceneChanges = true;
                 m_NetworkManager.StartHost();
             }
+
+            // Add any additional scenes we want to load here (just create the next index number for the index value)
             m_NetworkManager.SceneManager.AddRuntimeSceneName("SceneToLoad", (uint)m_NetworkManager.SceneManager.RegisteredSceneNames.Count);
 
-           yield return new WaitForSeconds(0.1f);
+            // Override NetworkSceneManager's scene loading 
             m_NetworkManager.SceneManager.OverrideLoadSceneAsync = TestRunerSceneLoadingOverride;
             m_NetworkManager.SceneManager.SwitchScene("SceneToLoad");
             m_SceneLoaded = false;
-            m_HadErrors = false;
 
-            while (!m_SceneLoaded && !m_HadErrors)
+            //Wait for the scene to load
+            timeOut = Time.realtimeSinceStartup + 2;
+            timedOut = false;
+            while (!m_SceneLoaded)
             {
                 yield return new WaitForSeconds(0.01f);
+                if (timeOut < Time.realtimeSinceStartup)
+                {
+                    timedOut = true;
+                    break;
+                }
             }
 
+            //Make sure we didn't time out and the scene loaded
+            Assert.IsFalse(timedOut);
             Assert.IsTrue(m_SceneLoaded);
-            Assert.IsTrue(!m_HadErrors);
+            Assert.NotNull(m_LoadedScene);
 
+            // Set the scene to be active if it is not the active scene
+            if (SceneManager.GetActiveScene().name != m_LoadedScene.name)
+            {
+                Debug.Log($"Loaded scene not active, activating scene: {scenePath}");
+                SceneManager.SetActiveScene(m_LoadedScene);
+                Assert.IsTrue(SceneManager.GetActiveScene().name == m_LoadedScene.name);
+            }
+            
         }
 
+        /// <summary>
+        /// Overrides what NetworkSceneManager uses to load scenes
+        /// </summary>
+        /// <param name="targetscene">string</param>
+        /// <param name="loadSceneMode">LoadSceneMode</param>
+        /// <returns>AsyncOperation</returns>
         public AsyncOperation TestRunerSceneLoadingOverride(string targetscene, LoadSceneMode loadSceneMode)
         {
 
@@ -95,12 +124,16 @@ namespace MLAPI.RuntimeTests
             return EditorSceneManager.LoadSceneAsyncInPlayMode(sceneToBeLoaded, new LoadSceneParameters(loadSceneMode));
         }
 
-        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        /// <summary>
+        /// When invoked, makes sure the scene loaded is the correct scene
+        /// and then set our scene loaded to true and keep a reference to the loaded scene
+        /// </summary>
+        private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode sceneMode)
         {
-            if(arg0 != null && m_TargetSceneNameToLoad.Contains(arg0.name))
+            if(scene != null && m_TargetSceneNameToLoad.Contains(scene.name))
             {
                 m_SceneLoaded = true;
-                m_LoadedScene = arg0;
+                m_LoadedScene = scene;
             }
         }
     }
