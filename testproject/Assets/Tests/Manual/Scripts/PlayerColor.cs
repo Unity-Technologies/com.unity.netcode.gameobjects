cba5025c6d8feb3d1b7711b8d494554f1b0bfbde
M. Fatih MAR <mfatihmar@gmail.com>
2021-05-10 14:48:31 +0100

    chore: update namespaces in testproject test scripts

--- testproject/Assets/Tests/Manual/Scripts/PlayerColor.cs
+++ testproject/Assets/Tests/Manual/Scripts/PlayerColor.cs
@@ -2,17 +2,20 @@ using MLAPI;
 using System;
 using UnityEngine;
 
-public class PlayerColor : MonoBehaviour
+namespace TestProject.ManualTests
 {
-    private static Color[] s_Colors = { Color.red, Color.yellow, Color.green, Color.blue, Color.cyan, Color.magenta, Color.white };
+    public class PlayerColor : MonoBehaviour
+    {
+        private static Color[] s_Colors = { Color.red, Color.yellow, Color.green, Color.blue, Color.cyan, Color.magenta, Color.white };
 
-    /// <summary>
-    /// Sets the mesh renderer's material's color based on the NetworkObject's OwnerClientId
-    /// </summary>
-    private void Start()
+        /// <summary>
+        /// Sets the mesh renderer's material's color based on the NetworkObject's OwnerClientId
+        /// </summary>
+        private void Start()
         {
             MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
             ulong myId = GetComponent<NetworkObject>().OwnerClientId;
             meshRenderer.material.color = s_Colors[myId % Convert.ToUInt64(s_Colors.Length)];
         }
+    }
 }
