d783a4e031db7741d4f8f3096e61ab06e98d6df7
Samuel Bellomo <samuel.bellomo@unity3d.com>
2021-05-20 13:15:57 -0400

    adding more tests

--- com.unity.multiplayer.mlapi/Tests/Runtime/NetworkSpawnManagerTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/NetworkSpawnManagerTests.cs
@@ -11,6 +11,7 @@ namespace MLAPI.RuntimeTests
     {
         private NetworkManager m_ServerNetworkManager;
         private NetworkManager[] m_ClientNetworkManagers;
+        private GameObject m_PlayerPrefab;
 
         [UnitySetUp]
         public IEnumerator Setup()
@@ -28,18 +29,18 @@ namespace MLAPI.RuntimeTests
             m_ClientNetworkManagers = clients;
 
             // Create playerPrefab
-            GameObject playerPrefab = new GameObject("Player");
-            NetworkObject networkObject = playerPrefab.AddComponent<NetworkObject>();
+            m_PlayerPrefab = new GameObject("Player");
+            NetworkObject networkObject = m_PlayerPrefab.AddComponent<NetworkObject>();
 
             // Make it a prefab
             MultiInstanceHelpers.MakeNetworkedObjectTestPrefab(networkObject);
 
             // Set the player prefab
-            server.NetworkConfig.PlayerPrefab = playerPrefab;
+            server.NetworkConfig.PlayerPrefab = m_PlayerPrefab;
 
             for (int i = 0; i < clients.Length; i++)
             {
-                clients[i].NetworkConfig.PlayerPrefab = playerPrefab;
+                clients[i].NetworkConfig.PlayerPrefab = m_PlayerPrefab;
             }
 
             // Start the instances
@@ -59,8 +60,8 @@ namespace MLAPI.RuntimeTests
             yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientConnectedToServer(server, nbClients:2));
         }
 
-        [Test]
-        public void TestClientCanOnlySeeItselfInClientList()
+        [UnityTest]
+        public IEnumerator TestClientCanOnlySeeItselfInClientList()
         {
             var serverSideClientId = m_ServerNetworkManager.ServerClientId;
             var clientSideClientId = m_ClientNetworkManagers[0].LocalClientId;
@@ -83,10 +84,10 @@ namespace MLAPI.RuntimeTests
             // test 3 client can't access server player
             Assert.Throws<NotServerException>(() =>
             {
-                m_ClientNetworkManagers[0].SpawnManager.GetPlayerNetworkObject(serverSideClientId); // expect exception
+                m_ClientNetworkManagers[0].SpawnManager.GetPlayerNetworkObject(serverSideClientId);
             });
 
-             // test 4 client can access own player
+            // test 4 client can access own player
             var clientSideClientPlayerObject = m_ClientNetworkManagers[0].SpawnManager.GetPlayerNetworkObject(clientSideClientId);
             Assert.NotNull(clientSideClientPlayerObject);
             Assert.AreEqual(clientSideClientId, clientSideClientPlayerObject.OwnerClientId);
@@ -94,8 +95,50 @@ namespace MLAPI.RuntimeTests
             // test 5 client can't access other player
             Assert.Throws<NotServerException>(() =>
             {
-                m_ClientNetworkManagers[0].SpawnManager.GetPlayerNetworkObject(otherClientSideClientId); // expect exception
+                m_ClientNetworkManagers[0].SpawnManager.GetPlayerNetworkObject(otherClientSideClientId);
             });
+
+            // test 6 server gets null value if invalid id
+            var nullPlayer = m_ServerNetworkManager.SpawnManager.GetPlayerNetworkObject(9999);
+            Assert.Null(nullPlayer);
+
+            // test server can use GetLocalPlayerObject
+            serverSideServerPlayerObject = m_ServerNetworkManager.SpawnManager.GetLocalPlayerObject();
+            Assert.NotNull(serverSideServerPlayerObject);
+            Assert.AreEqual(serverSideClientId, serverSideServerPlayerObject.OwnerClientId);
+
+            // test client can use GetLocalPlayerObject
+            clientSideClientPlayerObject = m_ClientNetworkManagers[0].SpawnManager.GetLocalPlayerObject();
+            Assert.NotNull(clientSideClientPlayerObject);
+            Assert.AreEqual(clientSideClientId, clientSideClientPlayerObject.OwnerClientId);
+
+            // test when client connects, player object is now available
+            // Create multiple NetworkManager instances
+            if (!MultiInstanceHelpers.Create(1, out NetworkManager server, out NetworkManager[] clients))
+            {
+                Debug.LogError("Failed to create instances");
+                Assert.Fail("Failed to create instances");
+            }
+
+            var newClientNetworkManager = clients[0];
+            newClientNetworkManager.NetworkConfig.PlayerPrefab = m_PlayerPrefab;
+            newClientNetworkManager.StartClient();
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientConnected(newClientNetworkManager));
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientConnectedToServer(m_ServerNetworkManager, nbClients:3)); // todo use wait for specific client
+            var newClientLocalClientId = newClientNetworkManager.LocalClientId;
+            var newPlayerObject = newClientNetworkManager.SpawnManager.GetLocalPlayerObject();
+            Assert.NotNull(newPlayerObject);
+            Assert.AreEqual(newClientLocalClientId, newPlayerObject.OwnerClientId);
+            var serverSideNewClientPlayer = m_ServerNetworkManager.SpawnManager.GetPlayerNetworkObject(newClientLocalClientId);
+            Assert.NotNull(serverSideNewClientPlayer);
+            Assert.AreEqual(newClientLocalClientId, serverSideNewClientPlayer.OwnerClientId);
+
+            // test when client disconnects, player object no longer available.
+            newClientNetworkManager.StopClient();
+            var nbConnectedClients = m_ServerNetworkManager.ConnectedClients.Count;
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForCondition(() => m_ServerNetworkManager.ConnectedClients.Count == nbConnectedClients - 1));
+            serverSideNewClientPlayer = m_ServerNetworkManager.SpawnManager.GetPlayerNetworkObject(newClientLocalClientId);
+            Assert.Null(serverSideNewClientPlayer);
         }
     }
 }
