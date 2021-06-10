0c6442c17321bf9dad45cbf4e1bea29cadc20ceb
josiemessa <josie@unity3d.com>
2021-06-10 14:59:08 +0100

    Move tests to their own directory, use new framework

--- com.unity.multiplayer.mlapi/Tests/Runtime/Metrics/NetworkObjects/NetworkObjectMetricsDispatchTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/Metrics/NetworkObjects/NetworkObjectMetricsDispatchTests.cs
@@ -5,50 +5,84 @@ using MLAPI.Metrics;
 using NUnit.Framework;
 using Unity.Multiplayer.NetworkProfiler;
 using Unity.Multiplayer.NetworkProfiler.Models;
+using UnityEngine;
 using UnityEngine.TestTools;
 
 namespace MLAPI.RuntimeTests.Metrics.NetworkObjects
 {
 #if true
-    public class NetworkObjectMetricsDispatchTests : MetricsTestBase
+    public class NetworkObjectMetricsDispatchTests
     {
-        NetworkManager m_NetworkManager;
-        NetworkMetrics m_NetworkMetrics;
+        NetworkManager m_Server;
+        NetworkManager m_Client;
+        NetworkMetrics m_ClientMetrics;
+        NetworkMetrics m_ServerMetrics;
 
-        [SetUp]
-        public void SetUp()
+        [UnitySetUp]
+        public IEnumerator SetUp()
         {
-            NetworkManagerHelper.StartNetworkManager(out m_NetworkManager);
-            m_NetworkMetrics = m_NetworkManager.NetworkMetrics as NetworkMetrics;
+            if (!MultiInstanceHelpers.Create(1, out m_Server, out var clients))
+            {
+                Debug.LogError("Failed to create instances");
+                Assert.Fail("Failed to create instances");
+            }
+
+            var playerPrefab = new GameObject("Player");
+            var networkObject = playerPrefab.AddComponent<NetworkObject>();
+
+            MultiInstanceHelpers.MakeNetworkedObjectTestPrefab(networkObject);
+
+            m_Server.NetworkConfig.PlayerPrefab = playerPrefab;
+
+            foreach (var client in clients)
+            {
+                client.NetworkConfig.PlayerPrefab = playerPrefab;
+            }
+
+            if (!MultiInstanceHelpers.Start(true, m_Server, clients))
+            {
+                Debug.LogError("Failed to start instances");
+                Assert.Fail("Failed to start instances");
+            }
+
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientsConnected(clients));
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientConnectedToServer(m_Server));
+
+            m_Client = clients.First();
+            m_ClientMetrics = m_Client.NetworkMetrics as NetworkMetrics;
+            m_ServerMetrics = m_Server.NetworkMetrics as NetworkMetrics;
         }
 
-        [TearDown]
-        public void TearDown()
+        [UnityTearDown]
+        public IEnumerator TearDown()
         {
-            NetworkManagerHelper.ShutdownNetworkManager();
+            MultiInstanceHelpers.Destroy();
+
+            yield return null;
         }
 
         [UnityTest]
-        public IEnumerator NetworkMetrics_WhenNetworkObjectSpawnSentToClient_TracksObjectSpawnSentMetric()
+        public IEnumerator TrackNetworkObjectSpawnSentMetric()
         {
             const string objectName = "TestNetworkObjectToSpawn";
-            const ulong clientId = 100;
-            var gameObjectId = NetworkManagerHelper.AddGameNetworkObject(objectName);
-            NetworkManagerHelper.InstantiatedNetworkObjects.TryGetValue(gameObjectId, out var networkObj);
-            NetworkManagerHelper.AddConnectedClient(clientId);
+            var playerPrefab = new GameObject(objectName);
+            var networkObject = playerPrefab.AddComponent<NetworkObject>();
+            networkObject.NetworkManagerOwner = m_Server;
 
-            m_NetworkMetrics.Dispatcher.RegisterObserver(new TestObserver(collection =>
-            {
-                var objectSpawnedMetric = AssertSingleMetricEventOfType<ObjectSpawnedEvent>(collection, MetricNames.ObjectSpawned);
-                Assert.AreEqual(1, objectSpawnedMetric.Values.Count);
-                Assert.AreEqual(clientId, objectSpawnedMetric.Values.Select(x => x.Connection.Id).First());
-                Assert.AreEqual(objectName, objectSpawnedMetric.Values.Select(x => x.NetworkId.Name));
-                m_NetworkManager.SpawnManager.DespawnObject(networkObj);
-            }));
-            networkObj.Observers.Add(clientId);
-            networkObj.Spawn();
+            MultiInstanceHelpers.MakeNetworkedObjectTestPrefab(networkObject);
+            // NetworkManagerHelper.AddConnectedClient(m_Client.LocalClientId);
 
-            yield return WaitForMetricsDispatch();
+            var waitForMetricEvent = new WaitForMetricValues<ObjectSpawnedEvent>(m_ServerMetrics.Dispatcher, MetricNames.ObjectSpawned);
+
+            // networkObj.Observers.Add(m_Client.LocalClientId);
+            networkObject.Spawn();
+
+            yield return waitForMetricEvent.WaitForAFewFrames();
+
+            var objectSpawnedSentMetricValues = waitForMetricEvent.Values;
+            Assert.AreEqual(1, objectSpawnedSentMetricValues.Count);
+            Assert.AreEqual(m_Client.LocalClientId, objectSpawnedSentMetricValues.Select(x => x.Connection.Id).First());
+            Assert.AreEqual(objectName, objectSpawnedSentMetricValues.Select(x => x.NetworkId.Name));
         }
     }
 #endif
