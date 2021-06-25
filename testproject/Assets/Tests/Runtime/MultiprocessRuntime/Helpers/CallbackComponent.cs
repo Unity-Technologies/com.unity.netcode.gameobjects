745ceaed088a5d1190ccd02f675702bc3375c4fb
Samuel Bellomo <samuel.bellomo@unity3d.com>
2021-06-24 20:31:49 -0400

    removing unused callback

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/CallbackComponent.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/CallbackComponent.cs
@@ -6,14 +6,7 @@ using UnityEngine;
 /// </summary>
 public class CallbackComponent : MonoBehaviour
 {
-    public Action OnStart;
     public Action<float> OnUpdate;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        OnStart?.Invoke();
-    }
 
     // Update is called once per frame
     private void Update()
