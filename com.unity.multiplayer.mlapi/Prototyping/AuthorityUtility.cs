8b30ce1480ae5263c3bcabf6566450a0eaaa10ef
Luke Stampfli <luke@stampfli.one>
2021-07-05 17:09:49 +0100

    docs: Add ref docs

--- com.unity.multiplayer.mlapi/Prototyping/AuthorityUtility.cs
+++ com.unity.multiplayer.mlapi/Prototyping/AuthorityUtility.cs
@@ -13,13 +13,30 @@ namespace MLAPI.Prototyping
         Server = 0, // default
         Owner,
     }
+
+    /// <summary>
+    /// Contains utility functions for implementing NetworkBehaviours which support multiple authority modes.
+    /// </summary>
     public static class AuthorityUtility
     {
+        /// <summary>
+        /// Check whether the given <see cref="NetworkBehaviour"/> has authority based on <see cref="NetworkObject"/> ownership.
+        /// </summary>
+        /// <param name="networkBehaviour">The NetworkBehaviour to check for authority.</param>
+        /// <param name="authority">The authority mode.</param>
+        /// <returns>True if the networkBehaviour has authority; Else false.</returns>
         public static bool HasAuthority(this NetworkBehaviour networkBehaviour, Authority authority)
         {
             return (networkBehaviour.IsClient && authority == Authority.Owner && networkBehaviour.IsOwner) || (networkBehaviour.IsServer && authority == Authority.Server);
         }
 
+        /// <summary>
+        /// Updates the read and write permissions of a network variable based on authority.
+        /// </summary>
+        /// <param name="variableToUpdate">The NetworkVariable for which to change the permissions.</param>
+        /// <param name="authority">The new authority mode.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <exception cref="NotImplementedException"></exception>
         public static void UpdateStateVariablePermission<T>(this NetworkVariable<T> variableToUpdate, Authority authority)
         {
             switch (authority)
