9f2c6f41ea767953ff5498b592c595c53ead0d58
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-07-07 15:08:32 -0500

    refactor
    Swapped out the switch scene cycle hack for actual toggle buttons that allow one to load each sample additive scene or unload it based on the state of the toggle button.

---
+++ testproject/Assets/Samples/SceneTransitioningAdditive/AdditiveSceneToggleHandler.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using MLAPI;
+using MLAPI.SceneManagement;
+
+namespace TestProject.ManualTests
+{
+    public class AdditiveSceneToggleHandler : NetworkBehaviour
+    {
+        public static bool ExitingNow { get; internal set; }
+
+        private Toggle m_ToggleObject;
+
+        [HideInInspector]
+        [SerializeField]
+        private string m_SceneToLoad;
+
+#if UNITY_EDITOR
+        [SerializeField]
+        private SceneAsset m_SceneAsset;
+        private void OnValidate()
+        {
+            if (m_SceneAsset != null && m_SceneAsset.name != m_SceneToLoad)
+            {
+                m_SceneToLoad = m_SceneAsset.name;
+            }
+        }
+#endif
+        private int m_CurrentSceneIndex;
+
+        private void Awake()
+        {
+            ExitingNow = false;
+        }
+
+        private void Start()
+        {
+            m_ToggleObject = gameObject.GetComponentInChildren<Toggle>();
+            StartCoroutine(CheckForVisibility());
+        }
+
+        private bool m_ExitingScene;
+        private void OnDestroy()
+        {
+            m_ExitingScene = true;
+            StopAllCoroutines();
+        }
+
+        private IEnumerator CheckForVisibility()
+        {
+            while (!m_ExitingScene)
+            {
+                if (NetworkManager.Singleton && NetworkManager.Singleton.IsListening && NetworkManager.Singleton.IsServer)
+                {
+                    if (m_ToggleObject)
+                    {
+                        m_ToggleObject.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (m_ToggleObject)
+                    {
+                        m_ToggleObject.gameObject.SetActive(false);
+                    }
+                }
+
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            yield return null;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            if (NetworkManager.Singleton && NetworkManager.Singleton.IsListening && NetworkManager.Singleton.IsServer)
+            {
+                if (m_ToggleObject)
+                {
+                    m_ToggleObject.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                if (m_ToggleObject)
+                {
+                    m_ToggleObject.gameObject.SetActive(false);
+                }
+            }
+            base.OnNetworkSpawn();
+        }
+
+        private SceneSwitchProgress m_CurrentSceneSwitchProgress;
+
+
+        public void OnToggle()
+        {
+            if (NetworkManager.Singleton && NetworkManager.Singleton.IsListening)
+            {
+                if (m_ToggleObject)
+                {
+                    if(m_ToggleObject.isOn)
+                    {
+                        m_CurrentSceneSwitchProgress = NetworkManager.Singleton.SceneManager.LoadScene(m_SceneToLoad);
+                    }
+                    else
+                    {
+                        m_CurrentSceneSwitchProgress = NetworkManager.Singleton.SceneManager.UnloadScene(m_SceneToLoad);
+                    }
+                }
+            }
+        }
+
+        public delegate void OnSceneSwitchCompletedDelegateHandler();
+
+        public event OnSceneSwitchCompletedDelegateHandler OnSceneSwitchCompleted;
+
+        private void CurrentSceneSwitchProgress_OnComplete(bool timedOut)
+        {
+            OnSceneSwitchCompleted?.Invoke();
+        }
+    }
+}
