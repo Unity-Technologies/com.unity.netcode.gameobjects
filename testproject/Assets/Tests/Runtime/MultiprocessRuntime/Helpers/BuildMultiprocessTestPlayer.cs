6c8efd66a78ea0977874282393c8a81ceb915b0e
Sam Bellomo <71790295+SamuelBellomo@users.noreply.github.com>
2021-07-12 19:57:26 -0400

    test: Perf tests part 4. Adding example of performance test with spawning x network objects at once (#925)
    Adding a first example of performance tests.
    This was used a test bench for multiprocess testing tools in Part 1-3.
    These tests will be skipped for normal CI as they need additional setup steps for device orchestration.
    They will spawn x number of objects server side, wait to see them client side and report on the number of failures, number of spawned objects per frame, etc.
    These tests create GC allocs before and after the tests. During the tests, the only allocs I could find were MLAPI related, which is kind of the point for these tests :)

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/BuildMultiprocessTestPlayer.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/BuildMultiprocessTestPlayer.cs
@@ -6,91 +6,118 @@ using UnityEditor.Build.Reporting;
 #endif
 using UnityEngine;
 
-/// <summary>
-/// This is needed as Unity throws "An abnormal situation has occurred: the PlayerLoop internal function has been called recursively. Please contact Customer Support with a sample project so that we can reproduce the problem and troubleshoot it."
-/// when trying to build from Setup() steps in tests.
-/// </summary>
-public static class BuildMultiprocessTestPlayer
+namespace MLAPI.MultiprocessRuntimeTests
 {
-    public const string MultiprocessBaseMenuName = "MLAPI/Multiprocess Test";
-    public const string BuildAndExecuteMenuName = MultiprocessBaseMenuName + "/Build Test Player #t";
-    public const string MainSceneName = "MultiprocessTestScene";
-    private static string BuildPathDirectory => Path.Combine(Path.GetDirectoryName(Application.dataPath), "Builds", "MultiprocessTests");
-    public static string BuildPath => Path.Combine(BuildPathDirectory, "MultiprocessTestPlayer");
+    /// <summary>
+    /// This is needed as Unity throws "An abnormal situation has occurred: the PlayerLoop internal function has been called recursively. Please contact Customer Support with a sample project so that we can reproduce the problem and troubleshoot it."
+    /// when trying to build from Setup() steps in tests.
+    /// </summary>
+    public static class BuildMultiprocessTestPlayer
+    {
+        public const string MultiprocessBaseMenuName = "MLAPI/Multiprocess Test";
+        public const string BuildAndExecuteMenuName = MultiprocessBaseMenuName + "/Build Test Player #t";
+        public const string MainSceneName = "MultiprocessTestScene";
+
+        public const string BuildInfoFileName = "buildInfo.json";
+
+        private static string BuildPathDirectory => Path.Combine(Path.GetDirectoryName(Application.dataPath), "Builds", "MultiprocessTests");
+        public static string BuildPath => Path.Combine(BuildPathDirectory, "MultiprocessTestPlayer");
 
 #if UNITY_EDITOR
-    [MenuItem(BuildAndExecuteMenuName)]
-    public static void BuildRelease()
-    {
-        var report = BuildPlayer();
-        if (report.summary.result != BuildResult.Succeeded)
+        [MenuItem(BuildAndExecuteMenuName)]
+        public static void BuildRelease()
         {
-            throw new Exception($"Build failed! {report.summary.totalErrors} errors");
-        }
-    }
-
-    [MenuItem(MultiprocessBaseMenuName + "/Build Test Player (Debug)")]
-    public static void BuildDebug()
-    {
-        var report = BuildPlayer(true);
-        if (report.summary.result != BuildResult.Succeeded)
-        {
-            throw new Exception($"Build failed! {report.summary.totalErrors} errors");
-        }
-    }
-
-    [MenuItem(MultiprocessBaseMenuName + "/Delete Test Build")]
-    public static void DeleteBuild()
-    {
-        if (Directory.Exists(BuildPathDirectory))
-        {
-            Directory.Delete(BuildPathDirectory, recursive: true);
-        }
-        else
-        {
-            Debug.Log($"[{nameof(BuildMultiprocessTestPlayer)}] build directory does not exist ({BuildPathDirectory}) not deleting anything");
-        }
-    }
-
-    /// <summary>
-    /// Needs a separate build than the standalone test builds since we don't want the player to try to connect to the editor to do test
-    /// reporting. We only want to main node to do that, worker nodes should be dumb
-    /// </summary>
-    /// <returns></returns>
-    private static BuildReport BuildPlayer(bool isDebug = false)
-    {
-        // Save standalone build path to file so we can read it from standalone tests (that are not running from editor)
-        File.WriteAllText(Path.Combine(Application.streamingAssetsPath, MultiprocessOrchestration.BuildInfoFileName), BuildPath);
-
-        // deleting so we don't end up testing on outdated builds if there's a build failure
-        DeleteBuild();
-
-        var buildOptions = BuildOptions.None;
-        buildOptions |= BuildOptions.IncludeTestAssemblies;
-        buildOptions |= BuildOptions.StrictMode;
-        if (isDebug)
-        {
-            buildOptions |= BuildOptions.Development;
-            buildOptions |= BuildOptions.AllowDebugging; // enable this if you want to debug your players. Your players
-            // will have more connection permission popups when launching though
+            var report = BuildPlayer();
+            if (report.summary.result != BuildResult.Succeeded)
+            {
+                throw new Exception($"Build failed! {report.summary.totalErrors} errors");
+            }
         }
 
-        var buildPathToUse = BuildPath;
-        if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+        [MenuItem(MultiprocessBaseMenuName + "/Build Test Player (Debug)")]
+        public static void BuildDebug()
         {
-            buildPathToUse += ".exe";
+            var report = BuildPlayer(true);
+            if (report.summary.result != BuildResult.Succeeded)
+            {
+                throw new Exception($"Build failed! {report.summary.totalErrors} errors");
+            }
         }
-        Debug.Log($"Starting multiprocess player build using path {buildPathToUse}");
 
-        buildOptions &= ~BuildOptions.AutoRunPlayer;
-        var buildReport = BuildPipeline.BuildPlayer(
-            new[] { $"Assets/Scenes/{MainSceneName}.unity" },
-            buildPathToUse,
-            EditorUserBuildSettings.activeBuildTarget,
-            buildOptions);
+        [MenuItem(MultiprocessBaseMenuName + "/Delete Test Build")]
+        public static void DeleteBuild()
+        {
+            if (Directory.Exists(BuildPathDirectory))
+            {
+                Directory.Delete(BuildPathDirectory, recursive: true);
+            }
+            else
+            {
+                Debug.Log($"[{nameof(BuildMultiprocessTestPlayer)}] build directory does not exist ({BuildPathDirectory}) not deleting anything");
+            }
+        }
 
-        Debug.Log("Build finished");
-        return buildReport;
-    }
+        /// <summary>
+        /// Needs a separate build than the standalone test builds since we don't want the player to try to connect to the editor to do test
+        /// reporting. We only want to main node to do that, worker nodes should be dumb
+        /// </summary>
+        /// <returns></returns>
+        private static BuildReport BuildPlayer(bool isDebug = false)
+        {
+            // Save standalone build path to file so we can read it from standalone tests (that are not running from editor)
+            SaveBuildInfo(new BuildInfo() { BuildPath = BuildPath, IsDebug = isDebug });
+
+            // deleting so we don't end up testing on outdated builds if there's a build failure
+            DeleteBuild();
+
+            var buildOptions = BuildOptions.None;
+            buildOptions |= BuildOptions.IncludeTestAssemblies;
+            buildOptions |= BuildOptions.StrictMode;
+            if (isDebug)
+            {
+                buildOptions |= BuildOptions.Development;
+                buildOptions |= BuildOptions.AllowDebugging; // enable this if you want to debug your players. Your players
+
+                // will have more connection permission popups when launching though
+            }
+
+            var buildPathToUse = BuildPath;
+            if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                buildPathToUse += ".exe";
+            }
+
+            Debug.Log($"Starting multiprocess player build using path {buildPathToUse}");
+
+            buildOptions &= ~BuildOptions.AutoRunPlayer;
+            var buildReport = BuildPipeline.BuildPlayer(
+                new[] { $"Assets/Scenes/{MainSceneName}.unity" },
+                buildPathToUse,
+                EditorUserBuildSettings.activeBuildTarget,
+                buildOptions);
+
+            Debug.Log("Build finished");
+            return buildReport;
+        }
 #endif
+
+        [Serializable]
+        public struct BuildInfo
+        {
+            public string BuildPath;
+            public bool IsDebug;
+        }
+
+        public static BuildInfo ReadBuildInfo()
+        {
+            var jsonString = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, BuildInfoFileName));
+            return JsonUtility.FromJson<BuildInfo>(jsonString);
+        }
+
+        public static void SaveBuildInfo(BuildInfo toSave)
+        {
+            var buildInfoJson = JsonUtility.ToJson(toSave);
+            File.WriteAllText(Path.Combine(Application.streamingAssetsPath, BuildInfoFileName), buildInfoJson);
+        }
+    }
 }
