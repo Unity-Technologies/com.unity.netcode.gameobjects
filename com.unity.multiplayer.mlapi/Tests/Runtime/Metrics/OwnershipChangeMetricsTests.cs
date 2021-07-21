6729a88389424d029b80c4fd2318ee1081f6b8cd
Benoit Doyon <benoit.doyon@unity3d.com>
2021-07-21 15:11:58 -0400

    Merge fixes

--- com.unity.multiplayer.mlapi/Tests/Runtime/Metrics/OwnershipChangeMetricsTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/Metrics/OwnershipChangeMetricsTests.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using MLAPI.Metrics;
 using MLAPI.RuntimeTests.Metrics.NetworkVariables;
 using NUnit.Framework;
-using Unity.Multiplayer.NetworkProfiler;
-using Unity.Multiplayer.NetworkProfiler.Models;
+using Unity.Multiplayer.MetricTypes;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace MLAPI.RuntimeTests.Metrics.Ownership
+namespace MLAPI.RuntimeTests.Metrics
 {
-    public class NetworkMetricsOwnershipChangeReceptionTests
+    public class OwnershipChangeMetricsTests
     {
         NetworkManager m_Server;
+        NetworkMetrics m_ServerMetrics;
         NetworkManager m_Client;
         NetworkMetrics m_ClientMetrics;
 
@@ -49,6 +49,7 @@ namespace MLAPI.RuntimeTests.Metrics.Ownership
             yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.WaitForClientConnectedToServer(m_Server));
 
             m_Client = clients.First();
+            m_ServerMetrics = m_Server.NetworkMetrics as NetworkMetrics;
             m_ClientMetrics = m_Client.NetworkMetrics as NetworkMetrics;
         }
 
@@ -61,22 +62,44 @@ namespace MLAPI.RuntimeTests.Metrics.Ownership
         }
 
         [UnityTest]
-        public IEnumerator TrackOwnershipChangeReceivedMetric()
+        public IEnumerator TrackOwnershipChangeSentMetric()
         {
-            var waitForMetricValues = new WaitForMetricValues<OwnershipChangeEvent>(m_ClientMetrics.Dispatcher, MetricNames.OwnershipChangeReceived);
-
             var gameObject = new GameObject(Guid.NewGuid().ToString());
             var networkObject = gameObject.AddComponent<NetworkObject>();
             networkObject.NetworkManagerOwner = m_Server;
             networkObject.Spawn();
 
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(0.2f);
+
+            var waitForMetricValues = new WaitForMetricValues<OwnershipChangeEvent>(m_ServerMetrics.Dispatcher, MetricNames.OwnershipChangeSent);
 
             networkObject.ChangeOwnership(1);
 
-            yield return waitForMetricValues.WaitForAFewFrames();
+            yield return waitForMetricValues.WaitForMetricsReceived();
 
-            var metricValues = waitForMetricValues.EnsureMetricValuesHaveBeenFound();
+            var metricValues = waitForMetricValues.AssertMetricValuesHaveBeenFound();
+
+            var ownershipChangeSent = metricValues.First();
+            Assert.AreEqual(m_Server.LocalClientId, ownershipChangeSent.Connection.Id);
+        }
+
+        [UnityTest]
+        public IEnumerator TrackOwnershipChangeReceivedMetric()
+        {
+            var gameObject = new GameObject(Guid.NewGuid().ToString());
+            var networkObject = gameObject.AddComponent<NetworkObject>();
+            networkObject.NetworkManagerOwner = m_Server;
+            networkObject.Spawn();
+
+            yield return new WaitForSeconds(0.2f);
+
+            var waitForMetricValues = new WaitForMetricValues<OwnershipChangeEvent>(m_ClientMetrics.Dispatcher, MetricNames.OwnershipChangeReceived);
+
+            networkObject.ChangeOwnership(1);
+
+            yield return waitForMetricValues.WaitForMetricsReceived();
+
+            var metricValues = waitForMetricValues.AssertMetricValuesHaveBeenFound();
             Assert.AreEqual(1, metricValues.Count);
         }
     }
