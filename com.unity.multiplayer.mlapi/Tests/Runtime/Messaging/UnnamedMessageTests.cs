e81169e78fd0295168d0f59595e18b7df737c2d6
Benoit Doyon <benoit.doyon@unity3d.com>
2021-07-28 11:02:22 -0400

    Add tests for multiple clients for named and unnamed messages

--- com.unity.multiplayer.mlapi/Tests/Runtime/Messaging/UnnamedMessageTests.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/Messaging/UnnamedMessageTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using MLAPI.Serialization;
@@ -11,9 +12,10 @@ namespace MLAPI.RuntimeTests.Messaging
 {
     public class UnnamedMessageTests : BaseMultiInstanceTest
     {
-        protected override int NbClients => 1;
+        protected override int NbClients => 2;
         
-        private NetworkManager Client => m_ClientNetworkManagers[0];
+        private NetworkManager FirstClient => m_ClientNetworkManagers[0];
+        private NetworkManager SecondClient => m_ClientNetworkManagers[1];
 
         [UnityTest]
         public IEnumerator UnnamedMessageIsReceivedOnClientWithContent()
@@ -21,11 +23,11 @@ namespace MLAPI.RuntimeTests.Messaging
             var messageContent = Guid.NewGuid().ToString();
             var buffer = new NetworkBuffer(Encoding.UTF8.GetBytes(messageContent));
 
-            m_ServerNetworkManager.CustomMessagingManager.SendUnnamedMessage(Client.LocalClientId, buffer);
+            m_ServerNetworkManager.CustomMessagingManager.SendUnnamedMessage(FirstClient.LocalClientId, buffer);
 
             ulong receivedMessageSender = 0;
             string receivedMessageContent = null;
-            m_ClientNetworkManagers[0].CustomMessagingManager.OnUnnamedMessage += (sender, stream) =>
+            FirstClient.CustomMessagingManager.OnUnnamedMessage += (sender, stream) =>
             {
                 receivedMessageSender = sender;
 
@@ -39,6 +41,47 @@ namespace MLAPI.RuntimeTests.Messaging
             Assert.NotNull(receivedMessageContent);
             Assert.AreEqual(messageContent, receivedMessageContent);
             Assert.AreEqual(m_ServerNetworkManager.LocalClientId, receivedMessageSender);
+        }
+        
+        [UnityTest]
+        public IEnumerator UnnamedMessageIsReceivedOnMultipleClientsWithContent()
+        {
+            var messageContent = Guid.NewGuid().ToString();
+            var buffer = new NetworkBuffer(Encoding.UTF8.GetBytes(messageContent));
+
+            m_ServerNetworkManager.CustomMessagingManager.SendUnnamedMessage(new List<ulong> { FirstClient.LocalClientId, SecondClient.LocalClientId }, buffer);
+
+            ulong firstReceivedMessageSender = 0;
+            string firstReceivedMessageContent = null;
+            FirstClient.CustomMessagingManager.OnUnnamedMessage += (sender, stream) =>
+            {
+                firstReceivedMessageSender = sender;
+
+                using var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                firstReceivedMessageContent = Encoding.UTF8.GetString(memoryStream.ToArray());
+            };
+            
+            ulong secondReceivedMessageSender = 0;
+            string secondReceivedMessageContent = null;
+            SecondClient.CustomMessagingManager.OnUnnamedMessage += (sender, stream) =>
+            {
+                secondReceivedMessageSender = sender;
+
+                using var memoryStream = new MemoryStream();
+                stream.CopyTo(memoryStream);
+                secondReceivedMessageContent = Encoding.UTF8.GetString(memoryStream.ToArray());
+            };
+
+            yield return new WaitForSeconds(0.2f);
+
+            Assert.NotNull(firstReceivedMessageContent);
+            Assert.AreEqual(messageContent, firstReceivedMessageContent);
+            Assert.AreEqual(m_ServerNetworkManager.LocalClientId, firstReceivedMessageSender);
+
+            Assert.NotNull(secondReceivedMessageContent);
+            Assert.AreEqual(messageContent, secondReceivedMessageContent);
+            Assert.AreEqual(m_ServerNetworkManager.LocalClientId, secondReceivedMessageSender);
         }
     }
 }
