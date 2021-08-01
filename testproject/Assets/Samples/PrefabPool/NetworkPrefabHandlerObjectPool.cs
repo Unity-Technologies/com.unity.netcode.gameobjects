ef971e13d2bc25117a1bfab2f1decf24e743a370
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-08-01 16:56:43 -0500

    refactor
    removing unused namespace.

--- testproject/Assets/Samples/PrefabPool/NetworkPrefabHandlerObjectPool.cs
+++ testproject/Assets/Samples/PrefabPool/NetworkPrefabHandlerObjectPool.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MLAPI;
-using MLAPI.Serialization.Pooled;
 using MLAPI.Spawning;
 
 public class NetworkPrefabHandlerObjectPool : NetworkBehaviour, INetworkPrefabInstanceHandler
