36e807be7d353fbc7d62d798622aa9f72a6e7d7f
Benoit Doyon <benoit.doyon@unity3d.com>
2021-08-11 11:22:18 -0400

    Markers for profiling

--- com.unity.multiplayer.mlapi/Runtime/Metrics/NetworkMetrics.cs
+++ com.unity.multiplayer.mlapi/Runtime/Metrics/NetworkMetrics.cs
@@ -57,130 +57,214 @@ namespace Unity.Netcode
 
         public void TrackNamedMessageSent(ulong receiverClientId, string messageName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackNamedMessageSent");
+
             m_NamedMessageSentEvent.Mark(new NamedMessageEvent(new ConnectionInfo(receiverClientId), messageName, bytesCount));
+            
+            Profiler.EndSample();
         }
 
         public void TrackNamedMessageSent(IReadOnlyCollection<ulong> receiverClientIds, string messageName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackNamedMessageSent - Multiple");
+
             foreach (var receiver in receiverClientIds)
             {
                 TrackNamedMessageSent(receiver, messageName, bytesCount);
             }
+
+            Profiler.EndSample();
         }
 
         public void TrackNamedMessageReceived(ulong senderClientId, string messageName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackNamedMessageReceived");
+
             m_NamedMessageReceivedEvent.Mark(new NamedMessageEvent(new ConnectionInfo(senderClientId), messageName, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackUnnamedMessageSent(ulong receiverClientId, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackUnnamedMessageSent");
+
             m_UnnamedMessageSentEvent.Mark(new UnnamedMessageEvent(new ConnectionInfo(receiverClientId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackUnnamedMessageSent(IReadOnlyCollection<ulong> receiverClientIds, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackUnnamedMessageSent");
+
             foreach (var receiverClientId in receiverClientIds)
             {
                 TrackUnnamedMessageSent(receiverClientId, bytesCount);
             }
+
+            Profiler.EndSample();
         }
 
         public void TrackUnnamedMessageReceived(ulong senderClientId, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackUnnamedMessageReceived");
+
             m_UnnamedMessageReceivedEvent.Mark(new UnnamedMessageEvent(new ConnectionInfo(senderClientId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackNetworkVariableDeltaSent(ulong receiverClientId, ulong networkObjectId, string gameObjectName, string variableName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackNetworkVariableDeltaSent");
+
             m_NetworkVariableDeltaSentEvent.Mark(new NetworkVariableEvent(new ConnectionInfo(receiverClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), variableName, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackNetworkVariableDeltaReceived(ulong senderClientId, ulong networkObjectId, string gameObjectName, string variableName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackNetworkVariableDeltaReceived");
+
             m_NetworkVariableDeltaReceivedEvent.Mark(new NetworkVariableEvent(new ConnectionInfo(senderClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), variableName, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackOwnershipChangeSent(ulong receiverClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackOwnershipChangeSent");
+
             m_OwnershipChangeSentEvent.Mark(new OwnershipChangeEvent(new ConnectionInfo(receiverClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackOwnershipChangeReceived(ulong senderClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackOwnershipChangeReceived");
+
             m_OwnershipChangeReceivedEvent.Mark(new OwnershipChangeEvent(new ConnectionInfo(senderClientId),
                 new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackObjectSpawnSent(ulong receiverClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackObjectSpawnSent");
+
             m_ObjectSpawnSentEvent.Mark(new ObjectSpawnedEvent(new ConnectionInfo(receiverClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackObjectSpawnReceived(ulong senderClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackObjectSpawnReceived");
+
             m_ObjectSpawnReceivedEvent.Mark(new ObjectSpawnedEvent(new ConnectionInfo(senderClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackObjectDestroySent(ulong receiverClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackObjectDestroySent");
+
             m_ObjectDestroySentEvent.Mark(new ObjectDestroyedEvent(new ConnectionInfo(receiverClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackObjectDestroySent(IReadOnlyCollection<ulong> receiverClientIds, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackObjectDestroySent - Multiple");
+
             foreach (var receiverClientId in receiverClientIds)
             {
                 TrackObjectDestroySent(receiverClientId, networkObjectId, gameObjectName, bytesCount);
             }
+
+            Profiler.EndSample();
         }
 
         public void TrackObjectDestroyReceived(ulong senderClientId, ulong networkObjectId, string gameObjectName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackObjectDestroyReceived");
+
             m_ObjectDestroyReceivedEvent.Mark(new ObjectDestroyedEvent(new ConnectionInfo(senderClientId), new NetworkObjectIdentifier(gameObjectName, networkObjectId), bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackRpcSent(ulong receiverClientId, ulong networkObjectId, string rpcName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackRpcSent");
+
             if (!m_NetworkGameObjects.TryGetValue(networkObjectId, out var networkObjectIdentifier))
             {
                 networkObjectIdentifier = new NetworkObjectIdentifier("", networkObjectId);
             }
 
             m_RpcSentEvent.Mark(new RpcEvent(new ConnectionInfo(receiverClientId), networkObjectIdentifier, rpcName, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackRpcSent(ulong[] receiverClientIds, ulong networkObjectId, string rpcName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackRpcSent - Multiple");
+
             foreach (var receiverClientId in receiverClientIds)
             {
                 TrackRpcSent(receiverClientId, networkObjectId, rpcName, bytesCount);
             }
+
+            Profiler.EndSample();
         }
 
         public void TrackRpcReceived(ulong senderClientId, ulong networkObjectId, string rpcName, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackRpcReceived");
+
             if (!m_NetworkGameObjects.TryGetValue(networkObjectId, out var networkObjectIdentifier))
             {
                 networkObjectIdentifier = new NetworkObjectIdentifier("", networkObjectId);
             }
 
             m_RpcReceivedEvent.Mark(new RpcEvent(new ConnectionInfo(senderClientId), networkObjectIdentifier, rpcName, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackServerLogSent(ulong receiverClientId, uint logType, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackServerLogSent");
+
             m_ServerLogSentEvent.Mark(new ServerLogEvent(new ConnectionInfo(receiverClientId), (Unity.Multiplayer.MetricTypes.LogLevel)logType, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void TrackServerLogReceived(ulong senderClientId, uint logType, long bytesCount)
         {
+            Profiler.BeginSample("NetworkMetrics - TrackServerLogReceived");
+
             m_ServerLogReceivedEvent.Mark(new ServerLogEvent(new ConnectionInfo(senderClientId), (Unity.Multiplayer.MetricTypes.LogLevel)logType, bytesCount));
+
+            Profiler.EndSample();
         }
 
         public void DispatchFrame()
         {
+            Profiler.BeginSample("NetworkMetrics - DispatchFrame");
+
             Dispatcher.Dispatch();
+
+            Profiler.EndSample();
         }
     }
 
