def254305d196105df8ce997a9aff1a007cc51e7
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-08-12 17:05:46 -0500

    style
    removing the TODO and my initials from MTT-860 comments and providing a bit more of an explanation

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -876,7 +876,8 @@ namespace Unity.Netcode
                 NetworkTickSystem.Tick -= OnNetworkManagerTick;
                 NetworkTickSystem = null;
             }
-            // NSS TODO: Remove this once MTT-860 is addressed or before PR
+            // This is required for handling the potential scenario where multiple NetworkManager instances are created.
+            // See MTT-860 for more information
 #if !UNITY_2020_2_OR_NEWER
             if (IsListening)
             {
@@ -917,7 +918,8 @@ namespace Unity.Netcode
                 BehaviourUpdater = null;
             }
 
-            // NSS TODO: Remove this once MTT-860 is addressed or before PR
+            // This is required for handling the potential scenario where multiple NetworkManager instances are created.
+            // See MTT-860 for more information
             if (IsListening)
             {
                 //The Transport is set during initialization, thus it is possible for the Transport to be null
