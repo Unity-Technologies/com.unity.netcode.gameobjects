d15d3f9447fcfba5d49d39d4a1214ee0451ace5e
Jesse Olmer <jesseo@unity3d.com>
2021-08-16 14:38:35 -0700

    test: Remove VisibleObjects from RandomInterestKernel
    Removed the list as it never worked "right", and with the new test suite it's not needed at all.

--- com.unity.multiplayer.mlapi/Tests/Runtime/Interest/RandomInterestKernel.cs
+++ com.unity.multiplayer.mlapi/Tests/Runtime/Interest/RandomInterestKernel.cs
@@ -9,15 +9,10 @@ namespace MLAPI.RuntimeTests
     {
         public float DropRate = 0.1f;
 
-        public readonly List<NetworkObject> VisibleObjects = new List<NetworkObject>();
-
         public override void QueryFor(in NetworkClient _, in NetworkObject obj, HashSet<NetworkObject> results)
         {
-            VisibleObjects.Clear();
-
             if (Random.value > DropRate)
             {
-                VisibleObjects.Add(obj);
                 results.Add(obj);
             }
         }
