172cce8b92572099c31df3e5fe3ff3e9551d5d8e
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-08-17 22:30:03 -0500

    fix
    Due to recent permissions changes and how near future permissions will impact movement, the following fixes issue with player movement and handling the wall collision changing direction.

--- testproject/Assets/Tests/Manual/Scripts/RandomMovement.cs
+++ testproject/Assets/Tests/Manual/Scripts/RandomMovement.cs
@@ -17,29 +17,81 @@ namespace TestProject.ManualTests
             m_Rigidbody = GetComponent<Rigidbody>();
             if (NetworkObject != null && m_Rigidbody != null)
             {
-                m_Rigidbody.isKinematic = !NetworkObject.IsOwner;
-                if (!m_Rigidbody.isKinematic)
+                //m_Rigidbody.isKinematic = !NetworkObject.NetworkManager.IsServer;
+                if (NetworkObject.IsOwner)
                 {
                     ChangeDirection(true, true);
                 }
             }
         }
 
+        private void MovePlayer(Vector3 moveTowards)
+        {
+            m_MoveTowardsPosition = moveTowards;
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void MovePlayerServerRpc(Vector3 moveTowards)
+        {
+            m_MoveTowardsPosition = moveTowards;
+        }
+
+        private Vector3 m_MoveTowardsPosition;
+
         public void Move(int speed)
         {
-            m_Rigidbody.MovePosition(transform.position + m_Direction * (speed * Time.fixedDeltaTime));
+            var nextMoveToPositioin = (m_Direction * speed);
+            if (IsServer && IsOwner)
+            {
+                MovePlayer(nextMoveToPositioin);
+            }
+            else if (!IsServer && IsOwner)
+            {
+                MovePlayerServerRpc(nextMoveToPositioin);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (IsServer && NetworkObject && NetworkObject.NetworkManager && NetworkObject.NetworkManager.IsListening)
+            {
+                if (m_Rigidbody == null)
+                {
+                    m_Rigidbody = GetComponent<Rigidbody>();
+                }
+                if (m_Rigidbody != null)
+                {
+                    m_Rigidbody.MovePosition(transform.position + (m_MoveTowardsPosition * Time.fixedDeltaTime));
+                }
+            }
+        }
+
+        [ClientRpc]
+        private void ChangeDirectionClientRpc(Vector3 direction)
+        {
+            m_Direction = direction;
         }
 
         private void OnCollisionStay(Collision collision)
         {
-            if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("GenericObject"))
+            if (IsServer)
             {
-                return;
+                if (collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("GenericObject"))
+                {
+                    return;
+                }
+                Vector3 collisionPoint = collision.collider.ClosestPoint(transform.position);
+                bool moveRight = collisionPoint.x < transform.position.x;
+                bool moveDown = collisionPoint.z > transform.position.z;
+
+                ChangeDirection(moveRight, moveDown);
+
+                if (!IsOwner)
+                {
+                    m_MoveTowardsPosition = m_Direction * m_MoveTowardsPosition.magnitude;
+                    ChangeDirectionClientRpc(m_Direction);
+                }
             }
-            Vector3 collisionPoint = collision.collider.ClosestPoint(transform.position);
-            bool moveRight = collisionPoint.x < transform.position.x;
-            bool moveDown = collisionPoint.z > transform.position.z;
-            ChangeDirection(moveRight, moveDown);
         }
 
         private void ChangeDirection(bool moveRight, bool moveDown)
