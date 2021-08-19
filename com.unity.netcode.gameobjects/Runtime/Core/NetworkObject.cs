e5575e02557faef1ed541e9a69ce2b9a31bebdf7
M. Fatih MAR <mfatihmar@gmail.com>
2021-08-19 14:26:16 +0100

    fix: eliminate bad use-after-free(destroy) pattern (#1068)
    * fix: eliminate bad use-after-free(destroy) pattern
    
    * remove obsolete comment

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -380,8 +380,20 @@ namespace Unity.Netcode
             }
         }
 
+        private bool m_ApplicationQuitting = false;
+
+        private void OnApplicationQuit()
+        {
+            m_ApplicationQuitting = true;
+        }
+
         private void OnDestroy()
         {
+            if (m_ApplicationQuitting)
+            {
+                return;
+            }
+
             if (NetworkManager != null && NetworkManager.IsListening && NetworkManager.IsServer == false && IsSpawned)
             {
                 throw new NotServerException($"Destroy a spawned {nameof(NetworkObject)} on a non-host client is not valid. Call {nameof(Destroy)} or {nameof(Despawn)} on the server/host instead.");
