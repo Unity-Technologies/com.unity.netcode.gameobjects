0fce6104bf4c79ed283869f92404b232e47a1ff2
becksebenius-unity <74328025+becksebenius-unity@users.noreply.github.com>
2021-08-23 15:25:32 -0700

    Update com.unity.netcode.gameobjects/Editor/CodeGen/NetworkBehaviourILPP.cs
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>

--- com.unity.netcode.gameobjects/Editor/CodeGen/NetworkBehaviourILPP.cs
+++ com.unity.netcode.gameobjects/Editor/CodeGen/NetworkBehaviourILPP.cs
@@ -595,8 +595,8 @@ namespace Unity.Netcode.Editor.CodeGen
 
             // override NetworkBehaviour.__getTypeName() method to return concrete type
             {
-                var networkBehaviourType = m_NetworkBehaviour_TypeRef.Resolve();
-                var baseGetTypeNameMethod = networkBehaviourType.Methods.First(p => p.Name.Equals(nameof(NetworkBehaviour.__getTypeName)));
+                var networkBehaviour_TypeDef = m_NetworkBehaviour_TypeRef.Resolve();
+                var baseGetTypeNameMethod = networkBehaviour_TypeDef.Methods.First(p => p.Name.Equals(nameof(NetworkBehaviour.__getTypeName)));
 
                 var newGetTypeNameMethod = new MethodDefinition(
                     nameof(NetworkBehaviour.__getTypeName),
