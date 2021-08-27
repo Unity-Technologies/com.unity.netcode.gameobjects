0bdb073b1f9b48d30f3040b0d63205973dd7a874
M. Fatih MAR <mfatihmar@gmail.com>
2021-08-27 03:48:50 +0100

    mvp

--- com.unity.netcode.gameobjects/Editor/NetworkTransformEditor.cs
+++ com.unity.netcode.gameobjects/Editor/NetworkTransformEditor.cs
@@ -8,7 +8,17 @@ namespace Unity.Netcode.Editor
     public class NetworkTransformEditor : UnityEditor.Editor
     {
         private NetworkTransform m_NetworkTransform;
+
         private SerializedProperty m_InLocalSpaceProperty;
+        private SerializedProperty m_SyncPositionXProperty;
+        private SerializedProperty m_SyncPositionYProperty;
+        private SerializedProperty m_SyncPositionZProperty;
+        private SerializedProperty m_SyncRotationXProperty;
+        private SerializedProperty m_SyncRotationYProperty;
+        private SerializedProperty m_SyncRotationZProperty;
+        private SerializedProperty m_SyncScaleXProperty;
+        private SerializedProperty m_SyncScaleYProperty;
+        private SerializedProperty m_SyncScaleZProperty;
         private SerializedProperty m_PositionThresholdProperty;
         private SerializedProperty m_RotAngleThresholdProperty;
         private SerializedProperty m_ScaleThresholdProperty;
@@ -24,7 +34,15 @@ namespace Unity.Netcode.Editor
             m_NetworkTransform = target as NetworkTransform;
 
             m_InLocalSpaceProperty = serializedObject.FindProperty(nameof(NetworkTransform.InLocalSpace));
-
+            m_SyncPositionXProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncPositionX));
+            m_SyncPositionYProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncPositionY));
+            m_SyncPositionZProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncPositionZ));
+            m_SyncRotationXProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncRotAngleX));
+            m_SyncRotationYProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncRotAngleY));
+            m_SyncRotationZProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncRotAngleZ));
+            m_SyncScaleXProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncScaleX));
+            m_SyncScaleYProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncScaleY));
+            m_SyncScaleZProperty = serializedObject.FindProperty(nameof(NetworkTransform.SyncScaleZ));
             m_PositionThresholdProperty = serializedObject.FindProperty(nameof(NetworkTransform.PositionThreshold));
             m_RotAngleThresholdProperty = serializedObject.FindProperty(nameof(NetworkTransform.RotAngleThreshold));
             m_ScaleThresholdProperty = serializedObject.FindProperty(nameof(NetworkTransform.ScaleThreshold));
@@ -45,11 +63,11 @@ namespace Unity.Netcode.Editor
                 rect = EditorGUI.PrefixLabel(rect, ctid, s_PositionLabel);
                 rect.width = s_ToggleOffset;
 
-                EditorGUI.ToggleLeft(rect, "X", true);
+                m_SyncPositionXProperty.boolValue = EditorGUI.ToggleLeft(rect, "X", m_SyncPositionXProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Y", true);
+                m_SyncPositionYProperty.boolValue = EditorGUI.ToggleLeft(rect, "Y", m_SyncPositionYProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Z", true);
+                m_SyncPositionZProperty.boolValue = EditorGUI.ToggleLeft(rect, "Z", m_SyncPositionZProperty.boolValue);
 
                 GUILayout.EndHorizontal();
             }
@@ -62,11 +80,11 @@ namespace Unity.Netcode.Editor
                 rect = EditorGUI.PrefixLabel(rect, ctid, s_RotationLabel);
                 rect.width = s_ToggleOffset;
 
-                EditorGUI.ToggleLeft(rect, "X", true);
+                m_SyncRotationXProperty.boolValue = EditorGUI.ToggleLeft(rect, "X", m_SyncRotationXProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Y", true);
+                m_SyncRotationYProperty.boolValue = EditorGUI.ToggleLeft(rect, "Y", m_SyncRotationYProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Z", true);
+                m_SyncRotationZProperty.boolValue = EditorGUI.ToggleLeft(rect, "Z", m_SyncRotationZProperty.boolValue);
 
                 GUILayout.EndHorizontal();
             }
@@ -79,11 +97,11 @@ namespace Unity.Netcode.Editor
                 rect = EditorGUI.PrefixLabel(rect, ctid, s_ScaleLabel);
                 rect.width = s_ToggleOffset;
 
-                EditorGUI.ToggleLeft(rect, "X", true);
+                m_SyncScaleXProperty.boolValue = EditorGUI.ToggleLeft(rect, "X", m_SyncScaleXProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Y", true);
+                m_SyncScaleYProperty.boolValue = EditorGUI.ToggleLeft(rect, "Y", m_SyncScaleYProperty.boolValue);
                 rect.x += s_ToggleOffset;
-                EditorGUI.ToggleLeft(rect, "Z", true);
+                m_SyncScaleZProperty.boolValue = EditorGUI.ToggleLeft(rect, "Z", m_SyncScaleZProperty.boolValue);
 
                 GUILayout.EndHorizontal();
             }
@@ -93,6 +111,8 @@ namespace Unity.Netcode.Editor
             EditorGUILayout.PropertyField(m_PositionThresholdProperty);
             EditorGUILayout.PropertyField(m_RotAngleThresholdProperty);
             EditorGUILayout.PropertyField(m_ScaleThresholdProperty);
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
