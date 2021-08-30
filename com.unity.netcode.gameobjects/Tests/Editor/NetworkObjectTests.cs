f2bab31ffa065e8dae6d84d13df534f41cb8d378
zain-mecklai <zain.mecklai@unity3d.com>
2021-08-30 15:18:31 -0400

    revert test

--- com.unity.netcode.gameobjects/Tests/Editor/NetworkObjectTests.cs
+++ com.unity.netcode.gameobjects/Tests/Editor/NetworkObjectTests.cs
@@ -23,12 +23,12 @@ namespace Unity.Netcode.EditorTests
             // Set override
             networkObject.NetworkManagerOwner = networkManager;
 
-            Debug.Assert(networkObject.NetworkManager == networkManager);
-            Debug.Assert(singletonNetworkManager.NetworkConfig != null);
-            Debug.Assert(singletonNetworkManager.NetworkConfig.NetworkTransport != null);
+            // Debug.Assert(networkObject.NetworkManager == networkManager);
+            // Debug.Assert(singletonNetworkManager.NetworkConfig != null);
+            // Debug.Assert(singletonNetworkManager.NetworkConfig.NetworkTransport != null);
             Object.DestroyImmediate(singletonNetworkManager.gameObject);
-            // Object.DestroyImmediate(networkManager.gameObject);
-            // Object.DestroyImmediate(gameObject);
+            Object.DestroyImmediate(networkManager.gameObject);
+            Object.DestroyImmediate(gameObject);
         }
 
         [Test]
