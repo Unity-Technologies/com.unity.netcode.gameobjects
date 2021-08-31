c68e7c7e906375c48ec29945f22f3715db45c12f
Fernando Cortez <fernando.cortez@unity3d.com>
2021-08-31 15:31:49 -0400

    feat: bootstrap template sample featuring basic RPC usage & movement with NetworkTransform

---
+++ com.unity.netcode.gameobjects/Samples~/Bootstrap/Scripts/BootstrapManager.cs
@@ -0,0 +1,68 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace BoostrapSample
+{
+    /// <summary>
+    /// Class to display helper buttons and status labels on the GUI, as well as buttons to start host/client/server.
+    /// Once a connection has been established to the server, the local player can be moved via a GUI button.
+    /// </summary>
+    public class BootstrapManager : MonoBehaviour
+    {
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+
+                // move button will only be displayed on clients
+                if (NetworkManager.Singleton.IsClient)
+                {
+                    SubmitNewPosition();
+                }
+            }
+
+            GUILayout.EndArea();
+        }
+
+        static void StartButtons()
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+        }
+
+        static void StatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+
+        static void SubmitNewPosition()
+        {
+            if (GUILayout.Button("Move Local Player"))
+            {
+                // get local NetworkClient
+                if (NetworkManager.Singleton.ConnectedClients.TryGetValue(NetworkManager.Singleton.LocalClientId,
+                    out var networkedClient))
+                {
+                    // get BootstrapPlayer component on player
+                    if (networkedClient.PlayerObject.TryGetComponent(out BootstrapPlayer bootstrapPlayer))
+                    {
+                        // move player
+                        bootstrapPlayer.Move();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ com.unity.netcode.gameobjects/Samples~/Bootstrap/Scripts/BootstrapPlayer.cs
@@ -0,0 +1,57 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace BoostrapSample
+{
+    /// <summary>
+    /// Component attached to NetworkManager's "Player Prefab".
+    /// </summary>
+    public class BootstrapPlayer : NetworkBehaviour
+    {
+        /// <summary>
+        /// Move local player on network spawn.
+        /// </summary>
+        public override void OnNetworkSpawn()
+        {
+            if (IsLocalPlayer)
+            {
+                Move();
+            }
+        }
+
+        /// <summary>
+        /// Move requests are funneled through this method.
+        /// If this method is invoked on the client instance of this player, it will invoke a ServerRpc.
+        /// If this method is invoked on the server instance of this player, it will position the player at a random
+        /// point on a plane.
+        /// </summary>
+        /// <remarks>
+        /// Since a NetworkTransform component is attached to this player, and the authority on that component is set to
+        /// "Server", this transform's position modification can only be performed on the server, where it will then be
+        /// replicated on all other clients.
+        /// </remarks>
+        public void Move()
+        {
+            if (NetworkManager.Singleton.IsServer)
+            {
+                transform.position = GetRandomPositionOnXYPlane();
+            }
+            else
+            {
+                SubmitPositionRequestServerRpc();
+            }
+        }
+
+        [ServerRpc]
+        void SubmitPositionRequestServerRpc()
+        {
+            Debug.Log($"SubmitPositionRequestServerRpc received on server from client: {OwnerClientId}");
+            Move();
+        }
+
+        static Vector3 GetRandomPositionOnXYPlane()
+        {
+            return new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f);
+        }
+    }
+}
