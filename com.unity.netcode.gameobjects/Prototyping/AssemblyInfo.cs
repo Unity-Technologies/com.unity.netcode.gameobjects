c547360a64ffe201f30735db2c67bec3193b90ce
zain-mecklai <zain.mecklai@unity3d.com>
2021-09-02 08:42:50 -0400

    PR: remove extra conditional in AssemblyInfo

--- com.unity.netcode.gameobjects/Prototyping/AssemblyInfo.cs
+++ com.unity.netcode.gameobjects/Prototyping/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Runtime.CompilerServices;
 
-#if UNITY_EDITOR || UNITY_INCLUDE_TESTS
+#if UNITY_EDITOR
 [assembly: InternalsVisibleTo("Unity.Netcode.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.Editor.CodeGen")]
 [assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
