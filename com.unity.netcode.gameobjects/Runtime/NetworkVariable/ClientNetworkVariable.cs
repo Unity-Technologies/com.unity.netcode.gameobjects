7541e16b0ee28d3ecbe99a04ef116de34e995eab
Valere Plantevin <89042540+VALERE91Unity@users.noreply.github.com>
2021-09-03 08:52:09 -0400

    fix: a host cannot change its own ClientNetVar
    The host being a Client and a Server the Exception fires every time the Host is trying to modify its own ClientNetVar.
    
    Caveat : with this fix, a host can change the ClientNetVar of everybody

--- com.unity.netcode.gameobjects/Runtime/NetworkVariable/ClientNetworkVariable.cs
+++ com.unity.netcode.gameobjects/Runtime/NetworkVariable/ClientNetworkVariable.cs
@@ -40,7 +40,7 @@ namespace Unity.Netcode
 
                 // Also, note this is not really very water-tight, if you are running as a host
                 //  we cannot tell if a ClientNetworkVariable write is happening inside server-ish code
-                if (m_NetworkBehaviour.NetworkManager.IsServer)
+                if (m_NetworkBehaviour.NetworkManager.IsServer && !m_NetworkBehaviour.NetworkManager.IsHost)
                 {
                     throw new InvalidOperationException("Server not allowed to write to ClientNetworkVariables");
                 }
