1ebd1e5b001ba1d6ce55a2a3709b022331cb6e17
Sam Bellomo <71790295+SamuelBellomo@users.noreply.github.com>
2021-09-07 10:20:42 -0400

    Update com.unity.netcode.gameobjects/Prototyping/Interpolator/IInterpolator.cs
    Co-authored-by: M. Fatih MAR <mfatihmar@gmail.com>

--- com.unity.netcode.gameobjects/Prototyping/Interpolator/IInterpolator.cs
+++ com.unity.netcode.gameobjects/Prototyping/Interpolator/IInterpolator.cs
@@ -2,16 +2,16 @@ namespace Unity.Netcode
 {
     public interface IInterpolator<T>
     {
-        public void Awake();
-        public void OnNetworkSpawn();
-        public void Start();
-        public void OnEnable();
-        public T Update(float deltaTime);
-        public void FixedUpdate(float tickDeltaTime);
-        public void AddMeasurement(T newMeasurement, NetworkTime sentTime);
-        public T GetInterpolatedValue();
-        public void OnDestroy();
-        public void ResetTo(T targetValue);
-        public bool UseFixedUpdate { get; set; }
+        void Awake();
+        void OnNetworkSpawn();
+        void Start();
+        void OnEnable();
+        T Update(float deltaTime);
+        void FixedUpdate(float tickDeltaTime);
+        void AddMeasurement(T newMeasurement, NetworkTime sentTime);
+        T GetInterpolatedValue();
+        void OnDestroy();
+        void ResetTo(T targetValue);
+        bool UseFixedUpdate { get; set; }
     }
 }
