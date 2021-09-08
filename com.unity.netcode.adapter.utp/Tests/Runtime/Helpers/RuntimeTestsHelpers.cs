bceb0755e003ed760fa6f4e16933765b9a8037c7
Andrew Spiering <andrews@unity3d.com>
2021-09-08 21:22:26 +0200

    fix: fixing a compile issue

--- com.unity.netcode.adapter.utp/Tests/Runtime/Helpers/RuntimeTestsHelpers.cs
+++ com.unity.netcode.adapter.utp/Tests/Runtime/Helpers/RuntimeTestsHelpers.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Unity.Netcode.UTP.RuntimeTests
