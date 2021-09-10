57abd6585a6d47bf27b5b0498e97387b7713fe50
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2021-09-10 18:46:36 -0400

    fix: fixing testproject SampleScene for client-authoritative NetworkTransform

--- testproject/Assets/Scripts/PlayerMovement.cs
+++ testproject/Assets/Scripts/PlayerMovement.cs
@@ -28,7 +28,7 @@ public class PlayerMovement : NetworkBehaviour
             // If we don't set the non-local-player object as kinematic,
             // the local physics would apply and result in unwanted position
             // updates being sent up
-            m_Rigidbody.isKinematic = !IsLocalPlayer;
+            m_Rigidbody.isKinematic = !IsServer;
         }
     }
 
@@ -38,12 +38,24 @@ public class PlayerMovement : NetworkBehaviour
         Players[OwnerClientId] = this; // todo should really have a NetworkStop for unregistering this...
     }
 
+    [ServerRpc(RequireOwnership = true)]
+    private void MovePlayerServerRpc(Vector3 displacement, float rotation)
+    {
+        transform.position += displacement;
+        transform.rotation = Quaternion.Euler(0, rotation, 0) * transform.rotation;
+    }
+
     private void FixedUpdate()
     {
         if (IsLocalPlayer)
         {
-            transform.position += Input.GetAxis("Vertical") * m_Speed * Time.fixedDeltaTime * transform.forward;
-            transform.rotation = Quaternion.Euler(0, Input.GetAxis("Horizontal") * 90 * m_RotSpeed * Time.fixedDeltaTime, 0) * transform.rotation;
+            // we want to move by a given amount in the direction the client currently sees the player facing
+            var translation = Input.GetAxis("Vertical") * m_Speed * Time.fixedDeltaTime * transform.forward;
+            // we want to rotate by a given amount
+            var rotation = Input.GetAxis("Horizontal") * 90 * m_RotSpeed * Time.fixedDeltaTime;
+
+            // we instruct the server to do so
+            MovePlayerServerRpc(translation, rotation);
         }
     }
 }
