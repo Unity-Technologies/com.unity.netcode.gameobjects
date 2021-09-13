5b52d113fa3851c73c3114f2701a4dd45a321fc0
Matt Walsh <matt.walsh@unity3d.com>
2021-09-13 09:33:47 -0700

    remove set & dictionary

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkVariableTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkVariableTests.cs
@@ -49,8 +49,6 @@ namespace Unity.Netcode.RuntimeTests
     {
         public readonly NetworkVariable<int> TheScalar = new NetworkVariable<int>();
         public readonly NetworkList<int> TheList = new NetworkList<int>();
-        public readonly NetworkSet<int> TheSet = new NetworkSet<int>();
-        public readonly NetworkDictionary<int, int> TheDictionary = new NetworkDictionary<int, int>();
 
         public readonly NetworkVariable<FixedString32Struct> FixedStringStruct = new NetworkVariable<FixedString32Struct>();
 
@@ -59,35 +57,15 @@ namespace Unity.Netcode.RuntimeTests
             ListDelegateTriggered = true;
         }
 
-        private void SetChanged(NetworkSetEvent<int> e)
-        {
-            SetDelegateTriggered = true;
-        }
-
-        private void DictionaryChanged(NetworkDictionaryEvent<int, int> e)
-        {
-            DictionaryDelegateTriggered = true;
-        }
 
         public void Awake()
         {
             TheList.OnListChanged += ListChanged;
-            TheSet.OnSetChanged += SetChanged;
-            TheDictionary.OnDictionaryChanged += DictionaryChanged;
         }
-
-        public void OnDestroy()
-        {
-            TheSet.Dispose();
-            TheDictionary.Dispose();
-        }
-
 
         public readonly NetworkVariable<TestStruct> TheStruct = new NetworkVariable<TestStruct>();
 
         public bool ListDelegateTriggered;
-        public bool SetDelegateTriggered;
-        public bool DictionaryDelegateTriggered;
     }
 
     public class NetworkVariableTests : BaseMultiInstanceTest
@@ -166,14 +144,12 @@ namespace Unity.Netcode.RuntimeTests
             m_Player1OnClient2 = result.Result.GetComponent<NetworkVariableTest>();
 
             m_Player1OnServer.TheList.Clear();
-            m_Player1OnServer.TheSet.Clear();
-            m_Player1OnServer.TheDictionary.Clear();
 
-            if (m_Player1OnServer.TheList.Count > 0 || m_Player1OnServer.TheSet.Count > 0 || m_Player1OnServer.TheDictionary.Count > 0)
+            if (m_Player1OnServer.TheList.Count > 0)
             {
                 throw new Exception("at least one server network container not empty at start");
             }
-            if (m_Player1OnClient1.TheList.Count > 0 || m_Player1OnClient1.TheSet.Count > 0 || m_Player1OnClient1.TheDictionary.Count > 0)
+            if (m_Player1OnClient1.TheList.Count > 0)
             {
                 throw new Exception("at least one client network container not empty at start");
             }
@@ -323,225 +299,6 @@ namespace Unity.Netcode.RuntimeTests
                         m_Player1OnClient1.ListDelegateTriggered &&
                         m_Player1OnServer.TheList.Count == 0 &&
                         m_Player1OnClient1.TheList.Count == 0;
-                }
-            );
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkSetAdd([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheSet.Add(k_TestVal1);
-                    m_Player1OnServer.TheSet.Add(k_TestVal2);
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheSet.Count == 2 &&
-                           m_Player1OnClient1.TheSet.Count == 2 &&
-                           m_Player1OnServer.SetDelegateTriggered &&
-                           m_Player1OnClient1.SetDelegateTriggered &&
-                           m_Player1OnServer.TheSet.Contains(k_TestVal1) &&
-                           m_Player1OnClient1.TheSet.Contains(k_TestVal1) &&
-                           m_Player1OnServer.TheSet.Contains(k_TestVal2) &&
-                           m_Player1OnClient1.TheSet.Contains(k_TestVal2);
-                }
-            );
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkSetRemove([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            // first put some stuff in; re-use the add test
-            yield return NetworkSetAdd(useHost);
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheSet.Remove(k_TestVal1);
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheSet.Count == 1 &&
-                           m_Player1OnClient1.TheSet.Count == 1 &&
-                           m_Player1OnServer.SetDelegateTriggered &&
-                           m_Player1OnClient1.SetDelegateTriggered &&
-                           m_Player1OnServer.TheSet.Contains(k_TestVal2) &&
-                           m_Player1OnClient1.TheSet.Contains(k_TestVal2);
-                }
-            );
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkSetClear([Values(true, false)] bool useHost)
-        {
-            // first put some stuff in; re-use the add test
-            yield return NetworkSetAdd(useHost);
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheSet.Clear();
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheSet.Count == 0 &&
-                           m_Player1OnClient1.TheSet.Count == 0 &&
-                           m_Player1OnServer.SetDelegateTriggered &&
-                           m_Player1OnClient1.SetDelegateTriggered;
-                }
-            );
-        }
-
-        [Test]
-        public void NetworkDictionaryTryGetValue([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-            m_Player1OnServer.TheDictionary[k_TestKey1] = k_TestVal1;
-            int outValue;
-            var result = m_Player1OnServer.TheDictionary.TryGetValue(k_TestKey1, out outValue);
-            Assert.IsTrue(result == true);
-            Assert.IsTrue(outValue == k_TestVal1);
-        }
-
-        [Test]
-        public void NetworkDictionaryAddKeyValue([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            m_Player1OnServer.TheDictionary.Add(k_TestKey1, k_TestVal1);
-            Assert.IsTrue(m_Player1OnServer.TheDictionary[k_TestKey1] == k_TestVal1);
-        }
-
-        [Test]
-        public void NetworkDictionaryAddKeyValuePair([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            m_Player1OnServer.TheDictionary.Add(new KeyValuePair<int, int>(k_TestKey1, k_TestVal1));
-            Assert.IsTrue(m_Player1OnServer.TheDictionary[k_TestKey1] == k_TestVal1);
-        }
-
-        [Test]
-        public void NetworkDictionaryContainsKey([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            m_Player1OnServer.TheDictionary.Add(k_TestKey1, k_TestVal1);
-            Assert.IsTrue(m_Player1OnServer.TheDictionary.ContainsKey(k_TestKey1));
-        }
-
-        [Test]
-        public void NetworkDictionaryContains([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            m_Player1OnServer.TheDictionary.Add(k_TestKey1, k_TestVal1);
-            Assert.IsTrue(m_Player1OnServer.TheDictionary.Contains(new KeyValuePair<int, int>(k_TestKey1, k_TestVal1)));
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkDictionaryAdd([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheDictionary.Add(k_TestKey1, k_TestVal1);
-                    m_Player1OnServer.TheDictionary.Add(k_TestKey2, k_TestVal2);
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheDictionary.Count == 2 &&
-                           m_Player1OnClient1.TheDictionary.Count == 2 &&
-                           m_Player1OnServer.DictionaryDelegateTriggered &&
-                           m_Player1OnClient1.DictionaryDelegateTriggered &&
-                           m_Player1OnServer.TheDictionary[k_TestKey1] == k_TestVal1 &&
-                           m_Player1OnClient1.TheDictionary[k_TestKey1] == k_TestVal1 &&
-                           m_Player1OnServer.TheDictionary[k_TestKey2] == k_TestVal2 &&
-                           m_Player1OnClient1.TheDictionary[k_TestKey2] == k_TestVal2;
-                }
-            );
-        }
-
-        /* Note, not adding coverage for RemovePair, because we plan to remove
-         *  this in the next PR
-         */
-        [UnityTest]
-        public IEnumerator NetworkDictionaryRemoveByKey([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            // first put some stuff in; re-use the add test
-            yield return NetworkDictionaryAdd(useHost);
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheDictionary.Remove(k_TestKey2);
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheDictionary.Count == 1 &&
-                           m_Player1OnClient1.TheDictionary.Count == 1 &&
-                           m_Player1OnServer.DictionaryDelegateTriggered &&
-                           m_Player1OnClient1.DictionaryDelegateTriggered &&
-                           m_Player1OnServer.TheDictionary[k_TestKey1] == k_TestVal1 &&
-                           m_Player1OnClient1.TheDictionary[k_TestKey1] == k_TestVal1;
-                }
-            );
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkDictionaryChangeValue([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            // first put some stuff in; re-use the add test
-            yield return NetworkDictionaryAdd(useHost);
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheDictionary[k_TestKey1] = k_TestVal3;
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheDictionary.Count == 2 &&
-                           m_Player1OnClient1.TheDictionary.Count == 2 &&
-                           m_Player1OnServer.DictionaryDelegateTriggered &&
-                           m_Player1OnClient1.DictionaryDelegateTriggered &&
-                           m_Player1OnServer.TheDictionary[k_TestKey1] == k_TestVal3 &&
-                           m_Player1OnClient1.TheDictionary[k_TestKey1] == k_TestVal3;
-                }
-            );
-        }
-
-        [UnityTest]
-        public IEnumerator NetworkDictionaryClear([Values(true, false)] bool useHost)
-        {
-            m_TestWithHost = useHost;
-
-            // first put some stuff in; re-use the add test
-            yield return NetworkDictionaryAdd(useHost);
-
-            yield return MultiInstanceHelpers.RunAndWaitForCondition(
-                () =>
-                {
-                    m_Player1OnServer.TheDictionary.Clear();
-                },
-                () =>
-                {
-                    return m_Player1OnServer.TheDictionary.Count == 0 &&
-                           m_Player1OnClient1.TheDictionary.Count == 0 &&
-                           m_Player1OnServer.DictionaryDelegateTriggered &&
-                           m_Player1OnClient1.DictionaryDelegateTriggered;
                 }
             );
         }
