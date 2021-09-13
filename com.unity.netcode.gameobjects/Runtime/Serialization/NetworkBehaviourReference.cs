1a63b90c959fcc8d2f2a23072d600de5c6a17c6a
Luke Stampfli <luke@stampfli.one>
2021-09-13 12:19:08 +0100

    feat: make networkobjectref not stateful

---
+++ com.unity.netcode.gameobjects/Runtime/Serialization/NetworkBehaviourReference.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Unity.Netcode
+{
+    /// <summary>
+    /// A helper struct for serializing <see cref="NetworkBehaviour"/>s over the network. Can be used in RPCs.
+    /// </summary>
+    public struct NetworkBehaviourReference : INetworkSerializable, IEquatable<NetworkBehaviourReference>
+    {
+        private NetworkObjectReference m_NetworkObjectReference;
+        private ushort m_NetworkBehaviourId;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="NetworkBehaviourReference{T}"/> struct.
+        /// </summary>
+        /// <param name="networkBehaviour">The <see cref="NetworkBehaviour"/> to reference.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public NetworkBehaviourReference(NetworkBehaviour networkBehaviour)
+        {
+            if (networkBehaviour == null)
+            {
+                throw new ArgumentNullException(nameof(networkBehaviour));
+            }
+            if (networkBehaviour.NetworkObject == null)
+            {
+                throw new ArgumentException($"Cannot create {nameof(NetworkBehaviourReference)} from {nameof(NetworkBehaviour)} without a {nameof(NetworkObject)}.");
+            }
+
+            m_NetworkObjectReference = networkBehaviour.NetworkObject;
+            m_NetworkBehaviourId = networkBehaviour.NetworkBehaviourId;
+        }
+
+        /// <summary>
+        /// Tries to get the <see cref="NetworkBehaviour"/> referenced by this reference.
+        /// </summary>
+        /// <param name="networkBehaviour">The <see cref="NetworkBehaviour"/> which was found. Null if the corresponding <see cref="NetworkObject"/> was not found.</param>
+        /// <param name="networkManager">The networkmanager. Uses <see cref="NetworkManager.Singleton"/> to resolve if null.</param>
+        /// <returns>True if the <see cref="NetworkBehaviour"/> was found; False if the <see cref="NetworkBehaviour"/> was not found. This can happen if the corresponding <see cref="NetworkObject"/> has not been spawned yet. you can try getting the reference at a later point in time.</returns>
+        public bool TryGet(out NetworkBehaviour networkBehaviour, NetworkManager networkManager = null)
+        {
+            networkBehaviour = GetInternal(this, null);
+            return networkBehaviour != null;
+        }
+
+        /// <summary>
+        /// Tries to get the <see cref="NetworkBehaviour"/> referenced by this reference.
+        /// </summary>
+        /// <param name="networkBehaviour">The <see cref="NetworkBehaviour"/> which was found. Null if the corresponding <see cref="NetworkObject"/> was not found.</param>
+        /// <param name="networkManager">The networkmanager. Uses <see cref="NetworkManager.Singleton"/> to resolve if null.</param>
+        /// <typeparam name="T">The type of the networkBehaviour for convenience.</typeparam>
+        /// <returns>True if the <see cref="NetworkBehaviour"/> was found; False if the <see cref="NetworkBehaviour"/> was not found. This can happen if the corresponding <see cref="NetworkObject"/> has not been spawned yet. you can try getting the reference at a later point in time.</returns>
+        public bool TryGet<T>(out T networkBehaviour, NetworkManager networkManager = null) where T : NetworkBehaviour
+        {
+            networkBehaviour = (T)GetInternal(this, null);
+            return networkBehaviour != null;
+        }
+
+        /// <inheritdoc/>
+        public void NetworkSerialize(NetworkSerializer serializer)
+        {
+            m_NetworkObjectReference.NetworkSerialize(serializer);
+            serializer.Serialize(ref m_NetworkBehaviourId);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static NetworkBehaviour GetInternal(NetworkBehaviourReference networkBehaviourRef, NetworkManager networkManager = null)
+        {
+            if (networkBehaviourRef.m_NetworkObjectReference.TryGet(out NetworkObject networkObject, networkManager))
+            {
+                return networkObject.GetNetworkBehaviourAtOrderIndex(networkBehaviourRef.m_NetworkBehaviourId);
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(NetworkBehaviourReference other)
+        {
+            return m_NetworkObjectReference.Equals(other.m_NetworkObjectReference) && m_NetworkBehaviourId == other.m_NetworkBehaviourId;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj is NetworkBehaviourReference other && Equals(other);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (m_NetworkObjectReference.GetHashCode() * 397) ^ m_NetworkBehaviourId.GetHashCode();
+            }
+        }
+
+        public static implicit operator NetworkBehaviour(NetworkBehaviourReference networkBehaviourRef) => GetInternal(networkBehaviourRef);
+
+        public static implicit operator NetworkBehaviourReference(NetworkBehaviour networkBehaviour) => new NetworkBehaviourReference(networkBehaviour);
+    }
+}
---
+++ com.unity.netcode.gameobjects/Tests/Runtime/Serialization/NetworkBehaviourReferenceTests.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Unity.Netcode.RuntimeTests.Serialization
+{
+    /// <summary>
+    /// Unit tests to test:
+    /// - Serializing NetworkObject to NetworkObjectReference
+    /// - Deserializing NetworkObjectReference to NetworkObject
+    /// - Implicit operators of NetworkObjectReference
+    /// </summary>
+    public class NetworkBehaviourReferenceTests : IDisposable
+    {
+        private class TestNetworkBehaviour : NetworkBehaviour
+        {
+            public NetworkVariable<NetworkBehaviourReference> TestVariable = new NetworkVariable<NetworkBehaviourReference>();
+
+            public TestNetworkBehaviour rpcReceivedBehaviour;
+
+            [ServerRpc]
+            public void SendReferenceServerRpc(NetworkBehaviourReference value)
+            {
+                rpcReceivedBehaviour = (TestNetworkBehaviour)value;
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator TestRpc()
+        {
+            using var networkObjectContext = UnityObjectContext.CreateNetworkObject();
+            var testNetworkBehaviour = networkObjectContext.Object.gameObject.AddComponent<TestNetworkBehaviour>();
+            networkObjectContext.Object.Spawn();
+
+            using var otherObjectContext = UnityObjectContext.CreateNetworkObject();
+            otherObjectContext.Object.Spawn();
+
+            testNetworkBehaviour.SendReferenceServerRpc(testNetworkBehaviour);
+
+            // wait for rpc completion
+            float t = 0;
+            while (testNetworkBehaviour.rpcReceivedBehaviour == null)
+            {
+                t += Time.deltaTime;
+                if (t > 5f)
+                {
+                    new AssertionException("RPC with NetworkBehaviour reference hasn't been received");
+                }
+
+                yield return null;
+            }
+
+            // validate
+            Assert.AreEqual(testNetworkBehaviour, testNetworkBehaviour.rpcReceivedBehaviour);
+        }
+
+        [Test]
+        public void FailSerializeNonSpawnedNetworkObject()
+        {
+            using var networkObjectContext = UnityObjectContext.CreateNetworkObject();
+            var component = networkObjectContext.Object.gameObject.AddComponent<TestNetworkBehaviour>();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                NetworkBehaviourReference outReference = component;
+            });
+        }
+
+        [Test]
+        public void FailSerializeGameObjectWithoutNetworkObject()
+        {
+            using var gameObjectContext = UnityObjectContext.CreateGameObject();
+            var component = gameObjectContext.Object.gameObject.AddComponent<TestNetworkBehaviour>();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                NetworkBehaviourReference outReference = component;
+            });
+        }
+
+        [Test]
+        public void FailSerializeNullBehaviour()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                NetworkBehaviourReference outReference = null;
+            });
+        }
+
+        public void Dispose()
+        {
+            //Stop, shutdown, and destroy
+            NetworkManagerHelper.ShutdownNetworkManager();
+        }
+
+        public NetworkBehaviourReferenceTests()
+        {
+            //Create, instantiate, and host
+            NetworkManagerHelper.StartNetworkManager(out _);
+        }
+    }
+}
