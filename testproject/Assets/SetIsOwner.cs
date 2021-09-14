bdecea93170d0b68789e76666cdddfc52c3b34df
Samuel Bellomo <samuel.bellomo@unity3d.com>
2021-09-13 21:51:30 -0400

    adding missing script

---
+++ testproject/Assets/SetIsOwner.cs
@@ -0,0 +1,46 @@
+using System;
+using Unity.Netcode;
+using UnityEditor;
+using UnityEngine;
+
+public class SetIsOwner : MonoBehaviour
+{
+    public void Set(ulong clientID)
+    {
+        GetComponent<NetworkObject>().ChangeOwnership(clientID);
+    }
+
+    [CustomEditor(typeof(SetIsOwner))]
+    public class GameEventEditor : UnityEditor.Editor
+    {
+        private string clientID = "clientID (ulong)";
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var gameEvent = (SetIsOwner) target;
+
+
+            if (NetworkManager.Singleton != null && (NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening))
+            {
+                GUILayout.TextArea($"Current owner: {gameEvent.GetComponent<NetworkObject>().OwnerClientId}");
+                clientID = GUILayout.TextField(clientID);
+            }
+
+            if (GUILayout.Button("Set"))
+            {
+                gameEvent.Set(Convert.ToUInt64(clientID));
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (NetworkManager.Singleton != null && (NetworkManager.Singleton.IsConnectedClient || NetworkManager.Singleton.IsListening))
+        {
+            Debug.Log(NetworkManager.Singleton.LocalClientId);
+        }
+    }
+}
+
+
