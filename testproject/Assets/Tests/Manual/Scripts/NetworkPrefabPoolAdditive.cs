d6a85d0d098438235fc02ebb5b1925c1cb20152b
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-10-18 17:34:14 -0500

    Revert "test updates"
    This reverts commit 9d7a18c4266339b1533d41a3299b85d6e3380d20.

--- testproject/Assets/Tests/Manual/Scripts/NetworkPrefabPoolAdditive.cs
+++ testproject/Assets/Tests/Manual/Scripts/NetworkPrefabPoolAdditive.cs
@@ -68,7 +68,7 @@ namespace TestProject.ManualTests
         private void DeregisterCustomPrefabHandler()
         {
             // Register the custom spawn handler?
-            if (EnableHandler && IsSpawned && NetworkManager && NetworkManager.PrefabHandler != null)
+            if (EnableHandler && NetworkManager && NetworkManager.PrefabHandler != null)
             {
                 NetworkManager.PrefabHandler.RemoveHandler(ServerObjectToPool);
                 if (IsClient && m_ObjectToSpawn != null)
