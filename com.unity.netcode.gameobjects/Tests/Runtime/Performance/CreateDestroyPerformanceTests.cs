e83481d9095bb467aedae83b6dc6fd7be6fcced8
Albin Corén <twoten@twoten.io>
2021-10-22 11:37:19 +0200

    Cleanup

--- com.unity.netcode.gameobjects/Tests/Runtime/Performance/CreateDestroyPerformanceTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/Performance/CreateDestroyPerformanceTests.cs
@@ -1,13 +1,13 @@
 using NUnit.Framework;
 using Unity.PerformanceTesting;
+using Unity.PerformanceTesting.Measurements;
 using UnityEngine;
 
 namespace Unity.Netcode.RuntimeTests.Performance
 {
     public class CreateDestroyPerformanceTests
     {
-        [Test, Performance]
-        public void Spawn8NetworkObjectPerformance()
+        private void SpawnXNetworkObjectPerformance(int clientCount)
         {
             var prefab = new GameObject("Object");
             var networkedPrefab = prefab.AddComponent<NetworkObject>();
@@ -16,7 +16,7 @@ namespace Unity.Netcode.RuntimeTests.Performance
             MultiInstanceHelpers.MakeNetworkObjectTestPrefab(networkedPrefab);
 
             // Create multiple NetworkManager instances
-            if (!MultiInstanceHelpers.Create(8, out NetworkManager server, out NetworkManager[] clients, 60))
+            if (!MultiInstanceHelpers.Create(clientCount, out NetworkManager server, out NetworkManager[] clients, 60))
             {
                 Assert.Fail("Failed to create instances");
             }
@@ -58,168 +58,37 @@ namespace Unity.Netcode.RuntimeTests.Performance
                 .Run();
 
             MultiInstanceHelpers.Destroy();
+        }
+
+        [Test, Performance]
+        public void Spawn8NetworkObjectPerformance()
+        {
+            SpawnXNetworkObjectPerformance(8);
         }
 
         [Test, Performance]
         public void Spawn16NetworkObjectPerformance()
         {
-            var prefab = new GameObject("Object");
-            var networkedPrefab = prefab.AddComponent<NetworkObject>();
-
-            // Make it a prefab
-            MultiInstanceHelpers.MakeNetworkObjectTestPrefab(networkedPrefab);
-
-            // Create multiple NetworkManager instances
-            if (!MultiInstanceHelpers.Create(16, out NetworkManager server, out NetworkManager[] clients, 60))
-            {
-                Assert.Fail("Failed to create instances");
-            }
-
-            server.NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-            {
-                Prefab = prefab
-            });
-
-            for (int i = 0; i < clients.Length; i++)
-            {
-                clients[i].NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-                {
-                    Prefab = prefab
-                });
-            }
-
-            Assert.That(MultiInstanceHelpers.Start(true, server, clients));
-
-            NetworkObject no = null;
-
-            Measure.Method(() =>
-                {
-                    no.Spawn();
-                })
-                .SetUp(() =>
-                {
-                    var go = Object.Instantiate(prefab);
-                    no = go.GetComponent<NetworkObject>();
-                    no.NetworkManagerOwner = server;
-                })
-                .CleanUp(() =>
-                {
-                    Object.Destroy(no.gameObject);
-                })
-                .GC()
-                .WarmupCount(5)
-                .MeasurementCount(10)
-                .Run();
-
-            MultiInstanceHelpers.Destroy();
+            SpawnXNetworkObjectPerformance(16);
         }
 
         [Test, Performance]
         public void Spawn32NetworkObjectPerformance()
         {
-            var prefab = new GameObject("Object");
-            var networkedPrefab = prefab.AddComponent<NetworkObject>();
-
-            // Make it a prefab
-            MultiInstanceHelpers.MakeNetworkObjectTestPrefab(networkedPrefab);
-
-            // Create multiple NetworkManager instances
-            if (!MultiInstanceHelpers.Create(32, out NetworkManager server, out NetworkManager[] clients, 60))
-            {
-                Assert.Fail("Failed to create instances");
-            }
-
-            server.NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-            {
-                Prefab = prefab
-            });
-
-            for (int i = 0; i < clients.Length; i++)
-            {
-                clients[i].NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-                {
-                    Prefab = prefab
-                });
-            }
-
-            Assert.That(MultiInstanceHelpers.Start(true, server, clients));
-
-            NetworkObject no = null;
-
-            Measure.Method(() =>
-                {
-                    no.Spawn();
-                })
-                .SetUp(() =>
-                {
-                    var go = Object.Instantiate(prefab);
-                    no = go.GetComponent<NetworkObject>();
-                    no.NetworkManagerOwner = server;
-                })
-                .CleanUp(() =>
-                {
-                    Object.Destroy(no.gameObject);
-                })
-                .GC()
-                .WarmupCount(5)
-                .MeasurementCount(10)
-                .Run();
-
-            MultiInstanceHelpers.Destroy();
+            SpawnXNetworkObjectPerformance(32);
         }
+
+        [Test, Performance]
+        public void Spawn64NetworkObjectPerformance()
+        {
+            SpawnXNetworkObjectPerformance(64);
+        }
+
 
         [Test, Performance]
         public void Spawn128NetworkObjectPerformance()
         {
-            var prefab = new GameObject("Object");
-            var networkedPrefab = prefab.AddComponent<NetworkObject>();
-
-            // Make it a prefab
-            MultiInstanceHelpers.MakeNetworkObjectTestPrefab(networkedPrefab);
-
-            // Create multiple NetworkManager instances
-            if (!MultiInstanceHelpers.Create(128, out NetworkManager server, out NetworkManager[] clients, 60))
-            {
-                Assert.Fail("Failed to create instances");
-            }
-
-            server.NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-            {
-                Prefab = prefab
-            });
-
-            for (int i = 0; i < clients.Length; i++)
-            {
-                clients[i].NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab()
-                {
-                    Prefab = prefab
-                });
-            }
-
-            Assert.That(MultiInstanceHelpers.Start(true, server, clients));
-
-            NetworkObject no = null;
-
-            Measure.Method(() =>
-                {
-                    no.Spawn();
-                })
-                .SetUp(() =>
-                {
-                    var go = Object.Instantiate(prefab);
-                    no = go.GetComponent<NetworkObject>();
-                    no.NetworkManagerOwner = server;
-                })
-                .CleanUp(() =>
-                {
-                    Object.Destroy(no.gameObject);
-                })
-                .GC()
-                .WarmupCount(5)
-                .MeasurementCount(10)
-                .Run();
-
-            MultiInstanceHelpers.Destroy();
+            SpawnXNetworkObjectPerformance(128);
         }
     }
 }
