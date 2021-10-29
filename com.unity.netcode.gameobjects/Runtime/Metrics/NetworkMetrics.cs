dbeb62b03e02453e62519b153c10225b3a30844f
Benoit Doyon <benoit.doyon@unity3d.com>
2021-10-29 10:27:12 -0400

    Refac NetworkMetrics

--- com.unity.netcode.gameobjects/Runtime/Metrics/NetworkMetrics.cs
+++ com.unity.netcode.gameobjects/Runtime/Metrics/NetworkMetrics.cs
@@ -5,14 +5,11 @@ using Unity.Multiplayer.Tools;
 using Unity.Multiplayer.Tools.MetricTypes;
 using Unity.Multiplayer.Tools.NetStats;
 using Unity.Profiling;
-using UnityEngine;
 
 namespace Unity.Netcode
 {
     internal class NetworkMetrics : INetworkMetrics
     {
-        const ulong k_MaxMetricsPerFrame = 1000L;
-
         static Dictionary<uint, string> s_SceneEventTypeNames;
 
         static ProfilerMarker s_FrameDispatch = new ProfilerMarker($"{nameof(NetworkMetrics)}.DispatchFrame");
@@ -36,60 +33,15 @@ namespace Unity.Netcode
             return name;
         }
 
-        private readonly Counter m_TransportBytesSent = new Counter(NetworkMetricTypes.TotalBytesSent.Id)
-        {
-            ShouldResetOnDispatch = true,
-        };
-        private readonly Counter m_TransportBytesReceived = new Counter(NetworkMetricTypes.TotalBytesReceived.Id)
-        {
-            ShouldResetOnDispatch = true,
-        };
-
-        private readonly EventMetric<NetworkMessageEvent> m_NetworkMessageSentEvent = new EventMetric<NetworkMessageEvent>(NetworkMetricTypes.NetworkMessageSent.Id);
-        private readonly EventMetric<NetworkMessageEvent> m_NetworkMessageReceivedEvent = new EventMetric<NetworkMessageEvent>(NetworkMetricTypes.NetworkMessageReceived.Id);
-        private readonly EventMetric<NamedMessageEvent> m_NamedMessageSentEvent = new EventMetric<NamedMessageEvent>(NetworkMetricTypes.NamedMessageSent.Id);
-        private readonly EventMetric<NamedMessageEvent> m_NamedMessageReceivedEvent = new EventMetric<NamedMessageEvent>(NetworkMetricTypes.NamedMessageReceived.Id);
-        private readonly EventMetric<UnnamedMessageEvent> m_UnnamedMessageSentEvent = new EventMetric<UnnamedMessageEvent>(NetworkMetricTypes.UnnamedMessageSent.Id);
-        private readonly EventMetric<UnnamedMessageEvent> m_UnnamedMessageReceivedEvent = new EventMetric<UnnamedMessageEvent>(NetworkMetricTypes.UnnamedMessageReceived.Id);
-        private readonly EventMetric<NetworkVariableEvent> m_NetworkVariableDeltaSentEvent = new EventMetric<NetworkVariableEvent>(NetworkMetricTypes.NetworkVariableDeltaSent.Id);
-        private readonly EventMetric<NetworkVariableEvent> m_NetworkVariableDeltaReceivedEvent = new EventMetric<NetworkVariableEvent>(NetworkMetricTypes.NetworkVariableDeltaReceived.Id);
-        private readonly EventMetric<OwnershipChangeEvent> m_OwnershipChangeSentEvent = new EventMetric<OwnershipChangeEvent>(NetworkMetricTypes.OwnershipChangeSent.Id);
-        private readonly EventMetric<OwnershipChangeEvent> m_OwnershipChangeReceivedEvent = new EventMetric<OwnershipChangeEvent>(NetworkMetricTypes.OwnershipChangeReceived.Id);
-        private readonly EventMetric<ObjectSpawnedEvent> m_ObjectSpawnSentEvent = new EventMetric<ObjectSpawnedEvent>(NetworkMetricTypes.ObjectSpawnedSent.Id);
-        private readonly EventMetric<ObjectSpawnedEvent> m_ObjectSpawnReceivedEvent = new EventMetric<ObjectSpawnedEvent>(NetworkMetricTypes.ObjectSpawnedReceived.Id);
-        private readonly EventMetric<ObjectDestroyedEvent> m_ObjectDestroySentEvent = new EventMetric<ObjectDestroyedEvent>(NetworkMetricTypes.ObjectDestroyedSent.Id);
-        private readonly EventMetric<ObjectDestroyedEvent> m_ObjectDestroyReceivedEvent = new EventMetric<ObjectDestroyedEvent>(NetworkMetricTypes.ObjectDestroyedReceived.Id);
-        private readonly EventMetric<RpcEvent> m_RpcSentEvent = new EventMetric<RpcEvent>(NetworkMetricTypes.RpcSent.Id);
-        private readonly EventMetric<RpcEvent> m_RpcReceivedEvent = new EventMetric<RpcEvent>(NetworkMetricTypes.RpcReceived.Id);
-        private readonly EventMetric<ServerLogEvent> m_ServerLogSentEvent = new EventMetric<ServerLogEvent>(NetworkMetricTypes.ServerLogSent.Id);
-        private readonly EventMetric<ServerLogEvent> m_ServerLogReceivedEvent = new EventMetric<ServerLogEvent>(NetworkMetricTypes.ServerLogReceived.Id);
-        private readonly EventMetric<SceneEventMetric> m_SceneEventSentEvent = new EventMetric<SceneEventMetric>(NetworkMetricTypes.SceneEventSent.Id);
-        private readonly EventMetric<SceneEventMetric> m_SceneEventReceivedEvent = new EventMetric<SceneEventMetric>(NetworkMetricTypes.SceneEventReceived.Id);
-
-        private ulong m_NumberOfMetricsThisFrame;
+        private readonly INetcodeForGameObjectsDispatcher m_Dispatcher;
 
         public NetworkMetrics()
         {
-            Dispatcher = new MetricDispatcherBuilder()
-                .WithCounters(m_TransportBytesSent, m_TransportBytesReceived)
-                .WithMetricEvents(m_NetworkMessageSentEvent, m_NetworkMessageReceivedEvent)
-                .WithMetricEvents(m_NamedMessageSentEvent, m_NamedMessageReceivedEvent)
-                .WithMetricEvents(m_UnnamedMessageSentEvent, m_UnnamedMessageReceivedEvent)
-                .WithMetricEvents(m_NetworkVariableDeltaSentEvent, m_NetworkVariableDeltaReceivedEvent)
-                .WithMetricEvents(m_OwnershipChangeSentEvent, m_OwnershipChangeReceivedEvent)
-                .WithMetricEvents(m_ObjectSpawnSentEvent, m_ObjectSpawnReceivedEvent)
-                .WithMetricEvents(m_ObjectDestroySentEvent, m_ObjectDestroyReceivedEvent)
-                .WithMetricEvents(m_RpcSentEvent, m_RpcReceivedEvent)
-                .WithMetricEvents(m_ServerLogSentEvent, m_ServerLogReceivedEvent)
-                .WithMetricEvents(m_SceneEventSentEvent, m_SceneEventReceivedEvent)
-                .Build();
-
+            m_Dispatcher = new NetcodeForGameObjectsDispatcher();
             Dispatcher.RegisterObserver(NetcodeObserver.Observer);
         }
 
-        internal IMetricDispatcher Dispatcher { get; }
-
-        private bool CanSendMetrics => m_NumberOfMetricsThisFrame < k_MaxMetricsPerFrame;
+        internal IMetricDispatcher Dispatcher => m_Dispatcher.Dispatcher;
 
         public void SetConnectionId(ulong connectionId)
         {
@@ -98,45 +50,27 @@ namespace Unity.Netcode
 
         public void TrackTransportBytesSent(long bytesCount)
         {
-            m_TransportBytesSent.Increment(bytesCount);
+            m_Dispatcher.TrackTransportBytesSent(bytesCount);
         }
 
         public void TrackTransportBytesReceived(long bytesCount)
         {
-            m_TransportBytesReceived.Increment(bytesCount);
+            m_Dispatcher.TrackTransportBytesReceived(bytesCount);
         }
 
         public void TrackNetworkMessageSent(ulong receivedClientId, string messageType, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NetworkMessageSentEvent.Mark(new NetworkMessageEvent(new ConnectionInfo(receivedClientId), messageType, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackNetworkMessageSent(new NetworkMessageEvent(new ConnectionInfo(receivedClientId), messageType, bytesCount));
         }
 
         public void TrackNetworkMessageReceived(ulong senderClientId, string messageType, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NetworkMessageReceivedEvent.Mark(new NetworkMessageEvent(new ConnectionInfo(senderClientId), messageType, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackNetworkMessageReceived(new NetworkMessageEvent(new ConnectionInfo(senderClientId), messageType, bytesCount));
         }
 
         public void TrackNamedMessageSent(ulong receiverClientId, string messageName, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NamedMessageSentEvent.Mark(new NamedMessageEvent(new ConnectionInfo(receiverClientId), messageName, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackNamedMessageSent(new NamedMessageEvent(new ConnectionInfo(receiverClientId), messageName, bytesCount));
         }
 
         public void TrackNamedMessageSent(IReadOnlyCollection<ulong> receiverClientIds, string messageName, long bytesCount)
@@ -149,24 +83,12 @@ namespace Unity.Netcode
 
         public void TrackNamedMessageReceived(ulong senderClientId, string messageName, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NamedMessageReceivedEvent.Mark(new NamedMessageEvent(new ConnectionInfo(senderClientId), messageName, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackNamedMessageReceived(new NamedMessageEvent(new ConnectionInfo(senderClientId), messageName, bytesCount));
         }
 
         public void TrackUnnamedMessageSent(ulong receiverClientId, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_UnnamedMessageSentEvent.Mark(new UnnamedMessageEvent(new ConnectionInfo(receiverClientId), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackUnnamedMessageSent(new UnnamedMessageEvent(new ConnectionInfo(receiverClientId), bytesCount));
         }
 
         public void TrackUnnamedMessageSent(IReadOnlyCollection<ulong> receiverClientIds, long bytesCount)
@@ -179,13 +101,7 @@ namespace Unity.Netcode
 
         public void TrackUnnamedMessageReceived(ulong senderClientId, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_UnnamedMessageReceivedEvent.Mark(new UnnamedMessageEvent(new ConnectionInfo(senderClientId), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackUnnamedMessageReceived(new UnnamedMessageEvent(new ConnectionInfo(senderClientId), bytesCount));
         }
 
         public void TrackNetworkVariableDeltaSent(
@@ -195,19 +111,13 @@ namespace Unity.Netcode
             string networkBehaviourName,
             long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NetworkVariableDeltaSentEvent.Mark(
+            m_Dispatcher.TrackNetworkVariableDeltaSent(
                 new NetworkVariableEvent(
                     new ConnectionInfo(receiverClientId),
                     GetObjectIdentifier(networkObject),
                     variableName,
                     networkBehaviourName,
                     bytesCount));
-            IncrementMetricCount();
         }
 
         public void TrackNetworkVariableDeltaReceived(
@@ -217,86 +127,44 @@ namespace Unity.Netcode
             string networkBehaviourName,
             long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_NetworkVariableDeltaReceivedEvent.Mark(
+            m_Dispatcher.TrackNetworkVariableDeltaReceived(
                 new NetworkVariableEvent(
                     new ConnectionInfo(senderClientId),
                     GetObjectIdentifier(networkObject),
                     variableName,
                     networkBehaviourName,
                     bytesCount));
-            IncrementMetricCount();
         }
 
         public void TrackOwnershipChangeSent(ulong receiverClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_OwnershipChangeSentEvent.Mark(new OwnershipChangeEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackOwnershipChangeSent(new OwnershipChangeEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
         }
 
         public void TrackOwnershipChangeReceived(ulong senderClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_OwnershipChangeReceivedEvent.Mark(new OwnershipChangeEvent(new ConnectionInfo(senderClientId),
+            m_Dispatcher.TrackOwnershipChangeReceived(new OwnershipChangeEvent(new ConnectionInfo(senderClientId),
                 GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
         }
 
         public void TrackObjectSpawnSent(ulong receiverClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ObjectSpawnSentEvent.Mark(new ObjectSpawnedEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackObjectSpawnSent(new ObjectSpawnedEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
         }
 
         public void TrackObjectSpawnReceived(ulong senderClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ObjectSpawnReceivedEvent.Mark(new ObjectSpawnedEvent(new ConnectionInfo(senderClientId), GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackObjectSpawnReceived(new ObjectSpawnedEvent(new ConnectionInfo(senderClientId), GetObjectIdentifier(networkObject), bytesCount));
         }
 
         public void TrackObjectDestroySent(ulong receiverClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ObjectDestroySentEvent.Mark(new ObjectDestroyedEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackObjectDestroySent(new ObjectDestroyedEvent(new ConnectionInfo(receiverClientId), GetObjectIdentifier(networkObject), bytesCount));
         }
 
         public void TrackObjectDestroyReceived(ulong senderClientId, NetworkObject networkObject, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ObjectDestroyReceivedEvent.Mark(new ObjectDestroyedEvent(new ConnectionInfo(senderClientId), GetObjectIdentifier(networkObject), bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackObjectDestroyReceived(new ObjectDestroyedEvent(new ConnectionInfo(senderClientId), GetObjectIdentifier(networkObject), bytesCount));
         }
 
         public void TrackRpcSent(
@@ -306,19 +174,13 @@ namespace Unity.Netcode
             string networkBehaviourName,
             long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_RpcSentEvent.Mark(
+            m_Dispatcher.TrackRpcSent(
                 new RpcEvent(
                     new ConnectionInfo(receiverClientId),
                     GetObjectIdentifier(networkObject),
                     rpcName,
                     networkBehaviourName,
                     bytesCount));
-            IncrementMetricCount();
         }
 
         public void TrackRpcSent(
@@ -341,40 +203,22 @@ namespace Unity.Netcode
             string networkBehaviourName,
             long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_RpcReceivedEvent.Mark(
+            m_Dispatcher.TrackRpcReceived(
                 new RpcEvent(new ConnectionInfo(senderClientId),
                     GetObjectIdentifier(networkObject),
                     rpcName,
                     networkBehaviourName,
                     bytesCount));
-            IncrementMetricCount();
         }
 
         public void TrackServerLogSent(ulong receiverClientId, uint logType, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ServerLogSentEvent.Mark(new ServerLogEvent(new ConnectionInfo(receiverClientId), (Multiplayer.Tools.MetricTypes.LogLevel)logType, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackServerLogSent(new ServerLogEvent(new ConnectionInfo(receiverClientId), (Multiplayer.Tools.MetricTypes.LogLevel)logType, bytesCount));
         }
 
         public void TrackServerLogReceived(ulong senderClientId, uint logType, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_ServerLogReceivedEvent.Mark(new ServerLogEvent(new ConnectionInfo(senderClientId), (Multiplayer.Tools.MetricTypes.LogLevel)logType, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackServerLogReceived(new ServerLogEvent(new ConnectionInfo(senderClientId), (Multiplayer.Tools.MetricTypes.LogLevel)logType, bytesCount));
         }
 
         public void TrackSceneEventSent(IReadOnlyList<ulong> receiverClientIds, uint sceneEventType, string sceneName, long bytesCount)
@@ -387,24 +231,12 @@ namespace Unity.Netcode
 
         public void TrackSceneEventSent(ulong receiverClientId, uint sceneEventType, string sceneName, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_SceneEventSentEvent.Mark(new SceneEventMetric(new ConnectionInfo(receiverClientId), GetSceneEventTypeName(sceneEventType), sceneName, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackSceneEventSent(new SceneEventMetric(new ConnectionInfo(receiverClientId), GetSceneEventTypeName(sceneEventType), sceneName, bytesCount));
         }
 
         public void TrackSceneEventReceived(ulong senderClientId, uint sceneEventType, string sceneName, long bytesCount)
         {
-            if (!CanSendMetrics)
-            {
-                return;
-            }
-
-            m_SceneEventReceivedEvent.Mark(new SceneEventMetric(new ConnectionInfo(senderClientId), GetSceneEventTypeName(sceneEventType), sceneName, bytesCount));
-            IncrementMetricCount();
+            m_Dispatcher.TrackSceneEventReceived(new SceneEventMetric(new ConnectionInfo(senderClientId), GetSceneEventTypeName(sceneEventType), sceneName, bytesCount));
         }
 
         public void DispatchFrame()
@@ -412,12 +244,6 @@ namespace Unity.Netcode
             s_FrameDispatch.Begin();
             Dispatcher.Dispatch();
             s_FrameDispatch.End();
-            m_NumberOfMetricsThisFrame = 0;
-        }
-
-        private void IncrementMetricCount()
-        {
-            m_NumberOfMetricsThisFrame++;
         }
 
         private static NetworkObjectIdentifier GetObjectIdentifier(NetworkObject networkObject)
