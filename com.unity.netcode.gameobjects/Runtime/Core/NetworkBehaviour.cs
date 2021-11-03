d7c69b97ad59e8fd90bf49e8fa0f0a46ce86d6ae
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2021-11-03 10:10:33 -0500

    fix
    Adjusting the logic a bit on getting the NetworkObject associated with a NetworkBehaviour.

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkBehaviour.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkBehaviour.cs
@@ -282,10 +282,13 @@ namespace Unity.Netcode
                     m_NetworkObject = GetComponentInParent<NetworkObject>();
                 }
 
-                if (m_NetworkObject == null && NetworkLog.CurrentLogLevel <= LogLevel.Normal && NetworkManager.Singleton == null ||
-                    NetworkManager.Singleton != null && !NetworkManager.Singleton.ShutdownInProgress)
+                if (m_NetworkObject == null || NetworkManager.Singleton == null ||
+                    (NetworkManager.Singleton != null && !NetworkManager.Singleton.ShutdownInProgress))
                 {
-                    NetworkLog.LogWarning($"Could not get {nameof(NetworkObject)} for the {nameof(NetworkBehaviour)}. Are you missing a {nameof(NetworkObject)} component?");
+                    if (NetworkLog.CurrentLogLevel < LogLevel.Normal)
+                    {
+                        NetworkLog.LogWarning($"Could not get {nameof(NetworkObject)} for the {nameof(NetworkBehaviour)}. Are you missing a {nameof(NetworkObject)} component?");
+                    }
                 }
 
                 return m_NetworkObject;
