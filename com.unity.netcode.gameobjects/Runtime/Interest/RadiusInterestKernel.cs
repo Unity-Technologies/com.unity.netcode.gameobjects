63683cddc286df09ba9456c932e46cfa8d7b34d1
Matt Walsh <matt.walsh@unity3d.com>
2021-11-02 17:48:47 -0700

    missed cleanup

--- com.unity.netcode.gameobjects/Runtime/Interest/RadiusInterestKernel.cs
+++ com.unity.netcode.gameobjects/Runtime/Interest/RadiusInterestKernel.cs
@@ -8,12 +8,7 @@ namespace Unity.Netcode
     public class RadiusInterestKernel : IInterestKernel<NetworkObject>
     {
         public float Radius = 0.0f;
-        private NetworkManager m_NetworkManager;
 
-        public RadiusInterestKernel(NetworkManager nm)
-        {
-            m_NetworkManager = nm;
-        }
         public void QueryFor(NetworkObject clientNetworkObject, NetworkObject obj, HashSet<NetworkObject> results)
         {
             if (Vector3.Distance(obj.transform.position, clientNetworkObject.gameObject.transform.position) <= Radius)
