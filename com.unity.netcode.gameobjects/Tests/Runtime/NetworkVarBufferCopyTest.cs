54f02fd99c00add7a5cea06ae672afdde2e1c8f2
Jaedyn Draper <jaedyn.draper@unity3d.com>
2021-11-08 15:37:16 -0600

    Trying a fix for NetworkVarBufferCopyTest on consoles

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkVarBufferCopyTest.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkVarBufferCopyTest.cs
@@ -25,6 +25,11 @@ namespace Unity.Netcode.RuntimeTests
                 return Dirty;
             }
 
+            public DummyNetVar()
+            {
+
+            }
+
             public override void WriteDelta(FastBufferWriter writer)
             {
                 writer.TryBeginWrite(FastBufferWriter.GetWriteSize(k_DummyValue) + 1);
