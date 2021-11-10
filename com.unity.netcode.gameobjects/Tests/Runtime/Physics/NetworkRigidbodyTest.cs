6b5ba4bd39681248bb1233ae4ce5558701437db4
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2021-11-10 16:38:29 -0500

    test: making asserts more descriptive in TestRigidbodyKinematicEnableDisable

--- com.unity.netcode.gameobjects/Tests/Runtime/Physics/NetworkRigidbodyTest.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/Physics/NetworkRigidbodyTest.cs
@@ -52,29 +52,29 @@ namespace Unity.Netcode.RuntimeTests.Physics
             yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.GetNetworkObjectByRepresentation((x => x.IsPlayerObject && x.OwnerClientId == m_ClientNetworkManagers[0].LocalClientId), m_ClientNetworkManagers[0], clientClientPlayerResult));
             var clientPlayer = clientClientPlayerResult.Result.gameObject;
 
-            Assert.IsNotNull(serverPlayer);
-            Assert.IsNotNull(clientPlayer);
+            Assert.IsNotNull(serverPlayer, "serverPlayer is not null");
+            Assert.IsNotNull(clientPlayer, "clientPlayer is not null");
 
             yield return WaitForFrames(5);
 
             // server rigidbody has authority and should have a kinematic mode of false
-            Assert.True(serverPlayer.GetComponent<Rigidbody>().isKinematic == Kinematic);
-            Assert.AreEqual(RigidbodyInterpolation.Interpolate, serverPlayer.GetComponent<Rigidbody>().interpolation);
+            Assert.True(serverPlayer.GetComponent<Rigidbody>().isKinematic == Kinematic, "serverPlayer kinematic");
+            Assert.AreEqual(RigidbodyInterpolation.Interpolate, serverPlayer.GetComponent<Rigidbody>().interpolation, "server equal interpolate");
 
             // client rigidbody has no authority and should have a kinematic mode of true
-            Assert.True(clientPlayer.GetComponent<Rigidbody>().isKinematic);
-            Assert.AreEqual(RigidbodyInterpolation.None, clientPlayer.GetComponent<Rigidbody>().interpolation);
+            Assert.True(clientPlayer.GetComponent<Rigidbody>().isKinematic, "clientPlayer kinematic");
+            Assert.AreEqual(RigidbodyInterpolation.None, clientPlayer.GetComponent<Rigidbody>().interpolation, "client equal interpolate");
 
             // despawn the server player (but keep it around on the server)
             serverPlayer.GetComponent<NetworkObject>().Despawn(false);
 
             yield return WaitForFrames(5);
 
-            Assert.IsTrue(serverPlayer.GetComponent<Rigidbody>().isKinematic == Kinematic);
+            Assert.IsTrue(serverPlayer.GetComponent<Rigidbody>().isKinematic == Kinematic, "serverPlayer second kinematic");
 
             yield return WaitForFrames(5);
 
-            Assert.IsTrue(clientPlayer == null); // safety check that object is actually despawned.
+            Assert.IsTrue(clientPlayer == null, "clientPlayer being null"); // safety check that object is actually despawned.
         }
 
         public static IEnumerator WaitForFrames(int count)
