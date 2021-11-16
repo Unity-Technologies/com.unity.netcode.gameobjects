bce722caef67190c568115caf69cc994bbab5836
Jaedyn Draper <284434+ShadauxCat@users.noreply.github.com>
2021-11-16 14:18:14 -0600

    fix: Set shutdown flags back to false once shutdown is complete (#1425)
    * fix: Set m_ShuttingDown back to false once shutdown is complete
    
    Credit to @JayPeet, original PR #1423
    
    * Added a test, fixed a related issue with a second flag used in the same process that also needs to be reset.
    
    * standards
    
    * Fix missing meta file.

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1138,6 +1138,7 @@ namespace Unity.Netcode
 
             IsListening = false;
             m_ShuttingDown = false;
+            m_StopProcessingMessages = false;
         }
 
         // INetworkUpdateSystem
