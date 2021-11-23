25725de1fbd69b0c067ebea3a0c71985ea0803b5
zain-mecklai <zain.mecklai@unity3d.com>
2021-11-23 15:45:26 -0500

    try to do 3 machines

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
@@ -8,7 +8,8 @@ using UnityEngine.TestTools;
 namespace Unity.Netcode.MultiprocessRuntimeTests
 {
     [TestFixture(1, new string[] { "default-win:test-win" })]
-    [TestFixture(2, new string[] { "default-win:test-win", "default-win:test-win-2" })]    
+    [TestFixture(2, new string[] { "default-win:test-win", "default-win:test-win-2" })]
+    [TestFixture(3, new string[] { "default-win:test-win", "default-win:test-win-2", "default-win:test-win-3" })]
     public class TestCoordinatorTests : BaseMultiprocessTests
     {
         private int m_WorkerCount;
