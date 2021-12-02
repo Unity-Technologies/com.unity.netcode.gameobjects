ea1ff2cbaac3449d19900a38ffa981e9c9dc1674
Simon Lemay <simon.lemay@unity3d.com>
2021-12-02 13:20:53 -0500

    Allow tests to access internals

---
+++ com.unity.netcode.adapter.utp/Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Unity.Netcode.Adapter.UTP.EditorTests")]
+[assembly: InternalsVisibleTo("Unity.Netcode.Adapter.UTP.RuntimeTests")]
