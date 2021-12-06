20413ad80b56e1e61bf4f14b930abb9396f228e2
M. Fatih MAR <mfatihmar@gmail.com>
2021-12-06 17:45:06 +0000

    do less

--- dotnet-tools/netcode.standards/Program.cs
+++ dotnet-tools/netcode.standards/Program.cs
@@ -32,7 +32,7 @@ internal static class Program
 
         foreach (var file in Directory.GetFiles(project, pattern))
         {
-            var procInfo = new ProcessStartInfo("dotnet") { RedirectStandardOutput = true };
+            var procInfo = new ProcessStartInfo("dotnet");
 
             procInfo.Arguments = check
                 ? $"format {file} whitespace --no-restore --verify-no-changes --verbosity {verbosity}"
