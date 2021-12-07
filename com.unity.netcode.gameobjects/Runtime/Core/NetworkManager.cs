1e5e79d492b535accd7106003fc07d8fed98dc9d
Jaedyn Draper <jaedyn.draper@unity3d.com>
2021-12-07 11:22:42 -0600

    fix: error when serializing ConnectionApprovalMessage with scene management disabled when one or more objects is hidden via the CheckObjectVisibility delegate

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1655,7 +1655,6 @@ namespace Unity.Netcode
                     {
                         if (SpawnManager.SpawnedObjectsList.Count != 0)
                         {
-                            message.SceneObjectCount = SpawnManager.SpawnedObjectsList.Count;
                             message.SpawnedObjectsList = SpawnManager.SpawnedObjectsList;
                         }
                     }
