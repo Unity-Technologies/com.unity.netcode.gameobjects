5f045f4c9b925187518755101680761aba5fea2c
zain-mecklai <zain.mecklai@unity3d.com>
2021-12-10 13:11:16 -0500

    switch to mac client

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
@@ -8,7 +8,7 @@ using UnityEngine.TestTools;
 namespace Unity.Netcode.MultiprocessRuntimeTests
 {
     [TestFixture(1, new string[] { "default-win:test-win" })]
-    [TestFixture(2, new string[] { "default-win:test-win", "default-win:test-win-2" })]
+    [TestFixture(2, new string[] { "default-win:test-win", "default-mac:test-mac-2" })]
     public class TestCoordinatorTests : BaseMultiprocessTests
     {
         private int m_WorkerCount;
