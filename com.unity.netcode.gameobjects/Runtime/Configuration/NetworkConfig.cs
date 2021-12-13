4938a0d4c4326da82dec7f38bfd0817ffb2bb65d
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2021-12-13 14:32:38 -0500

    Removing the config entry NetworkManager.NetworkConfig.UseSnapshotSpawn. It is True, always, now.

--- com.unity.netcode.gameobjects/Runtime/Configuration/NetworkConfig.cs
+++ com.unity.netcode.gameobjects/Runtime/Configuration/NetworkConfig.cs
@@ -142,10 +142,7 @@ namespace Unity.Netcode
         /// Whether or not to enable Snapshot System for variable updates. Not supported in this version.
         /// </summary>
         public bool UseSnapshotDelta { get; internal set; } = false;
-        /// <summary>
-        /// Whether or not to enable Snapshot System for spawn and despawn commands. Not supported in this version.
-        /// </summary>
-        public bool UseSnapshotSpawn { get; internal set; } = true;
+
         /// <summary>
         /// When Snapshot System spawn is enabled: max size of Snapshot Messages. Meant to fit MTU.
         /// </summary>
