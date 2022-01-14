62dffcf82ab96721307a35bfbb76df861d4a3692
zain-mecklai <zain.mecklai@unity3d.com>
2022-01-14 13:24:58 -0500

    Launch the worker as client

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/BokkenMachine.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/BokkenMachine.cs
@@ -272,7 +272,7 @@ namespace Unity.Netcode.MultiprocessRuntimeTests
                 LogPath = @"C:\users\bokken\.multiprocess\" + $"logfile-mp-{DateTimeOffset.Now.ToUnixTimeSeconds()}.log";
                 string s = $" --command exec " +
                     $"--input-path {PathToJson} " +
-                    $"--remote-command \"com.unity.netcode.gameobjects\\testproject\\Builds\\MultiprocessTests\\MultiprocessTestPlayer.exe -isWorker -logFile {LogPath} -popupwindow -screen-width 100 -screen-height 100 -p 3076 -ip {ip}\"";
+                    $"--remote-command \"com.unity.netcode.gameobjects\\testproject\\Builds\\MultiprocessTests\\MultiprocessTestPlayer.exe -isWorker -m client -logFile {LogPath} -popupwindow -screen-width 100 -screen-height 100 -p 3076 -ip {ip}\"";
                 MultiprocessLogger.Log(s);
                 return s;
             }
