0d392b827bbb686e000700e85ae2ef8a200dc0ae
zain-mecklai <zain.mecklai@unity3d.com>
2022-01-14 23:21:54 -0500

    Run with only windows machines

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinatorTests.cs
@@ -7,7 +7,7 @@ using UnityEngine.TestTools;
 namespace Unity.Netcode.MultiprocessRuntimeTests
 {
     [TestFixture(1, new string[] { "default-win:test-win" })]
-    [TestFixture(3, new string[] { "default-win:test-win", "default-mac:test-mac-2", "default-win:test-win-2" })]
+    [TestFixture(2, new string[] { "default-win:test-win", "default-win:test-win-2" })]
     public class TestCoordinatorTests : BaseMultiprocessTests
     {
         private int m_WorkerCount;
