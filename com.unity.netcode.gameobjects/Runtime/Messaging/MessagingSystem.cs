6306b54918a7b6ac036a77133e24ab5ba888be2a
kaen <kaencastszk@gmail.com>
2022-01-15 19:00:00 -0800

    cast message size as uint to fix very large messages (fixes #1591)

--- com.unity.netcode.gameobjects/Runtime/Messaging/MessagingSystem.cs
+++ com.unity.netcode.gameobjects/Runtime/Messaging/MessagingSystem.cs
@@ -351,7 +351,7 @@ namespace Unity.Netcode
 
             var header = new MessageHeader
             {
-                MessageSize = (ushort)tmpSerializer.Length,
+                MessageSize = (uint)tmpSerializer.Length,
                 MessageType = m_MessageTypes[typeof(TMessageType)],
             };
             BytePacker.WriteValueBitPacked(headerSerializer, header.MessageType);
