e7e19b8fab75870885a98bd467accb9d44613ac9
Jaedyn Draper <jaedyn.draper@unity3d.com>
2022-01-18 16:51:52 -0600

    Fix: Server was able to be mapped as both ID 0 and ID 1
    This meant the first client to connect could sometimes see messages from the server as coming from itself, and could successfully send to its own LocalClientId (even though the message would go to the server). Other clients would be able to send and receive from id 1, but that would also be the server.

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1336,7 +1336,20 @@ namespace Unity.Netcode
                     s_TransportConnect.Begin();
 #endif
 
-                    clientId = m_NextClientId++;
+                    // Assumptions:
+                    // - When server receives a connection, it *must be* a client
+                    // - When client receives one, it *must be* the server
+                    // Client's can't connect to or talk to other clients.
+                    // Server is a sentinel so only one exists, if we are server, we can't be
+                    // connecting to it.
+                    if (IsServer)
+                    {
+                        clientId = m_NextClientId++;
+                    }
+                    else
+                    {
+                        clientId = ServerClientId;
+                    }
                     m_ClientIdToTransportIdMap[clientId] = transportId;
                     m_TransportIdToClientIdMap[transportId] = clientId;
 
@@ -1439,6 +1452,12 @@ namespace Unity.Netcode
                 }
                 return MessagingSystem.SendMessage(ref message, delivery, nonServerIds, newIdx);
             }
+            // else
+            if(clientIds.Count != 1 || clientIds[0] != ServerClientId)
+            {
+                throw new ArgumentException($"Clients may only send messages to {nameof(ServerClientId)}");
+            }
+
             return MessagingSystem.SendMessage(ref message, delivery, clientIds);
         }
 
@@ -1467,6 +1486,11 @@ namespace Unity.Netcode
                 }
                 return MessagingSystem.SendMessage(ref message, delivery, nonServerIds, newIdx);
             }
+            // else
+            if(numClientIds != 1 || clientIds[0] != ServerClientId)
+            {
+                throw new ArgumentException($"Clients may only send messages to {nameof(ServerClientId)}");
+            }
 
             return MessagingSystem.SendMessage(ref message, delivery, clientIds, numClientIds);
         }
@@ -1484,6 +1508,11 @@ namespace Unity.Netcode
             if (IsServer && clientId == ServerClientId)
             {
                 return 0;
+            }
+
+            if (!IsServer && clientId != ServerClientId)
+            {
+                throw new ArgumentException($"Clients may only send messages to {nameof(ServerClientId)}");
             }
             return MessagingSystem.SendMessage(ref message, delivery, clientId);
         }
