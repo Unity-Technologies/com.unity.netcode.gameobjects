3bcb7e7abed10482aa493dc361898dd05b8f5016
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-02-07 16:25:26 -0600

    fix
    Had to create a place holder class to assure the Unity.Netcode.TestHelpers existed in stand alone test runner builds.

---
+++ com.unity.netcode.gameobjects/Tests/TestHelpers/PlaceHolderForNameSpace.cs
@@ -0,0 +1,11 @@
+namespace Unity.Netcode.TestHelpers
+{
+    /// <summary>
+    /// This is a placeholder to make sure the namespace is created in a test runner stand alone build
+    /// TODO: Remove this once all helper methods are migrated over to this assembly
+    /// </summary>
+    public class PlaceHolderForNameSpace
+    {
+
+    }
+}
