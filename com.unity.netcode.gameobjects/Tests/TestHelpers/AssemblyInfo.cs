d85a63601a589f19c47f88f9bb87d547eee5c256
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-02-07 17:26:57 -0600

    test-fix
    At this point I am just sort of bumping through figuring out where the dependency issue is failing.

---
+++ com.unity.netcode.gameobjects/Tests/TestHelpers/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("TestProject.EditorTests")]
+[assembly: InternalsVisibleTo("TestProject.EditorTests")]
+[assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
+[assembly: InternalsVisibleTo("Unity.Netcode.UTP.EditorTests")]
+[assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
+#endif
+[assembly: InternalsVisibleTo("TestProject.ToolsIntegration.RuntimeTests")]
+[assembly: InternalsVisibleTo("Unity.Netcode.UTP.RuntimeTests")]
+
+
+
+
+
