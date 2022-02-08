7f5021cda238c67d0404844862685190f3402f33
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-02-07 20:03:56 -0600

    update
    one more...*sigh*

--- com.unity.netcode.gameobjects/Tests/TestHelpers/AssemblyInfo.cs
+++ com.unity.netcode.gameobjects/Tests/TestHelpers/AssemblyInfo.cs
@@ -1,12 +1,14 @@
 using System.Runtime.CompilerServices;
 #if UNITY_EDITOR
+[assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
+[assembly: InternalsVisibleTo("Unity.Netcode.EditorTests")]
 [assembly: InternalsVisibleTo("TestProject.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.UTP.EditorTests")]
-[assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
 #endif
 [assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
 [assembly: InternalsVisibleTo("TestProject.ToolsIntegration.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.UTP.RuntimeTests")]
+[assembly: InternalsVisibleTo("Unity.Netcode.RuntimeTests")]
 
 
 
