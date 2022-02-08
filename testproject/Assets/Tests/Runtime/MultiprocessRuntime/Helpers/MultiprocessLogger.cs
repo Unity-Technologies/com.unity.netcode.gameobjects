9fffcc9ab22ea77ad79ee72e8a453bd46e0009a4
zain-mecklai <zain.mecklai@unity3d.com>
2022-02-08 01:06:12 -0500

    Add remote logging capability to Multiprocess tests

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/MultiprocessLogger.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/Helpers/MultiprocessLogger.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using System;
+using System.Diagnostics;
+using System.Net.Http;
 using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Unity.Netcode.MultiprocessRuntimeTests
 {
     public class MultiprocessLogger
     {
         private static Logger s_Logger;
-
         static MultiprocessLogger() => s_Logger = new Logger(logHandler: new MultiprocessLogHandler());
-
         public static void Log(string msg)
         {
             s_Logger.Log(msg);
@@ -17,28 +21,113 @@ namespace Unity.Netcode.MultiprocessRuntimeTests
 
         public static void LogError(string msg)
         {
-            s_Logger.LogError("", msg);
+            s_Logger.LogError("ERROR " + msg, msg);
         }
 
         public static void LogWarning(string msg)
         {
-            s_Logger.LogWarning("", msg);
+            Log(msg);
+            s_Logger.LogWarning("WTAG " + msg, msg);
         }
     }
 
     public class MultiprocessLogHandler : ILogHandler
     {
+        private static HttpClient s_HttpClient = new HttpClient();
+        private static List<Task> s_AllTasks;
+        public static long JobId;
+        private static readonly object k_Tasklock = new object();
+        private static long s_EventIdCounter;
+
+        static MultiprocessLogHandler()
+        {
+            s_AllTasks = new List<Task>();
+            string sJobId = Environment.GetEnvironmentVariable("YAMATO_JOB_ID");
+            if (!long.TryParse(sJobId, out JobId))
+            {
+                JobId = -2;
+            }
+            s_EventIdCounter = 0;
+        }
+
+        public static string Flush()
+        {
+            bool interrupted = false;
+            var stopWatch = Stopwatch.StartNew();
+            lock (k_Tasklock)
+            {
+                foreach (var task in s_AllTasks)
+                {
+                    task.Wait();
+                    if (stopWatch.ElapsedMilliseconds > 20000)
+                    {
+                        interrupted = true;
+                        break;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            if (interrupted)
+            {
+                return $"Flush Logs took : {stopWatch.Elapsed} ticks: {stopWatch.ElapsedTicks} but was interrupted due to timeout";
+            }
+            return $"Flush Logs took : {stopWatch.Elapsed} ticks: {stopWatch.ElapsedTicks} ";
+        }
+
+        public static string ReportQueue()
+        {
+            int canceledCount = 0;
+            int totalCount = s_AllTasks.Count;
+            int ranToCompletionCount = 0;
+            int runningCount = 0;
+            int waitingForActivation = 0;
+            int waitingToRun = 0;
+            var stopWatch = Stopwatch.StartNew();
+            lock (k_Tasklock)
+            {
+                foreach (var task in s_AllTasks)
+                {
+                    if (task.Status == TaskStatus.Canceled)
+                    {
+                        canceledCount++;
+                    }
+                    else if (task.Status == TaskStatus.RanToCompletion)
+                    {
+                        ranToCompletionCount++;
+                    }
+                    else if (task.Status == TaskStatus.Running)
+                    {
+                        runningCount++;
+                    }
+                    else if (task.Status == TaskStatus.WaitingForActivation)
+                    {
+                        waitingForActivation++;
+                    }
+                    else if (task.Status == TaskStatus.WaitingToRun)
+                    {
+                        waitingToRun++;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            string msg = $"AllTasks.Count {totalCount} canceled: {canceledCount} completed: {ranToCompletionCount} running: {runningCount} waitingToRun: {waitingToRun} waitingForActivation: {waitingForActivation}";
+            return msg;
+        }
+
         public void LogException(Exception exception, UnityEngine.Object context)
         {
-            Debug.unityLogger.LogException(exception, context);
+            UnityEngine.Debug.unityLogger.LogException(exception, context);
         }
 
         public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args)
         {
             string testName = null;
+            string testClass = " - c -";
             try
             {
                 testName = TestContext.CurrentContext.Test.Name;
+                testClass = TestContext.CurrentContext.Test.ClassName;
+
             }
             catch (Exception)
             {
@@ -50,7 +139,125 @@ namespace Unity.Netcode.MultiprocessRuntimeTests
                 testName = "unknown";
             }
 
-            Debug.unityLogger.logHandler.LogFormat(logType, context, $"MPLOG({DateTime.Now:T}) : {testName} : {format}", args);
+            var st = new StackTrace(true);
+
+            string methods = "";
+
+            int maxFrame = 8;
+            for (int i = 3; i < st.FrameCount; i++)
+            {
+                string methodName = st.GetFrame(i).GetMethod().Name;
+                if (methodName.Contains("MoveNext") || methodName.Contains("Invoke"))
+                {
+                    maxFrame++;
+                }
+                else
+                {
+                    methods += " : " + methodName + "." + i;
+                }
+                if (i > maxFrame)
+                {
+                    break;
+                }
+            }
+
+            UnityEngine.Debug.LogFormat(logType, LogOption.NoStacktrace, context, $"MPLOG ({DateTime.Now:T}) : {methods} : {testName} : {format}", args);
+            var webLog = new WebLog();
+            webLog.Message = $"{testName} {args[0].ToString()}";
+            if (webLog.Message.Length > 1000)
+            {
+                webLog.Message = webLog.Message.Substring(0, 999);
+            }
+            webLog.ReferenceId = JobId;
+            webLog.EventId = s_EventIdCounter++;
+            webLog.TestMethod = testName;
+            webLog.TestClass = testClass;
+            webLog.ClientEventDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
+            // string json = JsonUtility.ToJson(webLog);
+            var cancelAfterDelay = new CancellationTokenSource(TimeSpan.FromSeconds(60));
+            Task t = PostBasicAsync(webLog, cancelAfterDelay.Token);
+            lock (k_Tasklock)
+            {
+                s_AllTasks.Add(t);
+            }
+        }
+
+        private static async Task PostBasicAsync(WebLog content, CancellationToken cancellationToken)
+        {
+            using var client = new HttpClient();
+            // TODO: Make this endpoint configurable and share this code across testproject
+            using var request = new HttpRequestMessage(HttpMethod.Post, "https://multiprocess-log-event-manager.cds.internal.unity3d.com/api/MultiprocessLogEvent");
+            var json = JsonUtility.ToJson(content);
+            using var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+            request.Content = stringContent;
+            var logginMetric = LoggingMetric.StartNew();
+
+            using var response = await client
+                .SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+                .ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // Task<string> responseContentTask = response.Content.ReadAsStringAsync();
+                // await responseContentTask;
+                //MultiprocessLogger.LogWarning($"POST called failed with {response.StatusCode} and message is {responseContentTask.Result}");
+                MultiprocessLogger.LogWarning($"POST called failed with {response.StatusCode}");
+            }
+            logginMetric.Stop(content, response);
+        }
+    }
+
+    public class LoggingMetric
+    {
+        public static List<LoggingMetric> LoggingMetricsDataArray;
+
+        public Stopwatch Stopwatch;
+
+        static LoggingMetric()
+        {
+            LoggingMetricsDataArray = new List<LoggingMetric>();
+        }
+
+        public LoggingMetric()
+        {
+            Stopwatch = Stopwatch.StartNew();
+        }
+
+        public static LoggingMetric StartNew()
+        {
+            var lm = new LoggingMetric();
+            LoggingMetricsDataArray.Add(lm);
+            return lm;
+        }
+
+        public void Stop(WebLog content, HttpResponseMessage responseMessage)
+        {
+            Stopwatch.Stop();
+            if (Stopwatch.ElapsedMilliseconds > 5000)
+            {
+                MultiprocessLogger.Log($"POST call took {Stopwatch.ElapsedMilliseconds} with response {responseMessage.StatusCode} for message: {content.EventId}");
+            }
+        }
+    }
+
+    [Serializable]
+    public class WebLog
+    {
+        public string Message;
+        public long ReferenceId;
+        public string TestMethod;
+        public string TestClass;
+        public string ClientEventDate;
+        public long EventId;
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + Message;
+        }
+
+        public WebLog()
+        {
+            Message = "Default message from constructor";
         }
     }
 }
