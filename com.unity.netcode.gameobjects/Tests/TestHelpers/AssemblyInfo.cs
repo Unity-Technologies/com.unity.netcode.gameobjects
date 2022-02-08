82948aebc1c95f84e109912457772f197d526410
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-02-07 19:51:17 -0600

    fix
    Fixing some internals issues.

--- com.unity.netcode.gameobjects/Tests/TestHelpers/AssemblyInfo.cs
+++ com.unity.netcode.gameobjects/Tests/TestHelpers/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 using System.Runtime.CompilerServices;
 #if UNITY_EDITOR
 [assembly: InternalsVisibleTo("TestProject.EditorTests")]
-[assembly: InternalsVisibleTo("TestProject.EditorTests")]
-[assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.UTP.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
 #endif
+[assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
 [assembly: InternalsVisibleTo("TestProject.ToolsIntegration.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.UTP.RuntimeTests")]
 
