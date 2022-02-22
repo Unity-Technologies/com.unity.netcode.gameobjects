a2ed0fd157cb266926df0695aa1c9a2c5c36ba54
Matt Walsh <matt.walsh@unity3d.com>
2022-02-22 11:30:22 -0800

    more tests

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkAnimator/NetworkAnimatorTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkAnimator/NetworkAnimatorTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Unity.Netcode.Components;
 using Unity.Netcode.RuntimeTests;
@@ -10,10 +11,16 @@ using UnityEngine.UI;
 
 namespace Unity.Netcode.RuntimeTest
 {
-    public class AnimatorTests : BaseMultiInstanceTest
+    public class NetworkAnimatorTests : BaseMultiInstanceTest
     {
         protected override int NbClients => 1;
         private GameObject m_TestPrefab;
+
+        private GameObject m_PlayerOnServer;
+        private GameObject m_PlayerOnClient;
+
+        private Animator m_PlayerOnServerAnimator;
+        private Animator m_PlayerOnClientAnimator;
 
         [UnitySetUp]
         public override IEnumerator Setup()
@@ -21,6 +28,8 @@ namespace Unity.Netcode.RuntimeTest
             yield return StartSomeClientsAndServerWithPlayers(useHost: true, nbClients: NbClients,
                 updatePlayerPrefab: playerPrefab =>
                 {
+                // ideally, we would build up the AnimatorController entirely in code and not need an asset,
+                //  but after some attempts this doesn't seem readily doable.  Instead, we load a controller
                     AnimatorController controller = Resources.Load("TestAnimatorController") as AnimatorController;
                     var animator = playerPrefab.AddComponent<Animator>();
                     animator.runtimeAnimatorController = controller;
@@ -28,42 +37,103 @@ namespace Unity.Netcode.RuntimeTest
                     var theAnimator = playerPrefab.AddComponent<NetworkAnimator>();
                     theAnimator.Animator = animator;
                 });
+
+            // This is the *SERVER VERSION* of the *CLIENT PLAYER*
+            var serverClientPlayerResult = new MultiInstanceHelpers.CoroutineResultWrapper<NetworkObject>();
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.GetNetworkObjectByRepresentation((x => x.IsPlayerObject && x.OwnerClientId == m_ClientNetworkManagers[0].LocalClientId), m_ServerNetworkManager, serverClientPlayerResult));
+            m_PlayerOnServer = serverClientPlayerResult.Result.gameObject;
+            m_PlayerOnServerAnimator = m_PlayerOnServer.GetComponent<Animator>();
+
+            // This is the *CLIENT VERSION* of the *CLIENT PLAYER*
+            var clientClientPlayerResult = new MultiInstanceHelpers.CoroutineResultWrapper<NetworkObject>();
+            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.GetNetworkObjectByRepresentation((x => x.IsPlayerObject && x.OwnerClientId == m_ClientNetworkManagers[0].LocalClientId), m_ClientNetworkManagers[0], clientClientPlayerResult));
+            m_PlayerOnClient = clientClientPlayerResult.Result.gameObject;
+            m_PlayerOnClientAnimator = m_PlayerOnClient.GetComponent<Animator>();
+        }
+
+        private bool HasClip(Animator animator, string clipName)
+        {
+            List<AnimatorClipInfo> clips = new List<AnimatorClipInfo>();
+            animator.GetCurrentAnimatorClipInfo(0, clips);
+            foreach (var clip in clips)
+            {
+                if (clip.clip.name == clipName)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         [UnityTest]
         public IEnumerator AnimationStateSyncTest()
         {
-            // This is the *SERVER VERSION* of the *CLIENT PLAYER*
-            var serverClientPlayerResult = new MultiInstanceHelpers.CoroutineResultWrapper<NetworkObject>();
-            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.GetNetworkObjectByRepresentation((x => x.IsPlayerObject && x.OwnerClientId == m_ClientNetworkManagers[0].LocalClientId), m_ServerNetworkManager, serverClientPlayerResult));
-            var serverPlayer = serverClientPlayerResult.Result.gameObject;
-            var serverAnimator =  serverPlayer.GetComponent<Animator>();
+            // check that we have started in the default state
+            Assert.True(m_PlayerOnServerAnimator.GetCurrentAnimatorStateInfo(0).IsName("DefaultState"));
+            Assert.True(m_PlayerOnClientAnimator.GetCurrentAnimatorStateInfo(0).IsName("DefaultState"));
 
-            // This is the *CLIENT VERSION* of the *CLIENT PLAYER*
-            var clientClientPlayerResult = new MultiInstanceHelpers.CoroutineResultWrapper<NetworkObject>();
-            yield return MultiInstanceHelpers.Run(MultiInstanceHelpers.GetNetworkObjectByRepresentation((x => x.IsPlayerObject && x.OwnerClientId == m_ClientNetworkManagers[0].LocalClientId), m_ClientNetworkManagers[0], clientClientPlayerResult));
-            var clientPlayer = clientClientPlayerResult.Result.gameObject;
-            var clientAnimator = clientPlayer.GetComponent<Animator>();
-
-            var serverAnimatorStateInfo = serverAnimator.GetCurrentAnimatorStateInfo(0);
-            var clientAnimatorStateInfo = clientAnimator.GetCurrentAnimatorStateInfo(0);
-
-            // we start in the default state
-            Assert.True(serverAnimatorStateInfo.IsName("DefaultState"));
-            Assert.True(clientAnimatorStateInfo.IsName("DefaultState"));
-
-            serverAnimator.SetBool("AlphaParameter", true);
-
-            // (ugh)
-            yield return new WaitForSeconds(4.0f);
+            // cause a change to the AlphaState state by setting AlphaParameter, which is
+            //  the variable bound to the transition from default to AlphaState (see the TestAnimatorController asset)
+            m_PlayerOnServerAnimator.SetBool("AlphaParameter", true);
 
             // ...and now we should be in the AlphaState having triggered the AlphaParameter
-            serverAnimatorStateInfo = serverAnimator.GetCurrentAnimatorStateInfo(0);
-            clientAnimatorStateInfo = clientAnimator.GetCurrentAnimatorStateInfo(0);
-            yield return WaitForConditionOrTimeOut(() => serverAnimatorStateInfo.IsName("AlphaState"));
-            Assert.True(serverAnimatorStateInfo.IsName("AlphaState"));
-            yield return WaitForConditionOrTimeOut(() => clientAnimatorStateInfo.IsName("AlphaState"));
-            Assert.True(clientAnimatorStateInfo.IsName("AlphaState"));
+            yield return WaitForConditionOrTimeOut(() =>
+                m_PlayerOnServerAnimator.GetCurrentAnimatorStateInfo(0).IsName("AlphaState"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Server failed to reach its animation state");
+
+            // ...and now the client should also have sync'd and arrived at the correct state
+            yield return WaitForConditionOrTimeOut(() =>
+                m_PlayerOnClientAnimator.GetCurrentAnimatorStateInfo(0).IsName("AlphaState"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Client failed to sync its animation state from the server");
+        }
+
+        [UnityTest]
+        public IEnumerator AnimationStateSyncTriggerTest()
+        {
+            // check that we have started in the default state
+            Assert.True(m_PlayerOnServerAnimator.GetCurrentAnimatorStateInfo(0).IsName("DefaultState"));
+            Assert.True(m_PlayerOnClientAnimator.GetCurrentAnimatorStateInfo(0).IsName("DefaultState"));
+
+            // cause a change to the AlphaState state by setting AlphaParameter, which is
+            //  the variable bound to the transition from default to AlphaState (see the TestAnimatorController asset)
+            m_PlayerOnServer.GetComponent<NetworkAnimator>().SetTrigger("TestTrigger");
+
+            // ...and now we should be in the AlphaState having triggered the AlphaParameter
+            yield return WaitForConditionOrTimeOut(() =>
+                m_PlayerOnServerAnimator.GetCurrentAnimatorStateInfo(0).IsName("TriggeredState"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Server failed to reach its animation state via trigger");
+
+            // ...and now the client should also have sync'd and arrived at the correct state
+            yield return WaitForConditionOrTimeOut(() =>
+                m_PlayerOnClientAnimator.GetCurrentAnimatorStateInfo(0).IsName("TriggeredState"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Client failed to sync its animation state from the server via trigger");
+        }
+
+        [UnityTest]
+        public IEnumerator AnimationStateSyncTestWithOverride()
+        {
+            // set up the animation override controller
+            AnimatorOverrideController overrideController = Resources.Load("TestAnimatorOverrideController") as AnimatorOverrideController;
+            m_PlayerOnServer.GetComponent<Animator>().runtimeAnimatorController = overrideController;
+            m_PlayerOnClient.GetComponent<Animator>().runtimeAnimatorController = overrideController;
+
+            // in our default state, we should see the OverrideDefaultAnimation clip
+            Assert.True(HasClip(m_PlayerOnServerAnimator, "OverrideDefaultAnimation"));
+            Assert.True(HasClip(m_PlayerOnClientAnimator, "OverrideDefaultAnimation"));
+
+            // cause a change to the AlphaState state by setting AlphaParameter, which is
+            //  the variable bound to the transition from default to AlphaState (see the TestAnimatorController asset)
+            m_PlayerOnServerAnimator.SetBool("AlphaParameter", true);
+
+            // ...and now we should be in the AlphaState having triggered the AlphaParameter
+            yield return WaitForConditionOrTimeOut(() =>
+                HasClip(m_PlayerOnServerAnimator, "OverrideAlphaAnimation"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Server failed to reach its overriden animation state");
+
+            // ...and now the client should also have sync'd and arrived at the correct state
+            yield return WaitForConditionOrTimeOut(() =>
+                HasClip(m_PlayerOnServerAnimator, "OverrideAlphaAnimation"));
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, "Client failed to reach its overriden animation state");
         }
     }
 }
