d66437b7852cf65ed5a777baf4a58693a1c9064f
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-02-25 13:15:14 -0600

    update
    minimal conversion of TransformInterpolationTests to new NetcodeIntegrationTest.

--- com.unity.netcode.gameobjects/Tests/Runtime/TransformInterpolationTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/TransformInterpolationTests.cs
@@ -1,10 +1,11 @@
-// This integration test has too many issues with it and shouldn't have been merged into develop
-#if DISABLE_UNTIL_FIXED
+using System;
 using System.Collections;
 using Unity.Netcode.Components;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using Unity.Netcode.TestHelpers.Runtime;
+
 
 namespace Unity.Netcode.RuntimeTests
 {
@@ -60,66 +61,42 @@ namespace Unity.Netcode.RuntimeTests
 
         protected override void OnServerAndClientsCreated()
         {
-            m_PrefabToSpawn = CreateNetworkObjectPrefab("TransformTestObject");
+            m_PrefabToSpawn = CreateNetworkObjectPrefab("InterpTestObject");
             m_PrefabToSpawn.AddComponent<NetworkTransform>();
             m_PrefabToSpawn.AddComponent<TransformInterpolationObject>();
-            base.OnServerAndClientsCreated();
         }
 
-        // TODO: This is a temporary comment of these methods to get this test working in this branch.
-        // TBD how/if this gets updated.
+        private IEnumerator RefreshNetworkObjects()
+        {
+            var clientId = m_ClientNetworkManagers[0].LocalClientId;
+            yield return WaitForConditionOrTimeOut(() => s_GlobalNetworkObjects.ContainsKey(clientId) &&
+            s_GlobalNetworkObjects[clientId].ContainsKey(m_AsNetworkObject.NetworkObjectId));
 
-        //private IEnumerator RefreshNetworkObjects()
-        //{
-        //    var serverClientPlayerResult = new MultiInstanceHelpers.CoroutineResultWrapper<NetworkObject>();
-        //    yield return MultiInstanceHelpers.Run(
-        //        MultiInstanceHelpers.GetNetworkObjectByRepresentation(
-        //            x => x.NetworkObjectId == m_AsNetworkObject.NetworkObjectId,
-        //            m_ClientNetworkManagers[0],
-        //            serverClientPlayerResult));
-        //    m_SpawnedObjectOnClient = serverClientPlayerResult.Result;
-
-        //    // make sure the objects are set with the right network manager
-        //    m_SpawnedObjectOnClient.NetworkManagerOwner = m_ClientNetworkManagers[0];
-        //}
-
-        //public GameObject PreparePrefab(Type type)
-        //{
-        //    var prefabToSpawn = new GameObject();
-        //    prefabToSpawn.AddComponent(type);
-        //    prefabToSpawn.AddComponent<NetworkTransform>();
-        //    var networkObjectPrefab = prefabToSpawn.AddComponent<NetworkObject>();
-        //    MultiInstanceHelpers.MakeNetworkObjectTestPrefab(networkObjectPrefab);
-        //    m_ServerNetworkManager.NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab() { Prefab = prefabToSpawn });
-        //    foreach (var clientNetworkManager in m_ClientNetworkManagers)
-        //    {
-        //        clientNetworkManager.NetworkConfig.NetworkPrefabs.Add(new NetworkPrefab() { Prefab = prefabToSpawn });
-        //    }
-        //    return prefabToSpawn;
-        //}
+            Assert.False(s_GloabalTimeOutHelper.TimedOut, $"Timed out waiting for client side {nameof(NetworkObject)} ID of {m_AsNetworkObject.NetworkObjectId}");
+            m_SpawnedObjectOnClient = s_GlobalNetworkObjects[clientId][m_AsNetworkObject.NetworkObjectId];
+            // make sure the objects are set with the right network manager
+            m_SpawnedObjectOnClient.NetworkManagerOwner = m_ClientNetworkManagers[0];
+        }
 
         [UnityTest]
-
         public IEnumerator TransformInterpolationTest()
         {
             m_ClientId0 = m_ClientNetworkManagers[0].LocalClientId;
 
             // create an object
-            var baseObject = SpawnObject(m_PrefabToSpawn, m_ServerNetworkManager);
-            var spawnedObject = SpawnObject(m_PrefabToSpawn, m_ClientNetworkManagers[0]);
-            //var spawnedObject = UnityEngine.Object.Instantiate(m_PrefabToSpawn);
-            //var baseObject = UnityEngine.Object.Instantiate(m_PrefabToSpawn);
-            //baseObject.GetComponent<NetworkObject>().NetworkManagerOwner = m_ServerNetworkManager;
-            //baseObject.GetComponent<NetworkObject>().Spawn();
+            var spawnedObject = UnityEngine.Object.Instantiate(m_PrefabToSpawn);
+            var baseObject = UnityEngine.Object.Instantiate(m_PrefabToSpawn);
+            baseObject.GetComponent<NetworkObject>().NetworkManagerOwner = m_ServerNetworkManager;
+            baseObject.GetComponent<NetworkObject>().Spawn();
 
-            //m_AsNetworkObject = spawnedObject.GetComponent<NetworkObject>();
-            //m_AsNetworkObject.NetworkManagerOwner = m_ServerNetworkManager;
             m_AsNetworkObject = spawnedObject.GetComponent<NetworkObject>();
-            m_AsNetworkObject.TrySetParent(baseObject);
-            //m_AsNetworkObject.Spawn();
-            yield return s_DefaultWaitForTick;
+            m_AsNetworkObject.NetworkManagerOwner = m_ServerNetworkManager;
 
-            //yield return RefreshNetworkObjects();
+            m_AsNetworkObject.TrySetParent(baseObject);
+
+            m_AsNetworkObject.Spawn();
+
+            yield return RefreshNetworkObjects();
 
             m_AsNetworkObject.TrySetParent(baseObject);
 
@@ -127,22 +104,14 @@ namespace Unity.Netcode.RuntimeTests
             spawnedObject.GetComponent<TransformInterpolationObject>().IsMoving = true;
 
             // Give two seconds for the object to settle
-            // yield return new WaitForSeconds(2.0f);
-            yield return s_DefaultWaitForTick;
-
-            m_SpawnedObjectOnClient = s_GlobalNetworkObjects[m_ClientNetworkManagers[0].LocalClientId][m_AsNetworkObject.NetworkObjectId];
+            yield return new WaitForSeconds(2.0f);
 
             m_SpawnedObjectOnClient.GetComponent<TransformInterpolationObject>().CheckPosition = true;
 
             // Test that interpolation works correctly for 10 seconds
             // Increasing this duration gives you the opportunity to go check in the Editor how the objects are setup
             // and how they move
-
-            // [NSS]: Disabling this as this just delays the total time to complete all tests and the objects spawned have no
-            // way to visually determine if it is working properly or not.  As well, integration tests should not require someone
-            // to visually detect if it is working or not.
-            //yield return new WaitForSeconds(10.0f);
+            yield return new WaitForSeconds(10.0f);
         }
     }
 }
-#endif
