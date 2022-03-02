4b06dbb409e79b229d95a2a22acb43f9ac1f81ac
zain-mecklai <zain.mecklai@unity3d.com>
2022-03-02 12:07:12 -0500

    For some visual feedback, include a text in the screen that shows some state and progress information

---
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/ThreeDText.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using Unity.Netcode;
+#if UNITY_UNET_PRESENT
+using Unity.Netcode.Transports.UNET;
+#endif
+
+namespace Unity.Netcode.MultiprocessRuntimeTests
+{
+    public class ThreeDText : MonoBehaviour
+    {
+        public bool IsTestCoordinatorActiveAndEnabled = false;
+        public string CommandLineArguments = "";
+        private long m_UpdateCounter;
+        private bool m_HasFired;
+        // Start is called before the first frame update
+        public void Start()
+        {
+            Debug.Log("ThreeDText - Start");
+            m_HasFired = false;
+            m_UpdateCounter = 0;
+            var jsonTextFile = Resources.Load<TextAsset>("Text/multiprocess_tests");
+            Debug.Log(jsonTextFile);
+
+            var t = GetComponent<TextMesh>();
+            t.text = "On Start";
+            CommandLineArguments = System.Environment.CommandLine;
+
+            string[] args = System.Environment.GetCommandLineArgs();
+            foreach (var arg in args)
+            {
+                Debug.Log(arg);
+                CommandLineArguments += " " + arg;
+            }
+            Debug.Log($"CommandLineArguments {CommandLineArguments}");
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+            m_UpdateCounter++;
+            var testCoordinator = TestCoordinator.Instance;
+            if (testCoordinator == null)
+            {
+                return;
+            }
+
+            var transport = NetworkManager.Singleton != null ? NetworkManager.Singleton.NetworkConfig.NetworkTransport : null;
+            var transportString = "";
+            if (transport == null)
+            {
+                transportString = "null";
+            }
+            else
+            {
+                transportString = transport.ToString();
+            }
+
+            var t = GetComponent<TextMesh>();
+
+            if (IsTestCoordinatorActiveAndEnabled != testCoordinator.isActiveAndEnabled ||
+                !m_HasFired ||
+                m_UpdateCounter % 100 == 0)
+            {
+                m_HasFired = true;
+                t.text = $"On Update -\ntestCoordinator.isActiveAndEnabled:{testCoordinator.isActiveAndEnabled}\n"+
+                    $"Transport: {transportString}\n"+
+                    $"{CommandLineArguments}\n"+
+                    $"{m_UpdateCounter}\n";
+                Debug.Log(t.text);
+                MultiprocessLogger.Log(t.text);
+                IsTestCoordinatorActiveAndEnabled = testCoordinator.isActiveAndEnabled;
+            }
+        }
+    }
+}
