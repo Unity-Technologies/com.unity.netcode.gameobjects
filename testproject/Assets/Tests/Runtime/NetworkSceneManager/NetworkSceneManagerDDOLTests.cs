a2258fc0913e07c4cf4b861bb301fce1da34a1b0
Noel Stephens <noel.stephens@unity3d.com>
2022-03-04 12:20:22 -0600

    fix: NetworkSceneManagerTests simplification and missed updates to NetcodeIntegrationTest (#1773)
    * update
    
    This is a WIP version.
    This file needs to be broken up into several different individual tests with accompanying individual files.
    There are issues with the way this test was halfway converted and for now the first thing is to do a full conversion which includes breaking this mong-test into individual tests.
    
    * update
    
    Still have a few more to extract, but this is the largest chunk from the NetworkSceneManagerTests class itself.
    
    * update
    
    Migrated the three individual classes out into their own files.
    
    * Update
    
    This is the last test that needed to be moved outside of the NetworkSceneManagerTests file.
    
    * update
    
    Just deleting the NetworkSceneManagerTests file as it is no longer needed.
    
    * style
    
    whitespace

---
+++ testproject/Assets/Tests/Runtime/NetworkSceneManager/NetworkSceneManagerDDOLTests.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using Unity.Netcode;
+using Unity.Netcode.TestHelpers.Runtime;
+using Object = UnityEngine.Object;
+
+namespace TestProject.RuntimeTests
+{
+    public class NetworkSceneManagerDDOLTests
+    {
+        private NetworkManager m_ServerNetworkManager;
+        private GameObject m_NetworkManagerGameObject;
+        private GameObject m_DDOL_ObjectToSpawn;
+
+        protected float m_ConditionMetFrequency = 0.1f;
+
+        [UnitySetUp]
+        protected IEnumerator SetUp()
+        {
+            m_NetworkManagerGameObject = new GameObject("NetworkManager - Host");
+            m_ServerNetworkManager = m_NetworkManagerGameObject.AddComponent<NetworkManager>();
+
+            m_DDOL_ObjectToSpawn = new GameObject();
+            var networkObject = m_DDOL_ObjectToSpawn.AddComponent<NetworkObject>();
+            m_DDOL_ObjectToSpawn.AddComponent<DDOLBehaviour>();
+
+            NetcodeIntegrationTestHelpers.MakeNetworkObjectTestPrefab(networkObject);
+
+            m_ServerNetworkManager.NetworkConfig = new NetworkConfig()
+            {
+                ConnectionApproval = false,
+                NetworkPrefabs = new List<NetworkPrefab>() { new NetworkPrefab() { Prefab = m_DDOL_ObjectToSpawn } },
+                NetworkTransport = m_NetworkManagerGameObject.AddComponent<SIPTransport>(),
+            };
+            m_ServerNetworkManager.StartHost();
+            yield break;
+        }
+
+        [UnityTearDown]
+        protected IEnumerator TearDown()
+        {
+            m_ServerNetworkManager.Shutdown();
+
+            Object.Destroy(m_NetworkManagerGameObject);
+            Object.Destroy(m_DDOL_ObjectToSpawn);
+
+            yield break;
+        }
+
+        public enum DefaultState
+        {
+            IsEnabled,
+            IsDisabled
+        }
+
+        public enum MovedIntoDDOLBy
+        {
+            User,
+            NetworkSceneManager
+        }
+
+        public enum NetworkObjectType
+        {
+            InScenePlaced,
+            DynamicallySpawned
+        }
+
+        /// <summary>
+        /// Tests to make sure NetworkObjects moved into the DDOL will
+        /// restore back to their currently active state when a full
+        /// scene transition is complete.
+        /// This tests both in-scene placed and dynamically spawned NetworkObjects
+        [UnityTest]
+        public IEnumerator InSceneNetworkObjectState([Values(DefaultState.IsEnabled, DefaultState.IsDisabled)] DefaultState activeState,
+            [Values(MovedIntoDDOLBy.User, MovedIntoDDOLBy.NetworkSceneManager)] MovedIntoDDOLBy movedIntoDDOLBy,
+            [Values(NetworkObjectType.InScenePlaced, NetworkObjectType.DynamicallySpawned)] NetworkObjectType networkObjectType)
+        {
+            var isActive = activeState == DefaultState.IsEnabled ? true : false;
+            var isInScene = networkObjectType == NetworkObjectType.InScenePlaced ? true : false;
+            var objectInstance = Object.Instantiate(m_DDOL_ObjectToSpawn);
+
+            var networkObject = objectInstance.GetComponent<NetworkObject>();
+            var ddolBehaviour = objectInstance.GetComponent<DDOLBehaviour>();
+
+            // All tests require this to be false
+            networkObject.DestroyWithScene = false;
+
+            if (movedIntoDDOLBy == MovedIntoDDOLBy.User)
+            {
+                ddolBehaviour.MoveToDDOL();
+            }
+
+            // Sets whether we are in-scene or dynamically spawned NetworkObject
+            ddolBehaviour.SetInScene(isInScene);
+
+            networkObject.Spawn();
+
+            Assert.That(networkObject.IsSpawned);
+
+            objectInstance.SetActive(isActive);
+
+            m_ServerNetworkManager.SceneManager.MoveObjectsToDontDestroyOnLoad();
+
+            yield return new WaitForSeconds(0.03f);
+
+            // It should be isActive when MoveObjectsToDontDestroyOnLoad is called.
+            Assert.That(networkObject.isActiveAndEnabled == isActive);
+
+            m_ServerNetworkManager.SceneManager.MoveObjectsFromDontDestroyOnLoadToScene(SceneManager.GetActiveScene());
+
+            yield return new WaitForSeconds(0.03f);
+
+            // It should be isActive when MoveObjectsFromDontDestroyOnLoadToScene is called.
+            Assert.That(networkObject.isActiveAndEnabled == isActive);
+
+            //Done
+            networkObject.Despawn();
+        }
+
+        public class DDOLBehaviour : NetworkBehaviour
+        {
+            public void MoveToDDOL()
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+
+            public void SetInScene(bool isInScene)
+            {
+                var networkObject = GetComponent<NetworkObject>();
+                networkObject.IsSceneObject = isInScene;
+            }
+        }
+
+    }
+}
