e0fd09e0b47d9fd1de93a35e258627b7acfd7d3c
Kitty Draper <kitty.draper@unity3d.com>
2022-03-07 14:18:36 -0600

    An attempt at ensuring assets are properly built for CI use. Who knows if this will work or not...

---
+++ testproject/Assets/Scripts/AddressablesBuilder.cs
@@ -0,0 +1,43 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
+using UnityEngine;
+
+public class AddressablesBuilder
+{
+    static public bool PreExport()
+    {
+        if (AddressableAssetSettingsDefaultObject.Settings != null)
+        {
+            Debug.Log("AddressablesBuilder.PreExport start");
+            AddressableAssetSettings.CleanPlayerContent(AddressableAssetSettingsDefaultObject.Settings.ActivePlayerDataBuilder);
+            AddressableAssetSettings.BuildPlayerContent();
+            Debug.Log("AddressablesBuilder.PreExport done");
+            return true;
+        }
+
+        return false;
+    }
+
+    [InitializeOnLoadMethod]
+    private static void Initialize()
+    {
+        AddressableAssetSettings.OnModificationGlobal += ModificationHandler;
+        EditorApplication.update += Update;
+    }
+
+    private static void Update()
+    {
+        if (PreExport())
+        {
+            EditorApplication.update -= Update;
+        }
+    }
+
+    private static void ModificationHandler(AddressableAssetSettings settings, AddressableAssetSettings.ModificationEvent eventType, object obj)
+    {
+        PreExport();
+    }
+}
+#endif
