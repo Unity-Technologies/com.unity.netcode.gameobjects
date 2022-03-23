a3bc06805eef8f2a224304a45a9ef85b874f801b
Fatih Mar <mfatihmar@gmail.com>
2022-03-23 20:54:22 +0000

    implement `UTPAdapterChecker` to remove UTP Adapter package automatically from the project

--- com.unity.netcode.gameobjects/Runtime/AssemblyInfo.cs
+++ com.unity.netcode.gameobjects/Runtime/AssemblyInfo.cs
@@ -10,3 +10,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.RuntimeTests")]
 [assembly: InternalsVisibleTo("Unity.Netcode.TestHelpers.Runtime")]
+[assembly: InternalsVisibleTo("Unity.Netcode.Adapter.UTP")]
