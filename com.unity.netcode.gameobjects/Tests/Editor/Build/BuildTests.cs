8c93bd8570118226b706aaee9f3d0635bc34c0e5
ashwinimurt <ashwini@unity3d.com>
2022-04-23 10:24:19 -0700

    fix: Fix build tests when target support is not installed/supported

--- com.unity.netcode.gameobjects/Tests/Editor/Build/BuildTests.cs
+++ com.unity.netcode.gameobjects/Tests/Editor/Build/BuildTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Unity.Netcode.EditorTests
 {
@@ -16,13 +17,25 @@ namespace Unity.Netcode.EditorTests
         {
             var execAssembly = Assembly.GetExecutingAssembly();
             var packagePath = UnityEditor.PackageManager.PackageInfo.FindForAssembly(execAssembly).assetPath;
+            var buildTarget = EditorUserBuildSettings.activeBuildTarget;
+            var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(buildTarget);
+            var buildTargetSupported = BuildPipeline.IsBuildTargetSupported(buildTargetGroup, buildTarget);
+
             var buildReport = BuildPipeline.BuildPlayer(
                 new[] { Path.Combine(packagePath, DefaultBuildScenePath) },
                 Path.Combine(Path.GetDirectoryName(Application.dataPath), "Builds", nameof(BuildTests)),
-                EditorUserBuildSettings.activeBuildTarget,
+                buildTarget,
                 BuildOptions.None
             );
-            Assert.AreEqual(BuildResult.Succeeded, buildReport.summary.result);
+
+            if (buildTargetSupported)
+            {
+                Assert.AreEqual(BuildResult.Succeeded, buildReport.summary.result);
+            }
+            else
+            {
+                LogAssert.Expect(LogType.Error, "Error building player because build target was unsupported");
+            }
         }
     }
 }
