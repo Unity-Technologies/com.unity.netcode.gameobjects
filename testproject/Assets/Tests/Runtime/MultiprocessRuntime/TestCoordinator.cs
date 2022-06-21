384f53ab2849ba7cc6409c87b1402731aabfc427
zain-mecklai <zain.mecklai@unity3d.com>
2022-06-21 11:22:50 -0400

    update timeout

--- testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinator.cs
+++ testproject/Assets/Tests/Runtime/MultiprocessRuntime/TestCoordinator.cs
@@ -27,7 +27,7 @@ public class TestCoordinator : NetworkBehaviour
 {
     public const int PerTestTimeoutSec = 5 * 60; // seconds
 
-    public const float MaxWaitTimeoutSec = 20;
+    public const float MaxWaitTimeoutSec = 60;
     private const char k_MethodFullNameSplitChar = '@';
 
     private bool m_ShouldShutdown;
