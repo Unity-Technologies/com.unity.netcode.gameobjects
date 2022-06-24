e5870ed9eedad03c32c4b58514a907d5f3b821e6
ashwinimurt <ashwini@unity3d.com>
2022-06-24 09:18:57 -0700

    fix docs for InvalidChannelException

--- com.unity.netcode.gameobjects/Runtime/Exceptions/SpawnStateException.cs
+++ com.unity.netcode.gameobjects/Runtime/Exceptions/SpawnStateException.cs
@@ -26,8 +26,15 @@ namespace Unity.Netcode
         public SpawnStateException(string message, Exception inner) : base(message, inner) { }
     }
 
+    /// <summary>
+    /// Exception thrown when a channel is invalid
+    /// </summary>
     public class InvalidChannelException : Exception
     {
+        /// <summary>
+        /// Constructs a InvalidChannelException with a message
+        /// </summary>
+        /// <param name="message">The exception message</param>
         public InvalidChannelException(string message) : base(message) { }
     }
 }
