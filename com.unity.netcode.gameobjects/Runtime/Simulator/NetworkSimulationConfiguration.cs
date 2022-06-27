f64ca527ce2ec97d80f444cd8a1fdc8ed50b566d
JS Fauteux <js.fauteux@unity3d.com>
2022-06-27 10:00:21 -0400

    Adding ifdef

--- com.unity.netcode.gameobjects/Runtime/Simulator/NetworkSimulationConfiguration.cs
+++ com.unity.netcode.gameobjects/Runtime/Simulator/NetworkSimulationConfiguration.cs
@@ -1,4 +1,15 @@
-﻿using UnityEngine;
+﻿// NetSim Implementation compilation boilerplate
+// All references to UNITY_MP_TOOLS_NETSIM_ENABLED should be defined in the same way,
+// as any discrepancies are likely to result in build failures
+// ---------------------------------------------------------------------------------------------------------------------
+#if UNITY_EDITOR || ((DEVELOPMENT_BUILD && !UNITY_MP_TOOLS_NETSIM_DISABLED_IN_DEVELOP) || (!DEVELOPMENT_BUILD && UNITY_MP_TOOLS_NETSIM_ENABLED_IN_RELEASE))
+    #define UNITY_MP_TOOLS_NETSIM_ENABLED
+#endif
+// ---------------------------------------------------------------------------------------------------------------------
+
+#if UNITY_MP_TOOLS_NETSIM_ENABLED
+
+using UnityEngine;
 
 namespace Unity.Netcode
 {
@@ -103,3 +114,5 @@ namespace Unity.Netcode
         }
     }
 }
+
+#endif
--- com.unity.netcode.gameobjects/Runtime/Simulator/NetworkTypePresets.cs
+++ com.unity.netcode.gameobjects/Runtime/Simulator/NetworkTypePresets.cs
@@ -1,4 +1,15 @@
-﻿namespace Unity.Netcode
+﻿// NetSim Implementation compilation boilerplate
+// All references to UNITY_MP_TOOLS_NETSIM_ENABLED should be defined in the same way,
+// as any discrepancies are likely to result in build failures
+// ---------------------------------------------------------------------------------------------------------------------
+#if UNITY_EDITOR || ((DEVELOPMENT_BUILD && !UNITY_MP_TOOLS_NETSIM_DISABLED_IN_DEVELOP) || (!DEVELOPMENT_BUILD && UNITY_MP_TOOLS_NETSIM_ENABLED_IN_RELEASE))
+    #define UNITY_MP_TOOLS_NETSIM_ENABLED
+#endif
+// ---------------------------------------------------------------------------------------------------------------------
+
+#if UNITY_MP_TOOLS_NETSIM_ENABLED
+
+namespace Unity.Netcode
 {
     public class NetworkTypePresets
     {
@@ -36,3 +47,5 @@
         };
     }
 }
+
+#endif
