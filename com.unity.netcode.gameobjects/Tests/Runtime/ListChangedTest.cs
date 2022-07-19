9ab0652ddbbcea2ae12b726c8aa28b7ec4ab2ab9
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2022-07-19 12:42:24 -0400

    coding standard fix. Unnecessary includes

--- com.unity.netcode.gameobjects/Tests/Runtime/ListChangedTest.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/ListChangedTest.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using Unity.Netcode.TestHelpers.Runtime;
