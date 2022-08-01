abd8bd5528acc3266b8c9951da0347d5c1744dff
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2022-08-01 18:06:14 -0400

    Reordering messages when host and client have different sets

---
+++ com.unity.netcode.gameobjects/Runtime/Messaging/Messages/OrderingMessage.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+
+namespace Unity.Netcode
+{
+    /// <summary>
+    /// This particular struct is a little weird because it doesn't actually contain the data
+    /// it's serializing. Instead, it contains references to the data it needs to do the
+    /// serialization. This is due to the generally amorphous nature of network variable
+    /// deltas, since they're all driven by custom virtual method overloads.
+    /// </summary>
+    internal struct OrderingMessage : INetworkMessage
+    {
+        public int Order;
+        public int Hash;
+
+        public void Serialize(FastBufferWriter writer)
+        {
+            if (!writer.TryBeginWrite(FastBufferWriter.GetWriteSize(Order) + FastBufferWriter.GetWriteSize(Hash)))
+            {
+                throw new OverflowException($"Not enough space in the buffer to write {nameof(OrderingMessage)}");
+            }
+
+            writer.WriteValue(Order);
+            writer.WriteValue(Hash);
+        }
+
+        public bool Deserialize(FastBufferReader reader, ref NetworkContext context)
+        {
+            if (!reader.TryBeginRead(FastBufferWriter.GetWriteSize(Order) + FastBufferWriter.GetWriteSize(Hash)))
+            {
+                throw new OverflowException($"Not enough data in the buffer to read {nameof(OrderingMessage)}");
+            }
+
+            reader.ReadValue(out Order);
+            reader.ReadValue(out Hash);
+
+            return true;
+        }
+
+        public void Handle(ref NetworkContext context)
+        {
+            NetworkManager.Singleton.MessagingSystem.ReorderMessage(Order, Hash);
+        }
+    }
+}
