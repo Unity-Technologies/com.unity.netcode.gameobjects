da75b453e58aa0b5a5c789093a2f5f8749fffc61
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2022-08-24 12:37:03 -0400

    fix: Not throwing an exception when NetworkVariable is written to before being spawned. Logging a warning instead

--- com.unity.netcode.gameobjects/Runtime/NetworkVariable/NetworkVariableBase.cs
+++ com.unity.netcode.gameobjects/Runtime/NetworkVariable/NetworkVariableBase.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace Unity.Netcode
 {
@@ -79,6 +80,14 @@ namespace Unity.Netcode
         public virtual void SetDirty(bool isDirty)
         {
             m_IsDirty = isDirty;
+
+            if (m_NetworkBehaviour == null)
+            {
+                Debug.LogWarning($"NetworkVariable is written to, but doesn't know its NetworkBehaviour yet. " +
+                                 "Are you modifying a NetworkVariable before the NetworkObject is spawned?");
+                return;
+            }
+
             if (m_IsDirty)
             {
                 m_NetworkBehaviour.NetworkManager.MarkNetworkObjectDirty(m_NetworkBehaviour.NetworkObject);
