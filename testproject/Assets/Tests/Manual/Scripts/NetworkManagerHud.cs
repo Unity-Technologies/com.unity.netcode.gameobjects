2c79ed0028e2d6a87dbb8591b12f2e9ddc65ac97
Jesse Olmer <jesseo@unity3d.com>
2022-09-16 10:01:16 -0700

    test: NetworkManagerHud improvements
    - Debug draw location gizmo
    - Remove singleton uses
    - Configurable draw position

--- testproject/Assets/Tests/Manual/Scripts/NetworkManagerHud.cs
+++ testproject/Assets/Tests/Manual/Scripts/NetworkManagerHud.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Unity.Netcode;
 
@@ -7,11 +8,28 @@ namespace TestProject.ManualTests
     [DisallowMultipleComponent]
     public class NetworkManagerHud : MonoBehaviour
     {
+        public NetworkManager NetworkManager;
+        public Rect Dimensions = new Rect(10, 10, 300, 300);
+
+        private void Start()
+        {
+            if (NetworkManager == null)
+            {
+                NetworkManager = GetComponent<NetworkManager>();
+            }
+
+            if (NetworkManager == null)
+            {
+                Debug.Log("Warning, using NetworkManager Singleton");
+                NetworkManager = NetworkManager.Singleton;
+            }
+        }
+
         private void OnGUI()
         {
-            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            GUILayout.BeginArea(Dimensions);
 
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            if (!NetworkManager.IsClient && !NetworkManager.IsServer)
             {
                 StartButtons();
             }
@@ -23,29 +41,57 @@ namespace TestProject.ManualTests
             GUILayout.EndArea();
         }
 
-        private static void StartButtons()
+        private void OnDrawGizmosSelected()
+        {
+            Vector3 PixelToCameraClip(Camera camera, Vector3 screenPos)
+            {
+                screenPos.z = camera.nearClipPlane + 0.001f;
+                screenPos.y = camera.pixelHeight - screenPos.y;
+                return camera.ScreenToWorldPoint(screenPos);
+            }
+
+            Gizmos.color = Color.green;
+
+            var cam = FindObjectOfType<Camera>();
+            if (cam == null)
+            {
+                return;
+            }
+
+            Vector3 tl = PixelToCameraClip(cam, Dimensions.min);
+            Vector3 tr = PixelToCameraClip(cam, new Vector3(Dimensions.xMax, Dimensions.yMin));
+            Vector3 br = PixelToCameraClip(cam, Dimensions.max);
+            Vector3 bl = PixelToCameraClip(cam, new Vector3(Dimensions.xMin, Dimensions.yMax));
+
+            Gizmos.DrawLine(tl, tr);
+            Gizmos.DrawLine(tr, br);
+            Gizmos.DrawLine(br, bl);
+            Gizmos.DrawLine(bl, tl);
+        }
+
+        private void StartButtons()
         {
             if (GUILayout.Button("Host"))
             {
-                NetworkManager.Singleton.StartHost();
+                NetworkManager.StartHost();
             }
 
             if (GUILayout.Button("Client"))
             {
-                NetworkManager.Singleton.StartClient();
+                NetworkManager.StartClient();
             }
 
             if (GUILayout.Button("Server"))
             {
-                NetworkManager.Singleton.StartServer();
+                NetworkManager.StartServer();
             }
         }
 
-        private static void StatusLabels()
+        private void StatusLabels()
         {
-            var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            var mode = NetworkManager.IsHost ? "Host" : NetworkManager.IsServer ? "Server" : "Client";
 
-            GUILayout.Label($"Transport: {NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name}");
+            GUILayout.Label($"Transport: {NetworkManager.NetworkConfig.NetworkTransport.GetType().Name}");
             GUILayout.Label($"Mode: {mode}");
         }
     }
