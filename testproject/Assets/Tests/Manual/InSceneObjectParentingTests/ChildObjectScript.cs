1c5945766799dd5d2a8e197e93a8b9cd05c6a4d7
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-09-19 09:59:55 -0500

    update
    Cleaning up the code a bit.  Server controls the final position, rotation, and scale.

--- testproject/Assets/Tests/Manual/InSceneObjectParentingTests/ChildObjectScript.cs
+++ testproject/Assets/Tests/Manual/InSceneObjectParentingTests/ChildObjectScript.cs
@@ -5,71 +5,78 @@ namespace TestProject.ManualTests
 {
     public class ChildObjectScript : NetworkBehaviour
     {
+        private bool m_WorldPositionStays;
+        private NetworkObject m_LastParent;
+        private Vector3 m_OriginalLocalPosition;
+        private Quaternion m_OriginalLocalRotation;
+        private Vector3 m_OriginalLocalScale;
+
         private void Update()
         {
             if (IsSpawned)
             {
+                // Parents with WorldPositionStays set to true
                 if (Input.GetKeyDown(KeyCode.G))
                 {
                     PickupItemServerRpc();
                 }
-                if (Input.GetKeyDown(KeyCode.D))
-                {
-                    DropItemServerRpc();
-                }
+
+                // Parents with WorldPositionStays set to false
                 if (Input.GetKeyDown(KeyCode.F))
                 {
                     PickupItemServerRpc(false);
-                    if (!IsServer)
-                    {
-                        m_WorldPositionStays = false;
-                    }
                 }
-                if (Input.GetKeyDown(KeyCode.E))
+
+                if (Input.GetKeyDown(KeyCode.D))
                 {
                     DropItemServerRpc();
                 }
             }
         }
 
-        private Vector3 m_OriginalLocalPosition;
-        private Quaternion m_OriginalLocalRotation;
-        private Vector3 m_OriginalLocalScale;
-
         public override void OnNetworkSpawn()
         {
-            m_OriginalLocalPosition = transform.localPosition;
-            m_OriginalLocalRotation = transform.localRotation;
-            m_OriginalLocalScale = transform.localScale;
+            if (IsServer)
+            {
+                m_OriginalLocalPosition = transform.localPosition;
+                m_OriginalLocalRotation = transform.localRotation;
+                m_OriginalLocalScale = transform.localScale;
+            }
             base.OnNetworkSpawn();
         }
 
-        private NetworkObject m_LastParent;
-
+        /// <inheritdoc/>
+        /// <remarks>
+        /// This is an example of how to adjust the child object's local space transform
+        /// values on the server side that will be synchronized with clients without the
+        /// need to have a NetworkTransform component attached to the child object.
+        /// </remarks>
         public override void OnNetworkObjectParentChanged(NetworkObject parentNetworkObject)
         {
-            if (!IsSpawned || m_WorldPositionStays)
+            // If we are not spawned, world position is "staying", or we are not the server
+            // then don't do anything
+            if (!IsSpawned || m_WorldPositionStays || !IsServer)
             {
                 return;
             }
 
             if (parentNetworkObject != null)
             {
+                // This preserves the original position offset of the object when parented with
+                // WorldPositionStays set to false.
                 transform.localPosition = m_OriginalLocalPosition;
+
+                // Optionally, you can also make other modifications to rotation or scale
                 transform.localRotation = m_OriginalLocalRotation;
                 transform.localScale = m_OriginalLocalScale;
             }
-            else if (parentNetworkObject == null && m_LastParent)
+            else
+            if (parentNetworkObject == null && m_LastParent)
             {
+                // This example will drop the object at the current world position while
+                // also preserving the original local position.
                 transform.position = m_LastParent.transform.position + m_OriginalLocalPosition;
-                if (!IsServer)
-                {
-                    m_WorldPositionStays = true;
-                }
             }
-
-            transform.localRotation = m_OriginalLocalRotation;
-            transform.localScale = m_OriginalLocalScale;
 
             m_LastParent = parentNetworkObject;
             base.OnNetworkObjectParentChanged(parentNetworkObject);
@@ -79,29 +86,16 @@ namespace TestProject.ManualTests
         {
             if (transform.parent != null && player != null )
             {
-                if (transform.parent == player.transform)
-                {
-                    Debug.Log($"{player.name} already picked up {name}!");
-                }
-                else
-                {
-                    Debug.Log($"{name} cannot be picked up by {player.name} as it is already picked up by another player!");
-                }
+                Debug.Log(transform.parent == player.transform ? $"{player.name} already picked up {name}!" : $"{name} cannot be picked up by {player.name}! It is already parented under another player!");
                 return;
             }
             m_WorldPositionStays = worldPositionStays;
+
             NetworkObject.TrySetParent(player, worldPositionStays);
-            if (player == null)
-            {
-                Debug.Log($"{name} is no longer parented.");
-            }
-            else
-            {
-                Debug.Log($"{name} is now parented under {player.name}!");
-            }
+
+            Debug.Log(player == null ? $"{name} is no longer parented." : $"{name} is now parented under {player.name}!");
         }
 
-        private bool m_WorldPositionStays;
 
         [ServerRpc(RequireOwnership = false)]
         public void PickupItemServerRpc(bool worldPositionStays = true, ServerRpcParams serverRpcParams = default)
