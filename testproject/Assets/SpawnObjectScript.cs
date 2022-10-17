b15d9584aafcc3c73bb2f285574bff8b10f2eb6a
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-10-16 23:22:17 -0500

    test - manual
    Some updates to the manual test for this experimental branch.
    Added the ability to enable/disable Use Quaternion Synchronization and Interpolation for the Wheeels.

--- testproject/Assets/SpawnObjectScript.cs
+++ testproject/Assets/SpawnObjectScript.cs
@@ -1,20 +1,31 @@
 
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 
 public class SpawnObjectScript : NetworkBehaviour
 {
     public GameObject AssetToSpawn;
 
-    [Range(0.0f,30.0f)]
-    public float RotationSpeed = 1.0f;
+    [Range(0.0f, 3.0f)]
+    public float CarRotationSpeed = 0.1f;
+
+    [Range(0.0f, 30.0f)]
+    public float WheelRotationSpeed = 1.0f;
 
     [Range(0.1f, 5.0f)]
     public float WheelScale = 1.5f;
 
+    public bool UseQuaternionSync;
+
+    public bool Interpolate = true;
+
     private NetworkObject m_AssetInstance;
 
     private MockCarParts m_MockCarParts;
+
+    private Dictionary<GameObject, NetworkTransform> m_WheelNetworkTransforms = new Dictionary<GameObject, NetworkTransform>();
 
     private float m_CurrentWheelScale;
 
@@ -29,6 +40,10 @@ public class SpawnObjectScript : NetworkBehaviour
             foreach (var wheel in m_MockCarParts.Wheels)
             {
                 wheel.transform.localScale = Vector3.one * WheelScale;
+                var networkTransform = wheel.GetComponent<NetworkTransform>();
+                m_WheelNetworkTransforms.Add(wheel, networkTransform);
+                networkTransform.UseQuaternionSynch = UseQuaternionSync;
+                networkTransform.Interpolate = Interpolate;
             }
 
             m_AssetInstance.Spawn();
@@ -48,22 +63,20 @@ public class SpawnObjectScript : NetworkBehaviour
 
     private void Update()
     {
-        if (IsSpawned && IsServer)
+        if (IsServer && IsSpawned)
         {
-            var wheelQuatShown = false;
+            m_AssetInstance.transform.Rotate(0.0f, CarRotationSpeed, 0.0f, Space.World);
             foreach (var wheel in m_MockCarParts.Wheels)
             {
-                wheel.transform.Rotate(RotationSpeed, 0.0f, 0.0f, Space.Self);
-                if (!wheelQuatShown)
-                {
-                    Debug.Log($"Rotation: {wheel.transform.rotation}");
-                    wheelQuatShown = true;
-                }
+                wheel.transform.Rotate(WheelRotationSpeed, 0.0f, 0.0f, Space.Self);
 
                 if (WheelScale != m_CurrentWheelScale)
                 {
                     wheel.transform.localScale = Vector3.one * WheelScale;
                 }
+
+                m_WheelNetworkTransforms[wheel].UseQuaternionSynch = UseQuaternionSync;
+                m_WheelNetworkTransforms[wheel].Interpolate = Interpolate;
             }
             if (WheelScale != m_CurrentWheelScale)
             {
