86ba1138b8b848fb6154cc6c01726018dfb52b22
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-10-17 21:16:58 -0500

    test - manual
    added some addition debugging capabilities to get down to the core issue with using Euler values for rotation.

--- testproject/Assets/SpawnObjectScript.cs
+++ testproject/Assets/SpawnObjectScript.cs
@@ -21,6 +21,16 @@ public class SpawnObjectScript : NetworkBehaviour
 
     public bool Interpolate = true;
 
+    public bool DebugEulerInterpolation;
+    private NetworkVariable<bool> m_DebugEulerInterpolation = new NetworkVariable<bool>();
+
+    [Range(0,3)]
+    public int DebugWheelIndex;
+
+    private NetworkVariable<int> m_WheelIndexToDebug = new NetworkVariable<int>();
+
+    private NetworkVariable<NetworkBehaviourReference> m_WheelToDebug = new NetworkVariable<NetworkBehaviourReference>();
+
     private NetworkObject m_AssetInstance;
 
     private MockCarParts m_MockCarParts;
@@ -46,7 +56,13 @@ public class SpawnObjectScript : NetworkBehaviour
                 networkTransform.Interpolate = Interpolate;
             }
 
+            m_DebugEulerInterpolation.Value = DebugEulerInterpolation;
+            m_WheelIndexToDebug.Value = DebugWheelIndex;
+
             m_AssetInstance.Spawn();
+
+            var wheelToDebug = m_MockCarParts.Wheels[DebugWheelIndex];
+            m_WheelToDebug.Value = new NetworkBehaviourReference(m_WheelNetworkTransforms[wheelToDebug]);
         }
         base.OnNetworkSpawn();
     }
@@ -63,25 +79,67 @@ public class SpawnObjectScript : NetworkBehaviour
 
     private void Update()
     {
-        if (IsServer && IsSpawned)
+        if (!IsSpawned)
         {
-            m_AssetInstance.transform.Rotate(0.0f, CarRotationSpeed, 0.0f, Space.World);
-            foreach (var wheel in m_MockCarParts.Wheels)
+            return;
+        }
+
+        if (IsServer)
+        {
+            if (m_WheelIndexToDebug.Value != DebugWheelIndex)
             {
-                wheel.transform.Rotate(WheelRotationSpeed, 0.0f, 0.0f, Space.Self);
-
-                if (WheelScale != m_CurrentWheelScale)
-                {
-                    wheel.transform.localScale = Vector3.one * WheelScale;
-                }
-
-                m_WheelNetworkTransforms[wheel].UseQuaternionSynch = UseQuaternionSync;
-                m_WheelNetworkTransforms[wheel].Interpolate = Interpolate;
+                m_WheelIndexToDebug.Value = DebugWheelIndex;
+                var wheel = m_MockCarParts.Wheels[DebugWheelIndex];
+                m_WheelToDebug.Value = new NetworkBehaviourReference(m_WheelNetworkTransforms[wheel]);
             }
+
+            if (m_DebugEulerInterpolation.Value != DebugEulerInterpolation)
+            {
+                m_DebugEulerInterpolation.Value = DebugEulerInterpolation;
+            }
+        }
+        else
+        if (!IsServer && m_DebugEulerInterpolation.Value)
+        {
+            var networkTransform = (NetworkTransform)null;
+            if (m_WheelToDebug.Value.TryGet(out networkTransform))
+            {
+                WheelDebugInfoServerRpc(networkTransform.transform.localEulerAngles, networkTransform.transform.eulerAngles);
+            }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void WheelDebugInfoServerRpc(Vector3 localEuler, Vector3 euler)
+    {
+        var index = m_WheelIndexToDebug.Value;
+        var wheel = m_MockCarParts.Wheels[index];
+        var localTransform = m_WheelNetworkTransforms[wheel].transform;
+        Debug.Log($"[Wheel-{DebugWheelIndex}] LocalEuler-Client {localEuler} vs LocalEuler-Server {localTransform.localEulerAngles} | Euler-Client {euler} vs Euler-Server {localTransform.eulerAngles}");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!IsServer || !IsSpawned)
+        {
+            return;
+        }
+        m_AssetInstance.transform.Rotate(0.0f, CarRotationSpeed, 0.0f, Space.World);
+        foreach (var wheel in m_MockCarParts.Wheels)
+        {
+            wheel.transform.Rotate(WheelRotationSpeed, 0.0f, 0.0f, Space.Self);
+
             if (WheelScale != m_CurrentWheelScale)
             {
-                m_CurrentWheelScale = WheelScale;
+                wheel.transform.localScale = Vector3.one * WheelScale;
             }
+
+            m_WheelNetworkTransforms[wheel].UseQuaternionSynch = UseQuaternionSync;
+            m_WheelNetworkTransforms[wheel].Interpolate = Interpolate;
+        }
+        if (WheelScale != m_CurrentWheelScale)
+        {
+            m_CurrentWheelScale = WheelScale;
         }
     }
 }
