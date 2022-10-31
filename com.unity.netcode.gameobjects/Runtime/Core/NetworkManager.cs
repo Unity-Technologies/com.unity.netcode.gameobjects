34715279c4d3805b7b35ddf17e5763ca547fdedf
Jeffrey Rainy <jeffrey.rainy@unity3d.com>
2022-10-31 15:42:50 -0400

    adding line removed mistakenly

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1354,6 +1354,7 @@ namespace Unity.Netcode
         private void DisconnectRemoteClient(ulong clientId)
         {
             var transportId = ClientIdToTransportId(clientId);
+            MessagingSystem.ProcessSendQueues();
             NetworkConfig.NetworkTransport.DisconnectRemoteClient(transportId);
         }
 
