fdeb4805d0d646a0354e517604137cfae506f57d
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-11-22 17:40:49 -0600

    style
    adding comment

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -2082,6 +2082,11 @@ namespace Unity.Netcode
                             }
                             else
                             {
+                                // Call despawn to assure NetworkBehaviour.OnNetworkDespawn is invoked
+                                // on the server-side (when the client side disconnected).
+                                // This prevents the issue (when just destroying the GameObject) where
+                                // any NetworkBehaviour component(s) destroyed before the NetworkObject
+                                // would not have OnNetworkDespawn invoked.
                                 SpawnManager.DespawnObject(playerObject, true);
                             }
                         }
