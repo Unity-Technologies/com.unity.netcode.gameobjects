44b03cbeba92db7cf01d07e4d87bb994452a1025
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-12-06 20:26:31 -0600

    update
    Improved the original DeltaPositionCompressor so it now has 11 bit precision for the smallest axial values.
    Added some additional remarks about usage along with a precision accuracy table.
    This version can handle +/-15,330 UWS units per second rate of change.

--- com.unity.netcode.gameobjects/Runtime/Serialization/Compression/DeltaPositionCompressor.cs
+++ com.unity.netcode.gameobjects/Runtime/Serialization/Compression/DeltaPositionCompressor.cs
@@ -9,7 +9,7 @@ namespace Unity.Netcode
         /// Contains additional compression information
         /// </summary>
         /// <remarks>
-        /// Bits 0 - 10: 1/1000th decimal place precision value of magnitude
+        /// Bits 0 - 10: 1/1000th decimal place precision value of the magnitude
         /// Bits 11 - 13: Sign values of the largest axial value and the two smallest values
         /// bits 14 - 15: The index of the largest axial value
         /// </remarks>
@@ -17,18 +17,35 @@ namespace Unity.Netcode
 
         /// <summary>
         /// Contains the compressed values
+        /// Bits 0-21: The smallest two normalized axial values with 11 bit precision
+        /// Bits 22-31: The unsigned int value of the magnitude
         /// </summary>
         public uint Compressed;
     }
 
     /// <summary>
-    /// Compresses a delta position using a smallest two approach and using the 3rd 10 bit slot
-    /// to store the magnitude.
-    /// <see cref="QuaternionCompressor"/> for more details
+    /// Compresses a delta position using a "smallest two approach" and using the 3rd 10 bit slot
+    /// to store the magnitude. This should only be used when determining the change in a position
+    /// and not used to store a world space position.
     /// </summary>
+    /// <remarks>
+    /// Specifications for usage:
+    /// - This yields a 50% reduction in total size to maintain a reasonable precision
+    ///   - 12 bytes (Vector3) to 6 bytes <see cref="CompressedDeltaPosition"/>.
+    /// - Axial deltas should not exceed +/- 511.0f range.
+    ///   - At the default network tick of 30x per second this would yield +/- 15,330 unity world space units (UWS units) per second.
+    /// - Decompressed value(s) precision accuracy ranges (@30x per second):
+    ///   - @ +/- 511 delta ---> tolerance level is +/- 0.226f ---> +/-15,330 UWS units per second. (moving so fast you might not see it)
+    ///   - @ +/- 255 delta ---> tolerance level is +/- 0.113f ---> +/-7,650 UWS units per second.
+    ///   - @ +/- 127 delta ---> tolerance level is +/- 0.056f ---> +/-3,810 UWS units per second.
+    ///   - @ +/- 63 delta ----> tolerance level is +/- 0.028f ---> +/-1,890 UWS units per second.
+    ///   - @ +/- 31 delta ----> tolerance level is +/- 0.014f ---> +/-930 UWS units per second.
+    ///   (tolerance levels continue to drop as the delta range drops)
+    /// Each invocation (compressing or decompressing) should take about 1us or less to execute
+    /// </remarks>
     public static class DeltaPositionCompressor
     {
-        private const ushort k_PrecisionMask = (1 << 10) - 1;
+        private const ushort k_PrecisionMask = (1 << 11) - 1;
 
         // Square root of 2 over 2 (Mathf.Sqrt(2.0f) / 2.0f == 1.0f / Mathf.Sqrt(2.0f))
         // This provides encoding the smallest three components into a (+/-) Mathf.Sqrt(2.0f) / 2.0f range
@@ -42,7 +59,7 @@ namespace Unity.Netcode
         // that by k_SqrtTwoOverTwo (minor reduction of runtime calculations)
         private const float k_DcompressionDecodingMask = (1.0f / k_PrecisionMask) * k_SqrtTwoOverTwoEncoding;
 
-        private const ushort k_MagnitudePrecisionMask = 1023;
+        private const ushort k_MagnitudeMask = 1023;
         private const byte k_LargestIndexShift = 14;
         private const byte k_BaseSignShift = 11;
 
@@ -95,13 +112,13 @@ namespace Unity.Netcode
 
             // Store the index, magnitude precision, and largest value's sign
             compressedDeltaPosition.Header = (ushort)(indexToSkip << k_LargestIndexShift);
-            compressedDeltaPosition.Header |= (ushort)((ushort)Mathf.Round((magnitude - (uint)magnitude) * 1000) & k_MagnitudePrecisionMask);
+            compressedDeltaPosition.Header |= (ushort)((uint)Mathf.Round((magnitude - (uint)magnitude) * 1000) & k_MagnitudeMask);
             compressedDeltaPosition.Header |= (ushort)(vectMaxSign << (k_BaseSignShift + 2));
 
             // Store the non-decimal magnitude value
-            var compressed = (uint)magnitude;
-
+            compressedDeltaPosition.Compressed = ((uint)magnitude & k_MagnitudeMask) << 22;
             var currentIndex = 0;
+            var axialValues = (uint)0;
             for (int i = 0; i < 3; i++)
             {
                 if (i == indexToSkip)
@@ -110,11 +127,12 @@ namespace Unity.Netcode
                 }
                 // Store the axis sign in the header
                 compressedDeltaPosition.Header |= (ushort)((normalizedDir[i] < 0 ? k_True : k_False) << (k_BaseSignShift + currentIndex));
+
                 // Add the axis compressed value to the existing compressed values
-                compressed = (compressed << 10) | (ushort)Mathf.Round(k_CompressionEcodingMask * s_AbsValues[i]);
+                axialValues = (axialValues << 11) | (ushort)Mathf.Round(k_CompressionEcodingMask * s_AbsValues[i]);
                 currentIndex++;
             }
-            compressedDeltaPosition.Compressed = compressed;
+            compressedDeltaPosition.Compressed |= axialValues;
         }
 
         /// <summary>
@@ -132,6 +150,13 @@ namespace Unity.Netcode
             // Reverse out the values while skipping over the largest value index
             var sumOfSquaredMagnitudes = 0.0f;
             var currentIndex = 1;
+
+            // Get the magnitude of the delta position
+            var magnitude = (float)((compressed >> 22) & k_MagnitudeMask);
+
+            // Get the 1/1000th decimal place precision value of the magnitude
+            magnitude += (compressedDeltaPosition.Header & k_MagnitudeMask) * 0.001f;
+
             for (int i = 2; i >= 0; --i)
             {
                 if (i == indexToSkip)
@@ -143,16 +168,11 @@ namespace Unity.Netcode
 
                 deltaPosition[i] = (axisSign > 0 ? -1.0f : 1.0f) * (compressed & k_PrecisionMask) * k_DcompressionDecodingMask;
                 sumOfSquaredMagnitudes += deltaPosition[i] * deltaPosition[i];
-                compressed = compressed >> 10;
+                compressed = compressed >> 11;
                 currentIndex--;
             }
 
-            // Get the magnitude of the delta position
-            var magnitude = (float)(compressed & k_PrecisionMask);
-
-            // Get the 1/1000th decimal place precision value of the magnitude
-            magnitude += (compressedDeltaPosition.Header & k_MagnitudePrecisionMask) * 0.001f;
-
+            // Get the largest value's sign
             var largestSign = compressedDeltaPosition.Header & (1 << (k_BaseSignShift + 2));
 
             // Calculate the largest value from the sum of squares of the two smallest axis values
