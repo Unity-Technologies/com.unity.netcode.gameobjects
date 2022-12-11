420edb402b8be15d18931bf996d5e765b62d51a1
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-12-11 16:18:33 -0600

    fix
    This fixes the exception in 2020.3.x versions of Unity where the EditorStyles.label is not yet set when instantiating label and toggle elements during declaration (unlike later versions of Unity where it does).

--- com.unity.netcode.gameobjects/Editor/Configuration/NetcodeSettingsProvider.cs
+++ com.unity.netcode.gameobjects/Editor/Configuration/NetcodeSettingsProvider.cs
@@ -1,9 +1,10 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
 namespace Unity.Netcode.Editor.Configuration
 {
-    internal static class NetcodeSettingsProvider
+    public static class NetcodeSettingsProvider
     {
         [SettingsProvider]
         public static SettingsProvider CreateNetcodeSettingsProvider()
@@ -20,14 +21,44 @@ namespace Unity.Netcode.Editor.Configuration
             return provider;
         }
 
-        internal static NetcodeSettingsLabel NetworkObjectsSectionLabel = new NetcodeSettingsLabel("NetworkObject Helper Settings", 20);
-        internal static NetcodeSettingsToggle AutoAddNetworkObjectToggle = new NetcodeSettingsToggle("Auto-Add NetworkObjects", "When enabled, NetworkObjects are automatically added to GameObjects when NetworkBehaviours are added first.", 20);
-        internal static NetcodeSettingsLabel MultiplayerToolsLabel = new NetcodeSettingsLabel("Multiplayer Tools", 20);
-        internal static NetcodeSettingsToggle MultiplayerToolTipStatusToggle = new NetcodeSettingsToggle("Multiplayer Tools Install Reminder", "When enabled, the NetworkManager will display " +
-            "the notification to install the multiplayer tools package.", 20);
+        internal static NetcodeSettingsLabel NetworkObjectsSectionLabel;
+        internal static NetcodeSettingsToggle AutoAddNetworkObjectToggle;
+        internal static NetcodeSettingsLabel MultiplayerToolsLabel;
+        internal static NetcodeSettingsToggle MultiplayerToolTipStatusToggle;
+
+        /// <summary>
+        /// Creates an instance of the settings UI Elements if they don't already exist.
+        /// </summary>
+        /// <remarks>
+        /// We have to construct any NetcodeGUISettings derived classes here because in
+        /// version 2020.x.x EditorStyles.label does not exist yet (higher versions it does)
+        /// </remarks>
+        private static void CheckForInitialize()
+        {
+            if (NetworkObjectsSectionLabel == null)
+            {
+                NetworkObjectsSectionLabel = new NetcodeSettingsLabel("NetworkObject Helper Settings", 20);
+            }
+
+            if (AutoAddNetworkObjectToggle == null)
+            {
+                AutoAddNetworkObjectToggle = new NetcodeSettingsToggle("Auto-Add NetworkObjects", "When enabled, NetworkObjects are automatically added to GameObjects when NetworkBehaviours are added first.", 20);
+            }
+
+            if (MultiplayerToolsLabel == null)
+            {
+                MultiplayerToolsLabel = new NetcodeSettingsLabel("Multiplayer Tools", 20);
+            }
+
+            if (MultiplayerToolTipStatusToggle == null)
+            {
+                MultiplayerToolTipStatusToggle = new NetcodeSettingsToggle("Multiplayer Tools Install Reminder", "When enabled, the NetworkManager will display the notification to install the multiplayer tools package.", 20);
+            }
+        }
 
         private static void OnGuiHandler(string obj)
         {
+            CheckForInitialize();
             var autoAddNetworkObjectSetting = NetcodeForGameObjectsSettings.GetAutoAddNetworkObjectSetting();
             var multiplayerToolsTipStatus = NetcodeForGameObjectsSettings.GetNetcodeInstallMultiplayerToolTips() == 0;
             EditorGUI.BeginChangeCheck();
@@ -56,7 +87,7 @@ namespace Unity.Netcode.Editor.Configuration
         public NetcodeSettingsLabel(string labelText, float layoutOffset = 0.0f)
         {
             m_LabelContent = labelText;
-            AdjustLableSize(labelText, layoutOffset);
+            AdjustLabelSize(labelText, layoutOffset);
         }
     }
 
@@ -72,7 +103,7 @@ namespace Unity.Netcode.Editor.Configuration
 
         public NetcodeSettingsToggle(string labelText, string toolTip, float layoutOffset)
         {
-            AdjustLableSize(labelText, layoutOffset);
+            AdjustLabelSize(labelText, layoutOffset);
             m_ToggleContent = new GUIContent(labelText, toolTip);
         }
     }
@@ -84,11 +115,12 @@ namespace Unity.Netcode.Editor.Configuration
 
         protected GUILayoutOption m_LayoutWidth { get; private set; }
 
-        protected void AdjustLableSize(string labelText, float offset = 0.0f)
+        protected void AdjustLabelSize(string labelText, float offset = 0.0f)
         {
             m_LabelSize = Mathf.Min(k_MaxLabelWidth, EditorStyles.label.CalcSize(new GUIContent(labelText)).x);
             m_LayoutWidth = GUILayout.Width(m_LabelSize + offset);
         }
     }
-
 }
+#endif
+
