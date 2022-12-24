756c77f154336918c2745c4e9f031badef081c27
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2022-12-24 14:18:48 -0600

    test
    Adjustments for HalfVector3 delta position.  We no longer need all of the extra garbage.

--- testproject/Assets/Tests/Manual/Scripts/IntegrationNetworkTransform.cs
+++ testproject/Assets/Tests/Manual/Scripts/IntegrationNetworkTransform.cs
@@ -21,17 +21,6 @@ namespace TestProject.ManualTests
     {
 #if DEBUG_NETWORKTRANSFORM
         public bool IsServerAuthoritative = true;
-        public struct AuthorityStateUpdate
-        {
-            public int Tick;
-            public ulong OwnerClientId;
-            public ushort NetworkBehaviourId;
-            public Vector3 AuthorityPosition;
-            public Vector3 PredictedPosition;
-            public Vector3 PrecisionLoss;
-        }
-
-        protected Dictionary<ulong, Dictionary<ushort, Dictionary<int, AuthorityStateUpdate>>> m_TickPositionTable = new Dictionary<ulong, Dictionary<ushort, Dictionary<int, AuthorityStateUpdate>>>();
 
         public Vector3 LastUpdatedPosition;
         public Vector3 LastUpdatedScale;
@@ -86,100 +75,6 @@ namespace TestProject.ManualTests
         protected override void OnNetworkTransformStateUpdate(ref NetworkTransformStateUpdate networkTransformStateUpdate)
         {
             UpdateTransformHistory(networkTransformStateUpdate.PositionUpdate, networkTransformStateUpdate.RotationUpdate, networkTransformStateUpdate.ScaleUpdate);
-            if (networkTransformStateUpdate.PositionUpdate)
-            {
-                if (CanCommitToTransform)
-                {
-                    if (!m_TickPositionTable.ContainsKey(OwnerClientId))
-                    {
-                        m_TickPositionTable.Add(OwnerClientId, new Dictionary<ushort, Dictionary<int, AuthorityStateUpdate>>());
-                    }
-
-                    if (!m_TickPositionTable[OwnerClientId].ContainsKey(networkTransformStateUpdate.NetworkBehaviourId))
-                    {
-                        m_TickPositionTable[OwnerClientId].Add(networkTransformStateUpdate.NetworkBehaviourId, new Dictionary<int, AuthorityStateUpdate>());
-                    }
-
-                    if (!m_TickPositionTable[OwnerClientId][networkTransformStateUpdate.NetworkBehaviourId].ContainsKey(networkTransformStateUpdate.NetworkTick))
-                    {
-                        m_TickPositionTable[OwnerClientId][networkTransformStateUpdate.NetworkBehaviourId].Add(networkTransformStateUpdate.NetworkTick, new AuthorityStateUpdate()
-                        {
-                            Tick = networkTransformStateUpdate.NetworkTick,
-                            OwnerClientId = NetworkObject.OwnerClientId,
-                            NetworkBehaviourId = networkTransformStateUpdate.NetworkBehaviourId,
-                            AuthorityPosition = transform.position,
-                            PredictedPosition = networkTransformStateUpdate.TargetPosition,
-                            PrecisionLoss = networkTransformStateUpdate.PrecisionLoss
-                        });
-                    }
-                    else
-                    {
-                        Debug.LogWarning($"{gameObject.name}-{OwnerClientId}-{networkTransformStateUpdate.NetworkBehaviourId} Authority is trying to add more than one position change on network tick {networkTransformStateUpdate.NetworkTick}!\n");
-                    }
-                }
-                else
-                {
-                    // If we are server authoritative, then send position validation to the server
-                    if (OnIsServerAuthoritative() && IsOwnedByServer)
-                    {
-                        PositionValidationServerRpc(networkTransformStateUpdate.TargetPosition, networkTransformStateUpdate.NetworkTick, networkTransformStateUpdate.NetworkBehaviourId);
-                    }
-                    else // Otherwise, server sends the position validation back to the owner/authority
-                    {
-                        var clientRpcParams = new ClientRpcParams() { Send = new ClientRpcSendParams() { TargetClientIds = new List<ulong>() { OwnerClientId } } };
-                        PositionValidationClientRpc(networkTransformStateUpdate.TargetPosition, networkTransformStateUpdate.NetworkTick, networkTransformStateUpdate.NetworkBehaviourId, clientRpcParams);
-                    }
-                }
-            }
-        }
-
-
-        public delegate void PositionValidationHandler(ref NetworkObject networkObject, ref Vector3 position, ref AuthorityStateUpdate serverStateUpdate);
-        public PositionValidationHandler PositionValidationCallback;
-
-
-        protected virtual void OnPositionValidation(ref Vector3 position, ref AuthorityStateUpdate serverStateUpdate)
-        {
-            var networkObject = NetworkObject;
-            PositionValidationCallback?.Invoke(ref networkObject, ref position, ref serverStateUpdate);
-        }
-
-        private void PositionValidation(Vector3 positionUpdate, int tick, ushort networkBehaviourId)
-        {
-            if (m_TickPositionTable.ContainsKey(OwnerClientId))
-            {
-                if (m_TickPositionTable[OwnerClientId].ContainsKey(networkBehaviourId))
-                {
-                    if (m_TickPositionTable[OwnerClientId][networkBehaviourId].ContainsKey(tick))
-                    {
-
-                        var serverState = m_TickPositionTable[OwnerClientId][networkBehaviourId][tick];
-                        var position = positionUpdate;
-                        if (tick == m_LastTickSent)
-                        {
-                            Debug.LogWarning($"Client sent two RPCs with the same tick {tick}");
-                        }
-                        OnPositionValidation(ref position, ref serverState);
-                        m_LastTickSent = tick;
-                        m_TickPositionTable[OwnerClientId][networkBehaviourId].Remove(tick);
-                    }
-
-                }
-            }
-        }
-
-        private int m_LastTickSent;
-
-        [ServerRpc(RequireOwnership = false)]
-        private void PositionValidationServerRpc(Vector3 positionUpdate, int tick, ushort networkBehaviourId)
-        {
-            PositionValidation(positionUpdate, tick, networkBehaviourId);
-        }
-
-        [ClientRpc]
-        private void PositionValidationClientRpc(Vector3 positionUpdate, int tick, ushort networkBehaviourId, ClientRpcParams clientRpcParams = default)
-        {
-            PositionValidation(positionUpdate, tick, networkBehaviourId);
         }
 #endif
     }
