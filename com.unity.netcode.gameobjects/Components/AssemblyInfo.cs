1dcff56a0708558212f8fb879275277c014710f3
Fatih Mar <mfatihmar@gmail.com>
2023-01-18 15:59:47 +0300

    refactor: reorganize `InternalsVisibleTo` directives in `AssemblyInfo.cs` files

--- com.unity.netcode.gameobjects/Components/AssemblyInfo.cs
+++ com.unity.netcode.gameobjects/Components/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 using System.Runtime.CompilerServices;
 
 #if UNITY_EDITOR
-[assembly: InternalsVisibleTo("Unity.Netcode.Editor.CodeGen")]
 [assembly: InternalsVisibleTo("Unity.Netcode.Editor")]
-#endif
+[assembly: InternalsVisibleTo("Unity.Netcode.Editor.CodeGen")]
+#endif // UNITY_EDITOR
 
 #if UNITY_INCLUDE_TESTS
+[assembly: InternalsVisibleTo("Unity.Netcode.RuntimeTests")]
+[assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
 #if UNITY_EDITOR
 [assembly: InternalsVisibleTo("Unity.Netcode.EditorTests")]
 [assembly: InternalsVisibleTo("TestProject.EditorTests")]
-#endif
-[assembly: InternalsVisibleTo("TestProject.RuntimeTests")]
-[assembly: InternalsVisibleTo("Unity.Netcode.RuntimeTests")]
-#endif
+#endif // UNITY_EDITOR
+#endif // UNITY_INCLUDE_TESTS
