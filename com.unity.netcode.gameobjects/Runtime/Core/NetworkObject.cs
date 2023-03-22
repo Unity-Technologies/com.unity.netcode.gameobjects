fdc58b06f740dca4c3353eada17e5b568c8ebf2b
samlucas-unity <107124926+samlucas-unity@users.noreply.github.com>
2023-03-22 22:25:14 +0000

    feat: Added a way to safely access GlobalObjectIdHash via PrefabHashId [MTT-1645] (#2437)
    * Added a way to access the GlobalObjectIdHash via PrefabHashId.
    * Added PrefabHashId tests.
    
    ---------
    
    Co-authored-by: Fatih Mar <mfatihmar@gmail.com>
    Co-authored-by: Noel Stephens <noel.stephens@unity3d.com>
    Co-authored-by: NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -17,6 +17,28 @@ namespace Unity.Netcode
         [SerializeField]
         internal uint GlobalObjectIdHash;
 
+        /// <summary>
+        /// Gets the Prefab Hash Id of this object if the object is registerd as a prefab otherwise it returns 0
+        /// </summary>
+        [HideInInspector]
+        public uint PrefabIdHash
+        {
+            get
+            {
+                foreach (var prefab in NetworkManager.NetworkConfig.Prefabs.Prefabs)
+                {
+                    if (prefab.Prefab == gameObject)
+                    {
+                        return GlobalObjectIdHash;
+                    }
+                }
+
+                return 0;
+            }
+        }
+
+        private bool m_IsPrefab;
+
 #if UNITY_EDITOR
         private void OnValidate()
         {
