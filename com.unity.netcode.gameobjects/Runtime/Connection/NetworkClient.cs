0a8ccb252a3dcc68e3ec05d2cdc8f6fd3d52671e
Fatih Mar <mfatihmar@gmail.com>
2023-04-06 21:30:05 +0300

    make `NetworkClient` bool props internal

--- com.unity.netcode.gameobjects/Runtime/Connection/NetworkClient.cs
+++ com.unity.netcode.gameobjects/Runtime/Connection/NetworkClient.cs
@@ -10,28 +10,28 @@ namespace Unity.Netcode
         /// <summary>
         /// Returns true if the session instance is considered a server
         /// </summary>
-        public bool IsServer { get; internal set; }
+        internal bool IsServer { get; set; }
 
         /// <summary>
         /// Returns true if the session instance is considered a client
         /// </summary>
-        public bool IsClient { get; internal set; }
+        internal bool IsClient { get; set; }
 
         /// <summary>
         /// Returns true if the session instance is considered a host
         /// </summary>
-        public bool IsHost => IsClient && IsServer;
+        internal bool IsHost => IsClient && IsServer;
 
         /// <summary>
         /// When true, the client is connected, approved, and synchronized with
         /// the server.
         /// </summary>
-        public bool IsConnected { get; internal set; }
+        internal bool IsConnected { get; set; }
 
         /// <summary>
         /// Is true when the client has been approved.
         /// </summary>
-        public bool IsApproved { get; internal set; }
+        internal bool IsApproved { get; set; }
 
         /// <summary>
         /// The ClientId of the NetworkClient
