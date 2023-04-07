fccabeb9035cdb292ab13fc3b26db2fc535fe69c
NoelStephensUnity <73188597+NoelStephensUnity@users.noreply.github.com>
2023-04-07 16:55:17 -0500

    test fix
    Fixing the editor test expecting a error log that no longer will happen with an invalid endpoint address.

--- com.unity.netcode.gameobjects/Tests/Editor/Transports/UnityTransportTests.cs
+++ com.unity.netcode.gameobjects/Tests/Editor/Transports/UnityTransportTests.cs
@@ -116,14 +116,14 @@ namespace Unity.Netcode.EditorTests
             transport.Initialize();
 
             transport.SetConnectionData("127.0.0.", 4242, "127.0.0.");
+
             Assert.False(transport.StartServer());
 
             LogAssert.Expect(LogType.Error, "Invalid network endpoint: 127.0.0.:4242.");
+            LogAssert.Expect(LogType.Error, $"Network listen address (127.0.0.) is Invalid!");
 #if UTP_TRANSPORT_2_0_ABOVE
             LogAssert.Expect(LogType.Error, "Socket creation failed (error Unity.Baselib.LowLevel.Binding+Baselib_ErrorState: Invalid argument (0x01000003) <argument name stripped>");
 #endif
-            LogAssert.Expect(LogType.Error, "Server failed to bind. This is usually caused by another process being bound to the same port.");
-
             transport.SetConnectionData("127.0.0.1", 4242, "127.0.0.1");
             Assert.True(transport.StartServer());
 
