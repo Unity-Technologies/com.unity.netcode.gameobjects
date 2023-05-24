aa22588ca135552cce6d0901c2ff2e01608e99b5
NoelStephensUnity <noel.stephens@unity3d.com>
2023-05-24 13:02:40 -0500

    style

--- com.unity.netcode.gameobjects/Tests/Runtime/ClientApprovalDenied.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/ClientApprovalDenied.cs
@@ -1,6 +1,6 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework;
 using Unity.Netcode.TestHelpers.Runtime;
 using UnityEngine.TestTools;
 
