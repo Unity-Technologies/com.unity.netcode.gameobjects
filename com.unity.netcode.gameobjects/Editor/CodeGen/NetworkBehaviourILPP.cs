75aeaa37f5cd1d1e9300eeb0311ca3e13a230959
Kitty Draper <kitty.draper@unity3d.com>
2023-06-30 16:41:00 -0500

    Added another fix for a similar issue.

--- com.unity.netcode.gameobjects/Editor/CodeGen/NetworkBehaviourILPP.cs
+++ com.unity.netcode.gameobjects/Editor/CodeGen/NetworkBehaviourILPP.cs
@@ -2225,6 +2225,12 @@ namespace Unity.Netcode.Editor.CodeGen
                     }
                     field = new FieldReference(fieldDefinition.Name, fieldDefinition.FieldType, genericType);
                 }
+
+                if (field.FieldType.Resolve() == null)
+                {
+                    continue;
+                }
+
                 if (!field.FieldType.IsArray && !field.FieldType.Resolve().IsArray && field.FieldType.IsSubclassOf(m_NetworkVariableBase_TypeRef))
                 {
                     // if({variable} == null) {
