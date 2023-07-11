6e968c90eac7d25e7326d282017f1fbdcb40919a
Kitty Draper <kitty.draper@unity3d.com>
2023-07-11 11:57:58 -0500

    revert __RpcParams to internal

--- com.unity.netcode.gameobjects/Runtime/Messaging/RpcParams.cs
+++ com.unity.netcode.gameobjects/Runtime/Messaging/RpcParams.cs
@@ -96,7 +96,7 @@ namespace Unity.Netcode
 
 #pragma warning disable IDE1006 // disable naming rule violation check
     // RuntimeAccessModifiersILPP will make this `public`
-    public struct __RpcParams
+    internal struct __RpcParams
 #pragma warning restore IDE1006 // restore naming rule violation check
     {
         public ServerRpcParams Server;
