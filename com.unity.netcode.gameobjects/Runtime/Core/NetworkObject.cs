2c62557ee75f95797e831a7eba37372d5efd4703
NoelStephensUnity <noel.stephens@unity3d.com>
2023-08-24 13:20:25 -0500

    Revert "update"
    This reverts commit c1eda40bab0144f6f57490c519b5ea4e09f58121.

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -41,20 +41,8 @@ namespace Unity.Netcode
                         return GlobalObjectIdHash;
                     }
                 }
-#if UNITY_EDITOR
-                // When in the editor, return 0 only if in play mode
-                if (Application.isPlaying)
-                {
-                    return 0;
-                }
-                else
-                {
-                    // Otherwise return the currently known GlobalObjectIdHash value
-                    return GlobalObjectIdHash;
-                }
-#else
+
                 return 0;
-#endif
             }
         }
 
