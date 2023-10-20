d89c68e65668d5eaa58bf292ca8b3e8a3c46431b
Tiphaine Rousseau <tiphaine.rousseau@unity3d.com>
2023-10-20 15:31:03 +0200

    feat!: no support of relay integration for 2021.3
    After discussion with product.
    This reverts f2e04638ea8a7b109c3290d32d594846e2ac72b7

--- com.unity.netcode.gameobjects/Editor/NetworkManagerEditor.cs
+++ com.unity.netcode.gameobjects/Editor/NetworkManagerEditor.cs
@@ -1,4 +1,4 @@
-#if (RELAY_SDK_INSTALLED && !UNITY_WEBGL ) || (RELAY_SDK_INSTALLED && UNITY_WEBGL && UTP_TRANSPORT_2_0_ABOVE)
+#if UNITY_2022_3_OR_NEWER && (RELAY_SDK_INSTALLED && !UNITY_WEBGL ) || (RELAY_SDK_INSTALLED && UNITY_WEBGL && UTP_TRANSPORT_2_0_ABOVE)
 #define RELAY_INTEGRATION_AVAILABLE
 #endif
 using System;
@@ -222,7 +222,7 @@ namespace Unity.Netcode.Editor
         private void DrawTransportField()
         {
 #if RELAY_INTEGRATION_AVAILABLE
-            var useRelay = EditorPrefs.GetBool(m_UseEasyRelayIntegrationKey, false);
+            var useRelay = EditorPrefs.GetBool(k_UseEasyRelayIntegrationKey, false);
 #else
             var useRelay = false;
 #endif
@@ -257,14 +257,7 @@ namespace Unity.Netcode.Editor
         }
 
 #if RELAY_INTEGRATION_AVAILABLE
-        private string m_UseEasyRelayIntegrationKey = "NetworkManagerUI_UseRelay"; // Value replaced in OnEnable
-
-        private void OnEnable()
-        {
-            // On 2021.3 dataPath cannot be called in a constructor
-            m_UseEasyRelayIntegrationKey = "NetworkManagerUI_UseRelay_" + Application.dataPath.GetHashCode();
-        }
-
+        private readonly string k_UseEasyRelayIntegrationKey = "NetworkManagerUI_UseRelay_" + Application.dataPath.GetHashCode();
         private string m_JoinCode = "";
         private string m_StartConnectionError = null;
         private string m_Region = "";
@@ -279,7 +272,7 @@ namespace Unity.Netcode.Editor
 
 #if RELAY_INTEGRATION_AVAILABLE
             // use editor prefs to persist the setting when entering / leaving play mode / exiting Unity
-            var useRelay = EditorPrefs.GetBool(m_UseEasyRelayIntegrationKey, false);
+            var useRelay = EditorPrefs.GetBool(k_UseEasyRelayIntegrationKey, false);
             GUILayout.BeginHorizontal();
             useRelay = GUILayout.Toggle(useRelay, "Try Relay in the editor");
 
@@ -291,9 +284,8 @@ namespace Unity.Netcode.Editor
             }
             GUILayout.EndHorizontal();
 
-            EditorPrefs.SetBool(m_UseEasyRelayIntegrationKey, useRelay);
-#if UNITY_2022_3_OR_NEWER
-            if(useRelay && !Application.isPlaying && !CloudProjectSettings.projectBound)
+            EditorPrefs.SetBool(k_UseEasyRelayIntegrationKey, useRelay);
+            if (useRelay && !Application.isPlaying && !CloudProjectSettings.projectBound)
             {
                 EditorGUILayout.HelpBox("To use relay, you need to setup your project in the Project Settings in the Services section.", MessageType.Warning);
                 if (GUILayout.Button("Open Project settings"))
@@ -301,7 +293,6 @@ namespace Unity.Netcode.Editor
                     SettingsService.OpenProjectSettings("Project/Services");
                 }
             }
-#endif
 #else
             var useRelay = false;
 #endif
