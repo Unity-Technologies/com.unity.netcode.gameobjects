f4d4c7fd12a91361d6917ee6aa0787e07e763492
Tiphaine Rousseau <tiphaine.rousseau@unity3d.com>
2023-10-30 15:17:37 +0100

    refactor: move everything to the editor assembly
    as discussed with Erick Boulay

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -1,17 +1,5 @@
-#if UNITY_2022_3_OR_NEWER && (RELAY_SDK_INSTALLED && !UNITY_WEBGL ) || (RELAY_SDK_INSTALLED && UNITY_WEBGL && UTP_TRANSPORT_2_0_ABOVE)
-#define RELAY_INTEGRATION_AVAILABLE
-#endif
 using System;
 using System.Collections.Generic;
-#if RELAY_INTEGRATION_AVAILABLE
-using System.Threading.Tasks;
-using Unity.Netcode.Transports.UTP;
-using Unity.Networking.Transport.Relay;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
-using Unity.Services.Relay;
-using Unity.Services.Relay.Models;
-#endif
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -917,106 +905,6 @@ namespace Unity.Netcode
 
             return IsListening;
         }
-
-#if RELAY_INTEGRATION_AVAILABLE
-#if UNITY_WEBGL
-        private const string k_DefaultConnectionType = "wss";
-#else
-        private const string k_DefaultConnectionType = "dtls";
-#endif
-
-        /// <summary>
-        /// Easy relay integration (host): it will initialize the unity services, sign in anonymously and start the host with a new relay allocation.
-        /// Note that this will force the use of Unity Transport.
-        /// </summary>
-        /// <param name="maxConnections">Maximum number of connections to the created relay.</param>
-        /// <param name="connectionType">The connection type of the <see cref="RelayServerData"/> (wss, ws, dtls or udp) </param>
-        /// <returns>The join code that a potential client can use and the allocation</returns>
-        /// <exception cref="ServicesInitializationException"> Exception when there's an error during services initialization </exception>
-        /// <exception cref="UnityProjectNotLinkedException"> Exception when the project is not linked to a cloud project id </exception>
-        /// <exception cref="CircularDependencyException"> Exception when two registered <see cref="IInitializablePackage"/> depend on the other </exception>
-        /// <exception cref="AuthenticationException"> The task fails with the exception when the task cannot complete successfully due to Authentication specific errors. </exception>
-        /// <exception cref="RequestFailedException"> See <see cref="IAuthenticationService.SignInAnonymouslyAsync"/></exception>
-        /// <exception cref="ArgumentException">Thrown when the maxConnections argument fails validation in Relay Service SDK.</exception>
-        /// <exception cref="RelayServiceException">Thrown when the request successfully reach the Relay Allocation Service but results in an error.</exception>
-        public async Task<(string, Allocation)> StartHostWithRelay(int maxConnections = 5, string connectionType = k_DefaultConnectionType)
-        {
-            var codeAndAllocation = await InitializeAndCreateAllocAsync(maxConnections, connectionType);
-            return StartHost() ? codeAndAllocation : (null, null);
-        }
-
-        /// <summary>
-        /// Easy relay integration (server): it will initialize the unity services, sign in anonymously and start the server with a new relay allocation.
-        /// Note that this will force the use of Unity Transport.
-        /// </summary>
-        /// <param name="maxConnections">Maximum number of connections to the created relay.</param>
-        /// <param name="connectionType">The connection type of the <see cref="RelayServerData"/> (wss, ws, dtls or udp) </param>
-        /// <returns>The join code that a potential client can use and the allocation.</returns>
-        /// <exception cref="ServicesInitializationException"> Exception when there's an error during services initialization </exception>
-        /// <exception cref="UnityProjectNotLinkedException"> Exception when the project is not linked to a cloud project id </exception>
-        /// <exception cref="CircularDependencyException"> Exception when two registered <see cref="IInitializablePackage"/> depend on the other </exception>
-        /// <exception cref="AuthenticationException"> The task fails with the exception when the task cannot complete successfully due to Authentication specific errors. </exception>
-        /// <exception cref="RequestFailedException"> See <see cref="IAuthenticationService.SignInAnonymouslyAsync"/></exception>
-        /// <exception cref="ArgumentException">Thrown when the maxConnections argument fails validation in Relay Service SDK.</exception>
-        /// <exception cref="RelayServiceException">Thrown when the request successfully reach the Relay Allocation Service but results in an error.</exception>
-        public async Task<(string, Allocation)> StartServerWithRelay(int maxConnections = 5, string connectionType = k_DefaultConnectionType)
-        {
-            var codeAndAllocation = await InitializeAndCreateAllocAsync(maxConnections, connectionType);
-            return StartServer() ? codeAndAllocation : (null, null);
-        }
-
-        /// <summary>
-        /// Easy relay integration (client): it will initialize the unity services, sign in anonymously, join the relay with the given join code and start the client.
-        /// Note that this will force the use of Unity Transport.
-        /// </summary>
-        /// <param name="joinCode">The join code of the allocation</param>
-        /// <param name="connectionType">The connection type of the <see cref="RelayServerData"/> (wss, ws, dtls or udp) </param>
-        /// <exception cref="ServicesInitializationException"> Exception when there's an error during services initialization </exception>
-        /// <exception cref="UnityProjectNotLinkedException"> Exception when the project is not linked to a cloud project id </exception>
-        /// <exception cref="CircularDependencyException"> Exception when two registered <see cref="IInitializablePackage"/> depend on the other </exception>
-        /// <exception cref="AuthenticationException"> The task fails with the exception when the task cannot complete successfully due to Authentication specific errors. </exception>
-        /// <exception cref="RequestFailedException">Thrown when the request does not reach the Relay Allocation Service.</exception>
-        /// <exception cref="ArgumentException">Thrown if the joinCode has the wrong format.</exception>
-        /// <exception cref="RelayServiceException">Thrown when the request successfully reach the Relay Allocation Service but results in an error.</exception>
-        /// <returns>True if starting the client was successful</returns>
-        public async Task<JoinAllocation> StartClientWithRelay(string joinCode, string connectionType = k_DefaultConnectionType)
-        {
-            await UnityServices.InitializeAsync();
-            if (!AuthenticationService.Instance.IsSignedIn)
-            {
-                await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            }
-            var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
-            GetUnityTransport(connectionType).SetRelayServerData(new RelayServerData(joinAllocation, k_DefaultConnectionType));
-            return StartClient() ? joinAllocation : null;
-        }
-
-        private async Task<(string, Allocation)> InitializeAndCreateAllocAsync(int maxConnections, string connectionType)
-        {
-            await UnityServices.InitializeAsync();
-            if (!AuthenticationService.Instance.IsSignedIn)
-            {
-                await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            }
-            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
-            GetUnityTransport(connectionType).SetRelayServerData(new RelayServerData(allocation, connectionType));
-            var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-            return (joinCode, allocation);
-        }
-
-        private UnityTransport GetUnityTransport(string connectionType)
-        {
-            if (!TryGetComponent<UnityTransport>(out var transport))
-            {
-                transport = gameObject.AddComponent<UnityTransport>();
-            }
-#if UTP_TRANSPORT_2_0_ABOVE
-            transport.UseWebSockets = connectionType.StartsWith("ws"); // Probably should be part of SetRelayServerData, but not possible at this point
-#endif
-            NetworkConfig.NetworkTransport = transport; // Force using UnityTransport
-            return transport;
-        }
-#endif
 
         /// <summary>
         /// Handles the host client creation logic along with
