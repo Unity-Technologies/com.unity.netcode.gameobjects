489273c5bc6082469ea225e1fa87a07549a1639b
NoelStephensUnity <noel.stephens@unity3d.com>
2023-11-16 14:09:06 -0600

    update
    Added some additional improvements to the simple spawn sample/manual test that also provides an example of the legacy/alternate way to spawn prefabs along with unique prefabs that are named appropriately for each scenario while also setting the color of the NetworkObjectId label to the assigned owner/client assigned color.

--- testproject/Assets/Samples/SpawnObject/SpawnObjectHandler.cs
+++ testproject/Assets/Samples/SpawnObject/SpawnObjectHandler.cs
@@ -7,27 +7,25 @@ using UnityEngine.UI;
 
 public class SpawnObjectHandler : NetworkBehaviour
 {
-    public Button SpawnObjectButton;
+    public Button AutoSpawnObjectButton;
+    public Button ManualSpawnObjectButton;
     public Dropdown ObjectToSpawnDropDown;
     public List<NetworkObject> PrefabsToSpawn;
 
     private Dictionary<int, NetworkObject> m_OptionToNetworkObject = new Dictionary<int, NetworkObject>();
 
+    private Vector3 m_SpawnOffset = new Vector3(0.0f, 0.5f, 0.0f);
 
     private void Start()
     {
-        SpawnObjectButton?.gameObject.SetActive(false);
+        AutoSpawnObjectButton?.gameObject.SetActive(false);
+        ManualSpawnObjectButton?.gameObject.SetActive(false);
         ObjectToSpawnDropDown.gameObject.SetActive(false);
     }
 
-    private void SpawnObject(ulong ownerId, int selection)
+    private void SetMotion(GameObject gameObject)
     {
-        if (!IsServer)
-        {
-            return;
-        }
-        var networkObject = NetworkManager.SpawnManager.InstantiateAndSpawn(m_OptionToNetworkObject[selection], ownerId);
-        var genericObjectBehaviour = networkObject.GetComponent<GenericNetworkObjectBehaviour>();
+        var genericObjectBehaviour = gameObject.GetComponent<GenericNetworkObjectBehaviour>();
         if (genericObjectBehaviour != null)
         {
             float ang = Random.Range(0.0f, 2 * Mathf.PI);
@@ -35,13 +33,45 @@ public class SpawnObjectHandler : NetworkBehaviour
         }
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    private void SpawnObjectServerRpc(int selection, ServerRpcParams serverRpcParams = default)
+    /// <summary>
+    /// Provides an alternate example of how to spawn objects while taking prefab overrides into consideration.
+    /// </summary>
+    private void ManualSpawnObject(ulong ownerId, int selection)
     {
-        SpawnObject(serverRpcParams.Receive.SenderClientId, selection);
+        var prefabToUse = NetworkManager.GetNetworkPrefabOverride(m_OptionToNetworkObject[selection].gameObject);
+        if (prefabToUse != null)
+        {
+            var instance = Instantiate(prefabToUse);
+            instance.transform.position += m_SpawnOffset;
+            var instanceNetworkObject = instance.GetComponent<NetworkObject>();
+            instanceNetworkObject.SpawnWithOwnership(ownerId);
+            SetMotion(instance);
+        }
     }
 
-    public void OnSpawnObjectClick()
+    /// <summary>
+    /// Provides an example of using <see cref="NetworkSpawnManager.InstantiateAndSpawn(NetworkObject, ulong, bool, bool, Vector3, Quaternion)"/> that
+    /// automatically determines the correct prefab to instantiate and spawn while also taking any prefab overrides into consideration.
+    /// Compare the steps of using this method to the <see cref="ManualSpawnObject(ulong, int)"/> script.
+    /// </summary>
+    private void AutoSpawnObject(ulong ownerId, int selection)
+    {
+        if (!IsServer)
+        {
+            return;
+        }
+
+        var networkObject = NetworkManager.SpawnManager.InstantiateAndSpawn(m_OptionToNetworkObject[selection], ownerId, position: m_SpawnOffset);
+        SetMotion(networkObject.gameObject);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void AutoSpawnObjectServerRpc(int selection, ServerRpcParams serverRpcParams = default)
+    {
+        AutoSpawnObject(serverRpcParams.Receive.SenderClientId, selection);
+    }
+
+    public void AutoSpawnClick()
     {
         if (!IsSpawned || ObjectToSpawnDropDown == null || PrefabsToSpawn.Count == 0)
         {
@@ -50,24 +80,48 @@ public class SpawnObjectHandler : NetworkBehaviour
 
         if (IsServer)
         {
-            SpawnObject(NetworkManager.ServerClientId, ObjectToSpawnDropDown.value);
+            AutoSpawnObject(NetworkManager.ServerClientId, ObjectToSpawnDropDown.value);
         }
         else
         {
-            SpawnObjectServerRpc(ObjectToSpawnDropDown.value);
+            AutoSpawnObjectServerRpc(ObjectToSpawnDropDown.value);
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void ManualSpawnObjectServerRpc(int selection, ServerRpcParams serverRpcParams = default)
+    {
+        AutoSpawnObject(serverRpcParams.Receive.SenderClientId, selection);
+    }
+
+    public void ManualSpawnClick()
+    {
+        if (!IsSpawned || ObjectToSpawnDropDown == null || PrefabsToSpawn.Count == 0)
+        {
+            return;
+        }
+
+        if (IsServer)
+        {
+            ManualSpawnObject(NetworkManager.ServerClientId, ObjectToSpawnDropDown.value);
+        }
+        else
+        {
+            ManualSpawnObjectServerRpc(ObjectToSpawnDropDown.value);
         }
     }
 
     public override void OnNetworkSpawn()
     {
-        SpawnObjectButton?.gameObject.SetActive(true);
+        AutoSpawnObjectButton?.gameObject.SetActive(true);
+        ManualSpawnObjectButton?.gameObject.SetActive(true);
         ObjectToSpawnDropDown?.gameObject.SetActive(true);
 
         if (ObjectToSpawnDropDown == null)
         {
             return;
         }
-
+        ObjectToSpawnDropDown.ClearOptions();
         foreach (var prefab in PrefabsToSpawn)
         {
             ObjectToSpawnDropDown.options.Add(new Dropdown.OptionData(prefab.name));
