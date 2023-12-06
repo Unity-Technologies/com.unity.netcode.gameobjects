6eea1eb03358debf8b2f83cbf927935a19eda23e
NoelStephensUnity <noel.stephens@unity3d.com>
2023-12-06 16:35:49 -0600

    update
    the test project exit button script is now a bit more intelligent with how it handles exiting and now logs the reason for disconnection.

--- testproject/Assets/Scripts/ExitButtonScript.cs
+++ testproject/Assets/Scripts/ExitButtonScript.cs
@@ -7,14 +7,68 @@ public class ExitButtonScript : MonoBehaviour
     [SerializeField]
     private MenuReference m_SceneMenuToLoad;
 
+    [SerializeField]
+    private bool m_DestroyNetworkManagerOnExit = true;
+
+    private void Start()
+    {
+        if (!NetworkManager.Singleton)
+        {
+            return;
+        }
+
+        NetworkManager.Singleton.OnServerStopped += OnNetworkManagerStopped;
+        NetworkManager.Singleton.OnClientStopped += OnNetworkManagerStopped;
+
+        NetworkManager.Singleton.OnServerStarted += OnNetworkManagerStarted;
+        NetworkManager.Singleton.OnClientStarted += OnNetworkManagerStarted;
+    }
+
+    private void OnNetworkManagerStarted()
+    {
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+    }
+
+    private void OnClientDisconnectCallback(ulong clientId)
+    {
+        var disconnectedReason = "Disconnected from session.";
+        if (NetworkManager.Singleton && !string.IsNullOrEmpty(NetworkManager.Singleton.DisconnectReason))
+        {
+            disconnectedReason = $"{NetworkManager.Singleton.DisconnectReason}";
+        }
+        Debug.Log($"[Client-{clientId}] {disconnectedReason}");
+    }
+
     /// <summary>
     /// A very basic way to exit back to the first scene in the build settings
     /// </summary>
     public void OnExitScene()
     {
-        if (NetworkManager.Singleton)
+        if (!NetworkManager.Singleton)
         {
-            NetworkManager.Singleton.Shutdown();
+            LoadExitScene();
+            return;
+        }
+
+        if (NetworkManager.Singleton.IsListening)
+        {
+            if (!NetworkManager.Singleton.ShutdownInProgress)
+            {
+
+                NetworkManager.Singleton.Shutdown();
+            }
+        }
+        else
+        {
+            LoadExitScene();
+        }
+    }
+
+    private void LoadExitScene()
+    {
+        if (m_DestroyNetworkManagerOnExit)
+        {
             Destroy(NetworkManager.Singleton.gameObject);
         }
 
@@ -26,6 +80,12 @@ public class ExitButtonScript : MonoBehaviour
         {
             SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
         }
+    }
 
+    private void OnNetworkManagerStopped(bool obj)
+    {
+        NetworkManager.Singleton.OnServerStopped -= OnNetworkManagerStopped;
+        NetworkManager.Singleton.OnClientStopped -= OnNetworkManagerStopped;
+        LoadExitScene();
     }
 }
