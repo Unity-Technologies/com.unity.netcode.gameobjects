d7f61599345a7feb2568b9edf8242cf1ebe64677
NoelStephensUnity <noel.stephens@unity3d.com>
2024-04-24 18:48:58 -0500

    update
    migrate when to apply in local space above the parenting check.

--- com.unity.netcode.gameobjects/Components/NetworkTransform.cs
+++ com.unity.netcode.gameobjects/Components/NetworkTransform.cs
@@ -1682,6 +1682,15 @@ namespace Unity.Netcode.Components
             var scale = transformToUse.localScale;
             networkState.IsSynchronizing = isSynchronization;
 
+            // All of the checks below, up to the delta position checking portion, are to determine if the
+            // authority changed a property during runtime that requires a full synchronizing.
+            if (InLocalSpace != networkState.InLocalSpace)
+            {
+                networkState.InLocalSpace = InLocalSpace;
+                isDirty = true;
+                networkState.IsTeleportingNextFrame = true;
+            }
+
             // Check for parenting when synchronizing and/or teleporting
             if (isSynchronization || networkState.IsTeleportingNextFrame)
             {
@@ -1735,15 +1744,6 @@ namespace Unity.Netcode.Components
                         networkState.InLocalSpace = true;
                     }
                 }
-            }
-
-            // All of the checks below, up to the delta position checking portion, are to determine if the
-            // authority changed a property during runtime that requires a full synchronizing.
-            if (InLocalSpace != networkState.InLocalSpace)
-            {
-                networkState.InLocalSpace = InLocalSpace;
-                isDirty = true;
-                networkState.IsTeleportingNextFrame = true;
             }
 
             if (Interpolate != networkState.UseInterpolation)
