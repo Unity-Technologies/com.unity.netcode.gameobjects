fd3bb9f5a601ee713d583dafe11cd65549d7b438
NoelStephensUnity <noel.stephens@unity3d.com>
2024-06-12 14:38:38 -0500

    update
    Modifying the EveryoneRpcTarget so that it uses the NotAuthorityRpcTarget and AuthorityRpcTarget when in distributed authority mode.

--- com.unity.netcode.gameobjects/Runtime/Messaging/RpcTargets/EveryoneRpcTarget.cs
+++ com.unity.netcode.gameobjects/Runtime/Messaging/RpcTargets/EveryoneRpcTarget.cs
@@ -4,23 +4,37 @@ namespace Unity.Netcode
     {
         private NotServerRpcTarget m_NotServerRpcTarget;
         private ServerRpcTarget m_ServerRpcTarget;
+        private NotAuthorityRpcTarget m_NotAuthorityRpcTarget;
+        private AuthorityRpcTarget m_AuthorityRpcTarget;
 
         public override void Dispose()
         {
             m_NotServerRpcTarget.Dispose();
             m_ServerRpcTarget.Dispose();
+            m_NotAuthorityRpcTarget.Dispose();
+            m_AuthorityRpcTarget.Dispose();
         }
 
         internal override void Send(NetworkBehaviour behaviour, ref RpcMessage message, NetworkDelivery delivery, RpcParams rpcParams)
         {
-            m_NotServerRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
-            m_ServerRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
+            if (NetworkManager.IsDistributedAuthority)
+            {
+                m_NotAuthorityRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
+                m_AuthorityRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
+            }
+            else
+            {
+                m_NotServerRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
+                m_ServerRpcTarget.Send(behaviour, ref message, delivery, rpcParams);
+            }
         }
 
         internal EveryoneRpcTarget(NetworkManager manager) : base(manager)
         {
             m_NotServerRpcTarget = new NotServerRpcTarget(manager);
             m_ServerRpcTarget = new ServerRpcTarget(manager);
+            m_NotAuthorityRpcTarget = new NotAuthorityRpcTarget(manager);
+            m_AuthorityRpcTarget = new AuthorityRpcTarget(manager);
         }
     }
 }
