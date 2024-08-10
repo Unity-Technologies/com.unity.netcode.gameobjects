523d8db8a2f832179a58ac7b4b81f5d67ab0b5ba
NoelStephensUnity <noel.stephens@unity3d.com>
2024-08-10 17:03:45 -0500

    fix
    Adjustments made due to other issues with direct assignment.

--- com.unity.netcode.gameobjects/Runtime/NetworkVariable/NetworkVariable.cs
+++ com.unity.netcode.gameobjects/Runtime/NetworkVariable/NetworkVariable.cs
@@ -79,6 +79,12 @@ namespace Unity.Netcode
         /// <summary>
         /// The value of the NetworkVariable container
         /// </summary>
+        /// <remarks>
+        /// When assigning collections to <see cref="Value"/>, unless it is a completely new collection this will not
+        /// detect any deltas with most managed collection classes since assignment of one collection value to another
+        /// is actually just a reference to the collection itself. <br />
+        /// To detect deltas in a collection, you should invoke <see cref="CheckDirtyState"/> after making modifications to the collection.
+        /// </remarks>
         public virtual T Value
         {
             get => m_InternalValue;
@@ -91,24 +97,25 @@ namespace Unity.Netcode
                 }
 
                 // Compare the Value being applied to the current value
-                if (!NetworkVariableSerialization<T>.AreEqual(ref m_InternalValue, ref value) ||
-                    // For collections, this will compare the items of the current vs previous to check for changes
-                    !NetworkVariableSerialization<T>.AreEqual(ref m_PreviousValue, ref m_InternalValue))
+                if (!NetworkVariableSerialization<T>.AreEqual(ref m_InternalValue, ref value))
                 {
-                    Set(value);
+                    T previousValue = m_InternalValue;
+                    m_InternalValue = value;
+                    SetDirty(true);
                     m_IsDisposed = false;
+                    OnValueChanged?.Invoke(previousValue, m_InternalValue);
                 }
             }
         }
 
         /// <summary>
-        /// Invoke this method to check if a managed collection's items are different. If so, mark the NetworkVariable dirty.
+        /// Invoke this method to check if a collection's items are dirty.
+        /// The default behavior is to exit early if the <see cref="NetworkVariable{T}"/> is already dirty.
         /// </summary>
         /// <param name="forceCheck"> when true, this check will force a full item collection check even if the NetworkVariable is already dirty</param>
         /// <remarks>
-        /// This is to be used as a way to check if a <see cref="NetworkVariable{T}"/> containing a managed collection has any changees to the collection items.
-        /// The default behavior is to exit early if the <see cref="NetworkVariable{T}"/> is already dirty.
-        /// You can set <param name="forceCheck"/> to true to force it to alawys check, even if dirty, in the event you want <see cref="OnValueChanged"/> to be triggered when already dirty.
+        /// This is to be used as a way to check if a <see cref="NetworkVariable{T}"/> containing a managed collection has any changees to the collection items.<br />
+        /// If you invoked this when a collection is dirty, it will not trigger the <see cref="OnValueChanged"/> unless you set <param name="forceCheck"/> to true. <br />
         /// </remarks>
         public bool CheckDirtyState(bool forceCheck = false)
         {
