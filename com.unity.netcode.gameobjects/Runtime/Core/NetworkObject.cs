76edc256c22bd1b8b95f303b0ddb6979e0dba666
NoelStephensUnity <noel.stephens@unity3d.com>
2025-02-18 15:50:22 -0600

    style
    adding API comments

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -509,10 +509,25 @@ namespace Unity.Netcode
         public enum OwnershipStatus
         {
             None = 0,
+            /// <summary>
+            ///  When set, this instance will be automatically redistributed when a client joins (if not locked or no request is pending) or leaves.
+            /// </summary>
             Distributable = 1 << 0,
+            /// <summary>
+            /// When set, a non-owner can obtain ownership immediately (without requesting and as long as it is not locked).
+            /// </summary>
             Transferable = 1 << 1,
+            /// <summary>
+            /// When set, a non-owner must request ownership from the owner (will always get locked once ownership is transferred).
+            /// </summary>
             RequestRequired = 1 << 2,
+            /// <summary>
+            /// When set, only the current session owner may have ownership over this object.
+            /// </summary>
             SessionOwner = 1 << 3,
+            /// <summary>
+            /// Used within the inspector view only. When selected it will set the Distributable, Transferable, and RequestRequired flags or if those flags are already set it will select the SessionOwner flag by itself.
+            /// </summary>
             All = ~0,
         }
 
