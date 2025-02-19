cb18994c1660956f7b23e8aefbe3b371e1e50818
michalChrobot <michal.chrobot@unity3d.com>
2025-02-19 10:15:47 +0100

    Example TODO comment

--- testproject/Legacy/MultiprocessRuntime/TestCoordinator.cs
+++ testproject/Legacy/MultiprocessRuntime/TestCoordinator.cs
@@ -114,7 +114,7 @@ public class TestCoordinator : NetworkBehaviour
         // Configuration via configuration file - all platform support but set at build time
         if (ConfigurationType == ConfigurationType.Unknown)
         {
-            //TODO: For next PR
+            //TODO: For next PR. Should we address this?
         }
 
         // configuration via WebApi - works on all platforms and is set at run time
