7d7594500d2074371b3319d573ccea1181e3a56f
NoelStephensUnity <noel.stephens@unity3d.com>
2025-03-11 21:59:11 -0500

    test
    enabling verbose debug mode

--- testproject/Assets/Tests/Runtime/ObjectParenting/ParentingWorldPositionStaysTests.cs
+++ testproject/Assets/Tests/Runtime/ObjectParenting/ParentingWorldPositionStaysTests.cs
@@ -158,6 +158,7 @@ namespace TestProject.RuntimeTests
 
         protected override IEnumerator OnSetup()
         {
+            m_EnableVerboseDebug = true;
             TestComponentHelper.ClientsRegistered.Clear();
             TestComponentHelper.NetworkObjectIdToIndex.Clear();
             for (int i = 0; i < k_NestedChildren; i++)
@@ -169,6 +170,7 @@ namespace TestProject.RuntimeTests
 
         protected override IEnumerator OnTearDown()
         {
+            m_EnableVerboseDebug = false;
             if (m_ServerSideParent != null && m_ServerSideParent.GetComponent<NetworkObject>().IsSpawned)
             {
                 // Clean up in reverse order (also makes sure we can despawn parents before children)
