a62099d7e075bce4ffaaa98eac16e05aa80a7af4
michalChrobot <michal.chrobot@unity3d.com>
2025-03-12 15:21:06 +0100

    Corrected library

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkTransformAnticipationTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkTransformAnticipationTests.cs
@@ -5,7 +5,7 @@ using NUnit.Framework;
 using Unity.Netcode.Components;
 using Unity.Netcode.TestHelpers.Runtime;
 using UnityEngine;
-using UnityEngine.TestTools;
+using UnityEngine.TestTools.Utils;
 using Object = UnityEngine.Object;
 
 namespace Unity.Netcode.RuntimeTests
