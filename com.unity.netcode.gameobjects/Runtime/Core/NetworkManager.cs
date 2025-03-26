ca7f9391b40c1d07f3a53752c7f4bfc650115a91
Jonathan Hertz <jonathan.hertz@unity3d.com>
2025-03-26 10:36:39 +0100

    Remove static from docs

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -478,7 +478,7 @@ namespace Unity.Netcode
         public event Action OnClientStarted = null;
 
         /// <summary>
-        /// Subscribe to this static event to get notifications before a <see cref="NetworkManager"/> instance is being destroyed.
+        /// Subscribe to this event to get notifications before a <see cref="NetworkManager"/> instance is being destroyed.
         /// This is useful if you want to use the state of anything the NetworkManager cleans up during its shutdown.
         /// </summary>
         public event Action OnPreShutdown = null;
