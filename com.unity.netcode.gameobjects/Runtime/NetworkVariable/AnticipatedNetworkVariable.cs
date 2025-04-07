40c6e58c3f1ac2af65b13f11c776d5e85f5b1687
michalChrobot <michal.chrobot@unity3d.com>
2025-04-07 14:54:39 +0200

    AnticipatedNetworkVariable corrected

--- com.unity.netcode.gameobjects/Runtime/NetworkVariable/AnticipatedNetworkVariable.cs
+++ com.unity.netcode.gameobjects/Runtime/NetworkVariable/AnticipatedNetworkVariable.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 namespace Unity.Netcode
 {
-
+    /// <summary>
+    /// Defines how anticipated network variables handle authoritative updates that are older than the current anticipated state
+    /// </summary>
     public enum StaleDataHandling
     {
+        /// <summary>
+        /// Ignores authoritative updates that are older than the current anticipated state.
+        /// The anticipated value will not be replaced until a newer authoritative update is received.
+        /// </summary>
         Ignore,
+
+        /// <summary>
+        /// Applies authoritative updates even if they are older than the current anticipated state.
+        /// This triggers reanticipation to calculate a new anticipated value based on the authoritative state.
+        /// </summary>
         Reanticipate
     }
 
@@ -85,6 +96,12 @@ namespace Unity.Netcode
 #pragma warning restore IDE0001
         public StaleDataHandling StaleDataHandling;
 
+        /// <summary>
+        /// Delegate for handling changes in the authoritative value
+        /// </summary>
+        /// <param name="variable">The network variable that changed</param>
+        /// <param name="previousValue">The previous value before the change</param>
+        /// <param name="newValue">The new value after the change</param>
         public delegate void OnAuthoritativeValueChangedDelegate(AnticipatedNetworkVariable<T> variable, in T previousValue, in T newValue);
 
         /// <summary>
@@ -121,6 +138,9 @@ namespace Unity.Netcode
 
         private AnticipatedObject m_AnticipatedObject;
 
+        /// <summary>
+        /// Initializes the network variable, setting up initial values and registering with the anticipation system
+        /// </summary>
         public override void OnInitialize()
         {
             m_AuthoritativeValue.Initialize(m_NetworkBehaviour);
@@ -133,6 +153,10 @@ namespace Unity.Netcode
             }
         }
 
+        /// <summary>
+        /// Checks if the current value has changed enough from its last synchronized value to warrant a new network update
+        /// </summary>
+        /// <returns>True if the value should be synchronized, false otherwise</returns>
         public override bool ExceedsDirtinessThreshold()
         {
             return m_AuthoritativeValue.ExceedsDirtinessThreshold();
@@ -227,6 +251,10 @@ namespace Unity.Netcode
         /// See <see cref="Mathf.Lerp"/>, <see cref="Vector3.Lerp"/>, <see cref="Vector3.Slerp"/>, and so on
         /// for examples.
         /// </summary>
+        /// <param name="authoritativeValue">The authoritative value to interpolate from</param>
+        /// <param name="anticipatedValue">The anticipated value to interpolate to</param>
+        /// <param name="amount">The interpolation factor between 0 and 1</param>
+        /// <returns>The interpolated value</returns>
         public delegate T SmoothDelegate(T authoritativeValue, T anticipatedValue, float amount);
 
         private SmoothDelegate m_SmoothDelegate = null;
@@ -242,6 +270,9 @@ namespace Unity.Netcode
             };
         }
 
+        /// <summary>
+        /// Updates the smooth interpolation state if active
+        /// </summary>
         public void Update()
         {
             if (m_CurrentSmoothTime < m_SmoothDuration)
@@ -253,6 +284,9 @@ namespace Unity.Netcode
             }
         }
 
+        /// <summary>
+        /// Releases all resources used by this network variable
+        /// </summary>
         public override void Dispose()
         {
             if (m_IsDisposed)
@@ -357,26 +391,45 @@ namespace Unity.Netcode
             m_HasSmoothValues = true;
         }
 
+        /// <summary>
+        /// Checks if the variable has been modified since the last network synchronization
+        /// </summary>
+        /// <returns>True if the variable needs to be synchronized, false otherwise</returns>
         public override bool IsDirty()
         {
             return m_AuthoritativeValue.IsDirty();
         }
 
+        /// <summary>
+        /// Resets the dirty state after network synchronization
+        /// </summary>
         public override void ResetDirty()
         {
             m_AuthoritativeValue.ResetDirty();
         }
 
+        /// <summary>
+        /// Writes only the changes in the variable's value to the network stream
+        /// </summary>
+        /// <param name="writer">Buffer to write the delta to</param>
         public override void WriteDelta(FastBufferWriter writer)
         {
             m_AuthoritativeValue.WriteDelta(writer);
         }
 
+        /// <summary>
+        /// Writes the complete state of the variable to the network stream
+        /// </summary>
+        /// <param name="writer">Buffer to write the field to</param>
         public override void WriteField(FastBufferWriter writer)
         {
             m_AuthoritativeValue.WriteField(writer);
         }
 
+        /// <summary>
+        /// Reads the complete state of the variable from the network stream
+        /// </summary>
+        /// <param name="reader">Buffer to read the field from</param>
         public override void ReadField(FastBufferReader reader)
         {
             m_AuthoritativeValue.ReadField(reader);
@@ -384,6 +437,11 @@ namespace Unity.Netcode
             NetworkVariableSerialization<T>.Duplicate(m_AnticipatedValue, ref m_PreviousAnticipatedValue);
         }
 
+        /// <summary>
+        /// Reads changes in the variable's value from the network stream
+        /// </summary>
+        /// <param name="reader">Buffer to read the delta from</param>
+        /// <param name="keepDirtyDelta">Whether to maintain the dirty state after reading</param>
         public override void ReadDelta(FastBufferReader reader, bool keepDirtyDelta)
         {
             m_AuthoritativeValue.ReadDelta(reader, keepDirtyDelta);
