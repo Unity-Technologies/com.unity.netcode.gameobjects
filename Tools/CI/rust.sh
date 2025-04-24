e5d76bbf2fe6a4e8365d2697333436fa40d8ce54
Emma <emma.mcmillan@unity3d.com>
2025-04-24 17:14:59 -0400

    use inline command instead of funtion

--- Tools/CI/rust.sh
+++ Tools/CI/rust.sh
@@ -15,13 +15,5 @@ cargo run --example ngo_echo_server -- --port $ECHO_SERVER_PORT &
 # Build the standalone server
 cargo build
 
-# Run the standalone server loop in the background
-runstandalone &
-
-# Use a function to run the standalone server in a loop, when it exits, it will always restart
-runstandalone() {
-  while :; do
-    # loop infinitely
-    cargo run -- --metrics-port 5000 standalone --port $COMB_SERVER_PORT -t 10m
-  done
-}
+# Run the standalone server on an infinite loop in the background
+while :; do cargo run -- --metrics-port 5000 standalone --port $COMB_SERVER_PORT -t 10m done &
