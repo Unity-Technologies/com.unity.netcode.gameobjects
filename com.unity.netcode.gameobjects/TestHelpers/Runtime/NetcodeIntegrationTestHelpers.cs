b2fed7f659d59700bcb01579a26661e1b8c33046
Emma <emma.mcmillan@unity3d.com>
2025-04-24 13:40:54 -0400

    Add missing import

--- com.unity.netcode.gameobjects/TestHelpers/Runtime/NetcodeIntegrationTestHelpers.cs
+++ com.unity.netcode.gameobjects/TestHelpers/Runtime/NetcodeIntegrationTestHelpers.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using NUnit.Framework;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
