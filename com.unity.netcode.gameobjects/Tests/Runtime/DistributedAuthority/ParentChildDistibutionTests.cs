ea7f253fbcba754dce397d39a93dc8f9ff1b14e3
NoelStephensUnity <noel.stephens@unity3d.com>
2025-04-25 15:28:51 -0500

    update: pvp 124-2
    Removing space at end of line.
    Minor private comment fixes.

--- com.unity.netcode.gameobjects/Tests/Runtime/DistributedAuthority/ParentChildDistibutionTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/DistributedAuthority/ParentChildDistibutionTests.cs
@@ -163,7 +163,17 @@ namespace Unity.Netcode.RuntimeTests
                 yield return StopOneClient(clientToReconnect);
             }
 
-            // When testing connect redistribution,
+            if (!UseCMBService())
+            {
+                m_NetworkManagers.Add(m_ServerNetworkManager);
+            }
+            m_NetworkManagers.AddRange(m_ClientNetworkManagers);
+            if (distributionType == DistributionTypes.UponConnect)
+            {
+                m_NetworkManagers.Remove(m_ClientNetworkManagers[3]);
+            }
+
+            // When testing connect redistribution
             var instances = distributionType == DistributionTypes.UponDisconnect ? 1 : 2;
             var rootObject = (GameObject)null;
             var childOne = (GameObject)null;
