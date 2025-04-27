f0d49aaad4d2f23828b1c732f73c6e14704789bb
Extrys <extrysgo@gmail.com>
2025-04-27 03:23:07 +0200

    insight on INetworkInstantiationPayloadSynchronizer in the INetworkPrefabInstanceHandler.Instantiate() documentation

--- com.unity.netcode.gameobjects/Runtime/Spawning/NetworkPrefabHandler.cs
+++ com.unity.netcode.gameobjects/Runtime/Spawning/NetworkPrefabHandler.cs
@@ -20,6 +20,9 @@ namespace Unity.Netcode
         ///
         /// Note on Pooling:  If you are using a NetworkObject pool, don't forget to make the NetworkObject active
         /// via the  <see cref="GameObject.SetActive(bool)"/> method.
+        /// 
+        /// If you need to pass custom data at instantiation time (e.g., selecting a variant, setting initialization parameters, or choosing a pre-instantiated object),
+        /// implement <see cref="INetworkInstantiationPayloadSynchronizer"/> alongside this interface to receive the data before instantiation.
         /// </summary>
         /// <param name="ownerClientId">the owner for the <see cref="NetworkObject"/> to be instantiated</param>
         /// <param name="position">the initial/default position for the <see cref="NetworkObject"/> to be instantiated</param>
