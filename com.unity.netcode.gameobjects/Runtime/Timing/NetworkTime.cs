51a18b4b9e06929082fb0d3c67446be4e25697b2
michalChrobot <michal.chrobot@unity3d.com>
2025-04-30 11:11:00 +0200

    Added change so wrench will regenerate

--- com.unity.netcode.gameobjects/Runtime/Timing/NetworkTime.cs
+++ com.unity.netcode.gameobjects/Runtime/Timing/NetworkTime.cs
@@ -119,7 +119,7 @@ namespace Unity.Netcode
         }
 
         /// <summary>
-        /// Calculates a NetworkTime value representing a point in the past relative to the current time (few ticks in the past)
+        /// Calculates a NetworkTime value representing a point in the past relative to the current time (few ticks in the past).
         /// </summary>
         /// <param name="ticks">The number of ticks ago we're querying the time.</param>
         /// <returns>A NetworkTime value representing the calculated past time point</returns>
