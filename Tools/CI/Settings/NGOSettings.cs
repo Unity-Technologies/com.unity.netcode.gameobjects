20f295252c438f3ec071c551c6d9a761d01d4a0a
Michał Chrobot <124174716+michalChrobot@users.noreply.github.com>
2025-08-20 14:49:43 +0200

    ci: Wrench settings update (#3612)
    ## Purpose of this PR
    Quick update of options that are no longer applicable 
    
    ### Jira ticket
    N/A
    
    ## Documentation
    N/A
    
    ## Testing & QA
    N/A
    
    ## Backport
    Will do

--- Tools/CI/Settings/NGOSettings.cs
+++ Tools/CI/Settings/NGOSettings.cs
@@ -32,12 +32,10 @@ public class NGOSettings : AnnotatedSettingsBase
     {
         Wrench = new WrenchSettings(
             packagesRootPaths,
-            PackageOptions,
-            false,
-            false,
-            @"Tools\CI\NGO.Cookbook.csproj"); // There should be fix soon and there should be no need of specifying the path
+            PackageOptions
+        );
 
-        Wrench.PvpProfilesToCheck = new HashSet<string>() { "supported" };
+    Wrench.PvpProfilesToCheck = new HashSet<string>() { "supported" };
     }
 
     public WrenchSettings Wrench { get; private set; }
