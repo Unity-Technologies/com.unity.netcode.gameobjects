327169aa9d05ae2f086965a8e26c1c8ef9971e7e
michal-chrobot <michal.chrobot@unity3d.com>
2025-09-11 13:40:42 +0200

    release.py update

--- Tools/scripts/release.py
+++ Tools/scripts/release.py
@@ -6,38 +6,42 @@ This python script makes the NGO package release ready. What it does is:
 Note that this script NEEDS TO BE RUN FROM THE ROOT of the project.
 """
 #!/usr/bin/env python3
+import json
 import os
+import re
 import sys
+import subprocess
+import platform
 
-UTILS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), './Utils'))
-sys.path.insert(0, UTILS_DIR)
-from general_utils import get_package_version_from_manifest, update_changelog, update_validation_exceptions # nopep8
-from config import getNetcodePackageName, getPackageManifestPath, getPackageValidationExceptionsPath, getPackageChangelogPath # nopep8
+from Utils.general_utils import get_package_version_from_manifest, update_changelog, update_validation_exceptions # nopep8
 
-if __name__ == '__main__':
-    
-    ngo_package_name = getNetcodePackageName()
-    ngo_manifest_path = getPackageManifestPath()
-    ngo_validation_exceptions_path = getPackageValidationExceptionsPath()
-    ngo_changelog_path = getPackageChangelogPath()
+def make_package_release_ready(manifest_path, changelog_path, validation_exceptions_path, package_version):
 
-    if not os.path.exists(ngo_manifest_path):
-        print(f" Path does not exist: {ngo_manifest_path}")
-        sys.exit(1)
-        
-    if not os.path.exists(ngo_changelog_path):
-        print(f" Path CHANGELOG does not exist: {ngo_changelog_path}")
+    if not os.path.exists(manifest_path):
+        print(f" Path does not exist: {manifest_path}")
         sys.exit(1)
 
-    ngo_package_version = get_package_version_from_manifest(ngo_manifest_path)
+    if not os.path.exists(changelog_path):
+        print(f" Path does not exist: {changelog_path}")
+        sys.exit(1)
 
-    if ngo_package_version is None:
-        print(f"Package version not found at {ngo_manifest_path}")
+    if package_version is None:
+        print(f"Package version not found at {manifest_path}")
         sys.exit(1)
 
     # Update the ValidationExceptions.json file
     # with the new package version OR remove it if not a release branch
-    update_validation_exceptions(ngo_validation_exceptions_path, ngo_package_version)
+    update_validation_exceptions(validation_exceptions_path, package_version)
     # Clean the CHANGELOG and add latest entry
-    # package version is already know as is always corresponds to current package state
-    update_changelog(ngo_changelog_path, ngo_package_version)
+    # package version is already know as explained in
+    # https://github.cds.internal.unity3d.com/unity/dots/pull/14318
+    update_changelog(changelog_path, package_version)
+
+
+if __name__ == '__main__':
+    manifest_path = 'com.unity.netcode.gameobjects/package.json'
+    changelog_path = 'com.unity.netcode.gameobjects/CHANGELOG.md'
+    validation_exceptions_path = 'com.unity.netcode.gameobjects/ValidationExceptions.json'
+    package_version = get_package_version_from_manifest(manifest_path)
+
+    make_package_release_ready(manifest_path, changelog_path, validation_exceptions_path, package_version)
