bb4cded6e1e8b8e0c81e9bb655a9995ea0a32ffa
Emma <emma.mcmillan@unity3d.com>
2025-10-08 09:31:49 -0400

    dotnet-fix

--- com.unity.netcode.gameobjects/Runtime/Spawning/NetworkSpawnManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Spawning/NetworkSpawnManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
-using Unity.Collections;
 using UnityEngine;
 
 namespace Unity.Netcode
