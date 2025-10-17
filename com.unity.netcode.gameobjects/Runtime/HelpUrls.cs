b4f85f205baacc783215d6f664b5311672f04497
Noel Stephens <noel.stephens@unity3d.com>
2025-10-17 09:14:55 -0500

    fix
    replaced .md with .html

--- com.unity.netcode.gameobjects/Runtime/HelpUrls.cs
+++ com.unity.netcode.gameobjects/Runtime/HelpUrls.cs
@@ -10,8 +10,8 @@ namespace Unity.Netcode.Runtime
         public const string NetworkManager = k_BaseManualUrl + "components/core/networkmanager.html";
         public const string NetworkObject = k_BaseManualUrl + "components/core/networkobject.html";
         public const string NetworkAnimator = k_BaseManualUrl + "components/helper/networkanimator.html";
-        public const string NetworkRigidbody = k_BaseManualUrl + "components/helper/networkrigidbody.md";
-        public const string NetworkRigidbody2D = k_BaseManualUrl + "components/helper/networkrigidbody.md";
+        public const string NetworkRigidbody = k_BaseManualUrl + "components/helper/networkrigidbody.html";
+        public const string NetworkRigidbody2D = k_BaseManualUrl + "components/helper/networkrigidbody.html";
         public const string RigidbodyContactEventManager = k_BaseApiUrl + ".Components.RigidbodyContactEventManager.html";
         public const string NetworkTransform = k_BaseManualUrl + "components/helper/networktransform.html";
         public const string AnticipatedNetworkTransform = k_BaseManualUrl + "advanced-topics/client-anticipation.html";
