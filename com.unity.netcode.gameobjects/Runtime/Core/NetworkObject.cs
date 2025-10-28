b430664b49496125304604f2d1a5d08903606ad7
Noel Stephens <noel.stephens@unity3d.com>
2025-10-28 10:14:40 -0500

    update
    Adding a warning generation method to make testing easier.

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkObject.cs
@@ -2619,6 +2619,11 @@ namespace Unity.Netcode
 
         private List<NetworkBehaviour> m_ChildNetworkBehaviours;
 
+        internal string GenerateDisabledNetworkBehaviourWarning(NetworkBehaviour networkBehaviour)
+        {
+            return $"[{name}][{networkBehaviour.GetType().Name}][{nameof(isActiveAndEnabled)}: {networkBehaviour.isActiveAndEnabled}] Disabled {nameof(NetworkBehaviour)}s are not supported and will be excluded from spawning and synchronization!";
+        }
+
         internal List<NetworkBehaviour> ChildNetworkBehaviours
         {
             get
@@ -2641,7 +2646,7 @@ namespace Unity.Netcode
                     }
                     else if (!networkBehaviours[i].isActiveAndEnabled)
                     {
-                        Debug.LogWarning($"[{name}][{networkBehaviours[i].GetType().Name}][{nameof(isActiveAndEnabled)}: {networkBehaviours[i].isActiveAndEnabled}] Disabled {nameof(NetworkBehaviour)}s are not supported and will be excluded from spawning and synchronization!");
+                        Debug.LogWarning(GenerateDisabledNetworkBehaviourWarning(networkBehaviours[i]));
                         continue;
                     }
 
