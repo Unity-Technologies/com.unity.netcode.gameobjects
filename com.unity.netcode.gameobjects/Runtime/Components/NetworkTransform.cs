07dc14d50f39f23d893d858e86e1889d51f5ec92
Emma <emma.mcmillan@unity3d.com>
2025-11-25 09:22:33 -0500

    Unbreak breaking change

--- com.unity.netcode.gameobjects/Runtime/Components/NetworkTransform.cs
+++ com.unity.netcode.gameobjects/Runtime/Components/NetworkTransform.cs
@@ -3817,6 +3817,17 @@ namespace Unity.Netcode.Components
 
         #region PARENTING AND OWNERSHIP
         /// <inheritdoc/>
+        public override void OnLostOwnership()
+        {
+            base.OnLostOwnership();
+        }
+
+        /// <inheritdoc/>
+        public override void OnGainedOwnership()
+        {
+            base.OnGainedOwnership();
+        }
+        /// <inheritdoc/>
         protected override void OnOwnershipChanged(ulong previous, ulong current)
         {
             // If we were the previous owner or the newly assigned owner then reinitialize
@@ -3831,17 +3842,6 @@ namespace Unity.Netcode.Components
         private List<NetworkObject> m_ParentedChildren = new List<NetworkObject>();
 
         private bool m_IsFirstNetworkTransform;
-        internal void ChildRegistration(NetworkObject child, bool isAdding)
-        {
-            if (isAdding)
-            {
-                m_ParentedChildren.Add(child);
-            }
-            else
-            {
-                m_ParentedChildren.Remove(child);
-            }
-        }
 
         /// <inheritdoc/>
         /// <remarks>
