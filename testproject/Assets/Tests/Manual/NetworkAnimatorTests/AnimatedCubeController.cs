b37d12172b1c36ab44d9ebf6eea8bc52e0ce408b
Noel Stephens <noel.stephens@unity3d.com>
2025-11-27 15:01:03 -0600

    fix: NetworkAnimator does not allow disabling or enabling parameters for synch (#3586)
    * update
    
    Provide users with a way to disable updating changes to specific parameters.
    
    * update
    
    Don't include excluded parameters during initial synchronization.
    
    * update
    
    Adding the ability to select server or owner authority mode via the inspector view.
    
    * update - wip
    
    Work in progress.
    
    * update
    
    Test project manual tests for animation (of which some are used for integration testing).
    
    * update
    
    Finalizing editor inspector view UI for excluding animator properties.
    Added some additional logic to NetworkAnimator.
    Added first test that verifies excluding parameters works.
    
    * update
    
    Renamed the new method `ToggleParameterSync` to `EnableParameterSynchronization`. Added  overridden version of `EnableParameterSynchronization` that accepts a string for the name and changed the original method to accept the hash value of the parameter name.
    
    Did some minor clean up in the `NetworkAnimatorStateChangeHandler.NetworkUpdate` method.
    
    Added some additional comments in various places.
    
    * update
    
    Reverted the change in HiddenScriptEditor.
    Moved away from a NetworkAnimator custom editor to a custom property drawer to handle displaying the Animator parameters to be synchronized.
    Handling the population of parameters and populating the NetworkAnimator.AnimatorParameterEntries during OnValidate.
    Had to re-apply unchecking the excluded parameter in some prefabs for testing purposes.
    
    * update
    
    Renaming NetworkAnimatorParameterEntryEditor to  NetworkAnimatorParameterEntryDrawer since it is a property drawer.
    
    Excluding NetworkAnimatorParameterEntryDrawer when the animation package is not installed.
    
    * update
    
    Converting all legacy ClientRpc and ServerRpc usages over to the newer universal RPC format.
    Using a pre-allocated Persistent RpcTargetGroup as opposed to the List.
    When using `NetworkAnimator.SetTrigger` with a client-server network topology and it is the server or host invoking the method, then go ahead and set the trigger since it no longer sends itself an RPC/no longer invokes the InternalSetTrigger via RPC.
    
    * update
    
    Adding change log entries.
    
    * style
    
    adjusting typo
    
    * update
    
    Implementing suggested changes.
    
    * fix
    
    Fixing issue discovered by Noellie in the HasAuthority check.
    
    * update
    
    fixing change log
    
    * update
    
    Always assure owner authoritative mode when in distributed authority mode.
    
    * doc update
    
    Updating the NetworkAnimator documentation for this fix.
    
    * style - PVP
    
    PVP-124-2
    Removing trailing spaces

--- testproject/Assets/Tests/Manual/NetworkAnimatorTests/AnimatedCubeController.cs
+++ testproject/Assets/Tests/Manual/NetworkAnimatorTests/AnimatedCubeController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using Unity.Netcode;
 using Unity.Netcode.Components;
@@ -12,19 +11,11 @@ namespace Tests.Manual.NetworkAnimatorTests
         private Animator m_Animator;
         private bool m_Rotate;
         private NetworkAnimator m_NetworkAnimator;
-        private bool m_IsServerAuthoritative = true;
+        private bool m_IsServerAuthoritative => m_NetworkAnimator ? m_NetworkAnimator.IsServerAuthoritative() : true;
 
         private void DetermineNetworkAnimatorComponentType()
         {
             m_NetworkAnimator = GetComponent<NetworkAnimator>();
-            if (m_NetworkAnimator != null)
-            {
-                m_IsServerAuthoritative = m_NetworkAnimator.GetType() != typeof(OwnerNetworkAnimator);
-            }
-            else
-            {
-                throw new Exception($"{nameof(AnimatedCubeController)} requires that it is paired with either a {nameof(NetworkAnimator)} or {nameof(OwnerNetworkAnimator)}.  Neither of the two components were found!");
-            }
         }
 
         public override void OnNetworkSpawn()
@@ -136,8 +127,8 @@ namespace Tests.Manual.NetworkAnimatorTests
             counter = 0.0f;
             while (counter < 100)
             {
-                m_Animator.SetFloat("TestFloat", UnityEngine.Random.Range(0.0f, 100.0f));
-                m_Animator.SetInteger("TestInt", UnityEngine.Random.Range(0, 100));
+                m_Animator.SetFloat("TestFloat", Random.Range(0.0f, 100.0f));
+                m_Animator.SetInteger("TestInt", Random.Range(0, 100));
                 counter++;
                 yield return waitForSeconds;
             }
