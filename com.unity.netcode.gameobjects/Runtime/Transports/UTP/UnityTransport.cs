80b0c3c78ffef537203bedc18b7921a14b8d1afa
Noel Stephens <noel.stephens@unity3d.com>
2025-12-01 13:27:49 -0600

    update
    Initial approach to resolve the circular dependency will be to use two unique defines created by both packages.
    NGO will use the assembly version define MP_TOOLS_2_2_8_OR_HIGHER.
    MP Tools will use the assembly version define NGO_2_8_0_OR_HIGHER.

--- com.unity.netcode.gameobjects/Runtime/Transports/UTP/UnityTransport.cs
+++ com.unity.netcode.gameobjects/Runtime/Transports/UTP/UnityTransport.cs
@@ -360,17 +360,14 @@ namespace Unity.Netcode.Transports.UTP
         /// TODO-FIXME:
         /// Multiplayer Tools subscribes to this event and does not have the EntityId udpate.
         /// </summary>
-#if FIXED
-#if UNITY_6000_2_OR_NEWER
+
+#if UNITY_6000_2_OR_NEWER && MP_TOOLS_2_2_8_OR_HIGHER
         internal static event Action<EntityId, NetworkDriver> TransportInitialized;
         internal static event Action<EntityId> TransportDisposed;
 #else
         internal static event Action<int, NetworkDriver> TransportInitialized;
         internal static event Action<int> TransportDisposed;
 #endif
-#endif
-        internal static event Action<int, NetworkDriver> TransportInitialized;
-        internal static event Action<int> TransportDisposed;
 
         /// <summary>
         /// Provides access to the <see cref="NetworkDriver"/> for this instance.
@@ -448,14 +445,12 @@ namespace Unity.Netcode.Transports.UTP
                 out m_UnreliableSequencedFragmentedPipeline,
                 out m_ReliableSequencedPipeline);
 #if UNITY_6000_2_OR_NEWER
+#if MP_TOOLS_2_2_8_OR_HIGHER
             var entityId = GetEntityId();
-#if UNITY_6000_3_0A6_OR_HIGHER
-            // TODO-FIXME: Since multiplayer tools subscribes to this and we have to validate against any package that
-            // might use this action, we have to cast it down temporarily to avoid being blocked from getting these fixes in place.
-            TransportInitialized?.Invoke((int)entityId.GetRawData(), m_Driver);
 #else
-            TransportInitialized?.Invoke(entityId, m_Driver);
+            var entityId = GetEntityId().GetHashCode();
 #endif
+            TransportInitialized?.Invoke(entityId, m_Driver);
 #else
             TransportInitialized?.Invoke(GetInstanceID(), m_Driver);
 #endif
@@ -476,15 +471,12 @@ namespace Unity.Netcode.Transports.UTP
             m_SendQueue.Clear();
 
 #if UNITY_6000_2_OR_NEWER
+#if MP_TOOLS_2_2_8_OR_HIGHER
             var entityId = GetEntityId();
-#if UNITY_6000_3_0A6_OR_HIGHER
-            // TODO-FIXME: Since multiplayer tools subscribes to this and we have to validate against any package that
-            // might use this action, we have to cast it down temporarily to avoid being blocked from getting these fixes in place.
-            TransportDisposed?.Invoke((int)entityId.GetRawData());
 #else
-            TransportDisposed?.Invoke(entityId, m_Driver);
+            var entityId = GetEntityId().GetHashCode();
 #endif
-
+            TransportDisposed?.Invoke(entityId);
 #else
             TransportDisposed?.Invoke(GetInstanceID());
 #endif
