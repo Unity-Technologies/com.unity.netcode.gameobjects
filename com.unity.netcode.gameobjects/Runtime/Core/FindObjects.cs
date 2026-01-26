db390f587c5f38a5faaee1b197f52040bc25e353
Noel Stephens <noel.stephens@unity3d.com>
2026-01-26 16:07:58 -0600

    Update com.unity.netcode.gameobjects/Runtime/Core/FindObjects.cs
    Co-authored-by: Emma <emma.mcmillan@unity3d.com>

--- com.unity.netcode.gameobjects/Runtime/Core/FindObjects.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/FindObjects.cs
@@ -10,12 +10,10 @@ namespace Unity.Netcode
         {
 #if NGO_FINDOBJECTS_NOSORTING
             var results = Object.FindObjectsByType<T>();
-#else
-#if UNITY_2023_1_OR_NEWER
+#elif UNITY_2023_1_OR_NEWER
             var results = Object.FindObjectsByType<T>(FindObjectsSortMode.None);
 #else
             var results = Object.FindObjectsOfType<T>();
-#endif
 #endif
             return results;
         }
