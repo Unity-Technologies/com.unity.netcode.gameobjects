bf4f27cf1974f49696e0dc29e1acc5bf99860930
Noel Stephens <noel.stephens@unity3d.com>
2026-04-29 11:38:48 -0500

    update
    Placing the UnifiedBootstrap into its own file.

--- com.unity.netcode.gameobjects/Runtime/Components/Helpers/NetworkObjectBridge.cs
+++ com.unity.netcode.gameobjects/Runtime/Components/Helpers/NetworkObjectBridge.cs
@@ -1,12 +1,8 @@
 #if UNIFIED_NETCODE
-using System;
-using Unity.Entities;
 using Unity.NetCode;
-using UnityEngine;
 
 namespace Unity.Netcode
 {
-
 #if UNIFIED_NETCODE
     /// <summary>
     /// TODO-UNIFIED: Needs further peer review and exploring alternate ways of handling this.
@@ -55,90 +51,5 @@ namespace Unity.Netcode
         }
     }
 #endif
-
-    /// <summary>
-    /// TODO-UNIFIED: Would need to be reviewed for alternate ways of handling this.
-    /// Creates the hosted world and provides a means to configuring
-    /// the 2nd port for unified netcode connection.
-    /// </summary>
-    internal class UnifiedBootStrap : ClientServerBootstrap
-    {
-        public static UnifiedBootStrap Instance { get; private set; }
-        public static Action OnInitialized;
-        public static ushort Port = 7979;
-        public static NetworkManager CurrentNetworkManagerForInitialization;
-
-        public static World LastCreatedWorld { get; private set; }
-
-        private static int s_WorldCounter = 0;
-        
-        public override bool Initialize(string defaultWorldName)
-        {
-            var networkManager = CurrentNetworkManagerForInitialization;
-            if (networkManager == NetworkManager.Singleton)
-            {
-                Instance = this;
-            }
-
-            AutoConnectPort = Port;
-            if (base.Initialize(defaultWorldName))
-            {
-                Debug.LogError($"[{nameof(UnifiedBootStrap)}] Auto-bootstrap is enabled!!! This will break the POC!");
-                return true;
-            }
-
-            if (networkManager != null)
-            {
-                Debug.Log($"Starting a world for {(networkManager.IsServer ? "Host" : "Client")}");
-                s_WorldCounter++;
-                LastCreatedWorld = networkManager.IsServer ? CreateSingleWorldHost($"HostSingleWorld-{s_WorldCounter}")
-                    : CreateClientWorld($"ClientWorld-{s_WorldCounter}");
-
-                if (LastCreatedWorld == null)
-                {
-                    s_WorldCounter--;
-                    Debug.LogError($"[{nameof(UnifiedBootStrap)}] World is null!");
-                    return false;
-                }
-
-                if (!LastCreatedWorld.IsCreated)
-                {
-                    s_WorldCounter--;
-                    Debug.LogError($"[{nameof(UnifiedBootStrap)}] World was not created!");
-                    return false;
-                }
-
-                //if (networkManager.LogLevel <= LogLevel.Developer)
-                {
-                    NetworkLog.LogInfo($"[{nameof(UnifiedBootStrap)}] Created world: {LastCreatedWorld.Name} / {LastCreatedWorld.SequenceNumber}");
-                }
-
-                networkManager.NetcodeWorld = (NetcodeWorld)LastCreatedWorld;
-                if (networkManager.NetworkConfig.Prefabs.HasPendingGhostPrefabs)
-                {
-                    if (networkManager.LogLevel <= LogLevel.Developer)
-                    {
-                        NetworkLog.LogInfo($"[{nameof(UnifiedBootStrap)}] Registering hybrid prefabs...");
-                    }
-
-                    networkManager.NetworkConfig.Prefabs.RegisterGhostPrefabs(networkManager);
-                }
-            }
-            else
-            {
-                LastCreatedWorld = CreateLocalWorld("LocalWorld");
-            }
-
-            OnInitialized?.Invoke();
-
-            return true;
-        }
-
-        ~UnifiedBootStrap()
-        {
-            LastCreatedWorld = null;
-            Instance = null;
-        }
-    }
 }
 #endif
---
+++ com.unity.netcode.gameobjects/Runtime/Components/Helpers/UnifiedBootstrap.cs
@@ -0,0 +1,94 @@
+#if UNIFIED_NETCODE
+using System;
+using Unity.Entities;
+using Unity.NetCode;
+using UnityEngine;
+
+namespace Unity.Netcode
+{
+    /// <summary>
+    /// TODO-UNIFIED: Would need to be reviewed for alternate ways of handling this.
+    /// Creates the hosted world and provides a means to configuring
+    /// the 2nd port for unified netcode connection.
+    /// </summary>
+    internal class UnifiedBootstrap : ClientServerBootstrap
+    {
+        public static UnifiedBootstrap Instance { get; private set; }
+        public static Action OnInitialized;
+        public static ushort Port = 7979;
+        public static NetworkManager CurrentNetworkManagerForInitialization;
+
+        public static World LastCreatedWorld { get; private set; }
+
+        private static int s_WorldCounter = 0;
+
+        public override bool Initialize(string defaultWorldName)
+        {
+            var networkManager = CurrentNetworkManagerForInitialization;
+            if (networkManager == NetworkManager.Singleton)
+            {
+                Instance = this;
+            }
+
+            AutoConnectPort = Port;
+            if (base.Initialize(defaultWorldName))
+            {
+                Debug.LogError($"[{nameof(UnifiedBootstrap)}] Auto-bootstrap is enabled!!! This will break the POC!");
+                return true;
+            }
+
+            if (networkManager != null)
+            {
+                Debug.Log($"Starting a world for {(networkManager.IsServer ? "Host" : "Client")}");
+                s_WorldCounter++;
+                LastCreatedWorld = networkManager.IsServer ? CreateSingleWorldHost($"HostSingleWorld-{s_WorldCounter}")
+                    : CreateClientWorld($"ClientWorld-{s_WorldCounter}");
+
+                if (LastCreatedWorld == null)
+                {
+                    s_WorldCounter--;
+                    Debug.LogError($"[{nameof(UnifiedBootstrap)}] World is null!");
+                    return false;
+                }
+
+                if (!LastCreatedWorld.IsCreated)
+                {
+                    s_WorldCounter--;
+                    Debug.LogError($"[{nameof(UnifiedBootstrap)}] World was not created!");
+                    return false;
+                }
+
+                //if (networkManager.LogLevel <= LogLevel.Developer)
+                {
+                    NetworkLog.LogInfo($"[{nameof(UnifiedBootstrap)}] Created world: {LastCreatedWorld.Name} / {LastCreatedWorld.SequenceNumber}");
+                }
+
+                networkManager.NetcodeWorld = (NetcodeWorld)LastCreatedWorld;
+                if (networkManager.NetworkConfig.Prefabs.HasPendingGhostPrefabs)
+                {
+                    if (networkManager.LogLevel <= LogLevel.Developer)
+                    {
+                        NetworkLog.LogInfo($"[{nameof(UnifiedBootstrap)}] Registering hybrid prefabs...");
+                    }
+
+                    networkManager.NetworkConfig.Prefabs.RegisterGhostPrefabs(networkManager);
+                }
+            }
+            else
+            {
+                LastCreatedWorld = CreateLocalWorld("LocalWorld");
+            }
+
+            OnInitialized?.Invoke();
+
+            return true;
+        }
+
+        ~UnifiedBootstrap()
+        {
+            LastCreatedWorld = null;
+            Instance = null;
+        }
+    }
+}
+#endif
