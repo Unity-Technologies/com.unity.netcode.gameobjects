ad904214034e50e12b3d043c36f1090b36004f3e
Noel Stephens <noel.stephens@unity3d.com>
2026-04-29 11:40:00 -0500

    test
    Wrapping the unified test within the UNIFIED_NETCODE define.

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkTransform/UnifiedNetworkTransformTest.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkTransform/UnifiedNetworkTransformTest.cs
@@ -1,3 +1,4 @@
+#if UNIFIED_NETCODE
 using System.Collections;
 using NUnit.Framework;
 using Unity.Netcode.Components;
@@ -130,3 +131,4 @@ namespace Unity.Netcode.RuntimeTests
         }
     }
 }
+#endif
