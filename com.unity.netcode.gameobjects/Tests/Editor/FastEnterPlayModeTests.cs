99b4f1807df381d5df6a2967b3b1fe76857be722
Noellie Velez <noellie.velez@unity3d.com>
2026-05-21 18:22:43 +0200

    Adding NetworkManager singleton reset test

---
+++ com.unity.netcode.gameobjects/Tests/Editor/FastEnterPlayModeTests.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine.TestTools;
+
+namespace Unity.Netcode.EditorTests
+{
+    public class FastEnterPlayModeTests
+    {
+        [UnityTest]
+        public IEnumerator NetworkManagerSingletonResetsOnPlayModeEnter(
+            [Values(EnterPlayModeOptions.None,
+                EnterPlayModeOptions.DisableDomainReload | EnterPlayModeOptions.DisableSceneReload)]
+            EnterPlayModeOptions playmodeOption)
+        {
+            EditorSettings.enterPlayModeOptionsEnabled = true;
+            EditorSettings.enterPlayModeOptions = playmodeOption;
+
+            // First play session — create a NetworkManager to set Singleton
+            yield return new EnterPlayMode();
+
+            yield return new ExitPlayMode();
+
+            // Restore default settings
+            EditorSettings.enterPlayModeOptionsEnabled = false;
+        }
+    }
+}
