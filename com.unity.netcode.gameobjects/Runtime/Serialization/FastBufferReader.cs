38a0f43d4fc48c83de54a216af6c692bcc527023
Noel Stephens <noel.stephens@unity3d.com>
2026-06-30 11:07:13 -0500

    style
    Removing the auto-added (and not used) UnityEngine.UIElements using directive.

--- com.unity.netcode.gameobjects/Runtime/Serialization/FastBufferReader.cs
+++ com.unity.netcode.gameobjects/Runtime/Serialization/FastBufferReader.cs
@@ -3,7 +3,6 @@ using System.Runtime.CompilerServices;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace Unity.Netcode
 {
