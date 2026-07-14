c538a0fedf0a0af2efbefd27b8a33f95a99e5959
Michał Chrobot <124174716+michalChrobot@users.noreply.github.com>
2026-07-14 13:15:47 +0200

    ci: Update of pinned trunk and ci stability check (#4074)
    * Added CodeCoverage job to run on PRs to enable coverage check
    
    * Updated trunk version
    
    * Updated CI deps and regenerated recipes
    
    * Removed develop branch from Renovate updates
    
    * Added renovate grouping
    
    * Disabled StopAndStartMotion test
    
    * fixes from tests
    
    1) Updated Nintendo SDK
    2) Corrected standards check for minimalproject
    3) Increased the timeout of WaitForNetworkEvent for lower end devices
    
    * Disabled Switch due to build failures
    
    * fixes
    
    * corrected standards
    
    * typo
    
    * reverted change

--- com.unity.netcode.gameobjects/Tests/Runtime/Physics/NetworkRigidbodyTest.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/Physics/NetworkRigidbodyTest.cs
@@ -1,4 +1,4 @@
-#if COM_UNITY_MODULES_PHYSICS || COM_UNITY_MODULES_PHYSICS2D
+#if COM_UNITY_MODULES_PHYSICS && COM_UNITY_MODULES_PHYSICS2D
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
@@ -236,4 +236,4 @@ namespace Unity.Netcode.RuntimeTests
         }
     }
 }
-#endif // COM_UNITY_MODULES_PHYSICS
+#endif // COM_UNITY_MODULES_PHYSICS && COM_UNITY_MODULES_PHYSICS2D
