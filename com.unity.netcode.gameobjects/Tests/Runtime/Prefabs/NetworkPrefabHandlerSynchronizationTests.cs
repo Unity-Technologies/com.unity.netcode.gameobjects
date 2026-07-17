a008abaa22e11ab1b6c5f81d831d8c09f07785cb
Michał Chrobot <michal.chrobot@unity3d.com>
2026-07-17 13:08:06 +0200

    Disabled NetworkPrefabHandlerSpawnAndSynchronizeTests for iOS and macOS platforms

--- com.unity.netcode.gameobjects/Tests/Runtime/Prefabs/NetworkPrefabHandlerSynchronizationTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/Prefabs/NetworkPrefabHandlerSynchronizationTests.cs
@@ -28,6 +28,7 @@ namespace Unity.Netcode.RuntimeTests
         }
 
         [UnityTest]
+        [UnityPlatform(exclude = new[] { RuntimePlatform.IPhonePlayer, RuntimePlatform.OSXPlayer, RuntimePlatform.OSXEditor })] // Ignored test tracked in MTT-15473
         public IEnumerator NetworkPrefabHandlerSpawnAndSynchronizeTests()
         {
             var nonAuthority = GetNonAuthorityNetworkManager();
