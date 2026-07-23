a84614e8e34456de15ef155ee3f1acf5c0af4fce
Noel Stephens <noel.stephens@unity3d.com>
2026-07-22 21:12:13 -0500

    Refactor conditional statement for readability

--- com.unity.netcode.gameobjects/Runtime/Components/NetworkAnimator.cs
+++ com.unity.netcode.gameobjects/Runtime/Components/NetworkAnimator.cs
@@ -1346,8 +1346,7 @@ namespace Unity.Netcode.Components
                         BytePacker.WriteValuePacked(writer, (uint)valueInt);
                     }
                 }
-                else
-                if (cacheValue.Type == AnimationParamEnumWrapper.AnimatorControllerParameterBool)
+                else if (cacheValue.Type == AnimationParamEnumWrapper.AnimatorControllerParameterBool)
                 {
                     var valueBool = m_Animator.GetBool(hash);
                     fixed (void* value = cacheValue.Value)
