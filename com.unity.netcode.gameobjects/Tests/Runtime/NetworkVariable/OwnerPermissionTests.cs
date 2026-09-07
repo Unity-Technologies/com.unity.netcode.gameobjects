2b5e5492a306b87a6ff6f454ad65085a08f480f7
Michał Chrobot <124174716+michalChrobot@users.noreply.github.com>
2026-09-08 00:47:56 +0200

    chore: Corrections for standards check (#4155)
    * corrected using statements for standards check
    
    * yaml fix
    
    * yaml fix

--- com.unity.netcode.gameobjects/Tests/Runtime/NetworkVariable/OwnerPermissionTests.cs
+++ com.unity.netcode.gameobjects/Tests/Runtime/NetworkVariable/OwnerPermissionTests.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+#if ENABLE_CORECLR
 using NUnit.Framework;
+#endif
 using Unity.Netcode.TestHelpers.Runtime;
 using UnityEngine;
 using UnityEngine.TestTools;
