ade7af483b38a6613bbd72a61e7bcc621d34aea3
Michał Chrobot <michal.chrobot@unity3d.com>
2026-09-08 13:27:33 +0200

    Corrected Netcode capital C case

--- com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
+++ com.unity.netcode.gameobjects/Runtime/Core/NetworkManager.cs
@@ -8,7 +8,7 @@ using Unity.Entities;
 // blanket import of it competes with Unity.Netcode on every name the two happen to share. Importing
 // only the types used here keeps that surface to exactly those names.
 using NetCodeConfig = Unity.NetCode.NetCodeConfig;
-using NetcodeWorld = Unity.NetCode.NetcodeWorld;
+using NetcodeWorld = Unity.Netcode.NetcodeWorld;
 #endif
 using Unity.Netcode.Components;
 using Unity.Netcode.GameObjects.Timing;
